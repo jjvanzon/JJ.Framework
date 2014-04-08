039a433a0eb73240da3921f7cf84fef70eb403e0
jjvanzon <janjoostvanzon@hotmail.com>
2014-04-08 21:01:25 +0000

    SetText Online/offline variations in WinForms

---
+++ Framework/JJ.Framework.Services/ServiceDingeser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JJ.Framework.Services
+{
+    public class ServiceConnector<TServiceInterface>
+    {
+        public ServiceConnector(string url)
+        {
+        }
+    }
+}
---
+++ Framework/JJ.Framework.Services/~Client.cs
@@ -0,0 +1,12 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.ServiceModel;
+//using System.Text;
+
+//namespace JJ.Framework.Services
+//{
+//    internal class Client<TServiceInterface> : ClientBase<TServiceInterface>
+//    {
+//    }
+//}
