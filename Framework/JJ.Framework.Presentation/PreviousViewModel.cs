90e3be9ec2ce34b9dc37f2e78e8797c849702b39
jjvanzon <janjoostvanzon@hotmail.com>
2014-12-11 21:49:36 +0000

    QuestionAndAnswer refactorings

---
+++ Framework/JJ.Framework.Presentation/PreviousViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJ.Framework.Presentation
+{
+    /// <summary>
+    /// In case a presenter cannot know where you came from,
+    /// but wants to send you back to the previous view.
+    /// </summary>
+    public class PreviousViewModel
+    { }
+}
