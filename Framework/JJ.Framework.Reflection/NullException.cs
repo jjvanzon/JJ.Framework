8533e7a9a3c82ed799270359928c93c778e2862e
jjvanzon <janjoostvanzon@hotmail.com>
2014-12-15 21:57:43 +0000

    QuestionAndAnswer refactorings

---
+++ Framework/JJ.Framework.Reflection/NullException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace JJ.Framework.Reflection
+{
+    public class NullException : Exception
+    {
+        public NullException(Expression<Func<object>> expression)
+            : base(ExpressionHelper.GetText(expression))
+        { }
+    }
+}
