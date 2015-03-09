f26f6ec00a406f5ede31dc212dc9f8e215189f99
jjvanzon <janjoostvanzon@hotmail.com>
2015-03-09 22:47:50 +0000

    Synthesizer: Sample: use wav header (finished)

--- Framework/JJ.Framework.IO/BinaryWriterExtensions.cs
+++ Framework/JJ.Framework.IO/BinaryWriterExtensions.cs
@@ -26,5 +26,24 @@ namespace JJ.Framework.IO
             writer.BaseStream.Write(buffer, 0, buffer.Length);
             gcHandle.Free();
         }
+        
+        /// <summary>
+        /// In other languages / environments I used to be able to write a struct directly to a file.
+        /// And in C# I can't. It is just a set of freaking bytes
+        /// litterly stored in memory and it needs to be streamed to a file. 
+        /// Why are things like that not possible using a BinaryWriter?!?
+        /// 
+        /// Beware that the performance might not be better than reading the values one by one.
+        /// </summary>
+        public static T ReadStruct<T>(this BinaryReader reader)
+            where T : struct
+        {
+            int size = Marshal.SizeOf(typeof(T));
+            byte[] buffer = reader.ReadBytes(size);
+            GCHandle gcHandle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+            T result = (T)Marshal.PtrToStructure(gcHandle.AddrOfPinnedObject(), typeof(T));
+            gcHandle.Free();
+            return result;
+        }
     }
 }
