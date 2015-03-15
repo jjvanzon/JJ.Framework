1171c1e36b49041c5538b9c636356b8e3a0e79fa
jjvanzon <janjoostvanzon@hotmail.com>
2015-03-15 00:00:35 +0000

    Synthesizer: NHibernate persistence in SQL Server database, preliminary Svg model.

--- Framework/JJ.Framework.Persistence/RepositoryFactory.cs
+++ Framework/JJ.Framework.Persistence/RepositoryFactory.cs
@@ -18,6 +18,19 @@ namespace JJ.Framework.Persistence
         /// The repository implementation must have a constructor that takes a single paramter of type IContext.
         /// </summary>
         /// <typeparam name="TRepositoryInterface">The repository interface type.</typeparam>
+        public static TRepositoryInterface CreateRepositoryFromConfiguration<TRepositoryInterface>(IContext context, PersistenceConfiguration configuration)
+        {
+            if (configuration == null) throw new NullException(() => configuration);
+            return RepositoryFactory.CreateRepository<TRepositoryInterface>(context, configuration.RepositoryAssemblies);
+        }
+
+        /// <summary>
+        /// Creates a context using the values out of the config file.
+        /// A configuration example can be found in your bin directory.
+        /// The repository implementation should be present in one of the repository assemblies mentioned in the config file.
+        /// The repository implementation must have a constructor that takes a single paramter of type IContext.
+        /// </summary>
+        /// <typeparam name="TRepositoryInterface">The repository interface type.</typeparam>
         public static TRepositoryInterface CreateRepositoryFromConfiguration<TRepositoryInterface>(IContext context)
         {
             PersistenceConfiguration configuration = PersistenceConfigurationHelper.GetPersistenceConfiguration();
