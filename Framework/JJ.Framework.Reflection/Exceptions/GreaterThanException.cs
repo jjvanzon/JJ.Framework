fdca9656612da543ea1af9e577232e2445970272
jjvanzon <janjoostvanzon@hotmail.com>
2015-06-23 19:22:35 +0000

    Synthesizer: Refactorings in preparation for the noise and filter band operators.

--- Framework/JJ.Framework.Reflection/Exceptions/GreaterThanException.cs
+++ Framework/JJ.Framework.Reflection/Exceptions/GreaterThanException.cs
@@ -8,10 +8,16 @@ namespace JJ.Framework.Reflection.Exceptions
 {
     public class GreaterThanException : Exception
     {
-        private const string MESSAGE = "{0} is greater than {1}.";
-
         public GreaterThanException(Expression<Func<object>> expression, object limit)
-            : base(String.Format(MESSAGE, ExpressionHelper.GetText(expression), limit))
+            : base(String.Format("{0} is greater than {1}.", ExpressionHelper.GetText(expression), limit))
+        { }
+
+        /// <summary>
+        /// Only use this overload if you wish to show the text of the limitExpression in the exception message.
+        /// If you only want to show the limit's value, use the other overload.
+        /// </summary>
+        public GreaterThanException(Expression<Func<object>> expression, Expression<Func<object>> limitExpression)
+            : base(String.Format("{0} is greater than {1} of {2}.", ExpressionHelper.GetText(expression), ExpressionHelper.GetText(limitExpression), ExpressionHelper.GetValue(limitExpression)))
         { }
     }
 }
