458b5394693b037bb44f150e003d2d700767718e
jjvanzon <janjoostvanzon@hotmail.com>
2015-06-25 22:12:06 +0000

    Synthesizer: WhiteNoiseCalculator: decouple from WhiteNoise_OperatorCalculator to set sampling rate in AudioFileOutputCalculatorBase. Make some operator calculators internal + refactor dependent code.

---
+++ Framework/JJ.Framework.Reflection/Exceptions/GreaterThanOrEqualException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace JJ.Framework.Reflection.Exceptions
+{
+    public class GreaterThanOrEqualException : Exception
+    {
+        public GreaterThanOrEqualException(Expression<Func<object>> expression, object limit)
+            : base(String.Format("{0} is greater than or equal to {1}.", ExpressionHelper.GetText(expression), limit))
+        { }
+
+        /// <summary>
+        /// Only use this overload if you wish to show the text of the limitExpression in the exception message.
+        /// If you only want to show the limit's value, use the other overload.
+        /// </summary>
+        public GreaterThanOrEqualException(Expression<Func<object>> expression, Expression<Func<object>> limitExpression)
+            : base(String.Format("{0} is greater than or equal to {1} of {2}.", ExpressionHelper.GetText(expression), ExpressionHelper.GetText(limitExpression), ExpressionHelper.GetValue(limitExpression)))
+        { }
+    }
+}
