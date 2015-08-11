afdb337005d83149dbe17452e8a2c1d48b44ac16
jjvanzon <janjoostvanzon@hotmail.com>
2015-08-11 18:26:21 +0000

    Synthesizer: ChildDocumentProperties view, program consistent response to enter key and double click in tree view, add response to enter key in child document grids, rename list views to grid views, correct bug binding drop downs (value change was not applied), correct enum extensions' handling of undefined,

---
+++ Framework/JJ.Framework.Reflection/Exceptions/NotIntegerException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace JJ.Framework.Reflection.Exceptions
+{
+    public class NotIntegerException : Exception
+    {
+        private const string MESSAGE = "{0} is not an integer number.";
+
+        public NotIntegerException(Expression<Func<object>> expression)
+            : base(String.Format(MESSAGE, ExpressionHelper.GetText(expression)))
+        { }
+    }
+}
