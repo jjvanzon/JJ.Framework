575fcb7bac2fd89436814d570419bae7e975a8a3
jjvanzon <janjoostvanzon@hotmail.com>
2015-09-16 21:25:52 +0200

    Synthesizer.VectorGraphics: Make LineGestures draw line from OutletPoint rather than MouseDown X and Y.

--- Framework/JJ.Framework.Mathematics/Geometry.cs
+++ Framework/JJ.Framework.Mathematics/Geometry.cs
@@ -13,5 +13,29 @@
             return x >= x1 && x <= x2 &&
                    y >= y1 && y <= y2;
         }
+
+        public static void GetCenter(double x1, double y1, double x2, double y2, out double x, out double y)
+        {
+            x = (x1 + x2) / 2.0;
+            y = (y1 + y2) / 2.0;
+        }
+
+        public static void GetCenter(float x1, float y1, float x2, float y2, out float x, out float y)
+        {
+            x = (x1 + x2) / 2f;
+            y = (y1 + y2) / 2f;
+        }
+
+        public static void GetCenter_ByWidthAndHeight(double left, double top, double width, double height, out double x, out double y)
+        {
+            x = left + width / 2.0;
+            y = top + height / 2.0;
+        }
+
+        public static void GetCenter_ByWidthAndHeight(float left, float top, float width, float height, out float x, out float y)
+        {
+            x = left + width / 2f;
+            y = top + height / 2f;
+        }
     }
 }
---
+++ Framework/JJ.Framework.Reflection/Exceptions/InvalidTypeExceptionOfT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace JJ.Framework.Reflection.Exceptions
+{
+    public class InvalidTypeException<T> : InvalidTypeException
+    {
+        public InvalidTypeException(Expression<Func<object>> expression)
+            : base(expression, typeof(T))
+        { }
+    }
+}
