dbdb78c73a087f644b5ca36fe23e54b2ea6acc2c
jjvanzon <janjoostvanzon@hotmail.com>
2015-10-02 00:43:03 +0200

    Synthesizer: Rename ScaleTypes back to singular and just differentiate between singular and pural at specific points in the UI.

---
+++ Framework/JJ.Framework.Common/EnumHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JJ.Framework.Common
+{
+    public static class EnumHelper
+    {
+        public static TEnum Parse<TEnum>(string value)
+        {
+            return (TEnum)Enum.Parse(typeof(TEnum), value);
+        }
+    }
+}
