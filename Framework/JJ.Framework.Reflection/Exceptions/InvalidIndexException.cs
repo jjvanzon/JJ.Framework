2e258e96c00eabdd5ce3bcdbbe2cf8363f69d510
jjvanzon <janjoostvanzon@hotmail.com>
2016-03-12 02:44:18 +0100

    Synthesizer: Preliminary reactivation of ToolTips in PatchDetails.

--- Framework/JJ.Framework.Reflection/Exceptions/InvalidIndexException.cs
+++ Framework/JJ.Framework.Reflection/Exceptions/InvalidIndexException.cs
@@ -9,11 +9,11 @@ namespace JJ.Framework.Reflection.Exceptions
         /// <summary> Example: "outletViewModel.Keys.OutletListIndex 1 is an invalid index for op.Outlets with count 0." </summary>
         private const string MESSAGE = "{0} {1} is an invalid index for {2} with count {3}.";
 
-        public InvalidIndexException(Expression<Func<object>> indexNumberExpression, Expression<Func<object>> countExpression)
+        public InvalidIndexException(Expression<Func<object>> listIndexExpression, Expression<Func<object>> countExpression)
             : base(String.Format(
                 MESSAGE, 
-                ExpressionHelper.GetText(indexNumberExpression), 
-                ExpressionHelper.GetValue(indexNumberExpression), 
+                ExpressionHelper.GetText(listIndexExpression), 
+                ExpressionHelper.GetValue(listIndexExpression), 
                 ExpressionHelper.GetText(countExpression).CutLeft('.').TrimEnd('.'), 
                 ExpressionHelper.GetValue(countExpression)))
         {  }
