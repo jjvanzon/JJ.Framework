ca6dbd77a7c0cb0cfbba00c60babb4fc6f7b68d6
jjvanzon <janjoostvanzon@hotmail.com>
2016-04-14 22:06:54 +0200

    - I introduced a potential bug: the array size in the DimensionStack class should be the max enum value of DimensionEnum, not the amount of enum values of the DimensionEnum. - DimensionEnum parameters of OperatorCalculator constructors’ must be verified.

--- Framework/JJ.Framework.Common/EnumHelper.cs
+++ Framework/JJ.Framework.Common/EnumHelper.cs
@@ -1,20 +1,27 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
 
 namespace JJ.Framework.Common
 {
     public static class EnumHelper
     {
         public static TEnum Parse<TEnum>(string value)
+            where TEnum : struct
         {
             return (TEnum)Enum.Parse(typeof(TEnum), value);
         }
 
         public static IList<TEnum> GetValues<TEnum>()
+            where TEnum : struct
         {
             return (TEnum[])Enum.GetValues(typeof(TEnum));
+        }
+
+        public static bool IsValidEnum<TEnum>(TEnum enumMember)
+            where TEnum : struct
+        {
+            return GetValues<TEnum>().Contains(enumMember);
         }
     }
 }
