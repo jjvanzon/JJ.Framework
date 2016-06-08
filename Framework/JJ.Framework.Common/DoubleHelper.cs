8f52adf12f33acba7510d4a8f8eeb4f3851425d2
jjvanzon <janjoostvanzon@hotmail.com>
2016-06-08 21:58:05 +0200

    Synthesizer: Range Operator: Specialized OperatorCalculators

--- Framework/JJ.Framework.Common/DoubleHelper.cs
+++ Framework/JJ.Framework.Common/DoubleHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace JJ.Framework.Common
 {
@@ -11,9 +12,17 @@ namespace JJ.Framework.Common
     /// </summary>
     public static class DoubleHelper
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool TryParse(string s, IFormatProvider provider, out double result)
         {
             return Double.TryParse(s, NumberStyles.Any, provider, out result);
+        }
+
+        /// <summary> Returns true if the value is NaN, PositiveInfinity or NegativeInfinity. </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsSpecialNumber(double value)
+        {
+            return Double.IsNaN(value) || Double.IsInfinity(value);
         }
     }
 }
