08e24dc4dd14c4f4f154e6996a3e0b3736c83150
jjvanzon <janjoostvanzon@hotmail.com>
2016-06-16 01:28:50 +0200

    Small change to NotUniqueException message.

--- Framework/JJ.Framework.Reflection/Exceptions/NotUniqueException.cs
+++ Framework/JJ.Framework.Reflection/Exceptions/NotUniqueException.cs
@@ -7,7 +7,7 @@ namespace JJ.Framework.Reflection.Exceptions
 {
     public class NotUniqueException : Exception
     {
-        private const string MESSAGE = "{0} is not unique.";
+        private const string MESSAGE = "{0} not unique.";
 
         public NotUniqueException(Expression<Func<object>> expression)
             : base(String.Format(MESSAGE, ExpressionHelper.GetText(expression)))
