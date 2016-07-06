97ec4abf6d80f20bba3f7d3ab0909d9bdba68899
jjvanzon <janjoostvanzon@hotmail.com>
2016-07-06 22:33:21 +0200

    Synthesizer: Filters: Clip upper frequency to below nyquist, or filter will fail with 0 sound.

--- Framework/JJ.Framework.Reflection/Exceptions/GreaterThanOrEqualException.cs
+++ Framework/JJ.Framework.Reflection/Exceptions/GreaterThanOrEqualException.cs
@@ -10,7 +10,7 @@ namespace JJ.Framework.Reflection.Exceptions
         { }
 
         /// <summary>
-        /// Only use this overload if you wish to show the text of the limitExpression in the exception message.
+        /// Only use this overload if you wish to show the text and value of the limitExpression in the exception message.
         /// If you only want to show the limit's value, use the other overload.
         /// </summary>
         public GreaterThanOrEqualException(Expression<Func<object>> expression, Expression<Func<object>> limitExpression)
