7ada9c3745c20f5fcfdebeb67c1531a3b7519ce9
jjvanzon <janjoostvanzon@hotmail.com>
2016-08-17 00:05:23 +0200

    Analysers: Check if fields are private or protected.

---
+++ Analysis/JJ.Analysis.TestCode/Fields_ThatAreNotPrivate.cs
@@ -0,0 +1,12 @@
+﻿namespace JJ.Analysis.TestCode
+{
+    internal class Fields_ThatAreNotPrivate
+    {
+        private int _privateField;
+        protected int _protectedField;
+
+        public int _publicField;
+        internal int _internalField;
+        protected internal int _protectedInternalField;
+    }
+}
--- Analysis/JJ.Analysis/Names/DiagnosticsIDs.cs
+++ Analysis/JJ.Analysis/Names/DiagnosticsIDs.cs
@@ -9,6 +9,7 @@ namespace JJ.Analysis.Names
         public const string ConstantNameUpperCaseOrSameAsValue = "ConstantNameUpperCaseOrSameAsValue";
         public const string FieldNameUnderscoredCamelCase = "FieldNameUnderscoredCamelCase";
         public const string FieldNameAbbreviationCasing = "FieldNameAbbreviationCasing";
+        public const string FieldsMustBePrivateOrProtected = "FieldsMustBePrivate";
         public const string LocalVariableNameAbbreviationCasing = "LocalVariableNameAbbreviationCasing";
         public const string LocalVariableNameStartWithLowerCase = "LocalVariableNameStartWithLowerCase";
         public const string MethodNameAbbreviationCasing = "MethodNameAbbreviationCasing";
