b71736fd603dbf7d998def9afcf8f27a2768e426
jjvanzon <janjoostvanzon@hotmail.com>
2016-10-20 21:19:27 +0200

    Change text of CollectionEmptyException.

--- Framework/Reflection/Exceptions/CollectionEmptyException.cs
+++ Framework/Reflection/Exceptions/CollectionEmptyException.cs
@@ -5,7 +5,7 @@ namespace JJ.Framework.Reflection.Exceptions
 {
     public class CollectionEmptyException : Exception
     {
-        private const string MESSAGE = "{0} is empty.";
+        private const string MESSAGE = "{0} collection is empty.";
 
         public CollectionEmptyException(Expression<Func<object>> expression)
             : base(String.Format(MESSAGE, ExpressionHelper.GetText(expression)))
--- Framework/Reflection/Exceptions/CollectionNotEmptyException.cs
+++ Framework/Reflection/Exceptions/CollectionNotEmptyException.cs
@@ -5,7 +5,7 @@ namespace JJ.Framework.Reflection.Exceptions
 {
     public class CollectionNotEmptyException : Exception
     {
-        private const string MESSAGE = "{0} should be empty.";
+        private const string MESSAGE = "{0} collection should be empty.";
 
         public CollectionNotEmptyException(Expression<Func<object>> expression)
             : base(String.Format(MESSAGE, ExpressionHelper.GetText(expression)))
