4d41d692f1315013d3ee7510b8edad46d0c20e71
jjvanzon <janjoostvanzon@hotmail.com>
2017-03-05 12:27:01 +0100

    Framework: Add and use new ValidatorBase, that actually does an argument null check. This prevents false positives about nullability from Resharper.

---
+++ Framework/Validation/ValidatorBase.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+using JJ.Framework.Exceptions;
+
+namespace JJ.Framework.Validation
+{
+    public abstract class ValidatorBase<TRootObject> : ValidatorBase_WithoutConstructorArgumentNullCheck<TRootObject>
+    {
+        /// <param name="postponeExecute">
+        /// When set to true, you can do initializations in your constructor
+        /// before Execute goes off. If so, then you have to call Execute in your own constructor.
+        /// </param>
+        public ValidatorBase([NotNull] TRootObject obj, bool postponeExecute = false)
+            : base(obj, postponeExecute: true)
+        {
+            if (obj == null) throw new NullException(() => obj);
+
+            if (!postponeExecute)
+            {
+                // ReSharper disable once VirtualMemberCallInConstructor
+                Execute();
+            }
+        }
+
+        [NotNull]
+        // ReSharper disable once AssignNullToNotNullAttribute
+        public override TRootObject Obj => base.Obj;
+    }
+}
