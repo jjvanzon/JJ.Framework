387043092b94496e0401959dee97de22162f29c7
jjvanzon <janjoostvanzon@hotmail.com>
2017-06-20 01:05:28 +0200

    JJ.Framework.Validation: Remove constructor parameters, type arguments and distinction between 'WithoutArgumentNullCheck' and with argument null check.

---
+++ Framework/Validation/2017-06-20 Backup of VersatileValidator.cs
@@ -0,0 +1,14 @@
+﻿//using JetBrains.Annotations;
+//using JJ.Framework.Exceptions;
+
+//namespace JJ.Framework.Validation
+//{
+//    public abstract class VersatileValidator<TRootObject> : VersatileValidator_WithoutConstructorArgumentNullCheck<TRootObject>
+//    {
+//        public VersatileValidator([NotNull] TRootObject obj)
+//            : base(obj)
+//        {
+//            if (obj == null) throw new NullException(() => obj);
+//        }
+//    }
+//}
