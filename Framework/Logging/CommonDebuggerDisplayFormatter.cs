0b38f307ecd5bac61e8333e2ee5788e66ccb3f74
jjvanzon <janjoostvanzon@hotmail.com>
2017-11-15 21:35:57 +0100

    Synthesizer: Undo: Draft of using ViewModelState.OriginalState for undo. Also make some DebuggerDisplays.

---
+++ Framework/Logging/CommonDebuggerDisplayFormatter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace JJ.Framework.Logging
+{
+    public static class CommonDebuggerDisplayFormatter
+    {
+        public static string GetDebuggDisplayWithIDAndName<T>(int id, string name)
+        {
+            string debuggerDisplay = GetDebuggDisplayWithIDAndName(typeof(T), id, name);
+            return debuggerDisplay;
+        }
+
+        public static string GetDebuggDisplayWithIDAndName(Type type, int id, string name)
+        {
+            if (type == null) throw new ArgumentNullException(nameof(type));
+            string debuggerDisplay = $"{{{type.Name}}} {name} (ID = {id})";
+            return debuggerDisplay;
+        }
+    }
+}
