945fa701e2110fe8eb9b521dcd3bd7bae226939e
jjvanzon <janjoostvanzon@hotmail.com>
2018-03-01 20:55:08 +0100

    JJ.Framework.Exceptions: Generically unit test all SimpleExceptionWithExpressionBase-derived exceptions.

--- Framework/Exceptions.Tests/ComparativeExceptionTests.cs
+++ Framework/Exceptions.Tests/ComparativeExceptionTests.cs
@@ -8,34 +8,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace JJ.Framework.Exceptions.Tests
 {
 	[TestClass]
-	public class ExceptionTests
+	public class ComparativeExceptionTests
 	{
-		private class Item
-		{
-			public Item Parent { get; }
-		}
-
-		[TestMethod]
-		public void Test_SimpleException_WithNameOf()
-		{
-			object value;
-			AssertHelper.ThrowsException<NullException>(() => throw new NullException(nameof(value)), "value is null.");
-		}
-
-		[TestMethod]
-		public void Test_SimpleException_WithExpression_WithSinglePart()
-		{
-			object value = null;
-			AssertHelper.ThrowsException<NullException>(() => throw new NullException(() => value), "value is null.");
-		}
-
-		[TestMethod]
-		public void Test_SimpleException_WithExpression_WithMultipleParts()
-		{
-			Item item = null;
-			AssertHelper.ThrowsException<NullException>(() => throw new NullException(() => item.Parent), "item.Parent is null.");
-		}
-
 		[TestMethod]
 		public void Test_ComparativeException_WithNameOf()
 		{
@@ -53,14 +27,14 @@ namespace JJ.Framework.Exceptions.Tests
 		[TestMethod]
 		public void Test_ComparativeException_WithExpression_WithMultipleParts()
 		{
-			Item item = null;
+			TestItem item = null;
 			AssertHelper.ThrowsException<LessThanException>(() => throw new LessThanException(() => item.Parent, 0), "item.Parent is less than 0.");
 		}
 
 		[TestMethod]
 		public void Test_ComparativeException_WithLimitExpression()
 		{
-			Item item = null;
+			TestItem item = null;
 			double number = 0;
 
 			AssertHelper.ThrowsException<LessThanException>(
