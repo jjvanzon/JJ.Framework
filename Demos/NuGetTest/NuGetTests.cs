f40a9006adeda5e0da00703353a004c6629b2ef2
jjvanzon <janjoostvanzon@hotmail.com>
2018-03-03 22:16:34 +0100

    Trivials.

--- Demos/NuGetTest/NuGetTests.cs
+++ Demos/NuGetTest/NuGetTests.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
+using System.Text;
 using JJ.Framework.Common;
+using JJ.Framework.Conversion;
 using JJ.Framework.Exceptions;
+using JJ.Framework.IO;
 using JJ.Framework.PlatformCompatibility;
 using JJ.Framework.Reflection;
 using JJ.Framework.Testing;
@@ -71,13 +75,13 @@ namespace JJ.Demos.NuGetTest
 			Assert.Fail("An exception should have been thrown.");
 		}
 
-		//[TestMethod]
-		//public void Test_NuGetReference_JJ_Framework_Conversion()
-		//{
-		//	string str = "1234";
-		//	int number = SimpleTypeConverter.ParseValue<int>(str);
-		//	Assert.AreEqual(1234, number);
-		//}
+		[TestMethod]
+		public void Test_NuGetReference_JJ_Framework_Conversion()
+		{
+			string str = "1234";
+			int number = SimpleTypeConverter.ParseValue<int>(str);
+			Assert.AreEqual(1234, number);
+		}
 
 		[TestMethod]
 		public void Test_NuGetReference_JJ_Framework_Testing()
@@ -85,6 +89,28 @@ namespace JJ.Demos.NuGetTest
 			var obj = new object();
 
 			AssertHelper.ThrowsException(() => AssertHelper.IsNull(() => obj), "Assert.IsNull failed. Tested member: 'obj'.");
+		}
+
+		[TestMethod]
+		public void Test_NuGetReference_JJ_Framework_IO()
+		{
+			string csvText = "1234,2345,3456,4567,5678" + Environment.NewLine +
+			                 "6789,7890,1234,2345,3456";
+
+			using (Stream stream = StreamHelper.StringToStream(csvText, Encoding.UTF8))
+			{
+				using (var csvReader = new CsvReader(stream))
+				{
+					while (csvReader.Read())
+					{
+						string val1 = csvReader[0];
+						string val2 = csvReader[1];
+						string val3 = csvReader[2];
+						string val4 = csvReader[3];
+						string val5 = csvReader[4];
+					}
+				}
+			}
 		}
 	}
 }
