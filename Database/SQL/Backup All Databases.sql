77210f5e5a23f3d835c9dd88e9f5421aca641349
jjvanzon <janjoostvanzon@hotmail.com>
2018-05-07 22:43:05 +0200

    Misc SQL

---
+++ Database/SQL/Backup All Databases.sql
@@ -0,0 +1,31 @@
+-- Based on: https://www.mssqltips.com/sqlservertip/1070/simple-script-to-backup-all-sql-server-databases/
+
+DECLARE @name VARCHAR(256) -- database name  
+DECLARE @path VARCHAR(256) -- path for backup files  
+DECLARE @fileName VARCHAR(256) -- filename for backup  
+DECLARE @fileDate VARCHAR(20) -- used for file name
+ 
+-- specify database backup directory
+SET @path = 'E:\Database Snapshots\'  
+ 
+-- specify filename format
+SELECT @fileDate = CONVERT(VARCHAR(20),GETDATE(),112) 
+ 
+DECLARE db_cursor CURSOR READ_ONLY FOR  
+SELECT name 
+FROM master.dbo.sysdatabases 
+WHERE name NOT IN ('master','model','msdb','tempdb')  -- exclude these databases
+ 
+OPEN db_cursor   
+FETCH NEXT FROM db_cursor INTO @name   
+ 
+WHILE @@FETCH_STATUS = 0   
+BEGIN   
+   SET @fileName = @path + @name + '_' + @fileDate + '.BAK'  
+   BACKUP DATABASE @name TO DISK = @fileName  
+ 
+   FETCH NEXT FROM db_cursor INTO @name   
+END   
+ 
+CLOSE db_cursor   
+DEALLOCATE db_cursor
