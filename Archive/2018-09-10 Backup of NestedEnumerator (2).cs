2f6a8c11661cfcbb965ea6ddfa4d986b6ad51f05
jjvanzon <janjoostvanzon@hotmail.com>
2018-09-11 23:58:21 +0200

    Synthesizer: * TestExecutor: The static methods check the const/var variations and were custom for each number of dimensions. Start at generalizing those methods, so it can use n dimensions. Not finished. * Copy more code from JJ.Framework.Collections to the Business.Synthesizer project upon pre-build and only use the ones in Business.Synthesizer to prevent namespace clashes. * JJ.Framework.Collections: Make a CrossJoin extension method for n collections along with a NestedEnumerator class. This is not trivial like when you know the number of nestings in advance.

---
+++ Archive/2018-09-10 Backup of NestedEnumerator (2).cs
@@ -0,0 +1,59 @@
+﻿//using System;
+//using System.Collections;
+
+//namespace JJ.Framework.Collections
+//{
+//    internal class NestedEnumerator
+//    {
+//        private readonly IEnumerable _enumerable;
+//        private readonly NestedEnumerator _deeperNestedEnumerator;
+
+//        private readonly object[] _currentItems;
+
+//        public Action<object[]> Callback { get; set; }
+
+//        /// <param name="deeperNestedEnumerator">nullable</param>
+//        public NestedEnumerator(IEnumerable enumerable, NestedEnumerator deeperNestedEnumerator)
+//        {
+//            _enumerable = enumerable ?? throw new ArgumentNullException(nameof(enumerable));
+//            _deeperNestedEnumerator = deeperNestedEnumerator;
+
+//            if (_deeperNestedEnumerator != null)
+//            {
+//                _deeperNestedEnumerator.Callback = DeeperCallback;
+//            }
+
+//            _currentItems = new object[GetDepth()];
+//        }
+
+//        public void Enumerate()
+//        {
+//            foreach (object item in _enumerable)
+//            {
+//                _currentItems[0] = item;
+
+//                _deeperNestedEnumerator?.Enumerate();
+//            }
+//        }
+
+//        private void DeeperCallback(object[] currentItems)
+//        {
+//            currentItems.CopyTo(_currentItems, 1);
+//            Callback(currentItems);
+//        }
+
+//        private int GetDepth()
+//        {
+//            var depth = 1;
+//            NestedEnumerator tempDeeperEnumerable = _deeperNestedEnumerator;
+
+//            while (tempDeeperEnumerable != null)
+//            {
+//                tempDeeperEnumerable = tempDeeperEnumerable._deeperNestedEnumerator;
+//                depth++;
+//            }
+
+//            return depth;
+//        }
+//    }
+//}
---
+++ Archive/2018-09-10 Backup of NestedEnumerator (4).cs
@@ -0,0 +1,100 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//// ReSharper disable PossibleNullReferenceException
+//// ReSharper disable InvertIf
+
+//namespace JJ.Framework.Collections
+//{
+//    internal class NestedEnumerator : IEnumerable<object[]>, IEnumerator<object[]>
+//    {
+//        private readonly NestedEnumerator _deeperNestedEnumerator;
+//        private readonly IEnumerator<object> _enumerator;
+
+//        // Construction, Destruction
+
+//        /// <param name="deeperNestedEnumerator">nullable</param>
+//        public NestedEnumerator(IEnumerable<object> enumerable, NestedEnumerator deeperNestedEnumerator)
+//        {
+//            if (enumerable == null) throw new ArgumentNullException(nameof(enumerable));
+//            _deeperNestedEnumerator = deeperNestedEnumerator;
+//            _enumerator = enumerable.GetEnumerator();
+//            Current = new object[GetDepth()];
+//            Reset();
+//        }
+
+//        private int GetDepth() => _deeperNestedEnumerator.SelfAndAncestors(x => x._deeperNestedEnumerator).Count();
+
+//        ~NestedEnumerator() => Dispose();
+
+//        public void Dispose()
+//        {
+//            _deeperNestedEnumerator?.Dispose();
+//            _enumerator?.Dispose();
+//        }
+
+//        // IEnumerable
+
+//        public object[] Current { get; }
+
+//        public void Reset()
+//        {
+//            _deeperNestedEnumerator.Reset();
+//            _enumerator.Reset();
+//        }
+
+//        public bool MoveNext()
+//        {
+//            // Case 1: There is no deeper enumerator.
+//            if (_deeperNestedEnumerator == null)
+//            {
+//                if (_enumerator.MoveNext())
+//                {
+//                    DeriveCurrent();
+//                    return true;
+//                }
+
+//                return false;
+//            }
+
+//            // Case 2: Deeper enumerator can be advanced.
+//            if (_deeperNestedEnumerator.MoveNext())
+//            {
+//                DeriveCurrent();
+//                return true;
+//            }
+
+//            // Case 3:
+//            // Deeper enumerator is done.
+//            // Reset deeper enumerator.
+//            // Move next on deeper enumerator to set it to the first item.
+//            // Advance this level's enumerator.
+//            _deeperNestedEnumerator.Reset();
+
+//            if (_deeperNestedEnumerator.MoveNext())
+//            {
+//                if (_enumerator.MoveNext())
+//                {
+//                    DeriveCurrent();
+//                    return true;
+//                }
+//            }
+
+//            // Case 4: This level's enumerator is done.
+//            return false;
+//        }
+
+//        private void DeriveCurrent()
+//        {
+//            _deeperNestedEnumerator.Current.CopyTo(Current, 1);
+//            Current[0] = _enumerator.Current;
+//        }
+
+//        // IEnumerator
+
+//        public IEnumerator<object[]> GetEnumerator() => this;
+//        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+//        object IEnumerator.Current => Current;
+//    }
+//}
