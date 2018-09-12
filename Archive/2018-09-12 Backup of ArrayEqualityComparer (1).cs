b48f14c8ac21052863c1508d7b010e8a0086dd87
jjvanzon <janjoostvanzon@hotmail.com>
2018-09-12 20:45:17 +0200

    * JJ.Framework.Collections: DistinctMany. * Synthesizer: Use DistinctMany in generalization for n-dimensional testing.

---
+++ Archive/2018-09-12 Backup of ArrayEqualityComparer (1).cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//namespace JJ.Framework.Collections
+//{
+//    internal class ArrayEqualityComparer<T> : IEqualityComparer<IEnumerable<T>>
+//    {
+//        public static ArrayEqualityComparer<T> Instance = new ArrayEqualityComparer<T>();
+
+//        private ArrayEqualityComparer() { }
+
+//        private static readonly EqualityComparer<T> _itemEqualityComparer = EqualityComparer<T>.Default;
+
+//        public bool Equals(IEnumerable<T> x, IEnumerable<T> y)
+//        {
+//            if (x == null) throw new ArgumentNullException(nameof(x));
+//            if (y == null) throw new ArgumentNullException(nameof(y));
+
+//            bool result = x.Zip(y).All(z => _itemEqualityComparer.Equals(z.Item1, z.Item2));
+
+//            return result;
+//        }
+
+//        public int GetHashCode(IEnumerable<T> obj) => obj.GetHashCode();
+//    }
+//}
---
+++ Archive/2018-09-12 Backup of ArrayEqualityComparer (2).cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//namespace JJ.Framework.Collections
+//{
+//    internal class ArrayEqualityComparer<TEnumerable, TItem> : IEqualityComparer<TEnumerable>
+//            where TEnumerable : IEnumerable<TItem>
+//    {
+//        public static ArrayEqualityComparer<TEnumerable, TItem> Instance = new ArrayEqualityComparer<TEnumerable, TItem>();
+
+//        private ArrayEqualityComparer() { }
+
+//        private static readonly EqualityComparer<TItem> _itemEqualityComparer = EqualityComparer<TItem>.Default;
+
+//        public bool Equals(TEnumerable collection1, TEnumerable collection2)
+//        {
+//            if (collection1 == null) throw new ArgumentNullException(nameof(collection1));
+//            if (collection2 == null) throw new ArgumentNullException(nameof(collection2));
+
+//            //bool result = collection1.Zip(collection2).All(z => _itemEqualityComparer.Equals(z.Item1, z.Item2));
+//            bool result = collection1.Zip(collection2).All(z => Equals(z.Item1, z.Item2));
+
+//            return result;
+//        }
+
+//        public int GetHashCode(TEnumerable obj) => obj.GetHashCode();
+//    }
+//}
