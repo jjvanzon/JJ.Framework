d808d5531de4b064bfa63429d3ef7e7a05b7d3e0
jjvanzon <janjoostvanzon@hotmail.com>
2018-10-16 20:51:52 +0200

    HtmlToXmlConverterTests

--- Framework/Xml/HtmlToXmlConverter.cs
+++ Framework/Xml/HtmlToXmlConverter.cs
@@ -1,24 +1,48 @@
 ﻿using System.IO;
 using System.Xml;
+using JJ.Framework.Text;
 using Sgml;
 
 namespace JJ.Framework.Xml
 {
 	public static class HtmlToXmlConverter
 	{
-		public static string Convert(string html)
+	    private const string HTML_OPEN_TAG = "<html>";
+	    private const string HTML_CLOSE_TAG = "</html>";
+
+		public static string Convert(string html, bool includeXmlHeader = true)
 		{
-			using (var stringReader = new StringReader(html))
+            if (html == null)
+            {
+                return null;
+            }
+
+		    html = html.Trim();
+
+		    bool hadHtmlTag = html.StartsWith(HTML_OPEN_TAG) && html.EndsWith(HTML_CLOSE_TAG);
+
+            if (!hadHtmlTag)
+            {
+                // SgmlReader 1.8.11 at one point messed up with a crash, while adding a root tag helped.
+                // (An older version 1.7 not available on NuGet did not have that problem.)
+                // (The newer version 1.8.11 did not understand a non-closing <br> enclosed in a <p>, so... switched back to 1.7)
+                html = $"{HTML_OPEN_TAG}{html}{HTML_CLOSE_TAG}";
+            }
+
+            using (var stringReader = new StringReader(html))
 			{
 				using (var sgmlReader = new SgmlReader { DocType = "HTML", InputStream = stringReader })
 				{
-					using (var stringWriter = new StringWriter())
+                    using (var stringWriter = new StringWriter())
 					{
 						using (var xmlTextWriter = new XmlTextWriter(stringWriter))
 						{
-							xmlTextWriter.WriteStartDocument(); // Write XML header
+						    if (includeXmlHeader)
+						    {
+						        xmlTextWriter.WriteStartDocument(); // Write XML header
+						    }
 
-							sgmlReader.Read();
+						    sgmlReader.Read();
 
 							while (!sgmlReader.EOF)
 							{
@@ -26,7 +50,14 @@ namespace JJ.Framework.Xml
 							}
 						}
 
-						return stringWriter.ToString();
+					    string xml = stringWriter.ToString();
+
+					    if (!hadHtmlTag)
+					    {
+					        xml = xml.TrimFirst(HTML_OPEN_TAG).TrimLast(HTML_CLOSE_TAG);
+					    }
+
+					    return xml;
 					}
 				}
 			}
