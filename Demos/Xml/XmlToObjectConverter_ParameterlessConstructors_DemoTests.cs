f7e66d066a2a0e20a56f145c63a32f50f21fb643
jjvanzon <janjoostvanzon@hotmail.com>
2018-10-19 14:56:38 +0200

    * JJ.Demos.Xml: Tests for the example code in the README.MD of JJ.Framework.Xml.* Try correcting JJ.Framework.Exceptions.Tests.csproj in Notepad with content about NuGet-referencing the 'wrong test' framework.

---
+++ Demos/Xml/XmlToObjectConverter_ParameterlessConstructors_DemoTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// ReSharper disable ArrangeTypeMemberModifiers
+
+namespace JJ.Demos.Xml
+{
+    [TestClass]
+    public class XmlToObjectConverter_ParameterlessConstructors_DemoTests
+    {
+        class MyClass
+        {
+            // Having this constructor with a constructor parameter causes an exception.
+            public MyClass(int myConstructorParameter) { }
+        }
+
+        [TestMethod]
+        public void Demo_XmlToObjectConverter_ParameterlessConstructors()
+        {
+            const string xml = @"<myClass />";
+
+            // Compiler error because there is no parameterless constructor.
+            //var converter = new XmlToObjectConverter<MyClass>();
+        }
+    }
+}
