ff607290ef8252b722fab5388c6d418f085a9cff
jjvanzon <janjoostvanzon@hotmail.com>
2018-10-23 19:47:40 +0200

    JJ.Framework.Reflection: NuGet publish with more of a word cloud as a description and summary. JJ.Framework.Xml doc tweak.

--- Framework/Reflection/Properties/AssemblyInfo.cs
+++ Framework/Reflection/Properties/AssemblyInfo.cs
@@ -5,7 +5,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JJ.Framework.Reflection")]
-[assembly: AssemblyDescription("Extensions to the System.Reflection and System.Linq.Expressions namespaces.")]
+[assembly: AssemblyDescription(
+@"Converts many types of lambda expressions into text and can retrieve its resulting value or return info like { ""MyMethod"", Parameters = { ""myParameter"", int, 3 } }; Accessor IsIndexer IsNullableType IsProperty IsReferenceType IsStatic IsSimpleType IsDefault GetBaseClasses GetImplementations GetItemType GetPropertyOrException GetUnderlyingNullableTypeFast TypesFromObjects. ReflectionCache with fast GetProperties GetFields GetConstructor GetTypeByShortName. Overloads for PropertInfo.GetValue IsAssignableFrom IsAssignableTo CreateInstance.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jan-Joost van Zon")]
 [assembly: AssemblyProduct("JJ.Framework")]
