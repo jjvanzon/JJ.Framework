jjvanzon/JJ.Synthesizer

Synthesizer: validations, warnings. Still only value, add and substract operators.

Business/JJ.Business.Synthesizer/Resources/Messages.Designer.cs
@@ -22,14 +22,14 @@ namespace JJ.Business.Synthesizer.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class PropertyDisplayNames {
+    internal class Messages {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal PropertyDisplayNames() {
+        internal Messages() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace JJ.Business.Synthesizer.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("JJ.Business.Synthesizer.Resources.PropertyDisplayNames", typeof(PropertyDisplayNames).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("JJ.Business.Synthesizer.Resources.Messages", typeof(Messages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -61,83 +61,20 @@ namespace JJ.Business.Synthesizer.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Add.
+        ///   Looks up a localized string similar to {0} operator named &apos;{1}&apos; does not have {2} filled in..
         /// </summary>
-        internal static string Add {
+        internal static string OperandNotSet {
             get {
-                return ResourceManager.GetString("Add", resourceCulture);
+                return ResourceManager.GetString("OperandNotSet", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Inlet.
+        ///   Looks up a localized string similar to Value of value operator named &apos;{0}&apos; is 0..
         /// </summary>
-        internal static string Inlet {
+        internal static string ValueOperatorValueIs0 {
             get {
-                return ResourceManager.GetString("Inlet", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Inlets.
-        /// </summary>
-        internal static string Inlets {
-            get {
-                return ResourceManager.GetString("Inlets", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Name.
-        /// </summary>
-        internal static string Name {
-            get {
-                return ResourceManager.GetString("Name", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Operator Type.
-        /// </summary>
-        internal static string OperatorTypeName {
-            get {
-                return ResourceManager.GetString("OperatorTypeName", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Outlet.
-        /// </summary>
-        internal static string Outlet {
-            get {
-                return ResourceManager.GetString("Outlet", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Outlets.
-        /// </summary>
-        internal static string Outlets {
-            get {
-                return ResourceManager.GetString("Outlets", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Substract.
-        /// </summary>
-        internal static string Substract {
-            get {
-                return ResourceManager.GetString("Substract", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Value.
-        /// </summary>
-        internal static string Value {
-            get {
-                return ResourceManager.GetString("Value", resourceCulture);
+                return ResourceManager.GetString("ValueOperatorValueIs0", resourceCulture);
             }
         }
     }

jjvanzon/JJ.Framework

Framework/JJ.Framework.Validation/Resources/Messages.Designer.cs
@@ -1,7 +1,7 @@
 ﻿//------------------------------------------------------------------------------
 // <auto-generated>
 //     This code was generated by a tool.
-//     Runtime Version:4.0.30319.18444
+//     Runtime Version:4.0.30319.34209
 //
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
@@ -22,7 +22,7 @@ namespace JJ.Framework.Validation.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Messages {
+    internal class Messages {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace JJ.Framework.Validation.Resources {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Resources.ResourceManager ResourceManager {
+        internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("JJ.Framework.Validation.Resources.Messages", typeof(Messages).Assembly);
@@ -51,7 +51,7 @@ namespace JJ.Framework.Validation.Resources {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Globalization.CultureInfo Culture {
+        internal static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -61,9 +61,18 @@ namespace JJ.Framework.Validation.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to {0} is integer..
+        /// </summary>
+        internal static string IsInteger {
+            get {
+                return ResourceManager.GetString("IsInteger", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to {0} is null..
         /// </summary>
-        public static string IsNull {
+        internal static string IsNull {
             get {
                 return ResourceManager.GetString("IsNull", resourceCulture);
             }
@@ -72,7 +81,7 @@ namespace JJ.Framework.Validation.Resources {
         /// <summary>
         ///   Looks up a localized string similar to {0} is null or white space..
         /// </summary>
-        public static string IsNullOrWhiteSpace {
+        internal static string IsNullOrWhiteSpace {
             get {
                 return ResourceManager.GetString("IsNullOrWhiteSpace", resourceCulture);
             }
@@ -81,7 +90,7 @@ namespace JJ.Framework.Validation.Resources {
         /// <summary>
         ///   Looks up a localized string similar to {0} is zero..
         /// </summary>
-        public static string IsZero {
+        internal static string IsZero {
             get {
                 return ResourceManager.GetString("IsZero", resourceCulture);
             }
@@ -90,7 +99,7 @@ namespace JJ.Framework.Validation.Resources {
         /// <summary>
         ///   Looks up a localized string similar to {0} is not above {1}..
         /// </summary>
-        public static string NotAbove {
+        internal static string NotAbove {
             get {
                 return ResourceManager.GetString("NotAbove", resourceCulture);
             }
@@ -99,7 +108,7 @@ namespace JJ.Framework.Validation.Resources {
         /// <summary>
         ///   Looks up a localized string similar to {0} is not {1}..
         /// </summary>
-        public static string NotIsValue {
+        internal static string NotIsValue {
             get {
                 return ResourceManager.GetString("NotIsValue", resourceCulture);
             }