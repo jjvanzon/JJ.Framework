jjvanzon/JJ.SaveText

Various SOAP features (not all of them yet)

Apps/JJ.Apps.SetText.Mvc/Helpers/PersistenceHelper.cs
@@ -37,7 +37,8 @@ namespace JJ.Apps.SetText.Mvc.Helpers
                 persistenceConfiguration.ContextType,
                 location,
                 persistenceConfiguration.ModelAssembly,
-                persistenceConfiguration.MappingAssembly);
+                persistenceConfiguration.MappingAssembly,
+                persistenceConfiguration.Dialect);
 
             return context;
         }

jjvanzon/JJ.Framework

Framework/JJ.Framework.Persistence.Tests/Helpers/PersistenceHelper.cs
@@ -24,7 +24,8 @@ namespace JJ.Framework.Persistence.Tests
                 contextType,
                 configuration.Location,
                 modelAssemblyName,
-                mappingAssemblyName);
+                mappingAssemblyName,
+                configuration.Dialect);
         }
     }
 }