jjvanzon/JJ.SaveText

ReSharper refactorings

Presentation/SaveText.Mvc/App_Start/FilterConfig.cs
@@ -4,9 +4,6 @@ namespace JJ.Presentation.SaveText.Mvc
 {
 	public class FilterConfig
 	{
-		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-		{
-			filters.Add(new HandleErrorAttribute());
-		}
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters) => filters.Add(new HandleErrorAttribute());
 	}
 }

jjvanzon/JJ.Framework

Demos/ReturnActions.Mvc/App_Start/FilterConfig.cs
@@ -5,9 +5,6 @@ namespace JJ.Demos.ReturnActions.Mvc
 {
 	public static class FilterConfig
 	{
-		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-	    {
-	        filters.Add(new BasicHandleErrorAttribute());
-	    }
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters) => filters.Add(new BasicHandleErrorAttribute());
 	}
 }