012eb92496056914df798db0c27c70d69786c9ae
jjvanzon <janjoostvanzon@hotmail.com>
2020-09-10 19:33:51 +0200

    TfsToDevOpsWorkItemMigration: rename json confi files. Small changes to PowerShell script

--- Demos/TfsToDevOpsWorkItemMigration/vsts-sync-migrator PowerShell Script.ps1
+++ Demos/TfsToDevOpsWorkItemMigration/vsts-sync-migrator PowerShell Script.ps1
@@ -1,19 +1,25 @@
-# Install chocolatey
+# Cahnge security setting
 Get-ExecutionPolicy
 Set-ExecutionPolicy AllSigned
 Get-ExecutionPolicy
+
+# Install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install vsts-sync-migrator
 choco install vsts-sync-migrator --force
 $env:Path += ";C:\tools\VSTSSyncMigration"
 
+# Go to the folder with config gile in it
+Set-Location "D:\Source\JJs Software\JJ.Framework\Demos\TfsToDevOpsWorkItemMigration"
+
 # Initialize vsts-sync-migrator
-cd "D:\Source\JJs Software\JJ.Framework\Demos\TfsToDevOpsWorkItemMigration"
 migration init
+
+# Try Upgrading vsts-sync-migrator
 # choco upgrade vsts-sync-migrator
 # It gave an error access denied. Trying to restart windows
 # I did see configuration.json appear so maybe things are OK.
 
 # Run vsts-sync-migrator
-migration execute –config configuration.json
+migration execute -c configuration.json
