0744f6bd99d78b2e1c0bfac0686a2de9634675dd
jjvanzon <janjoostvanzon@hotmail.com>
2020-10-01 19:59:08 +0200

    JJ.Framework.Resources.Tests: Extracted a base class that might help test other ResourceFormatters classes too. Tried adding some doc comment to it. JJ.Utilities.Deduplication: Tested the ResourceFormater using a base class from JJ.Framework.Resources.Tests.

---
+++ Utilities/FileDeduplication.Tests/ResourceFormatterTests.cs
@@ -0,0 +1,25 @@
+﻿using JJ.Framework.Resources.Tests;
+using JJ.Utilities.FileDeduplication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#pragma warning disable IDE0002 // Simplify Member Access
+
+namespace FileDeduplication.Tests
+{
+	[TestClass]
+	public class ResourceFormatterTests : ResourceFormatterTestBase
+	{
+		public ResourceFormatterTests()
+			: base(typeof(ResourceFormatter), 
+			       knownCultureNames: new[] { "nl-NL", "en-US" },
+			       unknownCultureName: "zh-CN") { }
+
+		[TestMethod]
+		public new void Test_ResourceFormatter_AllPublicStaticMembers_ReturnText_ForKnownCultures()
+			=> base.Test_ResourceFormatter_AllPublicStaticMembers_ReturnText_ForKnownCultures();
+
+		[TestMethod]
+		public new void Test_ResourceFormatter_UnknownCulture_DefaultsToEnUS()
+			=> base.Test_ResourceFormatter_UnknownCulture_DefaultsToEnUS();
+	}
+}
