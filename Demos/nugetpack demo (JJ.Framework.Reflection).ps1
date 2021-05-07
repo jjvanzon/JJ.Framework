8bae0f1297b158fa1c3d32a0fda914d253af5f1b
jjvanzon <janjoostvanzon@hotmail.com>
2021-05-07 18:29:40 +0200

    ✨ Added demo script for nuget pack command.

---
+++ Demos/nugetpack demo (JJ.Framework.Reflection).ps1
@@ -0,0 +1,6 @@
+﻿# May be run in Visual Studio Package Manager Console ?
+
+C:
+# cd "D:\JJ\Dev\1. Products\2. Code\1. Software System\X\9. JJ\Framework\Reflection"
+cd "C:\Repositories\JJ.Framework\Framework\Data"
+nuget pack -build JJ.Framework.Data.csproj -Prop Configuration=Release
