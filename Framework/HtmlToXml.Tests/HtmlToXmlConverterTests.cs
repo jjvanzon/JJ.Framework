4572530b95404a4c7346441b10c441d21a1a956c
jjvanzon <janjoostvanzon@hotmail.com>
2021-05-10 21:06:53 +0200

    🧪 Tried fixing unit test Test_HtmlToXmlConverter.

--- Framework/HtmlToXml.Tests/HtmlToXmlConverterTests.cs
+++ Framework/HtmlToXml.Tests/HtmlToXmlConverterTests.cs
@@ -9,7 +9,7 @@ namespace JJ.Framework.HtmlToXml.Tests
         public void Test_HtmlToXmlConverter()
         {
             const string input = @"<input type=""text"" disabled /> <p>Hello<crap bla tralala /> <br> Stuff after br </p> <p>Thingy";
-            const string expected = @"<input type=""text"" disabled="""" /> <p>Hello<crap bla="""" tralala="""" /> <br /> Stuff after br </p> <p>Thingy</p>";
+            const string expected = @"<html><input type=""text"" disabled="""" /> <p>Hello<crap bla="""" tralala="""" /> <br /> Stuff after br </p> <p>Thingy</p></html>";
             string actual = HtmlToXmlConverter.Convert(input, false);
             Assert.AreEqual(expected, actual);
         }
