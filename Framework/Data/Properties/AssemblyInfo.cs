f8e4b0ee0d3cfc4196eaa2cbe840914c1de2e0fa
jjvanzon <janjoostvanzon@hotmail.com>
2021-05-25 16:19:31 +0200

    ✍ Making AssemblyInfo more complete for JJ.Framework.Data.

--- Framework/Data/Properties/AssemblyInfo.cs
+++ Framework/Data/Properties/AssemblyInfo.cs
@@ -7,15 +7,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("JJ.Framework.Data")]
 [assembly: AssemblyDescription("May allow programming data access in one way, but switch between different types of data storage technology.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jan Joost van Zon")]
 [assembly: AssemblyProduct("JJ.Framework")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2013 - 2021 Jan Joost van Zon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
