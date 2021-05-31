512cb2da741a887ad22a70f2e2da114eb019477e
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-01 01:20:51 +0200

    ⚠ JJ.Framework.Logging: Added ExceptionExtensions that delegates to ExceptionHelper.

---
+++ Framework/Logging/ExceptionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JJ.Framework.Logging
+{
+    public static class ExceptionExtensions
+    {
+        public static string FormatExceptionWithInnerExceptions(this Exception ex, bool includeStackTrace)
+            => ExceptionHelper.FormatExceptionWithInnerExceptions(ex, includeStackTrace);
+
+        public static string FormatException(this Exception ex, bool includeStackTrace)
+            => ExceptionHelper.FormatException(ex, includeStackTrace);
+
+        public static Exception GetInnermostException(this Exception exception)
+            => ExceptionHelper.GetInnermostException(exception);
+
+        public static bool HasExceptionOrInnerExceptionsOfType<T>(this Exception exception)
+            => ExceptionHelper.HasExceptionOrInnerExceptionsOfType<T>(exception);
+    }
+}
