193307ef86f4fc7f0222c1659827fe50654eebc5
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-01 01:21:05 +0200

    🧪 JJ.Framework.Common.Tests: Test_CultureHelper_Obsolete_SetThreadCultureName_ThrowsException.

---
+++ Framework/Common.Tests/CultureHelper_Accessor.cs
@@ -0,0 +1,12 @@
+﻿using JJ.Framework.Reflection;
+
+namespace JJ.Framework.Common.Tests
+{
+    internal static class CultureHelper_Accessor
+    {
+        private static readonly Accessor _accessor = new Accessor(typeof(CultureHelper));
+
+        public static void SetThreadCultureName(string cultureName)
+            => _accessor.InvokeMethod(nameof(SetThreadCultureName), cultureName);
+    }
+}
