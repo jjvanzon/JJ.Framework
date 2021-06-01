e03af39066bd1abd657346a3fd940245551fbc51
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-01 16:07:14 +0200

    🐜 Reproducing a bug: Test_StaticReflectionCache_Bug_MethodNotFound_NotSolved.

---
+++ Framework/Reflection.Tests/StaticReflectionCacheTests/StaticReflectionCacheTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JJ.Framework.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JJ.Framework.Reflection.Tests.StaticReflectionCacheTests
+{
+    [TestClass]
+    public class StaticReflectionCacheTests
+    {
+        [TestMethod]
+        public void Test_StaticReflectionCache_Bug_MethodNotFound_NotSolved()
+        {
+            const string methodName = nameof(StaticReflectionCacheTests_BugMethodNotFound.Overload);
+            string expectedMessage = $"Method '{methodName}' not found.";
+            Type type = typeof(StaticReflectionCacheTests_BugMethodNotFound);
+
+            // For now I hope to reproduce...
+            AssertHelper.ThrowsException(
+                () => StaticReflectionCache.GetMethod<string, IFormatProvider, double>(type, methodName), 
+                expectedMessage);
+        }
+    }
+}
---
+++ Framework/Reflection.Tests/StaticReflectionCacheTests/StaticReflectionCacheTests_HelperClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JJ.Framework.Reflection.Tests.StaticReflectionCacheTests
+{
+    public static class StaticReflectionCacheTests_BugMethodNotFound
+    {
+        [Obsolete("", true)]
+        public static bool Overload(string s, IFormatProvider provider, out double result)
+            => throw new NotSupportedException();
+
+        [Obsolete("", true)]
+        public static bool Overload(string input, out double? output)
+            => throw new NotSupportedException();
+    }
+}
