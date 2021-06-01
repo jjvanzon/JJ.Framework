0729c3f3179ff9c18f93d32ee2056cb0f68d1df9
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-01 16:32:05 +0200

    🐛 Tests Bug_StaticReflectionCache_MethodNotFound_... demonstrating the difference between with an out parameter and without one.

--- Framework/Reflection.Tests/StaticReflectionCacheTests/StaticReflectionCacheTests.cs
+++ Framework/Reflection.Tests/StaticReflectionCacheTests/StaticReflectionCacheTests.cs
@@ -8,18 +8,24 @@ namespace JJ.Framework.Reflection.Tests.StaticReflectionCacheTests
     public class StaticReflectionCacheTests
     {
         [TestMethod]
-        public void Bug_StaticReflectionCache_MethodNotFound_NotSolved()
+        public void Bug_StaticReflectionCache_MethodNotFound_Fails_ThreeParameters_OfWhichOnOutParameter()
         {
             Type type = typeof(StaticReflectionCacheTests_BugMethodNotFound);
-            const string methodName = nameof(StaticReflectionCacheTests_BugMethodNotFound.Overload);
+            const string methodName = nameof(StaticReflectionCacheTests_BugMethodNotFound.Method_ThreeParameters_OfWhichOnOutParameter);
             string expectedMessage = $"Method '{methodName}' not found.";
 
-            // For now I hope to reproduce...
             AssertHelper.ThrowsException(
                 () => StaticReflectionCache.GetMethod<string, IFormatProvider, double>(type, methodName),
                 expectedMessage);
+        }
 
-            //StaticReflectionCache.GetMethod<string, IFormatProvider, double>(type, methodName);
+        [TestMethod]
+        public void Bug_StaticReflectionCache_MethodNotFound_Succeeds_ThreeParameters_NoOutParameter()
+        {
+            Type type = typeof(StaticReflectionCacheTests_BugMethodNotFound);
+            const string methodName = nameof(StaticReflectionCacheTests_BugMethodNotFound.Method_ThreeParameters_NoOutParameter);
+
+            StaticReflectionCache.GetMethod<string, IFormatProvider, int>(type, methodName);
         }
     }
 }
--- Framework/Reflection.Tests/StaticReflectionCacheTests/StaticReflectionCacheTests_HelperClass.cs
+++ Framework/Reflection.Tests/StaticReflectionCacheTests/StaticReflectionCacheTests_HelperClass.cs
@@ -4,7 +4,13 @@ namespace JJ.Framework.Reflection.Tests.StaticReflectionCacheTests
 {
     public static class StaticReflectionCacheTests_BugMethodNotFound
     {
-        public static bool Overload(string s, IFormatProvider provider, out double result)
+        public static bool Method_ThreeParameters_OfWhichOnOutParameter(string s, IFormatProvider provider, out double result)
+            => throw new NotSupportedException();
+
+        public static bool Method_OneOutParameter(out double result)
+            => throw new NotSupportedException();
+
+        public static bool Method_ThreeParameters_NoOutParameter(string s, IFormatProvider provider, int result)
             => throw new NotSupportedException();
     }
 }
