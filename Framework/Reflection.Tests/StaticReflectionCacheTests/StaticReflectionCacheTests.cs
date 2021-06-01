86f2f53bbef3b80e6a41515f8616327be0ea61ad
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-01 16:18:20 +0200

    🐜 Bug_StaticReflectionCache_MethodNotFound_NotSolved: Simplified situation and still reproducible. Seems dependent on out parameter.

--- Framework/Reflection.Tests/StaticReflectionCacheTests/StaticReflectionCacheTests.cs
+++ Framework/Reflection.Tests/StaticReflectionCacheTests/StaticReflectionCacheTests.cs
@@ -8,16 +8,18 @@ namespace JJ.Framework.Reflection.Tests.StaticReflectionCacheTests
     public class StaticReflectionCacheTests
     {
         [TestMethod]
-        public void Test_StaticReflectionCache_Bug_MethodNotFound_NotSolved()
+        public void Bug_StaticReflectionCache_MethodNotFound_NotSolved()
         {
+            Type type = typeof(StaticReflectionCacheTests_BugMethodNotFound);
             const string methodName = nameof(StaticReflectionCacheTests_BugMethodNotFound.Overload);
             string expectedMessage = $"Method '{methodName}' not found.";
-            Type type = typeof(StaticReflectionCacheTests_BugMethodNotFound);
 
             // For now I hope to reproduce...
             AssertHelper.ThrowsException(
-                () => StaticReflectionCache.GetMethod<string, IFormatProvider, double>(type, methodName), 
+                () => StaticReflectionCache.GetMethod<string, IFormatProvider, double>(type, methodName),
                 expectedMessage);
+
+            //StaticReflectionCache.GetMethod<string, IFormatProvider, double>(type, methodName);
         }
     }
 }
--- Framework/Reflection.Tests/StaticReflectionCacheTests/StaticReflectionCacheTests_HelperClass.cs
+++ Framework/Reflection.Tests/StaticReflectionCacheTests/StaticReflectionCacheTests_HelperClass.cs
@@ -4,12 +4,7 @@ namespace JJ.Framework.Reflection.Tests.StaticReflectionCacheTests
 {
     public static class StaticReflectionCacheTests_BugMethodNotFound
     {
-        [Obsolete("", true)]
         public static bool Overload(string s, IFormatProvider provider, out double result)
-            => throw new NotSupportedException();
-
-        [Obsolete("", true)]
-        public static bool Overload(string input, out double? output)
             => throw new NotSupportedException();
     }
 }
