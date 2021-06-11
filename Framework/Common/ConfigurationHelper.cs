33d61b7e057ea9591cfe2a8518fa170b761951a9
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-11 23:12:17 +0200

    📛 JJ.Framework.Common: ConfiguationHelper: A section name may now be specified.

--- Framework/Common/ConfigurationHelper.cs
+++ Framework/Common/ConfigurationHelper.cs
@@ -4,56 +4,74 @@ using JetBrains.Annotations;
 
 namespace JJ.Framework.Common
 {
-    /// <summary>
-    /// For using configuration settings when you cannot be dependent on System.Configuration.
-    /// </summary>
+    /// <summary> For using configuration settings where you cannot be dependent on System.Configuration. </summary>
     [PublicAPI]
     public static class ConfigurationHelper
     {
         private static readonly object _sectionsLock = new object();
-        private static readonly IDictionary<Type, object> _sections = new Dictionary<Type, object>();
+        private static readonly IDictionary<string, object> _sections = new Dictionary<string, object>();
 
-        // TODO: Low priority: Make the overloads with the explicit section name.
-
-        public static T GetSection<T>()
+        /// <param name="sectionName">If null it may be assumed to be assembly name in lower case.</param>
+        public static T GetSection<T>(string sectionName = null)
         {
             lock (_sectionsLock)
             {
-                object section = TryGetSection<T>();
+                sectionName = ResolveSectionName<T>(sectionName);
+
+                object section = TryGetSection<T>(sectionName);
+
                 if (section == null)
                 {
                     // ReSharper disable once UseStringInterpolation
-                    throw new ApplicationException(string.Format(
-                        "Configuration section of type '{0}' was not set. To allow {1} to use this configuration section, call {2}.SetSection.",
-                        typeof(T).FullName,
-                        typeof(ConfigurationHelper).Assembly.GetName().Name,
-                        typeof(ConfigurationHelper).FullName));
+                    throw new ApplicationException(
+                        $"Configuration section with name '{sectionName}' was not set. " +
+                        $"To allow {typeof(ConfigurationHelper).Assembly.GetName().Name} to use this configuration section, " +
+                        $"call {typeof(ConfigurationHelper).FullName}.SetSection.");
                 }
+
                 return (T)section;
             }
         }
 
-        public static T TryGetSection<T>()
+        /// <param name="sectionName">If null it may be assumed to be assembly name in lower case.</param>
+        public static T TryGetSection<T>(string sectionName = null)
         {
+            sectionName = ResolveSectionName<T>(sectionName);
+
             lock (_sectionsLock)
             {
-                _sections.TryGetValue(typeof(T), out object section);
+                _sections.TryGetValue(sectionName, out object section);
                 return (T)section;
             }
         }
 
-        public static void SetSection<T>(T section)
+        /// <param name="sectionName">If null it may be assumed to be assembly name in lower case.</param>
+        public static void SetSection<T>(T section, string sectionName = null)
         {
             if (section == null) throw new ArgumentNullException(nameof(section));
 
+            sectionName = ResolveSectionName<T>(sectionName);
+
             lock (_sectionsLock)
             {
-                if (_sections.ContainsKey(typeof(T)))
+                if (_sections.ContainsKey(sectionName))
                 {
-                    throw new ApplicationException($"Configuration section of type '{typeof(T).FullName}' was already set.");
+                    throw new ApplicationException($"Configuration section with with name '{sectionName}' was already set.");
                 }
-                _sections.Add(typeof(T), section);
+
+                _sections.Add(sectionName, section);
             }
+        }
+
+        /// <summary> Section name. when not provided, may be assumed to be assembly name in lower case. </summary>
+        private static string ResolveSectionName<T>(string sectionName)
+        {
+            if (!string.IsNullOrEmpty(sectionName))
+            {
+                return sectionName;
+            }
+
+            return typeof(T).Assembly.GetName().Name.ToLower();
         }
     }
 }
