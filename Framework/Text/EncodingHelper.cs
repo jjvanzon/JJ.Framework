bb4ef0b8f594edef5f668c3ba276662b07d3e196
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-16 18:48:20 +0200

    ✍ JJ.Framework.Text: EncodingHelper.AddUTF8ByteOrderMark.

---
+++ Framework/Text/EncodingHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace JJ.Framework.Text
+{
+    public static class EncodingHelper
+    {
+        public static byte[] AddUTF8ByteOrderMark(byte[] bytes)
+        {
+            if (bytes == null) throw new ArgumentNullException(nameof(bytes));
+            return Encoding.UTF8.GetPreamble().Concat(bytes).ToArray();
+        }
+    }
+}
