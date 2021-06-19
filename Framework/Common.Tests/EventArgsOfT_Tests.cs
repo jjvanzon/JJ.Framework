854726921b34e7bd3857af1d1c116ba024ae2e3c
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-20 00:02:53 +0200

    🔬 JJ.Framework.Common.Tests: EventArgsOfT_Tests.

---
+++ Framework/Common.Tests/EventArgsOfT_Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using JJ.Framework.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JJ.Framework.Common.Tests
+{
+    [TestClass]
+    public class EventArgsOfT_Tests 
+    {
+
+        [TestMethod]
+        public void Test_EventArgsOfT()
+        {
+            var eventArgs = new EventArgs<int>(123);
+            AssertHelper.IsOfType<EventArgs>(() => eventArgs);
+            AssertHelper.AreEqual(123, () => eventArgs.Value);
+        }
+    }
+}
