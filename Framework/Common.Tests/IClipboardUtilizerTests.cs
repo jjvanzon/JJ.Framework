fe015ee8c334c8d39597550bed1172a071fa313a
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-21 19:28:21 +0200

    📋 JJ.Framework.Common.Tests: IClipboardUtilizerTests: Just to demo the use of this interface, really.

---
+++ Framework/Common.Tests/IClipboardUtilizerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// ReSharper disable NotAccessedField.Local
+// ReSharper disable InconsistentNaming
+#pragma warning disable IDE0052 // Remove unread private members
+
+namespace JJ.Framework.Common.Tests
+{
+    [TestClass]
+    public class IClipboardUtilizerTests
+    {
+        [TestMethod]
+        public void Demo_IClipboardUtilizer()
+        {
+            IClipboardUtilizer clipboardUtilizer = new ClipboardUtilizer();
+            clipboardUtilizer.SetText("Something to put on the clipboard.");
+        }
+
+        private class ClipboardUtilizer : IClipboardUtilizer
+        {
+            private string _fakeStoringInClipboard;
+
+            public void SetText(string text) => _fakeStoringInClipboard = text;
+        }
+    }
+}
