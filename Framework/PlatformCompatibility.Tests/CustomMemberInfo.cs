d857888fc007979456a3cf8eb85ff7ed6feae230
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-22 21:29:54 +0200

    🔬 JJ.Framework.PlatformCompatibility.Tests: MemberTypes_PlatformSafe_Tests.

---
+++ Framework/PlatformCompatibility.Tests/CustomMemberInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace JJ.Framework.PlatformCompatibility.Tests
+{
+    internal class CustomMemberInfo : MemberInfo
+    {
+        public override object[] GetCustomAttributes(bool inherit) => throw new NotImplementedException();
+
+        public override bool IsDefined(Type attributeType, bool inherit) => throw new NotImplementedException();
+
+        public override MemberTypes MemberType => MemberTypes.Custom;
+        public override string Name { get; }
+        public override Type DeclaringType { get; }
+        public override Type ReflectedType { get; }
+
+        public override object[] GetCustomAttributes(Type attributeType, bool inherit) => throw new NotImplementedException();
+    }
+}
