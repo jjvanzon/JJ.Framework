6b09054740ded59ffb70ee9dff064e54a26a245b
jjvanzon <jjvanzon79@gmail.com>
2021-06-23 00:39:18 +0200

    🏳 JJ.Framework.PlatformCompatibility.Tests: CultureInfo_PlatformSafe tests. (🍒 cherry picked dd964a2176c24524c1a8b2a197d9ef6b919ac543)

---
+++ Framework/PlatformCompatibility.Tests/Obsolete/CultureInfo_PlatformSafe_Obsolete_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using JJ.Framework.Testing.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JJ.Framework.PlatformCompatibility.Tests.Obsolete
+{
+    [TestClass]
+    public class CultureInfo_PlatformSafe_Obsolete_Tests
+    {
+        [TestMethod]
+        public void Test_PlatformHelper_Obsolete_PlatformHelper_CultureInfo_GetCultureInfo_PlatformSafe_ThrowsException()
+            => AssertHelperLegacy.ThrowsException_OrInnerException<NotSupportedException>(
+                () => PlatformHelper_Accessor.CultureInfo_GetCultureInfo_PlatformSafe(default),
+                "Use GetCultureInfo_PlatformSafe instead.");
+    }
+}
