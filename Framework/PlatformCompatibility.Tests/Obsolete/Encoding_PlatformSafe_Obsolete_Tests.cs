5aa917ce284539a922a3388bf1e48f64b4b2dc78
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-23 17:53:40 +0200

    🧪 JJ.Framework.PlatformCompatibility.Tests: Encoding_PlatformSafe_Tests.

---
+++ Framework/PlatformCompatibility.Tests/Obsolete/Encoding_PlatformSafe_Obsolete_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using JJ.Framework.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JJ.Framework.PlatformCompatibility.Tests.Obsolete
+{
+    [TestClass]
+    public class Encoding_PlatformSafe_Obsolete_Tests
+    {
+        [TestMethod]
+        public void Test_Encoding_PlatformSafe_Obsolete_GetString_PlatformSafe_ThrowsException()
+            => AssertHelper.ThrowsException_OrInnerException<NotSupportedException>(
+                () => Encoding_PlatformSafe_Accessor.GetString_PlatformSafe(default, default),
+                "Use PlatformHelper.GetString_PlatformSafe instead.");
+    }
+}
