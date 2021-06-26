b7eb51879f07df21b1bf8180557c599d93fee760
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-26 18:02:19 +0200

    🧫 Testing JJ.Framework.Text: StringBuilderWithIndentationTests: Test set more or less complete.

--- Framework/Text.Tests/StringBuilderWithIndentationTests.cs
+++ Framework/Text.Tests/StringBuilderWithIndentationTests.cs
@@ -2,7 +2,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using static System.Environment;
 
+// ReSharper disable UseObjectOrCollectionInitializer
 // ReSharper disable ConvertToConstant.Local
+
+#pragma warning disable IDE0017 // Simplify object initialization
 
 namespace JJ.Framework.Text.Tests
 {
@@ -158,48 +161,282 @@ namespace JJ.Framework.Text.Tests
             AssertHelper.AreEqual(expectedText, () => actualText);
         }
 
-        /*
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_ToString_ReturnsFormattedText() => throw new NotImplementedException();
+        public void Test_StringBuilderWithIndentation_ToString_ReturnsFormattedText()
+        {
+            // Arrange
+            var sb = new StringBuilderWithIndentation("    ");
+
+            sb.AppendLine("Text1");
+            sb.Indent();
+            { 
+                sb.AppendLine("Text2");
+            }
+
+            // Act
+            string actualText = sb.ToString();
+
+            // Assert
+            string expectedText =
+                "Text1" + NewLine +
+                "    Text2" + NewLine;
+            AssertHelper.AreEqual(expectedText, () => actualText);
+        }
 
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_Indent_Works() => throw new NotImplementedException();
+        public void Test_StringBuilderWithIndentation_Indent_Works()
+        {
+            // Arrange
+            var sb = new StringBuilderWithIndentation("    ");
+            sb.AppendLine("Text1");
+
+            // Act
+            sb.Indent();
+
+            // Assert
+            sb.AppendLine("Text2");
+            string actualText = sb.ToString();
+            string expectedText =
+                "Text1" + NewLine +
+                "    Text2" + NewLine;
+            AssertHelper.AreEqual(expectedText, () => actualText);
+        }
 
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_Unindent_Works() => throw new NotImplementedException();
+        public void Test_StringBuilderWithIndentation_Unindent_Works()
+        {
+            // Arrange
+            var sb = new StringBuilderWithIndentation("    ");
+            sb.AppendLine("Text1");
+            sb.Indent();
+            {
+                sb.AppendLine("Text2");
+            }
+
+            // Act
+            sb.Unindent();
+
+            // Assert
+            sb.AppendLine("Text3");
+            string actualText = sb.ToString();
+            string expectedText =
+                "Text1" + NewLine +
+                "    Text2" + NewLine +
+                "Text3" + NewLine;
+            AssertHelper.AreEqual(expectedText, () => actualText);
+        }
 
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_AppendLine_WithoutText_Works() => throw new NotImplementedException();
+        public void Test_StringBuilderWithIndentation_AppendLine_WithoutText_Works()
+        {
+            // Arrange
+            var sb = new StringBuilderWithIndentation();
+            
+            // Act
+            sb.AppendLine();
+            sb.AppendLine();
+
+            // Assert.
+            string actualText = sb.ToString();
+            string expectedText = NewLine + NewLine;
+            AssertHelper.AreEqual(expectedText, () => actualText);
+        }
 
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_AppendLine_WithText_Works() => throw new NotImplementedException();
+        public void Test_StringBuilderWithIndentation_AppendLine_WithText_Works()
+        {
+            // Arrange
+            var sb = new StringBuilderWithIndentation();
+
+            // Act
+            sb.AppendLine("Text1");
+            sb.AppendLine("Text2");
+
+            // Assert.
+            string actualText = sb.ToString();
+            string expectedText =
+                "Text1" + NewLine +
+                "Text2" + NewLine;
+            AssertHelper.AreEqual(expectedText, () => actualText);
+        }
 
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_AppendLine_AppendsTabsBeforeLine() => throw new NotImplementedException();
+        public void Test_StringBuilderWithIndentation_AppendLine_AppendsTabsBeforeLineNotAfter()
+        {
+            // Arrange
+            var sb = new StringBuilderWithIndentation("    ");
+
+            // Act
+            sb.Indent();
+            sb.AppendLine("Text");
+
+            // Assert.
+            string actualText = sb.ToString();
+            string expectedText = "    Text" + NewLine;
+            AssertHelper.AreEqual(expectedText, () => actualText);
+        }
 
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_Append_Works() => throw new NotImplementedException();
+        public void Test_StringBuilderWithIndentation_Append_Works()
+        {
+            // Arrange
+            var sb = new StringBuilderWithIndentation();
+
+            // Act
+            sb.Append("Text1");
+            sb.Append(" ");
+            sb.Append("Text2");
+
+            // Assert.
+            string actualText = sb.ToString();
+            string expectedText = "Text1 Text2";
+            AssertHelper.AreEqual(expectedText, () => actualText);
+        }
 
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_AppendFormat_Works() => throw new NotImplementedException();
+        public void Test_StringBuilderWithIndentation_Append_RequiresManualAppendTabs()
+        {
+            // Arrange
+            var sb = new StringBuilderWithIndentation("    ");
+
+            // Act
+            sb.Indent();
+            sb.AppendTabs();
+            sb.Append("Text");
+
+            // Assert
+            string actualText = sb.ToString();
+            string expectedText = "    Text";
+            AssertHelper.AreEqual(expectedText, () => actualText);
+        }
 
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_IndentLevel_Assignment_Works() => throw new NotImplementedException();
+        public void Test_StringBuilderWithIndentation_Append_WithoutManualAppendTabs_AddsNoTabs()
+        {
+            // Arrange
+            var sb = new StringBuilderWithIndentation("    ");
+
+            // Act
+            sb.Indent();
+            sb.Append("Text");
+
+            // Assert
+            string actualText = sb.ToString();
+            string expectedText = "Text";
+            AssertHelper.AreEqual(expectedText, () => actualText);
+        }
 
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_IndentLevel_AssigningLessThan0_ThrowsException() => throw new NotImplementedException();
+        public void Test_StringBuilderWithIndentation_AppendFormat_Works()
+        {
+            // Arrange
+            string generatedClassName = "MyClass";
+            var sb = new StringBuilderWithIndentation("    ");
+
+            // Act
+            sb.AppendFormat("public class {0}", generatedClassName);
+
+            // Assert
+            string actualText = sb.ToString();
+            string expectedText = "public class MyClass";
+            AssertHelper.AreEqual(expectedText, () => actualText);
+        }
 
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_IndentLevel_ReturnsCorrectValue() => throw new NotImplementedException();
+        public void Test_StringBuilderWithIndentation_IndentLevel_Assignment_Works()
+        {
+            // Arrange
+            var sb = new StringBuilderWithIndentation("    ");
+
+            // Act
+            sb.IndentLevel = 0;
+            sb.AppendLine("Text1");
+            {
+                sb.IndentLevel = 1;
+                sb.AppendLine("Text2");
+                {
+                    sb.IndentLevel = 2;
+                    sb.AppendLine("Text3");
+                }
+                sb.IndentLevel = 1;
+                sb.AppendLine("Text4");
+            }
+            sb.IndentLevel = 0;
+            sb.AppendLine("Text5");
+
+            // Assert
+            string actualText = sb.ToString();
+            string expectedText =
+                "Text1" + NewLine +
+                "    Text2" + NewLine +
+                "        Text3" + NewLine +
+                "    Text4" + NewLine +
+                "Text5" + NewLine;
+            AssertHelper.AreEqual(expectedText, () => actualText);
+        }
 
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_AppendTabs_Works() => throw new NotImplementedException();
+        public void Test_StringBuilderWithIndentation_IndentLevel_AssigningLessThan0_ThrowsException()
+        {
+            var sb = new StringBuilderWithIndentation();
+            AssertHelper.ThrowsException(() => sb.IndentLevel = -1, "value cannot be less than 0.");
+        }
 
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_AppendTabs_UseCase_Calling_Publicly() => throw new NotImplementedException();
+        public void Test_StringBuilderWithIndentation_IndentLevel_ReturnsCorrectValue()
+        {
+            var sb = new StringBuilderWithIndentation();
+
+            AssertHelper.AreEqual(0, () => sb.IndentLevel);
+            sb.Indent();
+            {
+                AssertHelper.AreEqual(1, () => sb.IndentLevel);
+                sb.Indent();
+                {
+                    AssertHelper.AreEqual(2, () => sb.IndentLevel);
+                    sb.Unindent();
+                }
+                AssertHelper.AreEqual(1, () => sb.IndentLevel);
+                sb.Indent();
+                {
+                    AssertHelper.AreEqual(2, () => sb.IndentLevel);
+                    sb.Unindent();
+                }
+                AssertHelper.AreEqual(1, () => sb.IndentLevel);
+                sb.Unindent();
+            }
+            AssertHelper.AreEqual(0, () => sb.IndentLevel);
+        }
 
         [TestMethod]
-        public void Test_StringBuilderWithIndentation_GetTabs_ReturnsCorrectString() => throw new NotImplementedException();
-        */
+        public void Test_StringBuilderWithIndentation_AppendTabs_Works()
+        {
+            // Arrange
+            var sb = new StringBuilderWithIndentation("\t");
+            sb.IndentLevel = 2;
+
+            // Act
+            sb.AppendTabs();
+
+            // Assert
+            string actualText = sb.ToString();
+            string expectedText = "\t\t";
+            AssertHelper.AreEqual(expectedText, () => actualText);
+        }
+
+        [TestMethod]
+        public void Test_StringBuilderWithIndentation_GetTabs_ReturnsConcatenatedTabStrings()
+        {
+            // Arrange
+            var sb = new StringBuilderWithIndentation("    ");
+            sb.IndentLevel = 2;
+
+            // Act
+            string actualTabs = sb.GetTabs();
+
+            // Assert
+            string expectedTabs = "        ";
+            AssertHelper.AreEqual(expectedTabs, () => actualTabs);
+        }
     }
 }
