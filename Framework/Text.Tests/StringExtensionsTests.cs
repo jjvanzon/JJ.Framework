b0b86634601073e4811ad63ff98f476ba9eef7c7
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-28 20:48:37 +0200

    🧪 JJ.Framework.Text.Tests: StringExtensionsTests: Extended tests for Left method. 🌌 Added white space to tests for FromTill method, for easier viewing on smaller screen with word wrap.

--- Framework/Text.Tests/StringExtensionsTests.cs
+++ Framework/Text.Tests/StringExtensionsTests.cs
@@ -25,8 +25,9 @@ namespace JJ.Framework.Text.Tests
 
         [TestMethod]
         public void Test_StringExtensions_FromTill_StartIndex_Negative_ThrowsException()
-            => AssertHelper.ThrowsException(() => "0123456789".FromTill(-1, 9),
-                                            "startIndex of -1 is less than 0.");
+            => AssertHelper.ThrowsException(
+                () => "0123456789".FromTill(-1, 9),
+                "startIndex of -1 is less than 0.");
 
         [TestMethod]
         public void Test_StringExtensions_FromTill_NullInput_ThrowsException()
@@ -36,28 +37,61 @@ namespace JJ.Framework.Text.Tests
 
         [TestMethod]
         public void Test_StringExtensions_FromTill_StartIndex_ExceedsLastCharacter_ThrowsException()
-            => AssertHelper.ThrowsException(() => "0123456789".FromTill(10, 9),
-                                            "startIndex of 10 exceeds last character. input.Length = 10");
+            => AssertHelper.ThrowsException(
+                () => "0123456789".FromTill(10, 9),
+                "startIndex of 10 exceeds last character. input.Length = 10");
 
         [TestMethod]
         public void Test_StringExtensions_FromTill_EndIndex_Negative_ThrowsException()
-            => AssertHelper.ThrowsException(() => "0123456789".FromTill(0, -1), 
-                                            "endIndex of -1 is less than 0.");
+            => AssertHelper.ThrowsException(
+                () => "0123456789".FromTill(0, -1),
+                "endIndex of -1 is less than 0.");
 
         [TestMethod]
         public void Test_StringExtensions_FromTill_EndIndex_ExceedsLastCharacter_ThrowsException()
-            => AssertHelper.ThrowsException(() => "0123456789".FromTill(0, 10), 
-                                            "endIndex of 10 exceeds last character. input.Length = 10");
+            => AssertHelper.ThrowsException(
+                () => "0123456789".FromTill(0, 10),
+                "endIndex of 10 exceeds last character. input.Length = 10");
 
         [TestMethod]
         public void Test_StringExtensions_FromTill_StartIndex_GreaterThan_EndIndex_ThrowsException()
-            => AssertHelper.ThrowsException(() => "0123456789".FromTill(8, 1),
-                                            "startIndex of 8 is greater than endIndex of 1.");
+            => AssertHelper.ThrowsException(
+                () => "0123456789".FromTill(8, 1),
+                "startIndex of 8 is greater than endIndex of 1.");
 
-        // Other
+        // Left
 
         [TestMethod]
-        public void Test_StringExtensions_Left_NotEnoughCharacters_ThrowsException() => AssertHelper.ThrowsException(() => "1234".Left(5));
+        public void Test_StringExtensions_Left() 
+            => AssertHelper.AreEqual("12", () => "1234".Left(2));
+
+        [TestMethod]
+        public void Test_StringExtensions_Left_LengthZero()
+            => AssertHelper.AreEqual("", () => "1234".Left(0));
+
+        [TestMethod]
+        public void Test_StringExtensions_Left_EntireString()
+            => AssertHelper.AreEqual("1234", () => "1234".Left(4));
+
+        [TestMethod]
+        public void Test_StringExtensions_Left_NullInput_ThrowsException()
+            => AssertHelper.ThrowsException<ArgumentNullException>(
+                () => StringExtensions.Left(null, default),
+                $"Value cannot be null.{Environment.NewLine}Parameter name: input");
+
+        [TestMethod]
+        public void Test_StringExtensions_Left_LengthNegative_ThrowsException()
+            => AssertHelper.ThrowsException<ArgumentOutOfRangeException>(
+                () => "1234".Left(-1),
+                $"Length cannot be less than zero.{Environment.NewLine}Parameter name: length");
+
+        [TestMethod]
+        public void Test_StringExtensions_Left_NotEnoughCharacters_ThrowsException()
+            => AssertHelper.ThrowsException<ArgumentOutOfRangeException>(
+                () => "1234".Left(5),
+                $"Index and length must refer to a location within the string.{Environment.NewLine}Parameter name: length");
+
+        // Other
 
         [TestMethod]
         public void Test_StringExtensions_Right_NotEnoughCharacters_ThrowsException() => AssertHelper.ThrowsException(() => "1234".Right(5));
