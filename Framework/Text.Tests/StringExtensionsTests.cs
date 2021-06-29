ebb70c22dedd4afb7681878c6f923e7e55c89cbc
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-29 22:02:15 +0200

    🧪 Testing JJ.Framework.Text RemoveExcessiveWhiteSpace method of the StringExtensions class.

--- Framework/Text.Tests/StringExtensionsTests.cs
+++ Framework/Text.Tests/StringExtensionsTests.cs
@@ -90,6 +90,30 @@ namespace JJ.Framework.Text.Tests
                 () => "1234".Left(5),
                 $"Index and length must refer to a location within the string.{Environment.NewLine}Parameter name: length");
 
+        // RemoveExcessiveWhiteSpace
+
+        [TestMethod]
+        public void Test_StringExtensions_RemoveExcessiveWhiteSpace_ComplexExample()
+            => AssertHelper.AreEqual("Test 123", () => $"\u2007 \r Test \u2008 \t\t{Environment.NewLine}\t  123 \n\n ".RemoveExcessiveWhiteSpace());
+
+        [TestMethod]
+        public void Test_StringExtensions_RemoveExcessiveWhiteSpace_Trims_WhiteSpace() 
+            => AssertHelper.AreEqual("Test", () => $" {Environment.NewLine}Test\t".RemoveExcessiveWhiteSpace());
+
+        [TestMethod]
+        public void Test_StringExtensions_RemoveExcessiveWhiteSpace_ReplacesIntermediateWhiteSpace_BySingleSpace()
+            => AssertHelper.AreEqual("Test 123", () => $"Test {Environment.NewLine}\t  123".RemoveExcessiveWhiteSpace());
+
+        [TestMethod]
+        public void Test_StringExtensions_RemoveExcessiveWhiteSpace_EmptyString_ReturnsEmptyString()
+            => AssertHelper.AreEqual("", () => "".RemoveExcessiveWhiteSpace());
+
+        [TestMethod]
+        public void Test_StringExtensions_RemoveExcessiveWhiteSpace_Null_ThrowsException()
+            => AssertHelper.ThrowsException<ArgumentNullException>(
+                () => StringExtensions.RemoveExcessiveWhiteSpace(null), 
+                $"Value cannot be null.{Environment.NewLine}Parameter name: text");
+
         // Right
 
         [TestMethod]
