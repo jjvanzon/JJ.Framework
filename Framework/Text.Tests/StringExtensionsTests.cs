5aac76ad632623347d3e9ded7bd761f3146d904b
jjvanzon <janjoostvanzon@hotmail.com>
2021-06-29 23:07:17 +0200

    🥼 Testing JJ.Framework.Text Repeat method of the StringExtensions class.

--- Framework/Text.Tests/StringExtensionsTests.cs
+++ Framework/Text.Tests/StringExtensionsTests.cs
@@ -114,6 +114,36 @@ namespace JJ.Framework.Text.Tests
                 () => StringExtensions.RemoveExcessiveWhiteSpace(null), 
                 $"Value cannot be null.{Environment.NewLine}Parameter name: text");
 
+        // Repeat
+
+        [TestMethod]
+        public void Test_StringExtensions_Repeat_RepeatCount_Many()
+            => AssertHelper.AreEqual("123412341234", () => "1234".Repeat(3));
+
+        [TestMethod]
+        public void Test_StringExtensions_Repeat_RepeatCount_One()
+            => AssertHelper.AreEqual("321", () => "321".Repeat(1));
+
+        [TestMethod]
+        public void Test_StringExtensions_Repeat_RepeatCount_Zero_ReturnsEmptyString()
+            => AssertHelper.AreEqual("", () => "1234".Repeat(0));
+
+        [TestMethod]
+        public void Test_StringExtensions_Repeat_StringToRepeat_EmptyString_ReturnsEmptyString()
+            => AssertHelper.AreEqual("", () => "".Repeat(3));
+
+        [TestMethod]
+        public void Test_StringExtensions_Repeat_RepeatCount_Negative_ThrowsException()
+            => AssertHelper.ThrowsException(
+                () => "1234".Repeat(-1), 
+                "repeatCount of -1 is less than 0.");
+
+        [TestMethod]
+        public void Test_StringExtensions_Repeat_StringToRepeat_Null_ThrowsException()
+            => AssertHelper.ThrowsException<ArgumentNullException>(
+                () => StringExtensions.Repeat(null, default), 
+                $"Value cannot be null.{Environment.NewLine}Parameter name: stringToRepeat");
+
         // Right
 
         [TestMethod]
