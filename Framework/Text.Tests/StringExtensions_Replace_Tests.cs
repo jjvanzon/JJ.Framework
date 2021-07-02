46b4eb445e3348b413e5c815f5048144116a4127
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-02 15:03:34 +0200

    🥼 JJ.Framework.Text.Tests: StringExtensions_Replace_Tests: Varied the tested values more.

--- Framework/Text.Tests/StringExtensions_Replace_Tests.cs
+++ Framework/Text.Tests/StringExtensions_Replace_Tests.cs
@@ -22,45 +22,45 @@ namespace JJ.Framework.Text.Tests
         [TestMethod]
         public void Test_StringExtensions_Replace_IgnoreCase_True_NoMatch()
         {
-            var input = "<a><b><b>";
-            string actual = input.Replace("<d>", "<c>", ignoreCase: true);
-            string expected = "<a><b><b>";
+            var input = "<d><e><e>";
+            string actual = input.Replace("<_>", "<f>", ignoreCase: true);
+            string expected = "<d><e><e>";
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Test_StringExtensions_Replace_IgnoreCase_False_WithMatch()
         {
-            var input = "<a><b><b>";
-            string actual = input.Replace("<b>", "<c>", ignoreCase: false);
-            string expected = "<a><c><c>";
+            var input = "<g><h><h>";
+            string actual = input.Replace("<h>", "<i>", ignoreCase: false);
+            string expected = "<g><i><i>";
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Test_String_Replace_FromDotNet_WithMatch()
         {
-            var input = "<a><b><b>";
-            string actual = input.Replace("<b>", "<c>");
-            string expected = "<a><c><c>";
+            var input = "<j><k><k>";
+            string actual = input.Replace("<k>", "<l>");
+            string expected = "<j><l><l>";
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Test_StringExtensions_Replace_IgnoreCase_False_NoMatch()
         {
-            var input = "<a><b><b>";
-            string actual = input.Replace("<B>", "<c>", ignoreCase: false);
-            string expected = "<a><b><b>";
+            var input = "<m><n><n>";
+            string actual = input.Replace("<N>", "<o>", ignoreCase: false);
+            string expected = "<m><n><n>";
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Test_String_Replace_FromDotNet_NoMatch()
         {
-            var input = "<a><b><b>";
-            string actual = input.Replace("<B>", "<c>");
-            string expected = "<a><b><b>";
+            var input = "<p><q><q>";
+            string actual = input.Replace("<Q>", "<r>");
+            string expected = "<p><q><q>";
             Assert.AreEqual(expected, actual);
         }
 
@@ -69,7 +69,7 @@ namespace JJ.Framework.Text.Tests
         {
             string input = null;
             AssertHelper.ThrowsException<ArgumentNullException>(
-                () => input.Replace("<b>", "<c>", default),
+                () => input.Replace("<s>", "<t>", default),
                 $"Value cannot be null.{Environment.NewLine}Parameter name: input");
         }
 
@@ -78,45 +78,44 @@ namespace JJ.Framework.Text.Tests
         {
             string input = null;
             AssertHelper.ThrowsException<NullReferenceException>(
-                () => input.Replace("<b>", "<c>"),
+                () => input.Replace("<u>", "<v>"),
                 "Object reference not set to an instance of an object.");
         }
 
         [TestMethod]
         public void Test_StringExtensions_Replace_WithIgnoreCase_OldValueNull_ThrowsException()
         {
-            var input = "<a><b><b>";
+            var input = "<w><x><x>";
             AssertHelper.ThrowsException<ArgumentNullException>(
-                () => input.Replace(null, "<c>", default),
+                () => input.Replace(null, "<y>", default),
                 $"Value cannot be null.{Environment.NewLine}Parameter name: oldValue");
         }
 
         [TestMethod]
         public void Test_String_Replace_FromDotNet_OldValueNull_ThrowsException()
         {
-            var input = "<a><b><b>";
+            var input = "<z><aa><aa>";
             AssertHelper.ThrowsException<ArgumentNullException>(
-                () => input.Replace(null, "<c>"),
+                () => input.Replace(null, "<ab>"),
                 $"Value cannot be null.{Environment.NewLine}Parameter name: oldValue");
         }
 
         [TestMethod]
         public void Test_StringExtensions_Replace_WithIgnoreCase_NewValueNull_Succeeds()
         {
-            var input = "<a><b><b>";
-            string actual = input.Replace("<b>", null, default);
-            string expected = "<a>";
+            var input = "<ac><ad><ad>";
+            string actual = input.Replace("<ad>", null, default);
+            string expected = "<ac>";
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Test_String_Replace_FromDotNet_NewValueNull_Succeeds()
         {
-            var input = "<a><b><b>";
-            string actual = input.Replace("<b>", null);
-            string expected = "<a>";
+            var input = "<ae><af><af>";
+            string actual = input.Replace("<af>", null);
+            string expected = "<ae>";
             Assert.AreEqual(expected, actual);
         }
     }
 }
-
