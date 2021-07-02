14bbbc7eec310601dcf8ae791acc091be7d37f78
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-02 14:57:30 +0200

    🧪 JJ.Framework.Text.Tests: StringExtensions_Replace_Tests: Gave test values multiple matches to replace.

--- Framework/Text.Tests/StringExtensions_Replace_Tests.cs
+++ Framework/Text.Tests/StringExtensions_Replace_Tests.cs
@@ -13,54 +13,54 @@ namespace JJ.Framework.Text.Tests
         [TestMethod]
         public void Test_StringExtensions_Replace_IgnoreCase_True_WithMatch()
         {
-            var input = "<a><b>";
+            var input = "<a><b><b>";
             string actual = input.Replace("<B>", "<c>", ignoreCase: true);
-            string expected = "<a><c>";
+            string expected = "<a><c><c>";
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Test_StringExtensions_Replace_IgnoreCase_True_NoMatch()
         {
-            var input = "<a><b>";
+            var input = "<a><b><b>";
             string actual = input.Replace("<d>", "<c>", ignoreCase: true);
-            string expected = "<a><b>";
+            string expected = "<a><b><b>";
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Test_StringExtensions_Replace_IgnoreCase_False_WithMatch()
         {
-            var input = "<a><b>";
+            var input = "<a><b><b>";
             string actual = input.Replace("<b>", "<c>", ignoreCase: false);
-            string expected = "<a><c>";
+            string expected = "<a><c><c>";
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Test_String_Replace_FromDotNet_WithMatch()
         {
-            var input = "<a><b>";
+            var input = "<a><b><b>";
             string actual = input.Replace("<b>", "<c>");
-            string expected = "<a><c>";
+            string expected = "<a><c><c>";
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Test_StringExtensions_Replace_IgnoreCase_False_NoMatch()
         {
-            var input = "<a><b>";
+            var input = "<a><b><b>";
             string actual = input.Replace("<B>", "<c>", ignoreCase: false);
-            string expected = "<a><b>";
+            string expected = "<a><b><b>";
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Test_String_Replace_FromDotNet_NoMatch()
         {
-            var input = "<a><b>";
+            var input = "<a><b><b>";
             string actual = input.Replace("<B>", "<c>");
-            string expected = "<a><b>";
+            string expected = "<a><b><b>";
             Assert.AreEqual(expected, actual);
         }
 
@@ -85,7 +85,7 @@ namespace JJ.Framework.Text.Tests
         [TestMethod]
         public void Test_StringExtensions_Replace_WithIgnoreCase_OldValueNull_ThrowsException()
         {
-            var input = "<a><b>";
+            var input = "<a><b><b>";
             AssertHelper.ThrowsException<ArgumentNullException>(
                 () => input.Replace(null, "<c>", default),
                 $"Value cannot be null.{Environment.NewLine}Parameter name: oldValue");
@@ -94,7 +94,7 @@ namespace JJ.Framework.Text.Tests
         [TestMethod]
         public void Test_String_Replace_FromDotNet_OldValueNull_ThrowsException()
         {
-            var input = "<a><b>";
+            var input = "<a><b><b>";
             AssertHelper.ThrowsException<ArgumentNullException>(
                 () => input.Replace(null, "<c>"),
                 $"Value cannot be null.{Environment.NewLine}Parameter name: oldValue");
@@ -103,7 +103,7 @@ namespace JJ.Framework.Text.Tests
         [TestMethod]
         public void Test_StringExtensions_Replace_WithIgnoreCase_NewValueNull_Succeeds()
         {
-            var input = "<a><b>";
+            var input = "<a><b><b>";
             string actual = input.Replace("<b>", null, default);
             string expected = "<a>";
             Assert.AreEqual(expected, actual);
@@ -112,7 +112,7 @@ namespace JJ.Framework.Text.Tests
         [TestMethod]
         public void Test_String_Replace_FromDotNet_NewValueNull_Succeeds()
         {
-            var input = "<a><b>";
+            var input = "<a><b><b>";
             string actual = input.Replace("<b>", null);
             string expected = "<a>";
             Assert.AreEqual(expected, actual);
