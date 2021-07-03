40cd32a32bdd39ef98b6fa801e1342f2b2ef3cfb
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-03 22:56:09 +0200

    🔑 JJ.Framework.Common: KeyHelper: Extending with custom separator option.

--- Framework/Common/KeyHelper.cs
+++ Framework/Common/KeyHelper.cs
@@ -8,13 +8,21 @@ namespace JJ.Framework.Common
     /// </summary>
     public static class KeyHelper
     {
-        private const string SEPARATOR = "89765d597c724aa3832070f21446a543";
+        private const string DEFAULT_SEPARATOR = "89765d597c724aa3832070f21446a543";
 
         /// <inheritdoc cref="KeyHelper" />
         public static string CreateKey(IEnumerable<object> values)
             => CreateKey<object>(values);
 
         /// <inheritdoc cref="KeyHelper" />
-        public static string CreateKey<T>(IEnumerable<T> values) => string.Join(SEPARATOR, values);
+        public static string CreateKey<T>(IEnumerable<T> values) => string.Join(DEFAULT_SEPARATOR, values);
+
+        /// <inheritdoc cref="KeyHelper" />
+        public static string CreateKey(IEnumerable<object> values, string separator)
+            => CreateKey<object>(values, separator);
+
+        /// <inheritdoc cref="KeyHelper" />
+        public static string CreateKey<T>(IEnumerable<T> values, string separator) => string.Join(separator, values);
+
     }
 }
