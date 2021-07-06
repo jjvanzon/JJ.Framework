0ad79ea5a417cb0688d0e4fe9f457b6cfe32f24d
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-06 21:51:03 +0200

    🧫 Testing JJ.Framework.Text: Obsolete StringExtensions TakeLeft and TakeRight.

---
+++ Framework/Text.Tests/Obsolete/StringExtensions_Accessor.cs
@@ -0,0 +1,16 @@
+﻿using JJ.Framework.Reflection;
+// ReSharper disable UnusedParameter.Global
+
+namespace JJ.Framework.Text.Tests.Obsolete
+{
+    internal static class StringExtensions_Accessor
+    {
+        private static readonly Accessor _accessor = new Accessor(typeof(StringExtensions));
+
+        public static string TakeLeft(this string input, int length) 
+            => _accessor.InvokeMethod(() => TakeLeft(input, length));
+
+        public static string TakeRight(this string input, int length)
+            => _accessor.InvokeMethod(() => TakeRight(input, length));
+    }
+}
---
+++ Framework/Text.Tests/Obsolete/StringExtensions_Obsolete_Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using JJ.Framework.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JJ.Framework.Text.Tests.Obsolete
+{
+    [TestClass]
+    public class StringExtensions_Obsolete_Tests
+    {
+        [TestMethod]
+        public void Test_StringExtensions_Obsolete_TakeLeft_ThrowsException()
+            => AssertHelper.ThrowsException_OrInnerException<NotSupportedException>(
+                () => StringExtensions_Accessor.TakeLeft(default, default),
+                "Use TakeStart instead.");
+
+        [TestMethod]
+        public void Test_StringExtensions_Obsolete_TakeRight_ThrowsException()
+            => AssertHelper.ThrowsException_OrInnerException<NotSupportedException>(
+                () => StringExtensions_Accessor.TakeRight(default, default),
+                "Use TakeEnd instead.");
+    }
+}
