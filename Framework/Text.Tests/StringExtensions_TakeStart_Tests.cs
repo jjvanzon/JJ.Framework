8bd8b57ff9309cd6b10828cdd88a71b6d4d1f3da
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-08 19:52:01 +0200

    🧪 Extending tests of StringExtensions TakeStart method from JJ.Framework.Text.

--- Framework/Text.Tests/StringExtensions_TakeStart_Tests.cs
+++ Framework/Text.Tests/StringExtensions_TakeStart_Tests.cs
@@ -1,5 +1,7 @@
-﻿using JJ.Framework.Testing;
+﻿using System;
+using JJ.Framework.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+// ReSharper disable ConvertToConstant.Local
 
 namespace JJ.Framework.Text.Tests
 {
@@ -7,17 +9,44 @@ namespace JJ.Framework.Text.Tests
     public class StringExtensions_TakeStart_Tests
     {
         [TestMethod]
-        public void Test_StringExtensions_TakeStart()
+        public void Test_StringExtensions_TakeStart() 
+            => AssertHelper.AreEqual("123", () => "12345".TakeStart(3));
+
+        [TestMethod]
+        public void Test_StringExtensions_TakeStart_NotEnoughCharacters_ReturnsLessCharacters() 
+            => AssertHelper.AreEqual("12345", () => "12345".TakeStart(6));
+
+        [TestMethod]
+        public void Test_StringExtensions_TakeStart_InputEmptyString_ReturnsEmptyString()
         {
-            string output = "12345".TakeStart(4);
-            AssertHelper.AreEqual("1234", () => output);
+            int dummyLength = 3;
+            AssertHelper.AreEqual("", () => "".TakeStart(dummyLength));
         }
 
         [TestMethod]
-        public void Test_StringExtensions_TakeStart_NotEnoughCharacters_ReturnsLessCharacters()
+        public void Test_StringExtensions_TakeStart_LengthZero_ReturnsEmptyString()
         {
-            string output = "1234".TakeStart(5);
-            AssertHelper.AreEqual("1234", () => output);
+            string dummyInput = "12345";
+            AssertHelper.AreEqual("", () => dummyInput.TakeStart(0));
+        }
+
+        [TestMethod]
+        public void Test_StringExtensions_TakeStart_InputNull_ThrowsException()
+        {
+            string nullInput = null;
+            int dummyLength = 3;
+            AssertHelper.ThrowsException<NullReferenceException>(
+                () => nullInput.TakeStart(dummyLength),
+                "Object reference not set to an instance of an object.");
+        }
+
+        [TestMethod]
+        public void Test_StringExtensions_TakeStart_LengthNegative_ThrowsException()
+        {
+            string dummyInput = "12345";
+            AssertHelper.ThrowsException<ArgumentOutOfRangeException>(
+                () => dummyInput.TakeStart(-1),
+                $"Length cannot be less than zero.{Environment.NewLine}Parameter name: length");
         }
     }
 }
