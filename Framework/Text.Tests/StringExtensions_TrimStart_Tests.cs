3b7fb0e627539e686269a0c7b4d9c577a80fb23d
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-08 23:27:42 +0200

    🔬 Extending the tests for TrimStart from StringExtensions from JJ.Framework.Text.

--- Framework/Text.Tests/StringExtensions_TrimStart_Tests.cs
+++ Framework/Text.Tests/StringExtensions_TrimStart_Tests.cs
@@ -1,7 +1,8 @@
-﻿using JJ.Framework.Testing;
+﻿using System;
+using JJ.Framework.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 // ReSharper disable ConvertToConstant.Local
-// ReSharper disable StringLiteralTypo
 
 namespace JJ.Framework.Text.Tests
 {
@@ -10,18 +11,105 @@ namespace JJ.Framework.Text.Tests
     {
         [TestMethod]
         public void Test_StringExtensions_TrimStart_MultipleOccurrences()
-        {
-            var input = "BlaBlaBlaLala";
-            string output = input.TrimStart("Bla");
-            AssertHelper.AreEqual("Lala", () => output);
-        }
+            => AssertHelper.AreEqual("[b][a]", () => "[a][a][a][b][a]".TrimStart("[a]"));
 
         [TestMethod]
         public void Test_StringExtensions_TrimStart_OneOccurrence()
+            => AssertHelper.AreEqual("[b][b][a]", () => "[a][b][b][a]".TrimStart("[a]"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_NoOccurrences()
+            => AssertHelper.AreEqual("[b][b][b][a]", () => "[b][b][b][a]".TrimStart("[a]"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_StartStringWhiteSpace()
+            => AssertHelper.AreEqual("[a][a] ", () => " [a][a] ".TrimStart(" "));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_InputEmpty()
         {
-            var input = "BlaLala";
-            string output = input.TrimStart("Bla");
-            AssertHelper.AreEqual("Lala", () => output);
+            string dummyStartString = "[a]";
+            AssertHelper.AreEqual("", () => "".TrimStart(dummyStartString));
+        }
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_ClearingEntireString()
+            => AssertHelper.AreEqual("", () => "[a][a][a]".TrimStart("[a]"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_InputNull_ThrowsException()
+        {
+            string nullInput = null;
+            string dummyStartString = "[a]";
+            AssertHelper.ThrowsException<NullReferenceException>(
+                () => nullInput.TrimStart(dummyStartString),
+                "Object reference not set to an instance of an object.");
+        }
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_StartStringNull_ThrowsException()
+        {
+            string dummyInput = "[a]";
+            string nullStartString = null;
+            AssertHelper.ThrowsException(
+                () => dummyInput.TrimStart(nullStartString),
+                "start is null or empty.");
+        }
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_StartStringEmpty_ThrowsException()
+        {
+            string dummyInput = "[a]";
+            AssertHelper.ThrowsException(
+                () => dummyInput.TrimStart(""),
+                "start is null or empty.");
+        }
+
+        // With Length Parameter
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_WithLength()
+            => AssertHelper.AreEqual("][b][c]", () => "[a][b][c]".TrimStart(2));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_WithLength_EmptyInput()
+        {
+            int dummyLength = 0;
+            AssertHelper.AreEqual("", () => "".TrimStart(dummyLength));
+        }
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_WithLength_ClearingEntireString()
+        {
+            string input = "[a][b][c]";
+            AssertHelper.AreEqual("", () => input.TrimStart(input.Length));
+        }
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_WithLengthZero()
+            => AssertHelper.AreEqual("[a]", () => "[a]".TrimStart(0));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_WithLengthLargerThanInput()
+            => AssertHelper.AreEqual("", () => "[a]".TrimStart(4));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_WithLength_NullInput_ThrowsException()
+        {
+            string nullInput = null;
+            int dummyLength = 0;
+            AssertHelper.ThrowsException<NullReferenceException>(
+                () => nullInput.TrimStart(dummyLength),
+                "Object reference not set to an instance of an object.");
+        }
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStart_WithLengthNegative_ThrowsException()
+        {
+            string dummyInput = "[a]";
+            AssertHelper.ThrowsException(
+                () => dummyInput.TrimStart(-1),
+                "length of -1 is less than 0.");
         }
     }
 }
