28b4f95dadfcba603475176d5fa4cfb113e8557f
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-08 19:51:02 +0200

    📖 Readability fixes for StringExtensions_TakeEnd_Tests of JJ.Framework.Text.

--- Framework/Text.Tests/StringExtensions_TakeEnd_Tests.cs
+++ Framework/Text.Tests/StringExtensions_TakeEnd_Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using JJ.Framework.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+// ReSharper disable ConvertToConstant.Local
 
 namespace JJ.Framework.Text.Tests
 {
@@ -13,29 +14,39 @@ namespace JJ.Framework.Text.Tests
 
         [TestMethod]
         public void Test_StringExtensions_TakeEnd_NotEnoughCharacters_ReturnsLessCharacters()
-            => AssertHelper.AreEqual("1234", () => "1234".TakeEnd(5));
+            => AssertHelper.AreEqual("12345", () => "12345".TakeEnd(6));
 
         [TestMethod]
         public void Test_StringExtensions_TakeEnd_InputEmptyString_ReturnsEmptyString()
-            => AssertHelper.AreEqual("", () => "".TakeEnd(3));
+        {
+            int dummyLength = 3;
+            AssertHelper.AreEqual("", () => "".TakeEnd(dummyLength));
+        }
 
         [TestMethod]
         public void Test_StringExtensions_TakeEnd_LengthZero_ReturnsEmptyString()
-            => AssertHelper.AreEqual("", () => "1234".TakeEnd(0));
+        {
+            string dummyInput = "12345";
+            AssertHelper.AreEqual("", () => dummyInput.TakeEnd(0));
+        }
 
         [TestMethod]
         public void Test_StringExtensions_TakeEnd_InputNull_ThrowsException()
         {
-            string input = null;
+            string nullInput = null;
+            int dummyLength = 3;
             AssertHelper.ThrowsException<NullReferenceException>(
-                () => input.TakeEnd(3), 
+                () => nullInput.TakeEnd(dummyLength), 
                 "Object reference not set to an instance of an object.");
         }
 
         [TestMethod]
-        public void Test_StringExtensions_TakeEnd_LengthNegative_ThrowsException() 
-            => AssertHelper.ThrowsException(
-                () => "1234".TakeEnd(-1), 
+        public void Test_StringExtensions_TakeEnd_LengthNegative_ThrowsException()
+        {
+            string dummyInput = "12345";
+            AssertHelper.ThrowsException(
+                () => dummyInput.TakeEnd(-1),
                 "length of -1 is less than 0.");
+        }
     }
 }
