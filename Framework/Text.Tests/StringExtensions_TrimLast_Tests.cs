2240152ec0a5d7185c183a0f7381d5f578fc0dce
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-09 23:54:50 +0200

    🧫 Extending tests for TrimLast in from JJ.Framework.Text.

--- Framework/Text.Tests/StringExtensions_TrimLast_Tests.cs
+++ Framework/Text.Tests/StringExtensions_TrimLast_Tests.cs
@@ -1,4 +1,5 @@
-﻿using JJ.Framework.Testing;
+﻿using System;
+using JJ.Framework.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 // ReSharper disable ConvertToConstant.Local
 // ReSharper disable StringLiteralTypo
@@ -9,11 +10,35 @@ namespace JJ.Framework.Text.Tests
     public class StringExtensions_TrimLast_Tests
     {
         [TestMethod]
-        public void Test_StringExtensions_TrimLast()
-        {
-            var input = "LalaBlaBla";
-            string output = input.TrimLast("Bla");
-            AssertHelper.AreEqual("LalaBla", () => output);
-        }
+        public void Test_StringExtensions_TrimLast_MainCase()
+            => AssertHelper.AreEqual("[a][b][b][a]", () => "[a][b][b][a][a]".TrimLast("[a]"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimLast_NoOccurrences()
+            => AssertHelper.AreEqual("[a][b][b]", () => "[a][b][b]".TrimLast("[a]"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimLast_EndStringWhiteSpace()
+            => AssertHelper.AreEqual(" [a][a] ", () => " [a][a]  ".TrimLast(" "));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimLast_InputEmpty_ReturnsEmptyString()
+            => AssertHelper.AreEqual("", () => "".TrimLast("[a]"));
+        
+        [TestMethod]
+        public void Test_StringExtensions_TrimLast_ClearingEntireString()
+           => AssertHelper.AreEqual("", () => "[a]".TrimLast("[a]"));
+        
+        [TestMethod]
+        public void Test_StringExtensions_TrimLast_InputNull_ThrowsException()
+          => AssertHelper.ThrowsException<NullReferenceException>(
+              () => StringExtensions.TrimLast(null, "[a]"),
+              "Object reference not set to an instance of an object.");
+        
+        [TestMethod]
+        public void Test_StringExtensions_TrimLast_EndStringEmpty_ThrowsException()
+          => AssertHelper.ThrowsException(
+              () => "[a]".TrimLast(""),
+              "end is null or empty.");
     }
 }
