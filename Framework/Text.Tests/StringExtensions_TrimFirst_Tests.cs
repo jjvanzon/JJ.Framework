792159dc81d4f790a2b7aa9876482890d1d6ba07
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-09 23:01:46 +0200

    🥼 Extended test set for TrimFirst in JJ.Framework.Text.Tests.

--- Framework/Text.Tests/StringExtensions_TrimFirst_Tests.cs
+++ Framework/Text.Tests/StringExtensions_TrimFirst_Tests.cs
@@ -1,4 +1,5 @@
-﻿using JJ.Framework.Testing;
+﻿using System;
+using JJ.Framework.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 // ReSharper disable StringLiteralTypo
 // ReSharper disable ConvertToConstant.Local
@@ -9,11 +10,35 @@ namespace JJ.Framework.Text.Tests
     public class StringExtensions_TrimFirst_Tests
     {
         [TestMethod]
-        public void Test_StringExtensions_TrimFirst()
-        {
-            var input = "BlaBlaLala";
-            string output = input.TrimFirst("Bla");
-            AssertHelper.AreEqual("BlaLala", () => output);
-        }
+        public void Test_StringExtensions_TrimFirst_MainCase()
+            => AssertHelper.AreEqual("[a][b][b][a]", () => "[a][a][b][b][a]".TrimFirst("[a]"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimFirst_NoOccurrences()
+            => AssertHelper.AreEqual("[b][b][a]", () => "[b][b][a]".TrimFirst("[a]"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimFirst_StartStringWhiteSpace()
+            => AssertHelper.AreEqual(" [a][a] ", () => "  [a][a] ".TrimFirst(" "));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimFirst_InputEmpty_ReturnsEmptyString() 
+            => AssertHelper.AreEqual("", () => "".TrimFirst("[a]"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimFirst_ClearingEntireString()
+            => AssertHelper.AreEqual("", () => "[a]".TrimFirst("[a]"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimFirst_InputNull_ThrowsException()
+            => AssertHelper.ThrowsException<NullReferenceException>(
+                () => StringExtensions.TrimFirst(null, "[a]"),
+                "Object reference not set to an instance of an object.");
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimFirst_StartStringEmpty_ThrowsException()
+            => AssertHelper.ThrowsException(
+                () => "[a]".TrimFirst(""),
+                "start is null or empty.");
     }
 }
