17b09e4cfd2a1ab3864bc426611712fcc0497e90
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-10 22:59:36 +0200

    🧪 Extended test set of StringExtensions_TrimStartUntil_Tests (for JJ.Framework.Text).

--- Framework/Text.Tests/StringExtensions_TrimStartUntil_Tests.cs
+++ Framework/Text.Tests/StringExtensions_TrimStartUntil_Tests.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-// ReSharper disable ConvertToConstant.Local
-// ReSharper disable StringLiteralTypo
+﻿using System;
+using JJ.Framework.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static System.Environment;
 
 namespace JJ.Framework.Text.Tests
 {
@@ -8,11 +9,39 @@ namespace JJ.Framework.Text.Tests
     public class StringExtensions_TrimStartUntil_Tests
     {
         [TestMethod]
-        public void Test_StringExtensions_TrimStartUntil()
-        {
-            var input = "abcdefg";
-            string output = input.TrimStartUntil("de");
-            Assert.AreEqual("defg", output);
-        }
+        public void Test_StringExtensions_TrimStartUntil_MainCase()
+            => Assert.AreEqual("230123", "01230123".TrimStartUntil("23"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStartUntil_1stChar_ReturnsOriginalString()
+            => AssertHelper.AreEqual("01230123", () => "01230123".TrimStartUntil("0"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStartUntil_2ndChar()
+            => AssertHelper.AreEqual("1230123", () => "01230123".TrimStartUntil("1"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStartUntil_LastChar()
+            => AssertHelper.AreEqual("8", () => "01235678".TrimStartUntil("8"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStartUntil_UntilStringNotFound_ReturnsOriginalString()
+            => AssertHelper.AreEqual("01230123", () => "01230123".TrimStartUntil("45"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStartUntil_WithCharParameter()
+            => AssertHelper.AreEqual("30123", () => "01230123".TrimStartUntil('3'));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStartUntil_InputNull_ThrowsException()
+            => AssertHelper.ThrowsException<NullReferenceException>(
+                () => StringExtensions.TrimStartUntil(null, "0"),
+                "Object reference not set to an instance of an object.");
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimStartUntil_UntilStringNull_ThrowsException()
+            => AssertHelper.ThrowsException<ArgumentNullException>(
+                () => "".TrimStartUntil(null),
+                $"Value cannot be null.{NewLine}Parameter name: until");
     }
 }
