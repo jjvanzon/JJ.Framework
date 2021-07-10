87915042554c49953be4c2c288daf64433cb8afc
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-10 20:01:35 +0200

    🥼 Extending the tests for TrimEndUntil from StringExtensions in JJ.Framework.Text.

--- Framework/Text.Tests/StringExtensions_TrimEndUntil_Tests.cs
+++ Framework/Text.Tests/StringExtensions_TrimEndUntil_Tests.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using JJ.Framework.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static System.Environment;
+
 // ReSharper disable StringLiteralTypo
 // ReSharper disable ConvertToConstant.Local
 
@@ -8,11 +12,39 @@ namespace JJ.Framework.Text.Tests
     public class StringExtensions_TrimEndUntil_Tests
     {
         [TestMethod]
-        public void Test_StringExtensions_TrimEndUntil()
-        {
-            var input = "abcdefg";
-            string output = input.TrimEndUntil("de");
-            Assert.AreEqual("abcde", output);
-        }
+        public void Test_StringExtensions_TrimEndUntil_MainCase()
+            => AssertHelper.AreEqual("abcde", () => "abcdefg".TrimEndUntil("de"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimEndUntil_LastChar_ReturnsOriginalString()
+            => AssertHelper.AreEqual("abcdefg", () => "abcdefg".TrimEndUntil("g"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimEndUntil_SecondLastChar()
+            => AssertHelper.AreEqual("abcdef", () => "abcdefg".TrimEndUntil("f"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimEndUntil_FirstChar()
+            => AssertHelper.AreEqual("a", () => "abcdefg".TrimEndUntil("a"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimEndUntil_UntilStringNotFound_ReturnsOriginalString()
+            => AssertHelper.AreEqual("abcdefg", () => "abcdefg".TrimEndUntil("hi"));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimEndUntil_WithCharParameter()
+            => AssertHelper.AreEqual("abcd", () => "abcdefg".TrimEndUntil('d'));
+
+        [TestMethod]
+        public void Test_StringExtensions_TrimEndUntil_InputNull_ThrowsException()
+            => AssertHelper.ThrowsException<NullReferenceException>(
+                () => StringExtensions.TrimEndUntil(null, "a"),
+                "Object reference not set to an instance of an object.");
+        
+        [TestMethod]
+        public void Test_StringExtensions_TrimEndUntil_UntilStringNull_ThrowsException()
+            => AssertHelper.ThrowsException<ArgumentNullException>(
+                () => "".TrimEndUntil(null),
+                $"Value cannot be null.{NewLine}Parameter name: until");
     }
 }
