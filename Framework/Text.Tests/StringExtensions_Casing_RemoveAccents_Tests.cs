34a06908d2d8f06a85b7de9a0298040215299050
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-14 23:13:32 +0200

    🔬 StringExtensions_Casing_RemoveAccents_Tests (for JJ.Framework.Text).

---
+++ Framework/Text.Tests/StringExtensions_Casing_RemoveAccents_Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JJ.Framework.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// ReSharper disable StringLiteralTypo
+
+namespace JJ.Framework.Text.Tests
+{
+    [TestClass]
+    public class StringExtensions_Casing_RemoveAccents_Tests
+    {
+        [TestMethod]
+        public void Test_StringExtensions_Casing_RemoveAccents()
+            => AssertHelper.AreEqual("aecu and more words.", () => "àéçû and more words.".RemoveAccents());
+
+        [TestMethod]
+        public void Test_StringExtensions_Casing_RemoveAccents_NullInput_ThrowsException()
+            => AssertHelper.ThrowsException<NullReferenceException>(
+                () => StringExtensions_Casing.RemoveAccents(null),
+                "Object reference not set to an instance of an object.");
+    }
+}
