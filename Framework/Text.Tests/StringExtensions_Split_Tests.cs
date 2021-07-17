ac7659382d45b40637aeac86939e3a046ccb9696
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-18 00:48:31 +0200

    🧫 Testing more situations for SplitWithQuotation: quoted empty value and ending line with separator character (JJ.Framework.Text.Tests).

--- Framework/Text.Tests/StringExtensions_Split_Tests.cs
+++ Framework/Text.Tests/StringExtensions_Split_Tests.cs
@@ -12,12 +12,12 @@ namespace JJ.Framework.Text.Tests
         [TestMethod]
         public void Test_StringExtensions_SplitWithQuotation()
         {
-            //string input = "1234,'1234','12,34','12''34',1'23'4,'12'34','12'34','',";
-            string input = "1234,'1234','12,34','12''34',1'23'4,'12'34','12'34',''";
+            string input = "1234,'1234','12,34','12''34',1'23'4,'12'34','12'34','',";
+            
             IList<string> split = input.SplitWithQuotation(",", '\'');
 
             AssertHelper.IsNotNull(() => split);
-            AssertHelper.AreEqual(7, () => split.Count);
+            AssertHelper.AreEqual(8, () => split.Count);
             AssertHelper.AreEqual("1234", () => split[0]);
             AssertHelper.AreEqual("1234", () => split[1]);
             AssertHelper.AreEqual("12,34", () => split[2]);
@@ -25,6 +25,7 @@ namespace JJ.Framework.Text.Tests
             AssertHelper.AreEqual("1234", () => split[4]);
             AssertHelper.AreEqual("1234,1234", () => split[5]);
             AssertHelper.AreEqual("", () => split[6]);
+            AssertHelper.AreEqual("", () => split[7]);
         }
     }
 }
