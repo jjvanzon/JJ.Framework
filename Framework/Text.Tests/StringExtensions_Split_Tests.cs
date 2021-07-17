61e689e7aa3a3a1a7b8152f4f297ab2594789645
jjvanzon <janjoostvanzon@hotmail.com>
2021-07-18 00:13:47 +0200

    🧪 Extended test for SplitWithQuotation to assert things (for JJ.Framework.Text).

--- Framework/Text.Tests/StringExtensions_Split_Tests.cs
+++ Framework/Text.Tests/StringExtensions_Split_Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using JJ.Framework.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 // ReSharper disable UnusedVariable
 // ReSharper disable ConvertToConstant.Local
@@ -11,8 +12,19 @@ namespace JJ.Framework.Text.Tests
         [TestMethod]
         public void Test_StringExtensions_SplitWithQuotation()
         {
-            string input = @"1234,""1234"",""12,34"",""12""""34"",1""23""4,""12""34"",""12""34""";
-            IList<string> split2 = input.SplitWithQuotation(",", '"');
+            //string input = "1234,'1234','12,34','12''34',1'23'4,'12'34','12'34','',";
+            string input = "1234,'1234','12,34','12''34',1'23'4,'12'34','12'34',''";
+            IList<string> split = input.SplitWithQuotation(",", '\'');
+
+            AssertHelper.IsNotNull(() => split);
+            AssertHelper.AreEqual(7, () => split.Count);
+            AssertHelper.AreEqual("1234", () => split[0]);
+            AssertHelper.AreEqual("1234", () => split[1]);
+            AssertHelper.AreEqual("12,34", () => split[2]);
+            AssertHelper.AreEqual("12'34", () => split[3]);
+            AssertHelper.AreEqual("1234", () => split[4]);
+            AssertHelper.AreEqual("1234,1234", () => split[5]);
+            AssertHelper.AreEqual("", () => split[6]);
         }
     }
 }
