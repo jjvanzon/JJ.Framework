786abc9963ba97940f8014968cf478ecf3539b1d
jjvanzon <janjoostvanzon@hotmail.com>
2022-12-22 23:00:55 +0100

    🧪 Empty test project JJ.Demos.Data.

---
+++ Demos/Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JJ.Demos.Data")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jan Joost van Zon")]
+[assembly: AssemblyProduct("JJ.Framework")]
+[assembly: AssemblyCopyright("Copyright © 2022 Jan Joost van Zon")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f3bb5f9e-df53-4baa-adee-94aefcdd2c5d")]
+
+[assembly: AssemblyVersion("1.7.*")]
+
