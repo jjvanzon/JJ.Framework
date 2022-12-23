ae781c76d4997bd83dad94897dcf33105c0c43b7
jjvanzon <janjoostvanzon@hotmail.com>
2022-12-23 14:41:12 +0100

    🥼 JJ.Demos.Data: Adding a local DB, config settings, moving SQL folder contents to project root. Adding test to fire off the demo. Does not connect yet. Letting it return, postponing the work.

---
+++ Demos/Data/RepositorySqlExecutorDemoTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using JJ.Framework.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#pragma warning disable CS0162
+
+namespace JJ.Demos.Data
+{
+    [TestClass]
+    public class RepositorySqlExecutorDemoTests
+    {
+        [TestMethod]
+        public void Demo_RepositorySqlExecutor()
+        {
+            // Postponing this
+            return;
+
+            IContext context = ContextFactory.CreateContextFromConfiguration();
+            IMyRepository repository = new MyRepository(context);
+            IList<MyEntity> entities = repository.GetManyByCriteria(10, 20, DateTime.Parse("2022-01-01"));
+        }
+    }
+}
--- Demos/Data/Sql/MyEntity_GetIDsByCriteria.sql
+++
@@ -1,5 +0,0 @@
-﻿select e.ID from MyEntity
-where 
-  (@categoryID is null or e.CategoryID = @categoryID) and
-  (@kind is null or e.Kind = @kind) and
-  (@minStartDate is null or e.MinStartDate >= @minStartDate);
