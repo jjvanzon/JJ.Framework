1f945754dae14b6071fc97a80c4c0b222954e179
jjvanzon <jjvanzon79@gmail.com>
2024-09-10 04:08:14 +0200

    Removing some more dependencies on database tech. And upgraded vulnarable base web tech versions.

--- Framework/Mvc.TestViews/Scripts/_references.js
+++ Framework/Mvc.TestViews/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-3.3.1.js" />
+﻿/// <reference path="jquery-3.7.1.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="knockout-2.2.0.debug.js" />
