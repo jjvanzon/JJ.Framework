4c94a51f78aa63245da1ce2daf6d266f628aa6e1
jjvanzon <jjvanzon79@gmail.com>
2025-01-19 00:12:21 +0100

    🔧 FrameworkWishes: Port `ExceptionHelper` methods from JJ.Framework to JJ.Framework.Wishes (fix-up branch) for legacy use for testing.

--- Framework/JJ.Framework.Wishes/FrameworkWishes.cs
+++ Framework/JJ.Framework.Wishes/FrameworkWishes.cs
@@ -19,6 +19,7 @@ using static JJ.Framework.Wishes.Common.FilledInWishes;
 using static JJ.Framework.Wishes.Reflection.ReflectionWishes;
 using System.Linq.Expressions;
 using JJ.Framework.Reflection;
+using JJ.Framework.Wishes.Collections_Copied;
 
 namespace JJ.Framework.Wishes
 {
@@ -471,6 +472,52 @@ namespace JJ.Framework.Wishes
                 if (string.IsNullOrEmpty(extension)) return false;
                 return extension.Length <= maxExtensionLength;
             }
+        }
+    }
+    
+    namespace Logging_Copied
+    {
+        public static class ExceptionHelper_Copied
+        {
+            public static string FormatExceptionWithInnerExceptions(Exception ex, bool includeStackTrace)
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine(FormatException(ex, includeStackTrace));
+
+                while (ex.InnerException != null)
+                {
+                    sb.AppendLine("Inner exception:");
+                    sb.AppendLine(FormatException(ex.InnerException, includeStackTrace));
+                    ex = ex.InnerException;
+                }
+
+                return sb.ToString();
+            }
+            
+            public static string FormatException(Exception ex, bool includeStackTrace)
+            {
+                if (ex == null) throw new ArgumentNullException(nameof(ex));
+
+                string message = ex.Message;
+                if (includeStackTrace)
+                {
+                    message += Environment.NewLine + ex.StackTrace;
+                }
+                return message;
+            }
+            
+            public static bool HasExceptionOrInnerExceptionsOfType(this Exception exception, Type exceptionType, string message)
+            {
+                if (exception == null) throw new ArgumentNullException(nameof(exception));
+                if (exceptionType == null) throw new ArgumentNullException(nameof(exceptionType));
+
+                bool any = exception.SelfAndAncestors(x => x.InnerException)
+                                    .OfType(exceptionType)
+                                    .Where(x => string.Equals(x.Message, message, StringComparison.Ordinal))
+                                    .Any();
+                return any;
+            }
+
         }
     }
     
