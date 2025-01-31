afa0d7f12f592f945b7274441f72eae4ebe54423
jjvanzon <jjvanzon79@gmail.com>
2025-01-31 17:07:31 +0100

    ➕ JJ.Framework.Wishes.Tests: Preliminary TestWishesTests class.

---
+++ Framework/JJ.Framework.Wishes.Tests/TestWishesTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JJ.Framework.Wishes.Tests
+{
+    [TestClass]
+    public class TestWishesTests
+    {
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+            
+        //}
+    }
+}
