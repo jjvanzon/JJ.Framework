590d2f7c4db47ccd9c9c3c1fd1d29a7fbb3d94d7
jjvanzon <jjvanzon79@gmail.com>
2025-02-02 02:51:41 +0100

    🔮 JJ.Framework.Wishes: Added AccessorExtensions to leverange CallerMemberName and a type argument for shorter syntax.

---
+++ Framework/JJ.Framework.Wishes/Reflection/AccessorExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using JJ.Framework.Reflection;
+
+namespace JJ.Framework.Wishes.Reflection
+{
+    public static class AccessorExtensions
+    {
+        // Leverage CallerMemberName, though uncombinable with params[].
+
+        public static object InvokeMethod(this Accessor_Adapted accessor, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName);
+        public static object InvokeMethod(this Accessor accessor, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName);
+
+        public static object InvokeMethod<T>(this Accessor_Adapted accessor, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName);
+        public static object InvokeMethod<T>(this Accessor accessor, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName);
+
+        public static object InvokeMethod(this Accessor_Adapted accessor, object param1, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName, param1);
+        public static object InvokeMethod(this Accessor accessor, object param1, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName, param1);
+
+        public static T InvokeMethod<T>(this Accessor_Adapted accessor, object param1, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName, param1);
+        public static T InvokeMethod<T>(this Accessor accessor, object param1, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName, param1);
+
+        public static object InvokeMethod(this Accessor_Adapted accessor, object param1, object param2, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName, param1, param2);
+        public static object InvokeMethod(this Accessor accessor, object param1, object param2, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName, param1, param2);
+
+        public static T InvokeMethod<T>(this Accessor_Adapted accessor, object param1, object param2, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName, param1, param2);
+        public static T InvokeMethod<T>(this Accessor accessor, object param1, object param2, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName, param1, param2);
+
+        public static object InvokeMethod(this Accessor_Adapted accessor, object param1, object param2, object param3, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName, param1, param2, param3);
+        public static object InvokeMethod(this Accessor accessor, object param1, object param2, object param3, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName, param1, param2, param3);
+
+        public static T InvokeMethod<T>(this Accessor_Adapted accessor, object param1, object param2, object param3, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName, param1, param2, param3);
+        public static T InvokeMethod<T>(this Accessor accessor, object param1, object param2, object param3, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName, param1, param2, param3);
+    }
+}
