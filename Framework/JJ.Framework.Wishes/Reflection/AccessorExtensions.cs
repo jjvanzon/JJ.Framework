e7da7f8d3c96047edeb85b36d4c6d7626497feba
jjvanzon <jjvanzon79@gmail.com>
2025-02-05 19:14:23 +0100

    🔐 [Draft] JJ.Framework.Wishes: AccessorEx  - Refactored extension methods into a base class; added new members to prevent CallerMemberName overloads from shadowing better-matching base methods.

--- Framework/JJ.Framework.Wishes/Reflection/AccessorExtensions.cs
+++ Framework/JJ.Framework.Wishes/Reflection/AccessorExtensions.cs
@@ -1,38 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Text;
-using JJ.Framework.Reflection;
 
 namespace JJ.Framework.Wishes.Reflection
 {
-    public static class AccessorExtensions
+    // TODO: Rename File
+
+    public class AccessorEx : Accessor_Adapted
     {
-        // Leverage CallerMemberName, though uncombinable with params[].
+        public AccessorEx(string typeName, params object[] args) : base(typeName, args) { }
+        public AccessorEx(object obj) : base(obj) { }
+        public AccessorEx(Type objectType) : base(objectType) { }
+        public AccessorEx(object obj, Type objectType) : base(obj, objectType) { }
 
-        public static object InvokeMethod(this Accessor_Adapted accessor, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName);
-        public static object InvokeMethod(this Accessor accessor, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName);
+        // Variations with CallerMemberName
+        
+        public object InvokeMethod([CallerMemberName] string callerMemberName = null) => base.InvokeMethod(callerMemberName);
+        public object InvokeMethod<T>([CallerMemberName] string callerMemberName = null) => (T)base.InvokeMethod(callerMemberName);
+        public object InvokeMethod(object param1, [CallerMemberName] string callerMemberName = null) => base.InvokeMethod(callerMemberName, param1);
+        public T InvokeMethod<T>(object param1, [CallerMemberName] string callerMemberName = null) => (T)base.InvokeMethod(callerMemberName, param1);
+        public object InvokeMethod(object param1, object param2, [CallerMemberName] string callerMemberName = null) => base.InvokeMethod(callerMemberName, param1, param2);
+        public T InvokeMethod<T>(object param1, object param2, [CallerMemberName] string callerMemberName = null) => (T)base.InvokeMethod(callerMemberName, param1, param2);
+        public object InvokeMethod(object param1, object param2, object param3, [CallerMemberName] string callerMemberName = null) => base.InvokeMethod(callerMemberName, param1, param2, param3);
+        public T InvokeMethod<T>(object param1, object param2, object param3, [CallerMemberName] string callerMemberName = null) => (T)base.InvokeMethod(callerMemberName, param1, param2, param3);
+        public new object GetPropertyValue([CallerMemberName] string callerMemberName = null) => base.GetPropertyValue(callerMemberName);
+        public T GetPropertyValue<T>([CallerMemberName] string callerMemberName = null) => (T)base.GetPropertyValue(callerMemberName);
 
-        public static object InvokeMethod<T>(this Accessor_Adapted accessor, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName);
-        public static object InvokeMethod<T>(this Accessor accessor, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName);
+        // Redefine base members as new or worse-match overloads will be picked from this derived class.
 
-        public static object InvokeMethod(this Accessor_Adapted accessor, object param1, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName, param1);
-        public static object InvokeMethod(this Accessor accessor, object param1, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName, param1);
-
-        public static T InvokeMethod<T>(this Accessor_Adapted accessor, object param1, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName, param1);
-        public static T InvokeMethod<T>(this Accessor accessor, object param1, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName, param1);
-
-        public static object InvokeMethod(this Accessor_Adapted accessor, object param1, object param2, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName, param1, param2);
-        public static object InvokeMethod(this Accessor accessor, object param1, object param2, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName, param1, param2);
-
-        public static T InvokeMethod<T>(this Accessor_Adapted accessor, object param1, object param2, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName, param1, param2);
-        public static T InvokeMethod<T>(this Accessor accessor, object param1, object param2, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName, param1, param2);
-
-        public static object InvokeMethod(this Accessor_Adapted accessor, object param1, object param2, object param3, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName, param1, param2, param3);
-        public static object InvokeMethod(this Accessor accessor, object param1, object param2, object param3, [CallerMemberName] string callerMemberName = null) => accessor.InvokeMethod(callerMemberName, param1, param2, param3);
-
-        public static T InvokeMethod<T>(this Accessor_Adapted accessor, object param1, object param2, object param3, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName, param1, param2, param3);
-        public static T InvokeMethod<T>(this Accessor accessor, object param1, object param2, object param3, [CallerMemberName] string callerMemberName = null) => (T)accessor.InvokeMethod(callerMemberName, param1, param2, param3);
+        /// <inheritdoc cref="Accessor_Adapted.GetFieldValue{T}(Expression{Func{T}})" />
+        public new T GetFieldValue<T>(Expression<Func<T>> nameExpression) => base.GetFieldValue(nameExpression);
+        /// <inheritdoc cref="Accessor_Adapted.GetFieldValue(string)" />
+        public new object GetFieldValue(string name) => base.GetFieldValue(name);
+        /// <inheritdoc cref="Accessor_Adapted.SetFieldValue{T}(Expression{Func{T}}, T)" />
+        public new void SetFieldValue<T>(Expression<Func<T>> nameExpression, T value) => base.SetFieldValue(nameExpression, value);
+        /// <inheritdoc cref="Accessor_Adapted.SetFieldValue(string, object)" />
+        public new void SetFieldValue(string name, object value) => base.SetFieldValue(name, value);
+        /// <inheritdoc cref="Accessor_Adapted.GetPropertyValue{T}(Expression{Func{T}})" />
+        public new T GetPropertyValue<T>(Expression<Func<T>> nameExpression) => base.GetPropertyValue(nameExpression);
+        /// <inheritdoc cref="Accessor_Adapted.SetPropertyValue(string, object)" />
+        public new void SetPropertyValue(string name, object value) => base.SetPropertyValue(name, value);
+        /// <inheritdoc cref="Accessor_Adapted.SetPropertyValue{T}(Expression{Action})" />
+        public new void InvokeMethod(Expression<Action> callExpression) => base.InvokeMethod(callExpression);
+        /// <inheritdoc cref="Accessor_Adapted.InvokeMethod{T}(Expression{Func{T}})" />
+        public new T InvokeMethod<T>(Expression<Func<T>> callExpression) => base.InvokeMethod(callExpression);
+        /// <inheritdoc cref="Accessor_Adapted.InvokeMethod(LambdaExpression)" />
+        public new object InvokeMethod(LambdaExpression callExpression) => base.InvokeMethod(callExpression);
+        /// <inheritdoc cref="Accessor_Adapted.InvokeMethod(string, object[])" />
+        public new object InvokeMethod(string name, params object[] parameters) => base.InvokeMethod(name, parameters);
+        /// <inheritdoc cref="Accessor_Adapted.InvokeMethod(string, object[], Type[])" />
+        public new object InvokeMethod(string name, object[] parameters, Type[] parameterTypes) => base.InvokeMethod(name, parameters, parameterTypes);
+        /// <inheritdoc cref="Accessor_Adapted.InvokeMethod{TArg1}(string, TArg1)" />
+        public new object InvokeMethod<TArg1>(string name, TArg1 parameter) => base.InvokeMethod(name, parameter);
+        /// <inheritdoc cref="Accessor_Adapted.InvokeMethod{TArg1, TArg2}(string, TArg1, TArg2)" />
+        public new object InvokeMethod<TArg1, TArg2>(string name, params object[] parameters) => base.InvokeMethod(name, parameters);
+        /// <inheritdoc cref="Accessor_Adapted.InvokeMethod{TArg1, TArg2, TArg3}(string, TArg1, TArg2, TArg3)" />
+        public new object InvokeMethod<TArg1, TArg2, TArg3>(string name, params object[] parameters) => base.InvokeMethod(name, parameters);
+        /// <inheritdoc cref="Accessor_Adapted.InvokeMethod{TArg1, TArg2, TArg3, TArg4}(string, TArg1, TArg2, TArg3, TArg4)" />
+        public new object InvokeMethod<TArg1, TArg2, TArg3, TArg4>(string name, params object[] parameters) => base.InvokeMethod(name, parameters);
+        /// <inheritdoc cref="Accessor_Adapted.InvokeMethod{TArg1, TArg2, TArg3, TArg4, TArg5}(string, TArg1, TArg2, TArg3, TArg4, TArg5)" />
+        public new object InvokeMethod<TArg1, TArg2, TArg3, TArg4, TArg5>(string name, params object[] parameters) => base.InvokeMethod(name, parameters);
+        /// <inheritdoc cref="Accessor_Adapted.InvokeMethod{TArg1, TArg2, TArg3, TArg4, TArg5, TArg6}(string, TArg1, TArg2, TArg3, TArg4, TArg5, TArg6)" />
+        public new object InvokeMethod<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6>(string name, params object[] parameters) => base.InvokeMethod(name, parameters);
+        /// <inheritdoc cref="Accessor_Adapted.InvokeMethod{TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7}(string, TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7)" />
+        public new object InvokeMethod<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7>(string name, params object[] parameters) => base.InvokeMethod(name, parameters);
+        /// <inheritdoc cref="Accessor_Adapted.GetIndexerValue" />
+        public new object GetIndexerValue(params object[] parameters) => base.GetIndexerValue(parameters);
+        /// <inheritdoc cref="Accessor_Adapted.SetIndexerValue" />
+        public new void SetIndexerValue(params object[] parametersAndValue) => base.SetIndexerValue(parametersAndValue);
     }
 }
