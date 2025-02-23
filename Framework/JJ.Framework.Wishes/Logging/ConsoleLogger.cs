778d823946a0e15b7866462d158752b6c4f1e507
jjvanzon <jjvanzon79@gmail.com>
2025-02-23 02:15:37 +0100

    🧵 JJ.Framework.Wishes.Logging: Add Console.Out.Flush() in ConsoleLogger to prevent log mangling in highly threaded scenarios.

--- Framework/JJ.Framework.Wishes/Logging/ConsoleLogger.cs
+++ Framework/JJ.Framework.Wishes/Logging/ConsoleLogger.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace JJ.Framework.Wishes.Logging
 {
     internal class ConsoleLogger : LoggerBase
     {
-        public override void Log(string message) => Console.WriteLine(message);
+        public override void Log(string message)
+        {
+            Console.WriteLine(message);
+            
+            // Might help intense parallelism from mangling logs.
+            Console.Out.Flush();
+        }
     }
 }
