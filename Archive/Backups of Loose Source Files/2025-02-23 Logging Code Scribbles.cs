afb1df9c7adda9a08058f790727cdfa7a9dec5c5
jjvanzon <jjvanzon79@gmail.com>
2025-02-23 17:16:13 +0100

    🗄️ Archive

---
+++ Archive/Backups of Loose Source Files/2025-02-23 Logging Code Scribbles.cs
@@ -0,0 +1,19 @@
+        
+        //public VersatileLogger(LogConfig config)
+        //{
+        //    if (config == null) throw new NullException(() => config);
+        //    if (config.Logs == null) throw new NullException(() => config.Logs);
+            
+        //    // TODO: Move this to factory, so you can return different instances based on configuration?
+
+        //    var loggerConfigs = config.Logs.Where(x => x.Active ?? DefaultActive).ToArray();
+            
+        //    int count = loggerConfigs.Length;
+            
+        //    _loggers = new ILogger[count];
+            
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        _loggers[i] = CreateLogger(config.Logs[i].Type);
+        //    }   
+        //}
