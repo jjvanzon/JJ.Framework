8a894c03d4542023d6bf4a730bfdaa0862d1a9af
jjvanzon <jjvanzon79@gmail.com>
2025-02-23 02:13:16 +0100

    ⚙️ JJ.Framework.Wishes.Logging: Add XmlArrayItem attribute to explicitly define logger as the item name in config XML.

--- Framework/JJ.Framework.Wishes/Logging/LogConfig.cs
+++ Framework/JJ.Framework.Wishes/Logging/LogConfig.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace JJ.Framework.Wishes.Logging
 {
     public class LogConfig
     {
+        [XmlArrayItem("logger")]
         public IList<LoggerConfigElement> Loggers { get; set; }
     }
 }
