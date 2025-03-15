c6eddf845544da3a180939d4beff7e7a248a8609
jjvanzon <jjvanzon79@gmail.com>
2025-03-15 01:23:39 +0100

    📦 Split JJ.Framework.Mathematics.Core off into a separate project and moved relevant files from JJ.Framework.Core.

--- Framework/JJ.Framework.Mathematics.Core/RandomizerWishes.cs
+++ Framework/JJ.Framework.Mathematics.Core/RandomizerWishes.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace JJ.Framework.Core.Mathematics
+namespace JJ.Framework.Mathematics.Core
 {
     public static class RandomizerWishes
     {
