004db8159e12f868f35265378f000db91c128e91
jjvanzon <jjvanzon79@gmail.com>
2025-03-17 18:27:34 +0100

    📚 .NET 9 Migration: Added JJ.Framework.Configuration.Net48.csproj, sneakily keeping the assembly name as JJ.Framework.Configuration for compatibility with existing configurations. Updated references to the .Net48 version where needed.

---
+++ Archive/Backups of Loose Source Files/2025-03-17 .NET 9 Migration Code Scribbles.cs
@@ -0,0 +1,6 @@
+        //#if NET5_0_OR_GREATER
+            private const string JJ_FRAMEWORK_CONFIGURATION_ASSEMBLY_STRING = "JJ.Framework.Configuration";
+        //#else
+        //    private const string JJ_FRAMEWORK_CONFIGURATION_ASSEMBLY_STRING = "JJ.Framework.Configuration.Net48";
+        //#endif
+        
