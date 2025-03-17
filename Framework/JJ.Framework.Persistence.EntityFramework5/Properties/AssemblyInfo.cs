d81c54c6dcd0dd972414247eb75ebce83974dede
jjvanzon <jjvanzon79@gmail.com>
2025-03-17 14:54:36 +0100

    🔄 .NET 9 Migration: Converted JJ.Framework.Persistence.EntityFramework5 to .NET 9 while maintaining a .Net48 version for backward compatibility.

--- Framework/JJ.Framework.Persistence.EntityFramework5/Properties/AssemblyInfo.cs
+++ Framework/JJ.Framework.Persistence.EntityFramework5/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("JJ.Framework.Persistence.EntityFramework5")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JJ.Framework.Persistence.EntityFramework5")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("59da3359-eac6-4a0c-ba84-efaaf20bcdb3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
