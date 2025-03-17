e026cbe52a4f46debb9b6c16cc76a43cc778ee22
jjvanzon <jjvanzon79@gmail.com>
2025-03-17 03:37:56 +0100

    🚀 .NET 9 Migration: Converted JJ.Framework.Presentation.Resources to .NET 9 while maintaining a .Net48 version in a separate folder for backward compatibility.

--- Framework/JJ.Framework.Presentation.Resources/Properties/AssemblyInfo.cs
+++ Framework/JJ.Framework.Presentation.Resources/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("JJ.Framework.Presentation.Resources")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JJ.Framework.Presentation.Resources")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5302a3b1-a37b-4c3c-8c96-11c7b277fc75")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
