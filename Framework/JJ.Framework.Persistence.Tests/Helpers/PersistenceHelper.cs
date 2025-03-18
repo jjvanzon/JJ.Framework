a4f29859bccca8c17cac9da879c520c4c791dbae
jjvanzon <jjvanzon79@gmail.com>
2025-03-18 18:11:33 +0100

    ⚠️ JJ.Framework.Persistence.Tests: Added exception filtering to distinguish database connection failures from compatibility issues.

--- Framework/JJ.Framework.Persistence.Tests/Helpers/PersistenceHelper.cs
+++ Framework/JJ.Framework.Persistence.Tests/Helpers/PersistenceHelper.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using JJ.Framework.Configuration;
+#if NET5_0_OR_GREATER
+using ExceptionHelper = JJ.Framework.Logging.Core.ExceptionHelper_Copied;
+#else
+using JJ.Framework.Logging;
+#endif
 using JJ.Framework.Persistence;
 using JJ.Framework.Persistence.Tests.Model;
 using JJ.Framework.Persistence.Tests.NHibernate;
@@ -30,11 +36,43 @@ namespace JJ.Framework.Persistence.Tests
                     mappingAssemblyName,
                     configuration.Dialect);
             }
+            #if NET5_0_OR_GREATER
+            catch (SqlException ex)
+            {
+                AssertInconclusive(ex);
+                return null;
+            }
+            catch (Exception ex) when (ex.InnerException is SqlException)
+            {
+                AssertInconclusive(ex);
+                return null;
+            }
+            catch (Exception ex) when (ExceptionHelper.HasExceptionOrInnerExceptionsOfType<SqlException>(ex))
+            {
+                AssertInconclusive(ex);
+                return null;
+            }
+            catch (Exception ex) when (ExceptionHelper.HasExceptionOrInnerExceptionsOfType<TimeoutException>(ex))
+            {
+                AssertInconclusive(ex);
+                return null;
+            }
+            
+            #else
             catch (Exception ex)
             {
 				Assert.Inconclusive($"Database connection failed. Exception:{Environment.NewLine}{ex}");
                 throw;
 			}
+            #endif
 		}
+
+        #if NET5_0_OR_GREATER
+        private static void AssertInconclusive(Exception ex)
+        {
+            string message = ExceptionHelper.FormatExceptionWithInnerExceptions(ex, false);
+            Assert.Inconclusive(message);
+        }
+        #endif
     }
 }
