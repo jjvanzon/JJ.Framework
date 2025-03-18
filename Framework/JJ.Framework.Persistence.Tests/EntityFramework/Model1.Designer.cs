adaaf076b0b458bf43c05e9e46539b88fd538179
jjvanzon <jjvanzon79@gmail.com>
2025-03-18 18:26:15 +0100

    🛠 JJ.Framework.Persistence.Tests: Upgraded EntityFramework to 6.5.1. Updated NHibernate to 5.5.2 and downgraded FluentNHibernate to 1.2.x to match the original version used in .Net48 projects. NHibernate and EntityFramework tests now pass.

--- Framework/JJ.Framework.Persistence.Tests/EntityFramework/Model1.Designer.cs
+++ Framework/JJ.Framework.Persistence.Tests/EntityFramework/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Repositories\JJ.Framework\Framework\JJ.Framework.Persistence.Tests\EntityFramework\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Repositories\JJ.Synthesizer\JJ.Framework\Framework\JJ.Framework.Persistence.Tests\EntityFramework\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
