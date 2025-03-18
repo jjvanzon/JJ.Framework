10a116233b34ce1ef0731103631acbb05851178a
jjvanzon <jjvanzon79@gmail.com>
2025-03-18 16:43:39 +0100

    ⚙️ .NET 9 Upgrade: Added .Net48 variants for backward compatibility for SgmlReaderDll (external component) and JJ.Framework.Configuration.Tests. Upgraded the original versions to .NET 9 so those tests runs fully on .NET 9. Added copy actions in post-build event to have app.config match the entry-point assembly names in the different environments VS Test Explorer and NCrunch to finally have .NET 9 discover my app.config XML.

--- External Components/SgmlReader/Source Code Adapted/sgmlreaderdll.Net48/AssemblyInfo.cs
+++ External Components/SgmlReader/Source Code Adapted/sgmlreaderdll.Net48/AssemblyInfo.cs
@@ -1,9 +1,34 @@
 using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly: AssemblyTitle("SgmlReader")]
+[assembly: AssemblyDescription("Converts SGML to XML via XmlReader API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © 2002, Microsoft Corporation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 [assembly: CLSCompliant(true)]
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.7.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
--- Framework/JJ.Framework.Configuration.Tests/Properties/AssemblyInfo.cs
+++ Framework/JJ.Framework.Configuration.Tests/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("JJ.Framework.Configuration.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JJ.Framework.Configuration.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("eb6fa9a1-b8eb-4ee7-8a25-d26216280d5a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
