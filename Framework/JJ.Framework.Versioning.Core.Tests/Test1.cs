80b94d5aece4853d854fe29012b3032ce7d867d0
jjvanzon <jjvanzon79@gmail.com>
2025-03-21 21:21:27 +0100

    🥼 AutoIncrementVersion: Add empty test project JJ.Framework.Versioning.Core.Tests to verify NuGet package import later. Moved JJ.Framework.Versioning.Core to /Framework, where it belongs.

---
+++ Framework/JJ.Framework.Versioning.Core.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace JJ.Framework.Versioning.Core.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
