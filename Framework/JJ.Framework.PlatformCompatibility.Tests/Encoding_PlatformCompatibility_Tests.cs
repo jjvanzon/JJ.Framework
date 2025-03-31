0124031e26d6985743e76c62df86296e2665ca9a
jjvanzon <jjvanzon79@gmail.com>
2025-03-31 20:28:22 +0200

    🧪 JJ.Framework.PlatformCompatibility.Tests: Encoding: Draft of test for legacy platform-safe Encoding.GetString methods.

---
+++ Framework/JJ.Framework.PlatformCompatibility.Tests/Encoding_PlatformCompatibility_Tests.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace JJ.Framework.PlatformCompatibility.Tests
+{
+    [TestClass]
+    public sealed class Encoding_PlatformCompatibility_Tests
+    {
+        [TestMethod]
+        public void Test_Encoding_PlatformSafe()
+        {
+            foreach (var encoding in new [] { Encoding.UTF8/*, Encoding.Unicode, Encoding.UTF32*/ })
+            {
+                string expected = "Test Encoding";
+                byte[] bytes = encoding.GetBytes(expected);
+                string actual1 = Encoding_PlatformSafe.GetString_PlatformSafe(encoding, bytes);
+                string actual2 = encoding.GetString(bytes);
+            }
+        }
+    }
+}
