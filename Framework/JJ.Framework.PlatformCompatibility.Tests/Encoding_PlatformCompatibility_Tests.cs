f27d5ceceb781e258e155657ebcbea52f9884fd3
jjvanzon <jjvanzon79@gmail.com>
2025-03-31 21:11:05 +0200

    🧪 PlatformCompatibility.Tests: Check multiple encodings, check that  the byte count goes up with each successive encoding (UTF8, UTF16, UTF32), report values to Console for transparency.

--- Framework/JJ.Framework.PlatformCompatibility.Tests/Encoding_PlatformCompatibility_Tests.cs
+++ Framework/JJ.Framework.PlatformCompatibility.Tests/Encoding_PlatformCompatibility_Tests.cs
@@ -1,6 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace JJ.Framework.PlatformCompatibility.Tests
 {
@@ -10,12 +10,29 @@ namespace JJ.Framework.PlatformCompatibility.Tests
         [TestMethod]
         public void Test_Encoding_PlatformSafe()
         {
-            foreach (var encoding in new [] { Encoding.UTF8/*, Encoding.Unicode, Encoding.UTF32*/ })
+            int previousByteCount = 0;
+            
+            foreach (var encoding in new [] { Encoding.UTF8, Encoding.Unicode, Encoding.UTF32 })
             {
+                // Arrange
                 string expected = "Test Encoding";
                 byte[] bytes = encoding.GetBytes(expected);
+                int byteCount = bytes.Length;
+                
+                // Log
+                Console.WriteLine(@$"text = ""{expected}"", encoding = {encoding.WebName}, bytes = {byteCount}");
+                
+                // Act
                 string actual1 = Encoding_PlatformSafe.GetString_PlatformSafe(encoding, bytes);
                 string actual2 = encoding.GetString(bytes);
+
+                // Assert                
+                Assert.AreEqual(expected, actual1);
+                Assert.AreEqual(expected, actual2);
+                Assert.IsTrue(byteCount > previousByteCount, "byteCount > previousByteCount");
+                
+                previousByteCount = byteCount;
+
             }
         }
     }
