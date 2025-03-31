458a79afcc526cafb217d19ba947801add7ad72d
jjvanzon <jjvanzon79@gmail.com>
2025-03-31 21:09:22 +0200

    🧪 JJ.Framework.PlatformCompatibility.Tests: CultureInfo: Simplify the test by merging an alternative method's test into the main one.

--- Framework/JJ.Framework.PlatformCompatibility.Tests/CultureInfo_PlatformCompatibility_Tests.cs
+++ Framework/JJ.Framework.PlatformCompatibility.Tests/CultureInfo_PlatformCompatibility_Tests.cs
@@ -15,31 +15,16 @@ namespace JJ.Framework.PlatformCompatibility.Tests
                 var cultureInfo1 = CultureInfo.GetCultureInfo(cultureName);
                 var cultureInfo2 = new CultureInfo(cultureName);
                 var cultureInfo3 = CultureInfo_PlatformSafe.GetCultureInfo(cultureName);
+                var cultureInfo4 = PlatformHelper.CultureInfo_GetCultureInfo_PlatformSafe(cultureName);
                 
                 IsNotNull(cultureInfo1);
                 IsNotNull(cultureInfo2);
                 IsNotNull(cultureInfo3);
+                IsNotNull(cultureInfo4);
                 
                 AreEqual(cultureInfo1.Name, cultureInfo2.Name);
                 AreEqual(cultureInfo2.Name, cultureInfo3.Name);
-            }
-        }
-        
-        [TestMethod]
-        public void Test_PlatformHelper_CultureInfo_GetCultureInfo_PlatformSafe()
-        {
-            foreach (string cultureName in new[] { "nl-NL", "en-US", "de-DE", "zh-CN" })
-            {
-                var cultureInfo1 = CultureInfo.GetCultureInfo(cultureName);
-                var cultureInfo2 = new CultureInfo(cultureName);
-                var cultureInfo3 = PlatformHelper.CultureInfo_GetCultureInfo_PlatformSafe(cultureName);
-                
-                IsNotNull(cultureInfo1);
-                IsNotNull(cultureInfo2);
-                IsNotNull(cultureInfo3);
-                
-                AreEqual(cultureInfo1.Name, cultureInfo2.Name);
-                AreEqual(cultureInfo2.Name, cultureInfo3.Name);
+                AreEqual(cultureInfo3.Name, cultureInfo4.Name);
             }
         }
     }
