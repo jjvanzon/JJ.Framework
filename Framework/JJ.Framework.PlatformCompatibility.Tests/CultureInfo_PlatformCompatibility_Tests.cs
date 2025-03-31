12c301afa0cd41a6a9c91f5ea16246ceabccebc3
jjvanzon <jjvanzon79@gmail.com>
2025-03-31 20:27:31 +0200

    🧪 JJ.Framework.PlatformCompatibility.Tests: CultureInfo: Add test variable for PlatformHelper static methods.

--- Framework/JJ.Framework.PlatformCompatibility.Tests/CultureInfo_PlatformCompatibility_Tests.cs
+++ Framework/JJ.Framework.PlatformCompatibility.Tests/CultureInfo_PlatformCompatibility_Tests.cs
@@ -5,7 +5,7 @@ using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 namespace JJ.Framework.PlatformCompatibility.Tests
 {
     [TestClass]
-    public sealed class PlatformCompatibilityTests
+    public sealed class CultureInfo_PlatformCompatibility_Tests
     {
         [TestMethod]
         public void Test_CultureInfo_PlatformSafe()
@@ -15,6 +15,24 @@ namespace JJ.Framework.PlatformCompatibility.Tests
                 var cultureInfo1 = CultureInfo.GetCultureInfo(cultureName);
                 var cultureInfo2 = new CultureInfo(cultureName);
                 var cultureInfo3 = CultureInfo_PlatformSafe.GetCultureInfo(cultureName);
+                
+                IsNotNull(cultureInfo1);
+                IsNotNull(cultureInfo2);
+                IsNotNull(cultureInfo3);
+                
+                AreEqual(cultureInfo1.Name, cultureInfo2.Name);
+                AreEqual(cultureInfo2.Name, cultureInfo3.Name);
+            }
+        }
+        
+        [TestMethod]
+        public void Test_PlatformHelper_CultureInfo_GetCultureInfo_PlatformSafe()
+        {
+            foreach (string cultureName in new[] { "nl-NL", "en-US", "de-DE", "zh-CN" })
+            {
+                var cultureInfo1 = CultureInfo.GetCultureInfo(cultureName);
+                var cultureInfo2 = new CultureInfo(cultureName);
+                var cultureInfo3 = PlatformHelper.CultureInfo_GetCultureInfo_PlatformSafe(cultureName);
                 
                 IsNotNull(cultureInfo1);
                 IsNotNull(cultureInfo2);
