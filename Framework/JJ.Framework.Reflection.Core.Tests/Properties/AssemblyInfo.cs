b284b53fdd231b417c352ba74baeb06bbc8ce56f
jjvanzon <jjvanzon79@gmail.com>
2025-04-02 23:43:00 +0200

    🧹 Remove AssemblyInfo.cs files now redundant.

--- Framework/JJ.Framework.Reflection.Core.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("45a283b6-5c98-479d-ba08-ec1776a94782")]
