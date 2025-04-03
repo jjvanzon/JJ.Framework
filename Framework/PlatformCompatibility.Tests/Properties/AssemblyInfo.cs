be6e1d909f7c22b9cc0f93a8da564d2f0fe9dc23
jjvanzon <jjvanzon79@gmail.com>
2025-04-03 21:56:30 +0200

    🏛️ Architecture: Clean up project after conversion to .NET 9 - JJ.Framework.PlatformCompatibility.Tests

--- Framework/PlatformCompatibility.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("2f1c0212-86d0-4b85-b18a-6512d9064196")]
