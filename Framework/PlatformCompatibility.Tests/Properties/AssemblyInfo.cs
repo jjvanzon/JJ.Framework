3f0a05f6d280c1bc4a8f383d8b5b017c03cf6e73
jjvanzon <jjvanzon79@gmail.com>
2025-04-03 21:49:39 +0200

    🏛️ Architecture: Convert JJ.Framework.PlatformCompatibility.Tests to .NET 9.

--- Framework/PlatformCompatibility.Tests/Properties/AssemblyInfo.cs
+++ Framework/PlatformCompatibility.Tests/Properties/AssemblyInfo.cs
@@ -1,17 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("JJ.Framework.PlatformCompatibility.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jan Joost van Zon")]
-[assembly: AssemblyProduct("JJ.Framework")]
-[assembly: AssemblyCopyright("Copyright © 2021 Jan Joost van Zon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("2f1c0212-86d0-4b85-b18a-6512d9064196")]
-
-[assembly: AssemblyVersion("1.7.*")]
