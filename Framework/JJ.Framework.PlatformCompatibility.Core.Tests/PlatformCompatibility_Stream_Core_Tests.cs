d8c7d8fa684c68ed172ac65cc46f4808a2c7d7ea
jjvanzon <jjvanzon79@gmail.com>
2025-04-06 22:36:16 +0200

    🧪 PlatformCompatibility Tests: Add test for Stream.Copy helper.

---
+++ Framework/JJ.Framework.PlatformCompatibility.Core.Tests/PlatformCompatibility_Stream_Core_Tests.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+
+namespace JJ.Framework.PlatformCompatibility.Core.Tests;
+
+[TestClass]
+public class PlatformCompatibility_Stream_Core_Tests
+{
+    [TestMethod]
+    public void PlatformCompatibility_Stream_Copy_Core_Test()
+    {
+        // Arrange
+        int bufferSize = 5;
+        byte[] sourceArray  = [ 1, 2, 3, 4, 5 ];
+        Stream sourceStream = new MemoryStream(sourceArray);
+        
+        {
+            // Arrange
+            sourceStream.Position = 0;
+            var destArray = new byte[5];
+            Stream destStream = new MemoryStream(destArray);
+            
+            // Act
+            sourceStream.CopyTo(destStream, bufferSize);
+
+            // Assert
+            IsTrue(() => destArray.SequenceEqual(sourceArray));
+        }
+        
+        {
+            // Arrange
+            sourceStream.Position = 0;
+            var destArray = new byte[5];
+            Stream destStream = new MemoryStream(destArray);
+            
+            // Act
+            PlatformHelperLegacy.Stream_CopyTo_PlatformSupport(sourceStream, destStream, bufferSize);
+
+            // Assert
+            IsTrue(() => destArray.SequenceEqual(sourceArray));
+        }
+        
+        {
+            // Arrange
+            sourceStream.Position = 0;
+            var destArray = new byte[5];
+            Stream destStream = new MemoryStream(destArray);
+            
+            // Act
+            Stream_PlatformSupport.CopyTo(sourceStream, destStream, bufferSize);
+
+            // Assert
+            IsTrue(() => destArray.SequenceEqual(sourceArray));
+        }
+    }
+}
