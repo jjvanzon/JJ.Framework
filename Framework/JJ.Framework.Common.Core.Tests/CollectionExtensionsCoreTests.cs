b8f8b44c3c1707e2818056ce3563bc197cc3f12e
jjvanzon <jjvanzon79@gmail.com>
2025-04-10 22:45:03 +0200

    🧪 Common: Test InvalidValueException and ValueNotSupportedException's message formatting and null tolerance.

--- Framework/JJ.Framework.Common.Core.Tests/CollectionExtensionsCoreTests.cs
+++ Framework/JJ.Framework.Common.Core.Tests/CollectionExtensionsCoreTests.cs
@@ -194,7 +194,7 @@ public class CollectionExtensionsCoreTests
     public void CollectionExtensions_AddRange_NullExceptions_Core_Test()
     {
         IList<int>? nullList = null;
-        IList<int>  list     = new List<int> { 1, 2, 3 };
+        IList<int> list = new List<int> { 1, 2, 3 };
         ThrowsExceptionContaining(() => nullList.AddRange(list), "collection", "cannot be null");
         ThrowsExceptionContaining(() => list.AddRange(nullList), "items", "cannot be null");
     }
@@ -202,7 +202,7 @@ public class CollectionExtensionsCoreTests
     [TestMethod]
     public void CollectionExtensions_Add_WithParams_Core_Test()
     {
-        IList<int> list  = [ 1, 2, 3 ];
+        IList<int> list = [ 1, 2, 3 ];
         list.Add(3, 4, 5);
         int[] expected = [ 1, 2, 3, 3, 4, 5 ];
         int[] actual = list.ToArray();
---
+++ Framework/JJ.Framework.Common.Core.Tests/ExceptionTypesCoreTests.cs
@@ -0,0 +1,37 @@
+﻿namespace JJ.Framework.Common.Core.Tests;
+
+[TestClass]
+public class ExceptionTypesCoreTests
+{
+    [TestMethod]
+    public void Test_InvalidValueException_Message()
+    {
+        object value = "Hi";
+        var exception = new InvalidValueException(value);
+        AreEqual("Invalid System.String value: 'Hi'.", () => exception.Message);
+    }
+    
+    [TestMethod]
+    public void Test_ValueNotSupportedException_Message()
+    {
+        object value = "Hi";
+        var exception = new ValueNotSupportedException(value);
+        AreEqual("System.String value: 'Hi' is not supported.", () => exception.Message);
+    }
+    
+    // Null tolerance does not resolve pretty, but it does the job:
+    
+    [TestMethod]
+    public void Test_InvalidValueException_NullTolerance()
+    {
+        var exception = new InvalidValueException(null);
+        AreEqual("Invalid  value: ''.", () => exception.Message);
+    }
+    
+    [TestMethod]
+    public void Test_ValueNotSupportedException_NullTolerance()
+    {
+        var exception = new ValueNotSupportedException(null);
+        AreEqual(" value: '' is not supported.", () => exception.Message);
+    }
+}
