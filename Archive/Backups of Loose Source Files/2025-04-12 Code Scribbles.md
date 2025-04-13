2095bd20869b4631a3cf2d32cba88380f4d5a1cf
jjvanzon <jjvanzon79@gmail.com>
2025-04-13 19:18:55 +0200

    🧪 Common StringExtensions Tests: Made syntax more terse for overview. Replaced a redundant string-based test to char-based.

--- Archive/Backups of Loose Source Files/2025-04-12 Code Scribbles.md
+++ Archive/Backups of Loose Source Files/2025-04-12 Code Scribbles.md
@@ -1,6 +1,13 @@
 
-```cs        
+```cs
 Assert.IsTrue(list.Any(x => x.Key == "Name1" && x.Value.Equals(3)));
 Assert.IsTrue(list.Any(x => x.Key == "Name2" && x.Value.Equals(5)));
 Assert.IsTrue(list.Any(x => x.Key == "Name3" && x.Value.Equals(6)));
+```
+
+```cs
+// Redundant. Should use char variant.
+[TestMethod]
+public void CutLeft_NoMatch_Test() 
+    => AreEqual("Lalalaa", () => "Lalalaa".CutLeft("laa"));
 ```
--- Framework/JJ.Framework.Common.Core.Tests/StringExtensionsCommonCoreTests.cs
+++ Framework/JJ.Framework.Common.Core.Tests/StringExtensionsCommonCoreTests.cs
@@ -26,102 +26,54 @@ public class StringExtensionsCommonCoreTests
     // CutLeft
     
     [TestMethod]
-    public void CutLeft_OneOccurrence()
-    {
-        string input = "BlaLala";
-        string output = input.CutLeft("Bla");
-        AreEqual("Lala", () => output);
-    }
-
-    [TestMethod]
-    public void CutLeft_MultipleOccurrences_TrimsOne()
-    {
-        string input = "BlaBlaLala";
-        string output = input.CutLeft("Bla");
-        AreEqual("BlaLala", () => output);
-    }
+    public void CutLeft_String_NoMatch() 
+        => AreEqual("Lalalaa", () => "Lalalaa".CutLeft("la"));
     
     [TestMethod]
-    public void CutLeft_NoMatch()
-    {
-        string input  = "Lalalaa";
-        string output = input.CutLeft("la");
-        AreEqual("Lalalaa", () => output);
-    }
+    public void CutLeft_String_OneMatch() 
+        => AreEqual("Lala", () => "BlaLala".CutLeft("Bla"));
     
     [TestMethod]
-    public void CutLeft_Char_OneOccurrence()
-    {
-        string input  = "BlaLala";
-        string output = input.CutLeft('B');
-        AreEqual("laLala", () => output);
-    }
+    public void CutLeft_String_MoreMatches_TrimsOne() 
+        => AreEqual("BlaLala", () => "BlaBlaLala".CutLeft("Bla"));
     
     [TestMethod]
-    public void CutLeft_Char_MultipleOccurrences_TrimsOne()
-    {
-        string input  = "BBlaLala";
-        string output = input.CutLeft('B');
-        AreEqual("BlaLala", () => output);
-    }
+    public void CutLeft_Char_NoMatch() 
+        => AreEqual("Lalalaa", () => "Lalalaa".CutLeft('a'));
     
     [TestMethod]
-    public void CutLeft_NoMatch_Test()
-    {
-        string input  = "Lalalaa";
-        string output = input.CutLeft("laa");
-        AreEqual("Lalalaa", () => output);
-    }
+    public void CutLeft_Char_OneMatch() 
+        => AreEqual("laLala", () => "BlaLala".CutLeft('B'));
+    
+    [TestMethod]
+    public void CutLeft_Char_MoreMatches_TrimsOne() 
+        => AreEqual("BlaLala", () => "BBlaLala".CutLeft('B'));
     
     // CutRight
 
     [TestMethod]
-    public void CutRight_String_NoMatch()
-    {
-        string input  = "Lalalaa";
-        string output = input.CutRight("la");
-        AreEqual("Lalalaa", () => output);
-    }
-
-    [TestMethod]
-    public void CutRight_String_OneOccurrence()
-    {
-        string input = "LalaBla";
-        string output = input.CutRight("Bla");
-        AreEqual("Lala", () => output);
-    }
+    public void CutRight_String_NoMatch() 
+        => AreEqual("Lalalaa", () => "Lalalaa".CutRight("la"));
     
     [TestMethod]
-    public void CutRight_String_MultipleOccurrences_TrimsOne()
-    {
-        string input = "LalaBlaBla";
-        string output = input.CutRight("Bla");
-        AreEqual("LalaBla", () => output);
-    }
+    public void CutRight_String_OneMatch() 
+        => AreEqual("Lala", () => "LalaBla".CutRight("Bla"));
     
     [TestMethod]
-    public void CutRight_Char_NoMatch()
-    {
-        string input  = "Lalala";
-        string output = input.CutRight('l');
-        AreEqual("Lalala", () => output);
-    }
+    public void CutRight_String_MoreMatches_TrimsOne() 
+        => AreEqual("LalaBla", () => "LalaBlaBla".CutRight("Bla"));
     
     [TestMethod]
-    public void CutRight_Char_OneOccurrence()
-    {
-        string input  = "Lalala";
-        string output = input.CutRight('a');
-        AreEqual("Lalal", () => output);
-    }
+    public void CutRight_Char_NoMatch() 
+        => AreEqual("Lalala", () => "Lalala".CutRight('l'));
     
     [TestMethod]
-    public void CutRight_Char_MultipleOccurrences_TrimsOne()
-    {
-        string input  = "Lalalaa";
-        string output = input.CutRight('a');
-        AreEqual("Lalala", () => output);
-    }
+    public void CutRight_Char_OneMatch() 
+        => AreEqual("Lalal", () => "Lalala".CutRight('a'));
+    
+    [TestMethod]
+    public void CutRight_Char_MoreMatches_TrimsOne() 
+        => AreEqual("Lalala", () => "Lalalaa".CutRight('a'));
     
     // FromTill
     
@@ -134,15 +86,15 @@ public class StringExtensionsCommonCoreTests
         => AreEqual("1", () => "12345".FromTill(0, 0));
     
     [TestMethod]
-    public void FromTill_StartIndexIsZero() 
+    public void FromTill_StartIndex0() 
         => AreEqual("12", () => "12345".FromTill(0, 1));
     
     [TestMethod]
-    public void FromTill_StartIndexIsOne() 
+    public void FromTill_StartIndex1() 
         => AreEqual("23", () => "12345".FromTill(1, 2));
     
     [TestMethod]
-    public void FromTill_EndIndexIsLast() 
+    public void FromTill_EndIndexLast() 
         => AreEqual("12345", () => "12345".FromTill(0, 4));
     
     [TestMethod]
@@ -150,10 +102,10 @@ public class StringExtensionsCommonCoreTests
         => AreEqual("5", () => "12345".FromTill(4, 4));
     
     [TestMethod]
-    public void FromTill_OutsideLeftBound_ThrowsException() 
+    public void FromTill_OutsideBoundLeft_Exception() 
         => ThrowsException(() => "12345".FromTill(-1, 1));
     
     [TestMethod]
-    public void FromTill_OutsideRightBound_ThrowsException() 
+    public void FromTill_OutsideBoundRight_Exception() 
         => ThrowsException(() => "12345".FromTill(6, 8));
 }
