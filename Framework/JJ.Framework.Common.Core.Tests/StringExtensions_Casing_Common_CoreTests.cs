e6e91e4a55c6878a54b08105634f4b2a50e37779
jjvanzon <jjvanzon79@gmail.com>
2025-04-14 04:54:01 +0200

    🧪 Common StringExtensions Tests: Added StartsWithCap tests.

---
+++ Framework/JJ.Framework.Common.Core.Tests/StringExtensions_Casing_Common_CoreTests.cs
@@ -0,0 +1,23 @@
+﻿namespace JJ.Framework.Common.Core.Tests;
+
+[TestClass]
+public class StringExtensions_Casing_Common_CoreTests
+{
+    private static readonly string _null = null;
+    
+    [TestMethod]
+    public void StartWithCap_Core_Test() 
+        => AreEqual("Test", () => "test".StartWithCap());
+
+    [TestMethod]
+    public void StartWithCap_NullException() 
+        => ThrowsException(() => _null.StartWithCap());
+    
+    [TestMethod]
+    public void StartWithCap_EmptyString() 
+        => AreEqual("", () => "".StartWithCap());
+    
+    [TestMethod]
+    public void StartWithCap_AlreadyStartsWithCap() 
+        => AreEqual("Test", () => "Test".StartWithCap());
+}
