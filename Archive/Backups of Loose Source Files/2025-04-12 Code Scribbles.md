384fa67610d0e42c1a367c0845ac1f9d221ed52f
jjvanzon <jjvanzon79@gmail.com>
2025-04-15 02:21:39 +0200

    🍌 Common StringExtensions: Added unit tests for string splitting overloads.

--- Archive/Backups of Loose Source Files/2025-04-12 Code Scribbles.md
+++ Archive/Backups of Loose Source Files/2025-04-12 Code Scribbles.md
@@ -11,3 +11,28 @@ Assert.IsTrue(list.Any(x => x.Key == "Name3" && x.Value.Equals(6)));
 public void CutLeft_NoMatch_Test() 
     => AreEqual("Lalalaa", () => "Lalalaa".CutLeft("laa"));
 ```
+
+```cs
+Assert.AreEqual(expected.Length, actual.Length);
+for (int i = 0; i < expected.Length; i++)
+{
+    Assert.AreEqual(expected[i], actual[i]);
+}
+```
+
+
+```cs
+        IsNotNull(() => split1);
+        IsNotNull(() => split2);
+        AreEqual(1, () => split1.Length);
+        AreEqual(1, () => split2.Length);
+        IsNotNull(() => split1[0]);
+        IsNotNull(() => split2[0]);
+        AreEqual("a, b, c", () => split1[0]);
+        AreEqual("a, b, c", () => split2[0]);
+    
+    [TestMethod]
+    public void Split_StringSepAndOptions_EmptySepException()
+    {
+    }
+```
---
+++ Framework/JJ.Framework.Common.Core.Tests/StringExtensionsSplitCoreTests.cs
@@ -0,0 +1,140 @@
+﻿using static System.StringSplitOptions;
+
+namespace JJ.Framework.Common.Core.Tests;
+
+[TestClass]
+public class StringExtensionsSplitCoreTests
+{
+    private static readonly string _null = null;
+    
+    // Split With Char Sep
+    
+    [TestMethod]
+    public void Split_CharAndOptions_CoreTest()
+    {
+        string[] expected = [ "a", "b", "c", "e" ];
+        
+        string[] split1   = StringExtensions_Split.Split("a,b,c,,e", ',', RemoveEmptyEntries);
+        string[] split2   = "a,b,c,,e".Split(',', RemoveEmptyEntries);
+        
+        CollectionAssert.AreEqual(expected, split1);
+        CollectionAssert.AreEqual(expected, split2);
+    }
+    
+    [TestMethod]
+    public void Split_CharAndOptions_NullInputException()
+    {
+        ThrowsException(() => StringExtensions_Split.Split(null, ',', RemoveEmptyEntries));
+        ThrowsException(() => _null.Split(',', RemoveEmptyEntries));
+    }
+    
+    [TestMethod]
+    public void Split_Char_AndInvalidOptions()
+    {
+        ThrowsException(() => StringExtensions_Split.Split("a,b,c", ',', (StringSplitOptions)(-1)));
+        ThrowsException(() => "a,b,c".Split(',', (StringSplitOptions)(-1)));
+    }
+    
+    // Split with String Sep
+    
+    [TestMethod]
+    public void Split_StringSepAndOptions_CoreTest()
+    {
+        string[] expected = { "a", "b", "c", "e" };
+        
+        string[] split1   = StringExtensions_Split.Split("a, b, c, , e", ", ", RemoveEmptyEntries);
+        string[] split2   = "a, b, c, , e".Split(", ", RemoveEmptyEntries);
+        
+        CollectionAssert.AreEqual(expected, split1);
+        CollectionAssert.AreEqual(expected, split2);
+    }
+    
+    [TestMethod]
+    public void Split_StringSepOptions_NullInputException()
+    {
+        ThrowsException(() => StringExtensions_Split.Split(_null, ", ", RemoveEmptyEntries));
+        ThrowsException(() => _null.Split(", ", RemoveEmptyEntries));
+    }
+        
+    [TestMethod]
+    public void Split_StringSep_AndInvalidOptions()
+    {
+        ThrowsException(() => StringExtensions_Split.Split("a, b, c", ", ", (StringSplitOptions)(-1)));
+        ThrowsException(() => "a, b, c".Split(", ", (StringSplitOptions)(-1)));
+    }
+
+    [TestMethod]
+    public void Split_StringSepAndOptions_NullOrEmptySep_NoSplit()
+    {
+        // Apparently a null separator is ok to .NET, and doesn't throw exceptions.
+        //ThrowsException(() => StringExtensions_Split.Split("a, b, c", _null, RemoveEmptyEntries));
+        //ThrowsException(() => StringExtensions_Split.Split("a, b, c", "", RemoveEmptyEntries));
+        //ThrowsException(() => "a, b, c".Split(_null, RemoveEmptyEntries));
+        //ThrowsException(() => "a, b, c".Split("", RemoveEmptyEntries));
+        
+        IList<string[]> splits =
+        [
+            StringExtensions_Split.Split("a, b, c", _null, RemoveEmptyEntries),
+            StringExtensions_Split.Split("a, b, c", "",    RemoveEmptyEntries),
+            "a, b, c".Split(_null, RemoveEmptyEntries),
+            "a, b, c".Split("",    RemoveEmptyEntries)
+        ];
+        
+        foreach (string[] split in splits)
+        {
+            IsNotNull(() => split);
+            AreEqual(1, () => split.Length);
+            IsNotNull(() => split[0]);
+            AreEqual("a, b, c", () => split[0]);
+        }
+    }
+    
+    // Split with Params Seps
+    
+    [TestMethod]
+    public void Split_ParamsSeps_CoreTest()
+    {
+        string   input   = "a, b|c, , e";
+        string[] expected = [ "a", "b", "c", "", "e" ];
+        string[] split1 = StringExtensions_Split.Split(input, ", ", "|");
+        string[] split2 = input.Split(", ", "|");
+        
+        CollectionAssert.AreEqual(expected, split1);
+        CollectionAssert.AreEqual(expected, split2);
+    }
+    
+    [TestMethod]
+    public void Split_ParamsSeps_NullInputException()
+    {
+        ThrowsException(() => StringExtensions_Split.Split(_null, ", ", "|"));
+        ThrowsException(() => _null.Split(", ", "|"));
+    }
+    
+    [TestMethod]
+    public void Split_ParamsStringSeps_NullOrEmptySep_NoEffect()
+    {
+        // Apparently a null separator is ok to .NET and doesn't throw exceptions:
+        //ThrowsException(() => StringExtensions_Split.Split("a,b|c", _null, "|"));
+        //ThrowsException(() => StringExtensions_Split.Split("a,b|c", "", "|"));
+        //ThrowsException(() => "a,b|c".Split(_null, "|"));
+        //ThrowsException(() => "a,b|c".Split("", "|"));
+        
+        IList<string[]> splits =
+        [
+            StringExtensions_Split.Split("a,b|c", _null, "|"),
+            StringExtensions_Split.Split("a,b|c", "", "|"),
+            "a,b|c".Split(_null, "|"),
+            "a,b|c".Split("", "|")
+        ];
+        
+        foreach (string[] split in splits)
+        {
+            IsNotNull(() => split);
+            AreEqual(2, () => split.Length);
+            IsNotNull(() => split[0]);
+            IsNotNull(() => split[1]);
+            AreEqual("a,b", () => split[0]);
+            AreEqual("c", () => split[1]);
+        }
+    }
+}
