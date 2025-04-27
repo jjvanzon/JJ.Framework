d6f1385459361ebab328f1a719c646951d48a2ad
jjvanzon <jjvanzon79@gmail.com>
2025-04-27 17:56:12 +0200

    🧪 ReflectionCacheCoreTests - Extend with several StaticReflectionCache member tests.

--- Framework/JJ.Framework.Reflection.Core.Tests/ReflectionCacheCoreTests.cs
+++ Framework/JJ.Framework.Reflection.Core.Tests/ReflectionCacheCoreTests.cs
@@ -18,6 +18,14 @@ public class ReflectionCacheCoreTests
         public  int    TestProperty  { get => _testField;  set => _testField = value; } // ncrunch: no coverage
         public  string TestProperty2 { get => _testField2; set => _testField2 = value; } // ncrunch: no coverage
     }
+
+    private class ClassWithIndexers
+    {
+        public int this[int index] { get => default; set { } }
+        public int this[int index, string text] { get => default; set { } }
+    }
+    
+    private class ClassWithoutIndexer;
     
     private class NoConstructorClass
     {
@@ -32,6 +40,60 @@ public class ReflectionCacheCoreTests
 
     // NOTE: Tested methods are run twice to hit cache retrieval.
     
+    // Properties
+
+    [TestMethod]
+    public void StaticReflectionCache_GetProperty_Core_Test()
+    {
+        for (int i = 0; i < REPEATS; i++)
+        {
+            PropertyInfo field = StaticReflectionCache.GetProperty(typeof(TestClass), "TestProperty");
+            IsNotNull(() => field);
+            AreEqual("TestProperty", () => field.Name);
+            AreEqual(typeof(int), () => field.PropertyType);
+
+            PropertyInfo field2 = StaticReflectionCache.GetProperty(typeof(TestClass), "TestProperty2");
+            IsNotNull(() => field2);
+            AreEqual("TestProperty2", () => field2.Name);
+            AreEqual(typeof(string), () => field2.PropertyType);
+        }
+    }
+    
+    [TestMethod]
+    public void StaticReflectionCache_TryGetProperty_Core_Test()
+    {
+        for (int i = 0; i < REPEATS; i++)
+        {
+            PropertyInfo field = StaticReflectionCache.TryGetProperty(typeof(TestClass), "TestProperty");
+            IsNotNull(() => field);
+            AreEqual("TestProperty", () => field.Name);
+            AreEqual(typeof(int), () => field.PropertyType);
+
+            PropertyInfo field2 = StaticReflectionCache.TryGetProperty(typeof(TestClass), "TestProperty2");
+            IsNotNull(() => field2);
+            AreEqual("TestProperty2", () => field2.Name);
+            AreEqual(typeof(string), () => field2.PropertyType);
+        }
+    }
+        
+    [TestMethod]
+    public void StaticReflectionCache_GetProperty_NotFound_Exception()
+    {
+        ThrowsExceptionContaining(
+            () => StaticReflectionCache.GetProperty(typeof(TestClass), _nonExistentName), 
+            "Property", "not found");
+    }
+
+    [TestMethod]
+    public void StaticReflectionCache_TryGetProperty_NotFound_ReturnsNull()
+    {
+        for (int i = 0; i < REPEATS; i++)
+        {
+            PropertyInfo field = StaticReflectionCache.TryGetProperty(typeof(TestClass), _nonExistentName);
+            IsNull(() => field);
+        }
+    }
+    
     [TestMethod]
     public void ReflectionCache_GetProperties()
     {
@@ -39,17 +101,31 @@ public class ReflectionCacheCoreTests
         
         for (int i = 0; i < REPEATS; i++)
         {
-            PropertyInfo[] properties = reflectionCache.GetProperties(typeof(TestClass));
-            
-            IsNotNull(  () => properties);
-            AreEqual(2, () => properties.Length);
-            IsNotNull(  () => properties[0]);
-            IsNotNull(  () => properties[1]);
-            AreEqual(typeof(int),     () => properties[0].PropertyType);
-            AreEqual(typeof(string),  () => properties[1].PropertyType);
-            AreEqual("TestProperty",  () => properties[0].Name);
-            AreEqual("TestProperty2", () => properties[1].Name);
+            IList<PropertyInfo> properties = reflectionCache.GetProperties(typeof(TestClass));
+            AssertProperties(properties);
         }
+    }
+    
+    [TestMethod]
+    public void StaticReflectionCache_GetProperties()
+    {
+        for (int i = 0; i < REPEATS; i++)
+        {
+            IList<PropertyInfo> properties = StaticReflectionCache.GetProperties(typeof(TestClass), BINDING_FLAGS_ALL);
+            AssertProperties(properties);
+        }
+    }
+    
+    private void AssertProperties(IList<PropertyInfo> properties)
+    {
+        IsNotNull(  () => properties);
+        AreEqual(2, () => properties.Count);
+        IsNotNull(  () => properties[0]);
+        IsNotNull(  () => properties[1]);
+        AreEqual(typeof(int),     () => properties[0].PropertyType);
+        AreEqual(typeof(string),  () => properties[1].PropertyType);
+        AreEqual("TestProperty",  () => properties[0].Name);
+        AreEqual("TestProperty2", () => properties[1].Name);
     }
     
     [TestMethod]
@@ -72,6 +148,60 @@ public class ReflectionCacheCoreTests
         }
     }
     
+    // Fields
+
+    [TestMethod]
+    public void StaticReflectionCache_GetField_Core_Test()
+    {
+        for (int i = 0; i < REPEATS; i++)
+        {
+            FieldInfo field = StaticReflectionCache.GetField(typeof(TestClass), "_testField");
+            IsNotNull(() => field);
+            AreEqual("_testField", () => field.Name);
+            AreEqual(typeof(int), () => field.FieldType);
+
+            FieldInfo field2 = StaticReflectionCache.GetField(typeof(TestClass), "_testField2");
+            IsNotNull(() => field2);
+            AreEqual("_testField2", () => field2.Name);
+            AreEqual(typeof(string), () => field2.FieldType);
+        }
+    }
+    
+    [TestMethod]
+    public void StaticReflectionCache_TryGetField_Core_Test()
+    {
+        for (int i = 0; i < REPEATS; i++)
+        {
+            FieldInfo field = StaticReflectionCache.TryGetField(typeof(TestClass), "_testField");
+            IsNotNull(() => field);
+            AreEqual("_testField", () => field.Name);
+            AreEqual(typeof(int), () => field.FieldType);
+
+            FieldInfo field2 = StaticReflectionCache.TryGetField(typeof(TestClass), "_testField2");
+            IsNotNull(() => field2);
+            AreEqual("_testField2", () => field2.Name);
+            AreEqual(typeof(string), () => field2.FieldType);
+        }
+    }
+        
+    [TestMethod]
+    public void StaticReflectionCache_GetField_NotFound_Exception()
+    {
+        ThrowsExceptionContaining(
+            () => StaticReflectionCache.GetField(typeof(TestClass), _nonExistentName), 
+            "Field", "not found");
+    }
+
+    [TestMethod]
+    public void StaticReflectionCache_TryGetField_NotFound_ReturnsNull()
+    {
+        for (int i = 0; i < REPEATS; i++)
+        {
+            FieldInfo field = StaticReflectionCache.TryGetField(typeof(TestClass), _nonExistentName);
+            IsNull(() => field);
+        }
+    }
+    
     [TestMethod]
     public void ReflectionCache_GetFields()
     {
@@ -80,17 +210,33 @@ public class ReflectionCacheCoreTests
         for (int i = 0; i < REPEATS; i++)
         {
             FieldInfo[] fields = reflectionCache.GetFields(typeof(TestClass));
-            
-            IsNotNull(  () => fields);
-            AreEqual(2, () => fields.Length);
-            IsNotNull(  () => fields[0]);
-            IsNotNull(  () => fields[1]);
-            AreEqual(typeof(int),     () => fields[0].FieldType);
-            AreEqual(typeof(string),  () => fields[1].FieldType);
-            AreEqual("_testField",  () => fields[0].Name);
-            AreEqual("_testField2", () => fields[1].Name);
+            AssertFields(fields);
         }
     }
+    
+    [TestMethod]
+    public void StaticReflectionCache_GetFields()
+    {
+        for (int i = 0; i < REPEATS; i++)
+        {
+            FieldInfo[] fields = StaticReflectionCache.GetFields(typeof(TestClass), BINDING_FLAGS_ALL);
+            AssertFields(fields);
+        }
+    }
+    
+    private void AssertFields(FieldInfo[] fields)
+    {
+        IsNotNull(  () => fields);
+        AreEqual(2, () => fields.Length);
+        IsNotNull(  () => fields[0]);
+        IsNotNull(  () => fields[1]);
+        AreEqual(typeof(int),    () => fields[0].FieldType);
+        AreEqual(typeof(string), () => fields[1].FieldType);
+        AreEqual("_testField",   () => fields[0].Name);
+        AreEqual("_testField2",  () => fields[1].Name);
+    }
+    
+    // GetTypeByShortName
     
     [TestMethod]
     public void ReflectionCache_GetTypeByShortName_Core_Test()
@@ -205,7 +351,79 @@ public class ReflectionCacheCoreTests
         }
     }
     
-    // TODO: Tests for GetConstructor with private classes with 0, 1 or multiple matches. Only with 1 succeeds, others throw.
+    // Indexers
+    
+    [TestMethod]
+    public void StaticReflectionCache_GetIndexer_Core_Test()
+    {
+        for (int i = 0; i < REPEATS; i++)
+        {
+            PropertyInfo indexer = StaticReflectionCache.GetIndexer(typeof(ClassWithIndexers), typeof(int));
+            AssertIndexer1(indexer);
+             
+            PropertyInfo indexer2 = StaticReflectionCache.GetIndexer(typeof(ClassWithIndexers), typeof(int), typeof(string));
+            AssertIndexer2(indexer2);
+        }
+    }
+    
+    [TestMethod]
+    public void StaticReflectionCache_TryGetIndexer_Core_Test()
+    {
+        for (int i = 0; i < REPEATS; i++)
+        {
+            PropertyInfo indexer = StaticReflectionCache.TryGetIndexer(typeof(ClassWithIndexers), typeof(int));
+            AssertIndexer1(indexer);
+            
+            PropertyInfo indexer2 = StaticReflectionCache.TryGetIndexer(typeof(ClassWithIndexers), typeof(int), typeof(string));
+            AssertIndexer2(indexer2);
+        }
+    }
+
+    private void AssertIndexer1(PropertyInfo indexer)
+    {
+        IsNotNull(() => indexer);
+        AreEqual(typeof(int), () => indexer.PropertyType);
+        
+        ParameterInfo[] parameters = indexer.GetIndexParameters();
+        IsNotNull(() => parameters);
+        AreEqual(1, () => parameters.Length);
+        IsNotNull(() => parameters[0]);
+        AreEqual(typeof(int), () => parameters[0].ParameterType);
+    }
+        
+    private void AssertIndexer2(PropertyInfo indexer)
+    {
+        IsNotNull(() => indexer);
+        AreEqual(typeof(int), () => indexer.PropertyType);
+        
+        ParameterInfo[] parameters = indexer.GetIndexParameters();
+        IsNotNull(() => parameters);
+        AreEqual(2, () => parameters.Length);
+        IsNotNull(() => parameters[0]);
+        IsNotNull(() => parameters[1]);
+        AreEqual(typeof(int),    () => parameters[0].ParameterType);
+        AreEqual(typeof(string), () => parameters[1].ParameterType);
+    }
+    
+    [TestMethod]
+    public void StaticReflectionCache_GetIndexer_NotFound_Throws()
+    {
+        ThrowsExceptionContaining(
+            () => StaticReflectionCache.GetIndexer(typeof(ClassWithoutIndexer), typeof(int)), 
+            "Indexer not found");
+    }
+    
+    [TestMethod]
+    public void StaticReflectionCache_TryGetIndexer_NotFound_ReturnsNull()
+    {
+        for (int i = 0; i < REPEATS; i++)
+        {
+            PropertyInfo indexer = StaticReflectionCache.TryGetIndexer(typeof(ClassWithoutIndexer), typeof(int), typeof(string));
+            IsNull(() => indexer);
+        }
+    }
+    
+    // Constructor Tests
     
     [TestMethod]
     public void ReflectionCache_GetConstructor_Single()
