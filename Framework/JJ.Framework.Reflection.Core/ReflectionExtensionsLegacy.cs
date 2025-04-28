9000c29810176873deb60600336135fcb4a59df5
jjvanzon <jjvanzon79@gmail.com>
2025-04-28 22:59:39 +0200

    🧼 Reflection.Core: ReflectionExtensionLegacy - Comment out 2 methods copied from legacy, that might not be used in practice (by JJ.Synthesizer) anymore.

--- Framework/JJ.Framework.Reflection.Core/ReflectionExtensionsLegacy.cs
+++ Framework/JJ.Framework.Reflection.Core/ReflectionExtensionsLegacy.cs
@@ -8,33 +8,33 @@ namespace JJ.Framework.Reflection.Core
 {
     public static class ReflectionExtensionsLegacy
     {
-        /// <summary> Returns a type's base type and its base type etc. </summary>
-        public static IList<Type> GetBaseClasses(this Type type)
-        {
-            if (type == null) throw new ArgumentNullException(nameof(type));
+        ///// <summary> Returns a type's base type and its base type etc. </summary>
+        //public static IList<Type> GetBaseClasses(this Type type)
+        //{
+        //    if (type == null) throw new ArgumentNullException(nameof(type));
+        //
+        //    var types = new List<Type>();
+        //
+        //    while (type.BaseType != null)
+        //    {
+        //        types.Add(type.BaseType);
+        //
+        //        type = type.BaseType;
+        //    }
+        //
+        //    return types;
+        //}
 
-            var types = new List<Type>();
-
-            while (type.BaseType != null)
-            {
-                types.Add(type.BaseType);
-
-                type = type.BaseType;
-            }
-
-            return types;
-        }
-
-        /// <summary>
-        /// Type.GetProperty returns null if the property does not exist.
-        /// This method is a little safer than that and throws a clear exception if the property does not exist.
-        /// </summary>
-        public static PropertyInfo GetPropertyOrException(this Type type, string name)
-        {
-            if (type == null) throw new ArgumentNullException(nameof(type));
-            PropertyInfo property = type.GetProperty(name);
-            if (property == null) throw new Exception($"Property '{name}' not found on type '{type}'.");
-            return property;
-        }
+        ///// <summary>
+        ///// Type.GetProperty returns null if the property does not exist.
+        ///// This method is a little safer than that and throws a clear exception if the property does not exist.
+        ///// </summary>
+        //public static PropertyInfo GetPropertyOrException(this Type type, string name)
+        //{
+        //    if (type == null) throw new ArgumentNullException(nameof(type));
+        //    PropertyInfo property = type.GetProperty(name);
+        //    if (property == null) throw new Exception($"Property '{name}' not found on type '{type}'.");
+        //    return property;
+        //}
     }
 }
