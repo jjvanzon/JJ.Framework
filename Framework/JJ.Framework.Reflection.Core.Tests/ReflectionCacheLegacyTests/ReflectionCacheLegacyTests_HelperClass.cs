d43920343d939fb1f17d7785f5f9765dd11889de
jjvanzon <jjvanzon79@gmail.com>
2025-04-30 14:34:37 +0200

    🪩 Reflection.Core.Tests: Copy ReflectionCacheTests from legacy and rename to ReflectionCacheLegacyTests to accompany ReflectionCacheLegacy.

---
+++ Framework/JJ.Framework.Reflection.Core.Tests/ReflectionCacheLegacyTests/ReflectionCacheLegacyTests_HelperClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// ReSharper disable UnusedParameter.Global
+#pragma warning disable IDE0060 // Remove unused parameter
+
+namespace JJ.Framework.Reflection.Core.Tests.ReflectionCacheLegacyTests
+{
+    public static class ReflectionCacheLegacyTests_HelperClass
+    {
+        public static bool Method_OutParameter(out double result)
+            => throw new NotSupportedException();
+
+        public static bool Method_NonRefParameter(int result)
+            => throw new NotSupportedException();
+
+        public static bool Method_WithTypeArguments<T, U>(int a, double b)
+            => throw new NotSupportedException();
+    }
+}
