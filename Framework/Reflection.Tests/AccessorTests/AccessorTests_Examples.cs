f74b22cdca8d0738afa9f382e695fb66ddc448ab
jjvanzon <jjvanzon79@gmail.com>
2025-05-01 22:10:18 +0200

    🧪 Reflection.Tests: Include AccessorTests_Examples copy from main branch. Outcommented tests that don't apply (yet).

---
+++ Framework/Reflection.Tests/AccessorTests/AccessorTests_Examples.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JJ.Framework.Reflection.Tests.AccessorTests
+{
+    [TestClass]
+    public class AccessorTests_Examples
+    {
+        // Helper Classes
+        
+        private class MyClass : TheBaseClass
+        {
+            private string MyProperty { get; set; } = "10";
+            private string MyPrivateMethod(int para) => "20";
+            private string MyPrivateMethod2(int para1, int? para2) => "30";
+        }
+        
+        private static class MyStaticClass;
+
+        private class TheBaseClass
+        {
+            public int MyBaseProperty { get; set; }
+        }
+        
+        private class MyAccessorBase
+        {
+            protected Accessor _accessor = new(new MyClass());
+        }
+        
+        // Method Access
+
+        private class MyAccessor_MethodCall_ByLambda : MyAccessorBase
+        {
+            public string MyPrivateMethod(int para) =>
+                _accessor.InvokeMethod(() => MyPrivateMethod(para));
+        }
+
+        //private class MyAccessor_MethodCall_ByCallerMemberName : MyAccessorBase
+        //{
+        //    public string MyPrivateMethod(int para) =>
+        //        (string)_accessor.InvokeMethod(para);
+        //}
+        
+        private class MyAccessor_MethodCall_ByName : MyAccessorBase
+        {
+            public string MyPrivateMethod(int para) =>
+                (string)_accessor.InvokeMethod("MyPrivateMethod", 10);
+        }
+
+        // Property Access
+        
+        private class MyAccessor_Property_ByLambda : MyAccessorBase
+        {
+            public int MyProperty => _accessor.GetPropertyValue(() => MyProperty);
+        }
+
+        //private class MyAccessor_Property_ByCallerMemberName : MyAccessorBase
+        //{
+        //    public string MyProperty => (string)_accessor.GetPropertyValue();
+        //}
+        
+        //private class MyAccessor_Property_ByCallerMemberName_WithTypeArgument : MyAccessorBase
+        //{
+        //    public string MyProperty => _accessor.GetPropertyValue<string>();
+        //}
+        
+        private class MyAccessor_Property_ByName : MyAccessorBase
+        {
+            public string MyProperty => (string)_accessor.GetPropertyValue("MyProperty");
+        }
+        
+        //private class MyAccessor_Property_ByName_WithTypeArgument : MyAccessorBase
+        //{
+        //    public string MyProperty => _accessor.GetPropertyValue<string>("MyProperty");
+        //}
+        
+        // Construction
+        
+        [TestMethod]
+        public void Accessor_Construction_WithObject_Example()
+        {
+            var myObject = new MyClass();
+            var accessor = new Accessor(myObject);
+        }
+        
+        [TestMethod]
+        public void Accessor_Construction_WithType_Example()
+        {
+            var accessor = new Accessor(typeof(MyStaticClass));
+        }
+        
+        [TestMethod]
+        public void Accessor_Construction_WithObjectAndType_Example()
+        {
+            var myObject = new MyClass();
+            var accessor = new Accessor(myObject, typeof(TheBaseClass));
+        }
+        
+        [TestMethod]
+        public void Accessor_Construction_BaseAndDerived_Example()
+        { 
+            var myObject = new MyClass();
+            var concreteAccessor = new Accessor(myObject);
+            var baseAccessor = new Accessor(myObject, typeof(TheBaseClass));
+        }
+       
+        [TestMethod]
+        public void Accessor_Construction_WithTypeName_Example()
+        {
+            var myObject = new MyClass();
+            var accessor = new Accessor($"{MyNamespace}.{MyPrivateClass}, {MyAssembly}");
+        }
+        
+        string MyNamespace => GetType().Namespace; 
+        string MyPrivateClass => GetType().Name;
+        string MyAssembly => GetType().Assembly.GetName().Name;
+
+        // Wrappers
+
+        //[TestMethod]
+        //public void Accessor_Wrapper_Example()
+        //{
+        //    var accessor = new MyAccessor(new MyClass());
+        //    string myString= accessor.MyPrivateMethod(1);
+        //}
+        
+        //class MyAccessor(MyClass myObject)
+        //{
+        //    Accessor _accessor = new(myObject);
+            
+        //    public string MyPrivateMethod(int para) 
+        //        => (string)_accessor.InvokeMethod(para);
+        //}
+
+        // Overloaded Methods
+
+        //class OverloadAccessor1 : MyAccessorBase
+        //{
+        //    public int MyPrivateMethod(int? myParameter) =>
+        //        (int)_accessor.InvokeMethod( [ myParameter ], [ typeof(int?) ] );
+        //}
+
+        //class OverloadAccessor2 : MyAccessorBase
+        //{
+        //    public int MyPrivateMethod2(int? myParameter1, int myParameter2) =>
+        //        (int)_accessor.InvokeMethod( [ myParameter1, myParameter2 ], [ null, typeof(int) ] );
+        //}
+
+        class OverloadAccessor3 : MyAccessorBase
+        {
+            public string MyPrivateMethod2(object parameter, object parameter2) =>
+                (string)_accessor.InvokeMethod<int, int?>("MyPrivateMethod2", parameter, parameter2);
+        }
+    }
+}
