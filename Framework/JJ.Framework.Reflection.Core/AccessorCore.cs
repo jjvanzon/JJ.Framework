c84829cb72c0b4996cad5b35b4131de6536eb02c
jjvanzon <jjvanzon79@gmail.com>
2025-05-03 03:40:24 +0200

    🧑‍💻 Reflection: AccessorCore - Set OverloadResolutionPriority for Set method that doesn't take CallerMemberName, so explicit passing of 2 parameters should lead to that one.

--- Framework/JJ.Framework.Reflection.Core/AccessorCore.cs
+++ Framework/JJ.Framework.Reflection.Core/AccessorCore.cs
@@ -58,12 +58,18 @@ public class AccessorCore
     }
 
     /// <inheritdoc cref="_nameexpression" />
-    public void Set<T>  (Expression<Func<T>> nameLambda, T value    ) => SetCore(GetName(nameLambda), value);
-    //public void Set<T>  (T       value,   [Caller] string  name = "") => SetCore(name, value);
-    public void Set     (object? value,   [Caller] string  name = "") => SetCore(name, value);
-    //public void Set<T>  (string  name ,            T       value    ) => SetCore(name, value);
-    public void Set     (string  name ,            object? value    ) => SetCore(name, value);
-    private void SetCore(string  name ,            object? value    )
+    public   void Set<T> (Expression<Func<T>> nameLambda, T value    ) => SetCore(GetName(nameLambda), value);
+    public   void Set<T> (T       value,   [Caller] string  name = "") => SetCore(name, value);
+    //public void Set    (object? value,   [Caller] string  name = "") => SetCore(name, value);
+    #if NET9_0_OR_GREATER
+    [OverloadResolutionPriority(1)] 
+    #endif
+    public   void Set<T> (string  name ,            T       value    ) => SetCore(name, value);
+    //#if NET9_0_OR_GREATER
+    //[OverloadResolutionPriority(1)] 
+    //#end if
+    //public void Set    (string  name ,            object? value    ) => SetCore(name, value);
+    private  void SetCore(string  name ,            object? value    )
     {
         foreach (Type type in _types)
         {
