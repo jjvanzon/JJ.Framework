6dc4dcd1b9c65276d0b58798c1daa9aeba145376
jjvanzon <jjvanzon79@gmail.com>
2025-05-07 21:56:38 +0200

    🧪 Reflection: AccessorCore Tests - Finish up Call overloads that use CallerMemberName headed by up to 10 parameters.

--- Framework/JJ.Framework.Reflection.Core.Tests/AccessorCoreTests_Call.cs
+++ Framework/JJ.Framework.Reflection.Core.Tests/AccessorCoreTests_Call.cs
@@ -1,6 +1,8 @@
 ﻿using JJ.Framework.Reflection.Core.Tests.Helpers;
 using static System.TimeSpan;
 using static JJ.Framework.Reflection.Core.Tests.Helpers.FormatHelper;
+// ReSharper disable ReplaceWithSingleCallToCount
+// ReSharper disable RedundantBoolCompare
 
 namespace JJ.Framework.Reflection.Core.Tests;
 
@@ -21,45 +23,80 @@ public class AccessorCoreTests_Call
     }
     
     [TestMethod]
-    public void AccessorCore_Call_ByCallerMemberName()
+    public void AccessorCore_Call_WithCallerMemberName()
     {
         var myClass  = new MyClass();
         var accessor = new MyAccessor(myClass);
 
+        // 0 Args
         {
-            var number = accessor.MyMethod()!;
+            int number = accessor.MyMethod()!;
             AreEqual(1, () => number);
         }
+
+        // 1 Args
         {
-            var text = accessor.MyMethod(2)!;
+            string text = accessor.MyMethod(2)!;
             AreEqual("2", () => text);
         }
+
+        // 2 Args
         {
-            var number = accessor.Add(1, 2)!;
+            long number = accessor.Add(1, 2)!;
             AreEqual(3, () => number);
         }
 
-
-
+        // 3 Args
         {
-            var dateTime = accessor.Add(FromYear(2000), FromDays(1), 10);
-            AreEqual(DateTime.Parse("2000-01-02 10:00"), () => dateTime);
+            DateTime ret = accessor.AddDate(FromYear(2000), FromDays(1), 10);
+            AreEqual(DateTime.Parse("2000-01-02 10:00"), () => ret);
         }
+
+        // 4 Args
         {
-            var concat = accessor.Concat(1, 2, 'A', 3, 4);
+            string concat = accessor.Concat(1, true, 2.0f, 'A');
+            AreEqual("1True2A", () => concat);
+        }
+
+        // 5 Args
+        {
+            string concat = accessor.Concat(1, 2, "A", 3, 4);
             AreEqual("12A34", () => concat);
         }
+
+        // 6 Args
         {
-            var number = accessor.Add(1, 2, 3, 4, true, 5);
+            double number = accessor.Add(1, 2, 3, 4, true, 5);
             AreEqual(16, () => number);
         }
+
+        // 7 Args
         {
-            var number = accessor.AddLongs(1, 2, 3, 4, 5, 6, 7);
+            long number = accessor.AddLongs(1, 2, 3, 4, 5, 6, 7);
             AreEqual(28, () => number);
         }
+
+        // 8 Args
         {
-            var formattedDateTime = accessor.FormatDateTime(1, 2, 3, 4, 5, 6, 7, 'A');
+            string formattedDateTime = accessor.FormatDateTime(1, 2, 3, 4, 5, 6, 7, 'A');
             AreEqual("0001-02-03 04:05:06.007A", () => formattedDateTime);
+        }
+
+        // 9 Args
+        {
+            string text = accessor.FormatSum(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            AreEqual("Sum: 45", () => text);
+        }
+
+        // 9 Args with Void Return
+        {
+            accessor.LogSum(1, 2, 3, 4, 5, 6, 7, 8, 9); 
+        }
+
+        // 10 Args
+        {
+            int count = accessor.CountWhereTrue(true, false, true, true, true, false, true, false, true, false);
+            AreEqual(6, () => count);
         }
 
     }
@@ -69,21 +106,28 @@ public class AccessorCoreTests_Call
     {
         var myClass  = new MyClass();
         var accessor = new MyAccessor(myClass);
+
+        // Start with String
         {
             var concat = accessor.Concat("A", 1, true, 2);
             AreEqual("A1True2", () => concat);
         }
+
+        // Start with 2 Strings
         {
             var concat = accessor.Concat("A", "B", 12, 34);
             AreEqual("AB1234", () => concat);
         }
+
+        // End with String
         {
             var concat = accessor.Concat(1, 2, true, "A");
             AreEqual("12TrueA", () => concat);
         }
+        
+        // End with 2 Strings
         {
             var concat = accessor.Concat(1, 2, "A", "BC");
-
             AreEqual("12ABC", () => concat);
         }
     }
@@ -92,68 +136,126 @@ public class AccessorCoreTests_Call
     {
         private readonly AccessorCore _accessor = new(obj);
 
+        // 0 Args
         public int MyMethod() 
             => (int)_accessor.Call()!;
 
+        // 1 Arg
         public string MyMethod(int arg1) 
             => (string)_accessor.Call(arg1)!;
 
+        // 2 Args
         public long Add(float arg1, float arg2)
             => (long)_accessor.Call(arg1, arg2)!;
 
-        public DateTime Add(DateTime arg1, TimeSpan arg2, double hours) 
+        // 3 Args
+        public DateTime AddDate(DateTime arg1, TimeSpan arg2, double hours) 
             => (DateTime)_accessor.Call(arg1, arg2, hours)!;
 
-        public string FourArgs(int a, bool b, float c, string d) 
+        // 4 Args
+        public string Concat(int a, bool b, float c, char d) 
             => (string)_accessor.Call(a, b, c, d)!;
 
-        public string Concat(decimal arg1, long arg2, char arg3, short arg4, byte arg5) 
+        // 5 Args
+        public string Concat(decimal arg1, long arg2, string arg3, short arg4, byte arg5) 
             => (string)_accessor.Call(arg1, arg2, arg3, arg4, arg5)!;
 
+        // 6 Args
         public double Add(float a, int b, double c, long d, bool e, ushort f) 
             => (double)_accessor.Call(a, b, c, d, e, f)!;
 
+        // 7 Args
         public long AddLongs(long a1, long a2, long a3, long a4, long a5, long a6, long a7) 
             => (long)_accessor.Call(a1, a2, a3, a4, a5, a6, a7)!;
 
+        // 8 Args
         public string FormatDateTime(byte a, short b, int c, long d, float e, double f, decimal g, char h)
             => (string)_accessor.Call(a, b, c, d, e, f, g, h)!;
 
-        public void NineArgs(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9) 
+        // 9 Args
+        public string FormatSum(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9) 
+            => (string)_accessor.Call(a1, a2, a3, a4, a5, a6, a7, a8, a9);
+
+        // Void Return
+        public void LogSum(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9) 
             => _accessor.Call(a1, a2, a3, a4, a5, a6, a7, a8, a9);
 
-        public bool TenArgs(bool a1, bool a2, bool a3, bool a4, bool a5, bool a6, bool a7, bool a8, bool a9, bool a10)
-            => (bool)_accessor.Call(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10)!;
+        // 10 Args
+        public int CountWhereTrue(bool a1, bool a2, bool a3, bool a4, bool a5, bool a6, bool a7, bool a8, bool a9, bool a10)
+            => (int)_accessor.Call(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10)!;
 
 
-        // String Confused with Name: Solved With Collection Expression
+        // Strings at start or end:
+
+        // Interpreted as names. Distinguish them from parameters using square bracket [collection expressions].
+
+        // Start with String
         public string Concat(string arg1, float  arg2, bool   arg3, int    arg4) => (string)_accessor.Call([ arg1, arg2, arg3, arg4 ])!;
+
+        // Start with 2 Strings
         public string Concat(string arg1, string arg2, int    arg3, float  arg4) => (string)_accessor.Call([ arg1, arg2, arg3, arg4 ])!;
+
+        // End with String
         public string Concat(int    arg1, float  arg2, bool   arg3, string arg4) => (string)_accessor.Call([ arg1, arg2, arg3, arg4 ])!;
+
+        // End with 2 Strings
         public string Concat(int    arg1, float  arg2, string arg3, string arg4) => (string)_accessor.Call([ arg1, arg2, arg3, arg4 ])!;
 
     }
     
     private class MyClass
     {
-        private int      MyMethod      ()                                                                                                                    => 1;
-        private string   MyMethod      (int      arg1)                                                                                                       => arg1.ToString();
-        private long     Add           (float    arg1, float    arg2)                                                                                        => (long)(arg1 + arg2);
-        private DateTime Add           (DateTime arg1, TimeSpan arg2, double hours)                                                                          => arg1.Add(arg2).AddHours(hours);
-        private string   FourArgs      (int      a,    bool     b,    float  c,    string d)                                                                 => $"{a}{b}{c}{d}";
-        private string   Concat        (decimal  arg1, long     arg2, char   arg3, short  arg4, byte  arg5)                                                  => $"{arg1}{arg2}{arg3}{arg4}{arg5}";
-        private double   Add           (float    a,    int      b,    double c,    long   d,    bool  e,  ushort f)                                          => a  + b  + c  + d  + (e ? 1 : 0) + f;
-        private long     AddLongs      (long     a1,   long     a2,   long   a3,   long   a4,   long  a5, long   a6, long    a7)                             => a1 + a2 + a3 + a4 + a5 + a6 + a7;
-        private string   FormatDateTime(byte     a,    short    b,    int    c,    long   d,    float e,  double f,  decimal g,  char h)                     => FormatHelper.FormatDateTime(new DateTime(a, b, c, (int)d, (int)e, (int)f, (int)g)) + h;
-        private void     NineArgs      (int      a1,   int      a2,   int    a3,   int    a4,   int   a5, int    a6, int     a7, int  a8, int  a9)           => Console.WriteLine($"Sum: {a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9}");
-        private bool     TenArgs       (bool     a1,   bool     a2,   bool   a3,   bool   a4,   bool  a5, bool   a6, bool    a7, bool a8, bool a9, bool a10) => new[] { a1, a2, a3, a4, a5, a6, a7, a8, a9, a10 }.Count(x => x) >= 5;
+        // 0 Args
+        private int      MyMethod      ()                                                                                                                   => 1;
         
-        // String Confused with Name: Solved With Collection Expression
+        // 1 Arg
+        private string   MyMethod      (int      arg1)                                                                                                      => arg1.ToString();
+        
+        // 2 Args
+        private long     Add           (float    arg1, float    arg2)                                                                                       => (long)(arg1 + arg2);
+        
+        // 3 Args
+        private DateTime AddDate       (DateTime arg1, TimeSpan arg2, double hours)                                                                         => arg1.Add(arg2).AddHours(hours);
+        
+        // 4 Args
+        private string   Concat        (int      a,    bool     b,    float  c,    char  d)                                                                 => $"{a}{b}{c}{d}";
+        
+        // 5 Args
+        private string   Concat        (decimal  arg1, long     arg2, string arg3, short arg4, byte  arg5)                                                  => $"{arg1}{arg2}{arg3}{arg4}{arg5}";
+        
+        // 6 Args
+        private double   Add           (float    a,    int      b,    double c,    long  d,    bool  e,  ushort f)                                          => a  + b  + c  + d  + (e ? 1 : 0) + f;
+        
+        // 7 Args
+        private long     AddLongs      (long     a1,   long     a2,   long   a3,   long  a4,   long  a5, long   a6, long    a7)                             => a1 + a2 + a3 + a4 + a5 + a6 + a7;
+        
+        // 8 Args
+        private string   FormatDateTime(byte     a,    short    b,    int    c,    long  d,    float e,  double f,  decimal g,  char h)                     => FormatHelper.FormatDateTime(new DateTime(a, b, c, (int)d, (int)e, (int)f, (int)g)) + h;
+        
+
+        // 2 methods distinguished by name, while parameter types are same:
+
+        // 9 Args 
+        private string   FormatSum     (int      a1,   int      a2,   int    a3,   int   a4,   int   a5, int    a6, int     a7, int  a8, int  a9)           => $"Sum: {a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9}";
+        
+        // 8 Args with Void Return
+        private void     LogSum        (int      a1,   int      a2,   int    a3,   int   a4,   int   a5, int    a6, int     a7, int  a8, int  a9)           => Console.WriteLine(FormatSum(a1, a2, a3, a4, a5, a6, a7, a8, a9));
+        
+        // 10 Args
+        private int      CountWhereTrue(bool     a1,   bool     a2,   bool   a3,   bool  a4,   bool  a5, bool   a6, bool    a7, bool a8, bool a9, bool a10) => new[] { a1, a2, a3, a4, a5, a6, a7, a8, a9, a10 }.Where(x => x == true).Count();
+        
+        // Strings at start or end:
+
+        // Start with String
         private string Concat(string arg1, float  arg2, bool   arg3, int    arg4) => $"{arg1}{arg2}{arg3}{arg4}";
+
+        // Start with 2 Strings
         private string Concat(string arg1, string arg2, int    arg3, float  arg4) => $"{arg1}{arg2}{arg3}{arg4}";
+
+        // End with String
         private string Concat(int    arg1, float  arg2, bool   arg3, string arg4) => $"{arg1}{arg2}{arg3}{arg4}";
+
+        // End with 2 Strings
         private string Concat(int    arg1, float  arg2, string arg3, string arg4) => $"{arg1}{arg2}{arg3}{arg4}";
-        
-        // TODO: 2 methods distinguish only by name, while parameter types are the same.
     }
 }
