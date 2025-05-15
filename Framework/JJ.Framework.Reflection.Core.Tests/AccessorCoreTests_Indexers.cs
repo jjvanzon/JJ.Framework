aa6fdc3f51bb5b002b95b131c3b0a99c61c920bd
jjvanzon <jjvanzon79@gmail.com>
2025-05-15 19:24:26 +0200

    🧪 Reflection: AccessorCore Indexer Tests – Enabled nullable reference types; extended null and collection cases. Added TODOs for further scenarios.

--- Framework/JJ.Framework.Reflection.Core.Tests/AccessorCoreTests_Indexers.cs
+++ Framework/JJ.Framework.Reflection.Core.Tests/AccessorCoreTests_Indexers.cs
@@ -9,23 +9,31 @@ public class AccessorCoreTests_Indexers
     public void AccessorCore_Indexer_RawAccessor()
     {
         var obj = new MyClass();
-        var accessorRaw = new AccessorCore(obj);
+        var accessor = new AccessorCore(obj);
         
         // Get with indexer 
-        var number = (int?)accessorRaw[1];
+        var number = (int?)accessor[1];
         AreEqual(1, () => number);
         
         // Set with indexer 
-        accessorRaw[1] = 2;
-        AreEqual(2, accessorRaw[1]);
+        accessor[1] = 2;
+        AreEqual(2, accessor[1]);
 
         // Get with method
-        number = (int?)accessorRaw.Get(3);
+        number = (int?)accessor.Get(3);
         AreEqual(3, () => number);
 
         // Set with method
-        accessorRaw.Set(3, 4);
-        AreEqual(4, accessorRaw[3]);
+        accessor.Set(3, 4);
+        AreEqual(4, accessor[3]);
+
+        // Get with collection
+        number = (int?)accessor.Get([4]);
+        AreEqual(4, () => number);
+
+        // Set with collection
+        accessor.Set([4], 5);
+        AreEqual(5, accessor[4]);
     }
     
     [TestMethod]
@@ -86,7 +94,7 @@ public class AccessorCoreTests_Indexers
     {
         private readonly AccessorCore _accessor = new(obj);
         
-        public int this[object i]
+        public int this[object? i]
         {
             get => (int)_accessor[i]!;
             set => _accessor[i] = value;
@@ -97,7 +105,7 @@ public class AccessorCoreTests_Indexers
     {
         private Dictionary<object, int> _numberDic = CreateNumberDic();
         
-        private int this[object i]
+        private int this[object? i]
         {
             get => _numberDic[i ?? 0];
             set => _numberDic[i ?? 0] = value;
@@ -105,7 +113,7 @@ public class AccessorCoreTests_Indexers
     }
     
     // With Overloads
-                
+    
     [TestMethod]
     public void AccessorCore_Indexer_Overloads_NotFound()
     {
@@ -113,27 +121,66 @@ public class AccessorCoreTests_Indexers
         var accessor = new MyAccessor_WithOverloads(obj);
         
         ThrowsException(
-            () => { int number = accessor[null]; },
+            () => { int number = accessor [ null]; },
             "No indexer found with argument types [Object].");
     }
-                
+
     [TestMethod]
     public void AccessorCore_Indexer_Overload_ResolvedFromStackFrame()
     {
         var obj = new MyClass_WithOverloads();
         var accessor = new MyAccessor_WithOverloads(obj);
-        int? nully = null;
-        
-        string text = accessor[2, nully];
-        
-        AreEqual("", () => text);
+
+        // With null int
+        {
+            int? nullNum = null;
+            string text = accessor[2, nullNum];
+            AreEqual("", () => text);
+        }
+        // With nullable int
+        {
+            int? nullyNum = 3;
+            string text = accessor[4, nullyNum];
+            AreEqual("120", () => text);
+        }
+        // With int
+        {
+            string text = accessor[5, 6];
+            AreEqual("300", () => text);
+        }
+        // With null string
+        {
+            #pragma warning disable CS8604 // null ref arg
+            
+            string? input = null;
+            string text = accessor[7, input];
+            AreEqual("70", () => text);
+            
+            #pragma warning restore CS8604 // null ref arg
+        }
+        // With nullable string
+        {
+            // ReSharper disable once VariableCanBeNotNullable
+            string? input = "test";
+            string text = accessor[9, input];
+            AreEqual("90test", () => text);
+        }
+        // With string
+        {
+            string text = accessor[10, "test"];
+            AreEqual("100test", () => text);
+        }
     }
+                    
+    // TODO: Resolved with Type Args
+    
+    // TODO: Test the setters
 
     private class MyAccessor_WithOverloads(MyClass_WithOverloads obj)
     {
         private readonly AccessorCore _accessor = new(obj);
         
-        public int this[object i]
+        public int this[object? i]
         {
             get => (int)_accessor[i]!;
             set => _accessor[i] = value;
@@ -145,11 +192,11 @@ public class AccessorCoreTests_Indexers
             set => _accessor[para1, para2] = value;
         }
         
-        //public string this[int para1, string para2]
-        //{
-        //    get => (string)_accessor[para1, para2]!;
-        //    set => _accessor[para1, para2] = value;
-        //}
+        public string this[int para1, string? para2]
+        {
+            get => (string)_accessor[para1, para2]!;
+            set => _accessor[para1, para2] = value;
+        }
     }
 
     private class MyClass_WithOverloads
@@ -164,16 +211,18 @@ public class AccessorCoreTests_Indexers
 
         private string this[string i]
         {
-            get => default;
+            get => "";
             set { }
         }
 
-        private string this[int para1, int? para2] => (para1 * para2 * 10).ToString();
-        private string this[int para1, string para2] => $"{para1 * 10}{para2}";
+        private string this[int para1, int? para2] => $"{10 * para1 * para2}";
+        private string this[int para1, string? para2] => $"{para1 * 10}{para2}";
     }
 
     // Helpers
 
+    // TODO: Use inverse digits instead, for variance between index and value.
+    
     static Dictionary<object, int> CreateNumberDic() => new()
     {
         [0] = Int32.MaxValue,
