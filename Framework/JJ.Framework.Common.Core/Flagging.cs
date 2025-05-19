1be7a47086ff2affd02bdc037ab0a3866ef10530
jjvanzon <jjvanzon79@gmail.com>
2025-05-20 00:35:40 +0200

    🚩 Common.Core: Add Flag helpers, since I keep forgetting and wrongly applying bit operations.

---
+++ Framework/JJ.Framework.Common.Core/Flagging.cs
@@ -0,0 +1,21 @@
+﻿namespace JJ.Framework.Common.Core;
+
+public static class FlagHelper
+{
+    public static TEnum SetFlag<TEnum>(TEnum value, TEnum flag) where TEnum : Enum
+        => (TEnum)(object)SetFlag((int)(object)value, (int)(object)flag);
+    
+    public static TEnum ClearFlag<TEnum>(TEnum value, TEnum flag) where TEnum : Enum
+        => (TEnum)(object)ClearFlag((int)(object)value, (int)(object)flag);
+
+    public static int SetFlag(int value, int flag) => value | flag;
+    public static int ClearFlag(int value, int flag) => value & ~flag;
+}
+
+public static class FlagExtensions
+{
+    public static TEnum SetFlag<TEnum>(this TEnum value, TEnum flag) where TEnum : Enum => FlagHelper.SetFlag(value, flag);
+    public static TEnum ClearFlag<TEnum>(this TEnum value, TEnum flag) where TEnum : Enum => FlagHelper.ClearFlag(value, flag);
+    public static int SetFlag(this int value, int flag) => FlagHelper.SetFlag(value, flag);
+    public static int ClearFlag(this int value, int flag) => FlagHelper.ClearFlag(value, flag);
+}
