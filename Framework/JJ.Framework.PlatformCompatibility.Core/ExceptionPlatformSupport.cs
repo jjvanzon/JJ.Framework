011b9f16990bd4532a38c8afcc4a6dc99cf88034
jjvanzon <jjvanzon79@gmail.com>
2025-05-20 15:19:01 +0200

    🎯 DevOps: Targeted netstandard2.0 and 2.1 in test projects to ensure compatibility constraints are caught during build. Configured NCrunch to ignore these targets, as they are compiled but not individually runnable.

--- Framework/JJ.Framework.PlatformCompatibility.Core/ExceptionPlatformSupport.cs
+++ Framework/JJ.Framework.PlatformCompatibility.Core/ExceptionPlatformSupport.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace JJ.Framework.PlatformCompatibility.Core;
+﻿namespace JJ.Framework.PlatformCompatibility.Core;
 
 public static class ExceptionPlatformSupport
 {
@@ -12,9 +10,11 @@ public static class ExceptionPlatformSupport
 
     public static void ThrowIfNullOrWhiteSpace(string? argument, [CallerArgumentExpression(nameof(argument))] string expression = "")
     {
-        if (!string.IsNullOrWhiteSpace(argument)) return;
-        if (string.IsNullOrWhiteSpace(expression)) expression = nameof(argument);
-        throw new ArgumentException($"{expression} is null or white space.");
+        if (string.IsNullOrWhiteSpace(argument))
+        {
+            if (string.IsNullOrWhiteSpace(expression)) expression = nameof(argument);
+            throw new ArgumentException($"{expression} is null or white space.");
+        }
     }
     #endif
 }
