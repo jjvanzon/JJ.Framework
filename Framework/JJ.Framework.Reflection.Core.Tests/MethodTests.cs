2c81bbb213a7e500a2e4f6c38bc725130d5d9858
jjvanzon <jjvanzon79@gmail.com>
2025-05-24 03:53:22 +0200

    🧪 Reflection.Core: Method - Expand tests of not found scenarios.

--- Framework/JJ.Framework.Reflection.Core.Tests/MethodTests.cs
+++ Framework/JJ.Framework.Reflection.Core.Tests/MethodTests.cs
@@ -27,19 +27,94 @@ public class MethodTests : ReflectorTestBase
             AssertMethod0 (      reflect  .Method("MyType",  "Method0"));
             AssertMethod0 (      reflect  .Method <MyType>(  "Method0"));
         }
-        // Not Found
+        
+        // TODO: This could inspire many more supported parameter orders.
+        //AssertMethod0("Method0".Method <MyType>()); 
+    }
+
+    [TestMethod]
+    public void Method_NotFound()
+    {
+        {
+            IsNull        (                Method(_myObject, "NoName", null           ));
+            IsNull        (                Method(_myObject, "NoName", nullable       ));
+            IsNull        (                Method(_myObject, "NoName", nullable: true ));
+            ThrowsNotFound(() =>           Method(_myObject, "NoName", nullable: false));
+            ThrowsNotFound(() =>           Method(_myObject, "NoName"                 ));
+            IsNull        (                Method(_myType,   "NoName", null           ));
+            IsNull        (                Method(_myType,   "NoName", nullable       ));
+            IsNull        (                Method(_myType,   "NoName", nullable: true ));
+            ThrowsNotFound(() =>           Method(_myType,   "NoName", nullable: false));
+            ThrowsNotFound(() =>           Method(_myType,   "NoName"                 ));
+            IsNull        (                Method("MyType",  "NoName", null           ));
+            IsNull        (                Method("MyType",  "NoName", nullable       ));
+            IsNull        (                Method("MyType",  "NoName", nullable: true ));
+            ThrowsNotFound(() =>           Method("MyType",  "NoName", nullable: false));
+            ThrowsNotFound(() =>           Method("MyType",  "NoName"                 ));
+            IsNull        (                Method <MyType>(  "NoName", null           ));
+            IsNull        (                Method <MyType>(  "NoName", nullable       ));
+            IsNull        (                Method <MyType>(  "NoName", nullable: true ));
+            ThrowsNotFound(() =>           Method <MyType>(  "NoName", nullable: false));
+            ThrowsNotFound(() =>           Method <MyType>(  "NoName"                 ));
+            IsNull        (      _myObject.Method(           "NoName", null           ));
+            IsNull        (      _myObject.Method(           "NoName", nullable       ));
+            IsNull        (      _myObject.Method(           "NoName", nullable: true ));
+            ThrowsNotFound(() => _myObject.Method(           "NoName", nullable: false));
+            ThrowsNotFound(() => _myObject.Method(           "NoName"                 ));
+            IsNull        (      _myType  .Method(           "NoName", null           ));
+            IsNull        (      _myType  .Method(           "NoName", nullable       ));
+            IsNull        (      _myType  .Method(           "NoName", nullable: true ));
+            ThrowsNotFound(() => _myType  .Method(           "NoName", nullable: false));
+            ThrowsNotFound(() => _myType  .Method(           "NoName"                 ));
+            IsNull        (      "MyType" .Method(           "NoName", null           ));
+            IsNull        (      "MyType" .Method(           "NoName", nullable       ));
+            IsNull        (      "MyType" .Method(           "NoName", nullable: true ));
+            ThrowsNotFound(() => "MyType" .Method(           "NoName", nullable: false));
+            ThrowsNotFound(() => "MyType" .Method(           "NoName"                 ));
+            IsNull        (      Reflect.  Method(_myObject, "NoName", null           ));
+            IsNull        (      Reflect.  Method(_myObject, "NoName", nullable       ));
+            IsNull        (      Reflect.  Method(_myObject, "NoName", nullable: true ));
+            ThrowsNotFound(() => Reflect.  Method(_myObject, "NoName", nullable: false));
+            ThrowsNotFound(() => Reflect.  Method(_myObject, "NoName"                 ));
+            IsNull        (      Reflect.  Method(_myType,   "NoName", null           ));
+            IsNull        (      Reflect.  Method(_myType,   "NoName", nullable       ));
+            IsNull        (      Reflect.  Method(_myType,   "NoName", nullable: true ));
+            ThrowsNotFound(() => Reflect.  Method(_myType,   "NoName", nullable: false));
+            ThrowsNotFound(() => Reflect.  Method(_myType,   "NoName"                 ));
+            IsNull        (      Reflect.  Method("MyType",  "NoName", null           ));
+            IsNull        (      Reflect.  Method("MyType",  "NoName", nullable       ));
+            IsNull        (      Reflect.  Method("MyType",  "NoName", nullable: true ));
+            ThrowsNotFound(() => Reflect.  Method("MyType",  "NoName", nullable: false));
+            ThrowsNotFound(() => Reflect.  Method("MyType",  "NoName"                 ));
+            IsNull        (      Reflect.  Method <MyType>(  "NoName", null           ));
+            IsNull        (      Reflect.  Method <MyType>(  "NoName", nullable       ));
+            IsNull        (      Reflect.  Method <MyType>(  "NoName", nullable: true ));
+            ThrowsNotFound(() => Reflect.  Method <MyType>(  "NoName", nullable: false));
+            ThrowsNotFound(() => Reflect.  Method <MyType>(  "NoName"                 ));
+        }
         foreach (var reflect in _reflectors)
         {
+            IsNull        (      reflect  .Method(_myObject, "NoName", null           ));
+            IsNull        (      reflect  .Method(_myObject, "NoName", nullable       ));
+            IsNull        (      reflect  .Method(_myObject, "NoName", nullable: true ));
+            ThrowsNotFound(() => reflect  .Method(_myObject, "NoName", nullable: false));
+            ThrowsNotFound(() => reflect  .Method(_myObject, "NoName"                 ));
             IsNull        (      reflect  .Method(_myType,   "NoName", null           ));
             IsNull        (      reflect  .Method(_myType,   "NoName", nullable       ));
             IsNull        (      reflect  .Method(_myType,   "NoName", nullable: true ));
             ThrowsNotFound(() => reflect  .Method(_myType,   "NoName", nullable: false));
             ThrowsNotFound(() => reflect  .Method(_myType,   "NoName"                 ));
-
+            IsNull        (      reflect  .Method("MyType",  "NoName", null           ));
+            IsNull        (      reflect  .Method("MyType",  "NoName", nullable       ));
+            IsNull        (      reflect  .Method("MyType",  "NoName", nullable: true ));
+            ThrowsNotFound(() => reflect  .Method("MyType",  "NoName", nullable: false));
+            ThrowsNotFound(() => reflect  .Method("MyType",  "NoName"                 ));
+            IsNull        (      reflect  .Method <MyType>(  "NoName", null           ));
+            IsNull        (      reflect  .Method <MyType>(  "NoName", nullable       ));
+            IsNull        (      reflect  .Method <MyType>(  "NoName", nullable: true ));
+            ThrowsNotFound(() => reflect  .Method <MyType>(  "NoName", nullable: false));
+            ThrowsNotFound(() => reflect  .Method <MyType>(  "NoName"                 ));
         }
-        
-        // TODO: This could inspire many more supported parameter orders.
-        //AssertMethod0("Method0".Method <MyType>()); 
     }
     
     [TestMethod]
