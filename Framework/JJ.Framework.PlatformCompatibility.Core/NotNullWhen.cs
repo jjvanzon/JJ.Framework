ceb7ceca57815c41c3dbe69542f74c3801c30725
jjvanzon <jjvanzon79@gmail.com>
2025-05-24 20:04:15 +0200

    💁 PlatformCompatibility.Core: NotNullWhen stub for pre-.NET5 code to compile correctly.

---
+++ Framework/JJ.Framework.PlatformCompatibility.Core/NotNullWhen.cs
@@ -0,0 +1,8 @@
+﻿namespace System.Runtime.CompilerServices;
+
+#if NETSTANDARD2_0
+public sealed class NotNullWhenAttribute(bool returnValue) : Attribute
+{
+    public bool ReturnValue { get; } = returnValue;
+}
+#endif
