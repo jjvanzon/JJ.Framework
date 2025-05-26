17831b31d9a922beb5cee350816223adc11a91da
jjvanzon <jjvanzon79@gmail.com>
2025-05-27 01:14:12 +0200

    ❕ Testing.Core: Make IsType and NotType assertions workable. Mark unworkables as obsolete, keeping them as private overloads so the chance of adding them again by accident becomes smaller.

--- Framework/JJ.Framework.Testing.Core/AssertHelperCore.Type.cs
+++ Framework/JJ.Framework.Testing.Core/AssertHelperCore.Type.cs
@@ -1,16 +1,52 @@
-﻿namespace JJ.Framework.Testing.Core;
+﻿
+namespace JJ.Framework.Testing.Core;
 
 public static partial class AssertHelperCore
 {
-    public static void IsType<T>(object? actualObject, [ArgExpress(nameof(actualObject))] string message = null) 
-        => IsType(typeof(T), actualObject, message);
+    public static void IsType<TValue>(Type expected, TValue value, [ArgExpress(nameof(value))] string message = "")
+    {
+        var actual = CompileTimeType(value);
+        IsType(expected, actual, message);
+    }
     
-    public static void IsType(Type expectedType, object actualObject, [ArgExpress(nameof(actualObject))] string message = "") 
-        => Check(expectedType, message, () => CompileTimeType(actualObject) == expectedType);
+    private static void IsType(Type expected, Type actual, [ArgExpress(nameof(actual))] string message = "") 
+        => Check(expected, actual, message, () => expected == actual);
     
-    public static void IsType<T>(T actualValue, [ArgExpress(nameof(actualValue))] string message = "") 
-        => IsType(typeof(T), actualValue, message);
+    public static void NotType<TValue>(Type expected, TValue value, [ArgExpress(nameof(value))] string message = "")
+    {
+        var actual = CompileTimeType(value);
+        NotType(expected, actual, message);
+    }
     
-    private static void IsType<T>(Type expectedType, T actualValue, string message) 
-        => Check(expectedType, message, () => CompileTimeType(actualValue) == expectedType);
+    public static void NotType(Type expected, Type actual, [ArgExpress(nameof(actual))] string message = "")
+    {
+        Check(expected, actual, message, () => expected != actual);
+    }
+    
+    #region Obsolete
+
+    // ReSharper disable UnusedTypeParameter
+    // ReSharper disable UnusedParameter.Local
+    // ReSharper disable EntityNameCapturedOnly.Local
+
+    private const string ObsoleteTExpectedArg 
+        = "Overload unworkable. Here the argument is always the expected type, and a null/not-null difference cannot be evaluated.";
+        
+    private const string ObsoleteObjectArg
+        = "Overload unworkable. Here the argument is object? and the return type info from the argument is lost.";
+
+    [Obsolete(ObsoleteTExpectedArg, true)]
+    private static void IsType<TExpected>(TExpected value, [ArgExpress(nameof(value))] string message = "") 
+        => throw new NotSupportedException(ObsoleteTExpectedArg);
+    
+    [Obsolete(ObsoleteTExpectedArg, true)]
+    private static void NotType<TExpected>(TExpected value, [ArgExpress(nameof(value))] string message = "") 
+        => throw new NotSupportedException(ObsoleteTExpectedArg);
+        
+    [Obsolete(ObsoleteObjectArg, true)]
+    private static void NotType<TExpected>(object? value, [ArgExpress(nameof(value))] string message = "") 
+        => throw new NotSupportedException(ObsoleteObjectArg);
+    
+    #endregion
+        
 }
