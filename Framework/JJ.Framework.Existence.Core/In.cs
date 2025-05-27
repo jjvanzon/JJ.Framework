edcd1178a1ce9323e31d3fd9c4cec0cefae4415e
jjvanzon <jjvanzon79@gmail.com>
2025-05-27 18:54:12 +0200

    🍌 Existence.Core: Split into a file per concept.

---
+++ Framework/JJ.Framework.Existence.Core/In.cs
@@ -0,0 +1,31 @@
+﻿namespace JJ.Framework.Existence.Core;
+
+public static partial class FilledInHelper
+{
+    public static bool In   (     string? value,                  params IEnumerable<string?>? coll                 ) =>                    coll?.Contains(value, ignoreCase: true) ?? false;
+    public static bool In   (     string? value, bool ignoreCase, params IEnumerable<string?>? coll                 ) =>                    coll?.Contains(value, ignoreCase      ) ?? false;
+    public static bool In   (     string? value,                         IEnumerable<string?>? coll, bool ignoreCase) =>                    coll?.Contains(value, ignoreCase      ) ?? false;
+    public static bool In<T>(     T       value,                  params IEnumerable<T>?       coll)                  =>                    coll?.Contains(value                  ) ?? false; 
+    public static bool In<T>(     T       value,                  params IEnumerable<T?>?      coll) where T : struct =>                    coll?.Contains(value                  ) ?? false;
+    public static bool In<T>(     T?      value,                  params IEnumerable<T>?       coll) where T : struct => value.HasValue && (coll?.Contains(value.Value            ) ?? false);
+    public static bool In<T>(     T?      value,                  params IEnumerable<T?>?      coll) where T : struct =>                    coll?.Contains(value                  ) ?? false;
+    
+    public static bool Contains(IEnumerable<string> source, string match, bool ignoreCase = false)
+    {
+        ThrowIfNull(source);
+        return source.Any(x => (x ?? "").Equals(match, ignoreCase.ToStringComparison()));
+    }
+}
+
+public static partial class FilledInExtensions
+{
+    public static bool In   (this string? value,                  params IEnumerable<string?>? coll                 ) => FilledInHelper.In(value, coll);
+    public static bool In   (this string? value, bool ignoreCase, params IEnumerable<string?>? coll                 ) => FilledInHelper.In(value, ignoreCase, coll);
+    public static bool In   (this string? value,                         IEnumerable<string?>? coll, bool ignoreCase) => FilledInHelper.In(value, coll, ignoreCase);
+    public static bool In<T>(this T       value,                  params IEnumerable<T>?       coll)                  => FilledInHelper.In(value, coll);
+    public static bool In<T>(this T       value,                  params IEnumerable<T?>?      coll) where T : struct => FilledInHelper.In(value, coll);
+    public static bool In<T>(this T?      value,                  params IEnumerable<T>?       coll) where T : struct => FilledInHelper.In(value, coll);
+    public static bool In<T>(this T?      value,                  params IEnumerable<T?>?      coll) where T : struct => FilledInHelper.In(value, coll);
+    
+    public static bool Contains(this IEnumerable<string> source, string match, bool ignoreCase = false) => FilledInHelper.Contains(source, match, ignoreCase);
+}
---
+++ Framework/JJ.Framework.Existence.Core/Is.cs
@@ -0,0 +1,29 @@
+﻿namespace JJ.Framework.Existence.Core;
+
+public static partial class FilledInHelper
+{
+    public static bool Is(string? a, string? b) => Is(a, b, ignoreCase: true);
+    public static bool Is(string? a, string? b, bool ignoreCase)
+    {
+        if (a == b) return true;
+        
+        StringComparison compare = ignoreCase.ToStringComparison();
+        
+        a = (a ?? "").Trim(); b = (b ?? "").Trim();
+        if (string.Equals(a, b, compare)) return true;
+        
+        a = a.RemoveExcessiveWhiteSpace();
+        b = b.RemoveExcessiveWhiteSpace();
+        if (string.Equals(a, b, compare)) return true;
+        
+        a = a.RemoveAccents();
+        b = b.RemoveAccents();
+        return string.Equals(a, b, compare);
+    }
+}
+
+public static partial class FilledInExtensions
+{
+    public static bool Is(this string? value, string? comparison                 ) => FilledInHelper.Is(value, comparison       );
+    public static bool Is(this string? value, string? comparison, bool ignoreCase) => FilledInHelper.Is(value, comparison, ignoreCase);
+}
