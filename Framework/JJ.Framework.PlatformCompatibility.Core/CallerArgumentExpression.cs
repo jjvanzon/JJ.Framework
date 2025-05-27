0c13f3c62e8af17fabe32669eda14b69bfcc211c
jjvanzon <jjvanzon79@gmail.com>
2025-05-27 19:57:03 +0200

    ⚠️ PlatformCompatibility.Core: (Nullable) warnings as errors, but not missing doc.

--- Framework/JJ.Framework.PlatformCompatibility.Core/CallerArgumentExpression.cs
+++ Framework/JJ.Framework.PlatformCompatibility.Core/CallerArgumentExpression.cs
@@ -1,5 +1,7 @@
 ﻿namespace System.Runtime.CompilerServices;
 
+#pragma warning disable CS9113 // Parameter is unread.
+
 #if NETSTANDARD2_0 || NETSTANDARD2_1
 public class CallerArgumentExpressionAttribute(string parameterName) : Attribute;
 #endif
