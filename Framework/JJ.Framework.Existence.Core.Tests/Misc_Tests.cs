5f98011275a11b330f6cf081adacff668a07f015
jjvanzon <jjvanzon79@gmail.com>
2025-06-06 00:52:05 +0200

    🧪 Existence.Core Tests: Small test checking of NotNullWhen is correctly applied.

---
+++ Framework/JJ.Framework.Existence.Core.Tests/Misc_Tests.cs
@@ -0,0 +1,25 @@
+﻿// ReSharper disable ReturnTypeCanBeNotNullable
+namespace JJ.Framework.Existence.Core.Tests;
+
+[TestClass]
+public class Misc_Tests
+{
+    [TestMethod]
+    public void Test_NotNullWhen_Correctly_Applied()
+    {
+        string? text = GetText();
+
+        // Compiler says no.
+        //int lengthNo = text.Length;
+
+        if (Has(text))
+        {
+            // Compiler says yes.
+            int length = text.Length;
+
+            AreEqual(3, length);
+        }
+    }
+
+    private string? GetText() => "Hi!";
+}
