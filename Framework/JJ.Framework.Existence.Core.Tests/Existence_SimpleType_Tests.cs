7fbde7a192e00973de81b35406d5c34593a0fbe1
jjvanzon <jjvanzon79@gmail.com>
2025-06-05 18:21:41 +0200

    🧫 Existence.Core Tests: Added char tests revealing many non-characters are still treated as something rather than nothing.

--- Framework/JJ.Framework.Existence.Core.Tests/Existence_SimpleType_Tests.cs
+++ Framework/JJ.Framework.Existence.Core.Tests/Existence_SimpleType_Tests.cs
@@ -9,8 +9,7 @@ public class Existence_SimpleType_Tests
 {
     // TODO:
     // Char, Byte, IntPtr, UIntPtr
-    // the numeric types, their the signed and unsigned variations and
-    // DateTime, TimeSpan
+    // the numeric types, their the signed and unsigned variations and TimeSpan.
     // Newer .NETs probably have more.
     // But let's start with a basic spread:
     // - [x] Enum/Enum?
@@ -18,8 +17,8 @@ public class Existence_SimpleType_Tests
     // - [x] double
     // - [x] Guid
     // - [x] DateTime
-
-    // Bools
+    // - [ ] .. Char
+    // - [ ] Decimal
 
     bool True       = true ;
     bool  False      = false;
@@ -272,6 +271,8 @@ public class Existence_SimpleType_Tests
         NoNullRet(OneGuid, Coalesce(NullGuid, EmptyGuid).Coalesce(ZeroGuid, OneGuid, RandomGuid));
     }
 
+    // DateTime
+
     DateTime? NullDateTime         = null;
     DateTime  DefaultDateTime      = default;
     DateTime? DefaultNullyDateTime = default;
@@ -304,4 +305,65 @@ public class Existence_SimpleType_Tests
         IsTrue (Has(SomeNullyDateTime));
         NoNullRet(SomeDateTime, Coalesce(NewNullyDateTime, NewDateTime, SomeNullyDateTime, NowNullyDateTime, SomeDateTime));
     }
+
+    // Char
+
+    char? NullChar              = null;
+    char  DefaultChar           = default;
+    char? DefaultNullyChar      = default;
+    char  NewChar               = new();
+    char? NewNullyChar          = new();
+    char  ZeroValueChar         = '\0';
+    char? ZeroValueNullyChar    = '\0';
+    char  ZeroDigitChar         = '0';
+    char? ZeroDigitNullyChar    = '0';
+    char  SpaceChar             = ' ';
+    char? NullySpaceChar        = '\t';
+    char  ControlChar           = '\u0001';
+    char? NullyControlChar      = '\u0001';
+    char  SpecialSpace          = '\u2003';
+    char? NullySpecialSpace     = '\u2003';
+    char  ZeroWidthSpace        = '\u200B';
+    char? NullyZeroWidthSpace   = '\u200B';
+    char  NonBreakingSpace      = '\u00A0';
+    char? NullyNonBreakingSpace = '\u00A0';
+    char  HighSurrogate         = '\uD83D'; // Special char used in UTF-16 to help encode values outside the UTF-16 range.
+    char? NullyHighSurrogate    = '\uD83D'; // Special char used in UTF-16 to help encode values outside the UTF-16 range.
+    char  FilledChar             = 'a';
+    char? NullyFilledChar        = 'a';
+
+    [TestMethod]
+    public void Char_Existence_Tests()
+    {
+        // Abviously not filled
+        IsFalse(Has(NullChar));
+        IsFalse(Has(DefaultChar));
+        IsFalse(Has(DefaultNullyChar));
+        IsFalse(Has(NewChar));
+        IsFalse(Has(NewNullyChar));
+        IsFalse(Has(ZeroValueChar));
+        IsFalse(Has(ZeroValueNullyChar));
+
+        // Filled
+        IsTrue (Has(ZeroDigitChar));
+        IsTrue (Has(ZeroDigitNullyChar));
+        IsTrue (Has(FilledChar));
+        IsTrue (Has(NullyFilledChar));
+        NoNullRet('a', Coalesce(NullChar, ZeroValueChar, NullyFilledChar, 'b'));
+
+        // TODO: These should be false
+        IsTrue (Has(SpaceChar));
+        IsTrue (Has(NullySpaceChar));
+        IsTrue (Has(ControlChar));
+        IsTrue (Has(NullyControlChar));
+        IsTrue (Has(SpecialSpace));
+        IsTrue (Has(NullySpecialSpace));
+        IsTrue (Has(ZeroWidthSpace));
+        IsTrue (Has(NullyZeroWidthSpace));
+        IsTrue (Has(NonBreakingSpace));
+        IsTrue (Has(NullyNonBreakingSpace));
+        IsTrue (Has(HighSurrogate));
+        IsTrue (Has(NullyHighSurrogate));
+    }
 }
+
