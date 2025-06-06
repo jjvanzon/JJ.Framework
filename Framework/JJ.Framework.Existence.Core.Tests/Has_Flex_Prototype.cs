238ea2f6da070eaee27f113ce6af82fe90c7ec34
jjvanzon <jjvanzon79@gmail.com>
2025-06-06 22:20:31 +0200

    💪 Existence.Core: Refine `flex` flag prototype.

--- Framework/JJ.Framework.Existence.Core.Tests/Has_Flex_Prototype.cs
+++ Framework/JJ.Framework.Existence.Core.Tests/Has_Flex_Prototype.cs
@@ -1,18 +1,46 @@
-﻿using static JJ.Framework.Existence.Core.Tests.Has_Dynamic_Tests.FlexFlag;
+﻿using static JJ.Framework.Existence.Core.Tests.FlexFlag;
+using static JJ.Framework.Existence.Core.Tests.FilledInPrototype;
 
 namespace JJ.Framework.Existence.Core.Tests;
 
 [TestClass]
 public class Has_Dynamic_Tests
 {
-    // Demonstration
+    // Example use
     
     [TestMethod]
     public void Test_Has_Dynamic()
     {
-        object text = " ";
-        IsFalse(Has(text, flex));
+        // Problem: Object parameter isn't treated as text,
+        // so white space isn't treated as empty.
+
+        // Normal case
+        {
+            string text = " "; // type `string`
+            bool filled = Has(text);
+            // " " is considered not filled in, because Has uses parameter type string here.
+            IsFalse(filled);
+        }
+
+        // Problem case
+        {
+            object text = " "; // type `object`
+            bool filled = Has(text);
+            // " " is considered filled, because of parameter type object.
+            IsTrue(filled);
+        }
         
+        // Solution
+        {
+            object text = " ";
+            bool filled = Has(text, flex); // `flex` flag
+            // " " = not filled in here.
+            // Even though parameter typer is object,
+            // it's still recognized as text,
+            // by (slower) run-time inspection.
+            IsFalse(filled);
+        }
+
         // Former alternatives:
         //IsFalse(Has(text, dynamic));
         //IsFalse(Has(text, deep));
@@ -23,28 +51,34 @@ public class Has_Dynamic_Tests
         //IsFalse(Has(text, reflect));
     }
 
-    // Proposed Implementation
+}
 
-    public enum FlexFlag
-    {
-        flex = 1, synonym
-        
-        // Former alternatives:
-        //dynamic,
-        //deep,
-        //thorough,
-        //concrete,
-        //force,
-        //dyn,
-        //reflect,
-    }
+// Proposed Implementation
+
+internal enum FlexFlag
+{
+    flex = 1,
     
+    // Former alternatives:
+    //dynamic,
+    //deep,
+    //thorough,
+    //concrete,
+    //force,
+    //dyn,
+    //reflect,
+}
+
+internal static class FilledInPrototype
+{
+    // Overload for switching flex on and off with true/false.
     public static bool Has([NotNullWhen(true)] object? obj, bool flex)
     {
         if (flex) return Has(obj, FlexFlag.flex);
         return HasValOrObj(obj);
     }
 
+    // Overload with FlexFlag: the FlexFlax enum only has 1 possible value: `flex`.
     // ReSharper disable once UnusedParameter.Global
     public static bool Has([NotNullWhen(true)] object? obj, FlexFlag flex)
     {
@@ -57,7 +91,7 @@ public class Has_Dynamic_Tests
         }
     }
     
-    // Stubs for demo, not part of new implementation.
+    // Stubs for demo, already part of the main code.
     private static bool HasText([NotNullWhen(true)] string? text) => FilledInHelper.Has(text);
     private static bool HasSB([NotNullWhen(true)] StringBuilder? sb) => FilledInHelper.Has(sb);
     private static bool HasValOrObj([NotNullWhen(true)] object? valOrObj) => FilledInHelper.Has(valOrObj);
