b58747f37dfcd4b7ed07581570a96942b21b10c3
jjvanzon <jjvanzon79@gmail.com>
2025-06-10 20:39:35 +0200

    💁 PlatformCompatibility.Core: Test CallerArgumentExpression use across target frameworks; annotate netstandard2.1 stub with `ncrunch: no coverage`. It needs to be compiled in, but can't ever be hit.

---
+++ Framework/JJ.Framework.PlatformCompatibility.Core.Tests/PlatformCompatibility_Core_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+// ReSharper disable UnusedParameter.Local
+
+namespace JJ.Framework.PlatformCompatibility.Core.Tests;
+
+[TestClass]
+public class PlatformCompatibility_Core_Tests
+{
+    [TestMethod]
+    public void Test_PlatformCompatibility_Core()
+    {
+        int i = 5;
+        CallerArgumentExpressionDummy(i);
+    }
+    
+    private void CallerArgumentExpressionDummy(int arg, [CallerArgumentExpression("arg")] string expr = "")
+    {
+        AreEqual("i", expr);
+    }
+}
--- Framework/JJ.Framework.PlatformCompatibility.Core/CallerArgumentExpression.cs
+++ Framework/JJ.Framework.PlatformCompatibility.Core/CallerArgumentExpression.cs
@@ -1,7 +1,14 @@
-﻿namespace System.Runtime.CompilerServices;
+﻿#pragma warning disable CS9113 // Parameter is unread.
 
-#pragma warning disable CS9113 // Parameter is unread.
+namespace System.Runtime.CompilerServices;
 
-#if NETSTANDARD2_0 || NETSTANDARD2_1
+#if NETSTANDARD2_0
 public class CallerArgumentExpressionAttribute(string parameterName) : Attribute;
+#endif
+
+// Stub is needed to compile code for .NET Standard 2.1,
+// while all its supported frameworks already define it. (Even .NET 5 who officially does not.)
+// So a paradox: it can't be tested, but you can't compile .NET Standard 2.1 without.
+#if NETSTANDARD2_1
+public class CallerArgumentExpressionAttribute(string parameterName) : Attribute; // ncrunch: no coverage
 #endif
