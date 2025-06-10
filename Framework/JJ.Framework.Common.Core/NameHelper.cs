eff399d128bcf4a6a4b6a397a5231c6915349853
jjvanzon <jjvanzon79@gmail.com>
2025-06-10 23:28:36 +0200

    📟 Common.Core: Add TextOf method to easily get an expression as string.

--- Framework/JJ.Framework.Common.Core/NameHelper.cs
+++ Framework/JJ.Framework.Common.Core/NameHelper.cs
@@ -6,4 +6,9 @@ public static class NameHelper
     /// <inheritdoc cref="_name"/>
     public static string Name([CallerMemberName] string name = null)
         => name.CutLeft("get_").CutLeft("set_");
+    
+    // ReSharper disable once EntityNameCapturedOnly.Global
+    public static string TextOf(object? expression, [CallerArgumentExpression(nameof(expression))] string expressionString = null)
+        => expressionString;
+    
 }
