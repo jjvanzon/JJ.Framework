4110eccfa468d08a615312443300d1e7f95f0ee7
jjvanzon <jjvanzon79@gmail.com>
2025-06-15 01:59:39 +0200

    ✔️ Existance.Core: Add code samples in social post as tests to verify them.

---
+++ Framework/JJ.Framework.Existence.Core.Tests/ExistenceExamples.cs
@@ -0,0 +1,26 @@
+﻿namespace JJ.Framework.Existence.Core.Tests;
+
+[TestClass]
+public class ExistenceExamples
+{
+    int? num = null;
+    string? text = null;
+    IEnumerable<int> list = [ 1, 2, 3 ];
+    
+    [TestMethod]
+    public void Existence_Examples_Before()
+    {
+        if (IsNullOrWhiteSpace(text)) { }
+        if (num.HasValue && num.Value != 0) { }
+        if (list != null && list.Any()) { }
+        if (list?.Any() ?? false) { }
+    }
+    
+    [TestMethod]
+    public void Existence_Examples_After()
+    {
+        if (Has(text)) { }
+        if (num.IsNully()) { }
+        list = list.Where(FilledIn);
+    }
+}
