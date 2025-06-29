3c0fe08a0ee37972f5e63bfd57465d5d3afce05d
jjvanzon <jjvanzon79@gmail.com>
2025-06-29 21:59:39 +0200

    🆙 Upgrade some .Net48 projects to .Legacy package variants.

--- Framework/JJ.Framework.Configuration/Using.cs
+++ Framework/JJ.Framework.Configuration/Using.cs
@@ -1,2 +1,7 @@
 ﻿global using JJ.Framework.Reflection.Legacy;
 global using JJ.Framework.Xml.Legacy;
+
+// ReSharper disable CheckNamespace
+// ReSharper disable EmptyNamespace
+namespace JJ.Framework.Reflection { }
+namespace JJ.Framework.Xml { }
