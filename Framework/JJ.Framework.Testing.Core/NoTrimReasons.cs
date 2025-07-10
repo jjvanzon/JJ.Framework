eeb3839827a2f7d65c9b7f08e51993a9550c5125
jjvanzon <jjvanzon79@gmail.com>
2025-07-10 18:52:16 +0200

    🔬 Testing.Core: Add more entry-points to TestRunner (on Assembly and Type[]). Add Trimming annotations.

---
+++ Framework/JJ.Framework.Testing.Core/NoTrimReasons.cs
@@ -0,0 +1,8 @@
+﻿
+namespace JJ.Framework.Testing.Core;
+
+internal static class NoTrimReasons
+{
+    public const string MisingTypes = "Types might be removed";
+    public const string TypeArray = "Trimming not supported in case of Type arrays. Use other overloads for instance with a single Type instead.";
+}
--- Framework/JJ.Framework.Testing.Core/TestRunner.cs
+++ Framework/JJ.Framework.Testing.Core/TestRunner.cs
@@ -1,43 +1,37 @@
 ﻿// ReSharper disable VariableHidesOuterVariable
 
+
 namespace JJ.Framework.Testing.Core;
 
 /// <inheritdoc cref="_testrunner" />
 public static class TestRunner
 {
-    //public static void RunTests()
-    //{
-    //    Assembly assembly = Assembly.GetExecutingAssembly();
-    //    RunTests(assembly);
-    //}
+    [NoTrim(MisingTypes)]
+    public static void RunTests() => RunTests(GetExecutingAssembly());
 
-    //[NoTrim("Types might be removed")]
-    //public static void RunTests(Assembly assembly)
-    //{
-    //    Type[] testClasses = GetTestClasses(assembly);
-    //    RunTests(testClasses);
-    //}
+    [NoTrim(MisingTypes)]
+    public static void RunTests(Assembly assembly) => RunTests(GetTestClasses(assembly));
 
-    //[NoTrim("Types might be removed")]
-    //public static void RunTests(Type[] testClasses)
-    //{
-    //    foreach (Type testClass in testClasses)
-    //    {
-    //        RunTests(testClass);
-    //    }
-    //}
-
-    [NoTrim("Types might be removed")]
+    [NoTrim(MisingTypes)]
     public static Type[] GetTestClasses(Assembly assembly)
     {
         ThrowIfNull(assembly);
         return assembly.GetTypes()
-            .Where(x => x.GetCustomAttributes().Any(x => x.GetType().Name.StartsWith("TestClasws")))
-            .ToArray();
+                       .Where(x => x.GetCustomAttributes().Any(x => x.GetType().Name.StartsWith("TestClass")))
+                       .ToArray();
+    }
+    
+    [NoTrim(TypeArray)]
+    public static void RunTests(Type[] testClasses)
+    {
+        ThrowIfNull(testClasses);
+        foreach (Type testClass in testClasses)
+        {
+            RunTests(testClass);
+        }
     }
 
-    public static IList<string> RunTests<[Dyn(PublicMethods | DefaultCtor)] T>()
-        => RunTests(typeof(T));
+    public static IList<string> RunTests<[Dyn(PublicMethods | DefaultCtor)] T>() => RunTests(typeof(T));
 
     public static IList<string> RunTests([Dyn(PublicMethods | DefaultCtor)] Type testClass)
     {
