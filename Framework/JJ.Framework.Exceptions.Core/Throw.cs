413fb7fd793b54a7ac227d414d8465a87a2637a7
jjvanzon <jjvanzon79@gmail.com>
2025-07-10 20:12:55 +0200

    🤾 JJ.Framework.Exceptions.Core added with for now just a `NotNull` method as a chainable alternative to ThrowIfNull.

---
+++ Framework/JJ.Framework.Exceptions.Core/Throw.cs
@@ -0,0 +1,14 @@
+﻿namespace JJ.Framework.Exceptions.Core;
+
+public static class Throw
+{
+    public static T NotNull<T>([NotNull] T x, [ArgExpress(nameof(x))] string expression = "")
+    {
+        if (x == null)
+        {
+            throw new ArgumentNullException(expression, $"'{expression}' is null.");
+        }
+        return x;
+    }
+
+}
