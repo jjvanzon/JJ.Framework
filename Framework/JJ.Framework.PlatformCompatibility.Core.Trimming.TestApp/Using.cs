c7d29a99c1d5a200eb60594b6a967b061e4c8373
jjvanzon <jjvanzon79@gmail.com>
2025-07-14 05:39:25 +0200

    ☣️ [Draft] Trimming TestApp for PlatformCompatibility.Core: Reference original tests as linked files. Trimming itself may still fail. Swap MSTest for MSTestless. Adjust references, usings, and warnings. Configure NCrunch to skip trimming tests for incompatibility with publishing.

---
+++ Framework/JJ.Framework.PlatformCompatibility.Core.Trimming.TestApp/Using.cs
@@ -0,0 +1,17 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using JJ.Framework.Testing;
+global using JJ.Framework.PlatformCompatibility.Legacy;
+global using JJ.Framework.Testing.Core.MSTestless;
+global using static JJ.Framework.Testing.Legacy.AssertHelper;
+global using static JJ.Framework.Testing.Core.AssertCore;
+global using static JJ.Framework.Testing.Core.AssertHelperLegacy;
+global using static JJ.Framework.Common.Core.NameHelper;
+#if !NET8_0_OR_GREATER
+global using static JJ.Framework.PlatformCompatibility.Core.ExceptionSupport;
+#else
+global using static System.ArgumentException;
+global using static System.ArgumentNullException;
+#endif
