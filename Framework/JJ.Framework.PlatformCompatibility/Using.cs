31403689cbb07196287770857cb51bbf545d553b
jjvanzon <jjvanzon79@gmail.com>
2025-07-16 20:59:42 +0200

    💇 Trimmable Libs: Add .NET 5 build to PlatformCompatibility.Legacy for symmetry with trim tests and explicit .NET 5 compatibility, beyond .NET Standard.

--- Framework/JJ.Framework.PlatformCompatibility/Using.cs
+++ Framework/JJ.Framework.PlatformCompatibility/Using.cs
@@ -2,3 +2,4 @@
 global using JJ.Framework.PlatformCompatibility.Legacy;
 global using JJ.Framework.PlatformCompatibility.Legacy.docs;
 global using static System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes;
+global using static JJ.Framework.SharedProject.Core.DynamicallyAccessedMemberTypesEx;
