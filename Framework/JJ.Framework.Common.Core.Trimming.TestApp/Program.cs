f20149ad3e84341761b3236f789a4f2ba75c26f1
jjvanzon <jjvanzon79@gmail.com>
2025-07-17 21:06:15 +0200

    ✂️ [Draft] Add Common.Core.Trimming.Tests; earlier accomodations were prerequisites.

---
+++ Framework/JJ.Framework.Common.Core.Trimming.TestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿var result = new Result();
+
+RunTests<CollectionExtensions_Recursive_IEnumerable_Legacy_Tests>(result);
+RunTests<CollectionExtensions_Recursive_IList_Legacy_Tests>(result);
+RunTests<CollectionExtensionsCoreTests>(result);
+RunTests<ConfigurationHelperCoreTests>(result);
+RunTests<CultureHelperCoreTests>(result);
+RunTests<EmbeddedResourceHelperLegacyTests>(result);
+RunTests<EnvironmentHelperCoreTests>(result);
+RunTests<ExceptionTypesCoreTests>(result);
+RunTests<FlaggingTests>(result);
+RunTests<KeyValuePairHelperCoreTests>(result);
+RunTests<SplitCoreTests>(result);
+RunTests<SplitWithQuotationCoreTests>(result);
+RunTests<StringExtensionsCasingCoreTests>(result);
+RunTests<StringExtensionsCoreTests>(result);
+RunTests<TrimAllCoreTests>(result);
+
+result.Messages.ForEach(WriteLine);
+
+WriteLine("Done.");
+
+if (!result.Success) Exit(1);
