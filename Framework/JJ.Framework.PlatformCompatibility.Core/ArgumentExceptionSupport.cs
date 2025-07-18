f00c2638ef1d4cc15f8b9b9d9baecc9cb2078cf3
jjvanzon <jjvanzon79@gmail.com>
2025-07-18 02:56:43 +0200

    🍌 PlatformCompatibility.Core: Split ArgumentExceptionSupport and ArgumentNullExceptionSupport into 2 separate source files.

--- Framework/JJ.Framework.PlatformCompatibility.Core/ArgumentExceptionSupport.cs
+++ Framework/JJ.Framework.PlatformCompatibility.Core/ArgumentExceptionSupport.cs
@@ -1,4 +1,6 @@
-﻿// ReSharper disable UnusedMember.Global
+﻿// ReSharper disable RedundantUsingDirective
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UseSymbolAlias
 
 // ncrunch: no coverage start
 
@@ -8,18 +10,6 @@ using System.Runtime.CompilerServices;
 using static System.String;
 
 namespace JJ.Framework.PlatformCompatibility.Core;
-
-internal static class ArgumentNullExceptionSupport
-{
-    #if !NET6_0_OR_GREATER
-
-    public static void ThrowIfNull([System.Diagnostics.CodeAnalysis.NotNull] object? argument, [CallerArgumentExpression(nameof(argument))] string expression = "")
-    {
-        if (argument == null) throw new ArgumentNullException(expression);
-    }
-
-    #endif
-}
 
 internal static class ArgumentExceptionSupport
 {
