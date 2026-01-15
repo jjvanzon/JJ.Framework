f985b45378e577c947aeda5cff84d5a370e08b1f
jjvanzon <jjvanzon79@gmail.com>
2026-01-15 19:11:11 +0100

    🧫 Existence.Core.Tests Extend DateTime cases and split them up in methods.

--- Framework/JJ.Framework.Existence.Core.Tests/Existence_DateTime_Tests.cs
+++ Framework/JJ.Framework.Existence.Core.Tests/Existence_DateTime_Tests.cs
@@ -22,21 +22,88 @@ public class Existence_DateTime_Tests
     DateTime? SomeNullyDateTime    = new(2023, 10, 1, 12, 10, 23);
                                    
     [TestMethod]
-    public void Test_DateTime_FilledIn_Tests()
+    public void Test_DateTime_Has()
     {
-        IsFalse(Has(NullDateTime));
-        IsFalse(Has(DefaultDateTime));
-        IsFalse(Has(DefaultNullyDateTime));
-        IsFalse(Has(NewDateTime));
-        IsFalse(Has(NewNullyDateTime));
-        IsFalse(Has(MinDateTime));
-        IsFalse(Has(MinNullyDateTime));
-        IsTrue (Has(MaxDateTime));
-        IsTrue (Has(MaxNullyDateTime));
-        IsTrue (Has(NowDateTime));
-        IsTrue (Has(NowNullyDateTime));
-        IsTrue (Has(SomeDateTime));
-        IsTrue (Has(SomeNullyDateTime));
+        IsFalse(Has     (NullDateTime         ));
+        IsFalse(Has     (DefaultDateTime      ));
+        IsFalse(Has     (DefaultNullyDateTime ));
+        IsFalse(Has     (NewDateTime          ));
+        IsFalse(Has     (NewNullyDateTime     ));
+        IsFalse(Has     (MinDateTime          ));
+        IsFalse(Has     (MinNullyDateTime     ));
+        IsTrue (Has     (MaxDateTime          ));
+        IsTrue (Has     (MaxNullyDateTime     ));
+        IsTrue (Has     (NowDateTime          ));
+        IsTrue (Has     (NowNullyDateTime     ));
+        IsTrue (Has     (SomeDateTime         ));
+        IsTrue (Has     (SomeNullyDateTime    ));
+    }
+
+    [TestMethod]
+    public void Test_DateTime_FilledIn()
+    {
+        IsFalse(FilledIn(NullDateTime         ));
+        IsFalse(FilledIn(DefaultDateTime      ));
+        IsFalse(FilledIn(DefaultNullyDateTime ));
+        IsFalse(FilledIn(NewDateTime          ));
+        IsFalse(FilledIn(NewNullyDateTime     ));
+        IsFalse(FilledIn(MinDateTime          ));
+        IsFalse(FilledIn(MinNullyDateTime     ));
+        IsTrue (FilledIn(MaxDateTime          ));
+        IsTrue (FilledIn(MaxNullyDateTime     ));
+        IsTrue (FilledIn(NowDateTime          ));
+        IsTrue (FilledIn(NowNullyDateTime     ));
+        IsTrue (FilledIn(SomeDateTime         ));
+        IsTrue (FilledIn(SomeNullyDateTime    ));
+        IsFalse(NullDateTime        .FilledIn());
+        IsFalse(DefaultDateTime     .FilledIn());
+        IsFalse(DefaultNullyDateTime.FilledIn());
+        IsFalse(NewDateTime         .FilledIn());
+        IsFalse(NewNullyDateTime    .FilledIn());
+        IsFalse(MinDateTime         .FilledIn());
+        IsFalse(MinNullyDateTime    .FilledIn());
+        IsTrue (MaxDateTime         .FilledIn());
+        IsTrue (MaxNullyDateTime    .FilledIn());
+        IsTrue (NowDateTime         .FilledIn());
+        IsTrue (NowNullyDateTime    .FilledIn());
+        IsTrue (SomeDateTime        .FilledIn());
+        IsTrue (SomeNullyDateTime   .FilledIn());
+    }
+
+    [TestMethod]
+    public void Test_DateTime_IsNully()
+    {
+        IsTrue (IsNully( NullDateTime         ));
+        IsTrue (IsNully( DefaultDateTime      ));
+        IsTrue (IsNully( DefaultNullyDateTime ));
+        IsTrue (IsNully( NewDateTime          ));
+        IsTrue (IsNully( NewNullyDateTime     ));
+        IsTrue (IsNully( MinDateTime          ));
+        IsTrue (IsNully( MinNullyDateTime     ));
+        IsFalse(IsNully( MaxDateTime          ));
+        IsFalse(IsNully( MaxNullyDateTime     ));
+        IsFalse(IsNully( NowDateTime          ));
+        IsFalse(IsNully( NowNullyDateTime     ));
+        IsFalse(IsNully( SomeDateTime         ));
+        IsFalse(IsNully( SomeNullyDateTime    ));
+        IsTrue (NullDateTime        .IsNully ());
+        IsTrue (DefaultDateTime     .IsNully ());
+        IsTrue (DefaultNullyDateTime.IsNully ());
+        IsTrue (NewDateTime         .IsNully ());
+        IsTrue (NewNullyDateTime    .IsNully ());
+        IsTrue (MinDateTime         .IsNully ());
+        IsTrue (MinNullyDateTime    .IsNully ());
+        IsFalse(MaxDateTime         .IsNully ());
+        IsFalse(MaxNullyDateTime    .IsNully ());
+        IsFalse(NowDateTime         .IsNully ());
+        IsFalse(NowNullyDateTime    .IsNully ());
+        IsFalse(SomeDateTime        .IsNully ());
+        IsFalse(SomeNullyDateTime   .IsNully ());
+    }
+
+    [TestMethod]
+    public void Test_DateTime_Coalesce()
+    {
         NoNullRet(SomeDateTime, Coalesce(NewNullyDateTime, NewDateTime, SomeNullyDateTime, NowNullyDateTime, SomeDateTime));
     }
 }
