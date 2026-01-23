209d836e36cd20c34eb33272732b4b3c97762d97
jjvanzon <jjvanzon79@gmail.com>
2026-01-24 00:33:22 +0100

    🏷️ Rename

---
+++ Framework/JJ.Framework.Existence.Core/Util.cs
@@ -0,0 +1,14 @@
+﻿namespace JJ.Framework.Existence.Core;
+
+internal static class Util
+{
+    public const string IgnoreCaseWarning = 
+        "Use caseMatters instead. " +
+        "FLIP YOUR BOOLEANS IF NEEDED: " +
+        "Where ignoreCase: false, caseMatters should now be true. " +
+        "Default behavior is to ignore case.";
+
+    // Belongs in Text.Core but prevents shipping an extra dependency for now.
+    public static StringComparison CaseMattersToStringComparison(this bool caseMatters) 
+        => caseMatters ? Ordinal : OrdinalIgnoreCase;
+}
