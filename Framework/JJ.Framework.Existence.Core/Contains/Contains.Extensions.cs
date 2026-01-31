6841026bf4bd70e2b4dd9d3d5d054bc286ef3655
jjvanzon <jjvanzon79@gmail.com>
2026-01-31 19:47:04 +0100

    📫 Existence.Core: Contains overloads with different flag situations + preliminary test suite for it.

--- Framework/JJ.Framework.Existence.Core/Contains/Contains.Extensions.cs
+++ Framework/JJ.Framework.Existence.Core/Contains/Contains.Extensions.cs
@@ -4,10 +4,22 @@
 public static class ContainsExtensions
 {
     /// <inheritdoc cref="_contains" />
-    public static bool Contains(this IEnumerable<string?>? coll, string? value, bool caseMatters = false) 
+    public static bool Contains(this IEnumerable<string?>? coll, string? value) 
+        => ContainsUtil.Contains(coll, value);
+    
+    /// <inheritdoc cref="_contains" />
+    public static bool Contains(this IEnumerable<string?>? coll, string? value, bool caseMatters) 
         => ContainsUtil.Contains(coll, value, caseMatters);
     
     /// <inheritdoc cref="_contains" />
     public static bool Contains(this IEnumerable<string?>? coll, bool caseMatters, string? value) 
+        => ContainsUtil.Contains(coll, value, caseMatters);
+    
+    /// <inheritdoc cref="_contains" />
+    public static bool Contains(this IEnumerable<string?>? coll, string? value, CaseMatters caseMatters) 
+        => ContainsUtil.Contains(coll, value, caseMatters);
+    
+    /// <inheritdoc cref="_contains" />
+    public static bool Contains(this IEnumerable<string?>? coll, CaseMatters caseMatters, string? value) 
         => ContainsUtil.Contains(coll, value, caseMatters);
 }
--- Framework/JJ.Framework.Existence.Core/Contains/Contains.Util.cs
+++ Framework/JJ.Framework.Existence.Core/Contains/Contains.Util.cs
@@ -4,11 +4,27 @@
 internal static class ContainsUtil
 {
     /// <inheritdoc cref="_contains" />
-    public static bool Contains(IEnumerable<string?>? coll, string? value, bool caseMatters = false)
+    public static bool Contains(IEnumerable<string?>? coll, string? value)
+    {
+        if (coll == null) return false;
+        value = FormatValue(value);
+        return coll.Any(x => x == value || FormatValue(x).Equals(value, OrdinalIgnoreCase));
+    }
+
+    /// <inheritdoc cref="_contains" />
+    public static bool Contains(IEnumerable<string?>? coll, string? value, bool caseMatters)
     {
         if (coll == null) return false;
         value = FormatValue(value);
         var stringComparison = GetStringComparison(caseMatters);
         return coll.Any(x => x == value || FormatValue(x).Equals(value, stringComparison));
+    }
+
+    /// <inheritdoc cref="_contains" />
+    public static bool Contains(IEnumerable<string?>? coll, string? value, CaseMatters caseMatters)
+    {
+        if (coll == null) return false;
+        value = FormatValue(value);
+        return coll.Any(x => x == value || FormatValue(x).Equals(value, Ordinal));
     }
 }
