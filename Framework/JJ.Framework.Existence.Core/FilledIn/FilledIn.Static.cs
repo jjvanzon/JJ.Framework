9701d8785ee9b769a499227b3c963c5f09e3e222
jjvanzon <jjvanzon79@gmail.com>
2026-02-01 02:55:01 +0100

    ✏️ Trivials

--- Framework/JJ.Framework.Existence.Core/FilledIn/FilledIn.Static.cs
+++ Framework/JJ.Framework.Existence.Core/FilledIn/FilledIn.Static.cs
@@ -27,7 +27,7 @@ public static partial class FilledInHelper
     /// <inheritdoc cref="_filledin" />
     public static bool FilledIn<T>  ([NotNullWhen(true )]      T?             nullyVal, ZeroMatters  zeroMatters ) where T : struct => HasValNully(nullyVal, zeroMatters );
 
-    // Parameters Swapped
+    // Parameter Swap
 
     /// <inheritdoc cref="_filledin" />
     public static bool FilledIn     (bool         spaceMatters, [NotNullWhen(true )]      string?        text    )                  => HasText    (text,     spaceMatters);
@@ -46,7 +46,7 @@ public static partial class FilledInHelper
     /// <inheritdoc cref="_filledin" />
     public static bool FilledIn<T>  (ZeroMatters  zeroMatters,  [NotNullWhen(true )]      T?             nullyVal) where T : struct => HasValNully(nullyVal, zeroMatters );
 
-    // Disambiguate Booleans
+    // Boolean Disambiguation
 
     /// <inheritdoc cref="_filledin" />
     public static bool FilledIn     ([NotNullWhen(true )]      bool           val,      bool         zeroMatters )                  => HasVal     (val,      zeroMatters );
@@ -56,5 +56,4 @@ public static partial class FilledInHelper
     public static bool FilledIn     ([NotNullWhen(true )]      bool?          nullyVal, bool         zeroMatters )                  => HasValNully(nullyVal, zeroMatters );
     /// <inheritdoc cref="_filledin" />
     public static bool FilledIn     ([NotNullWhen(true )]      bool?          nullyVal, ZeroMatters  zeroMatters )                  => HasValNully(nullyVal, zeroMatters );
-
 }
