96390dbd667131d6a7dee275358f64ea92162370
jjvanzon <jjvanzon79@gmail.com>
2026-02-02 22:50:14 +0100

    🛰️ White space

--- Framework/JJ.Framework.Existence.Core/IsNully/IsNully.Static.cs
+++ Framework/JJ.Framework.Existence.Core/IsNully/IsNully.Static.cs
@@ -3,19 +3,19 @@
 public static partial class FilledInHelper
 {
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully      ([NotNullWhen(false)]      string?        text                               ) => !HasText(text);
+    public static bool IsNully      ([NotNullWhen(false)]      string?        text                               )                  => !HasText(text);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully      ([NotNullWhen(false)]      string?        text,     bool         spaceMatters) => !HasText(text, spaceMatters);
+    public static bool IsNully      ([NotNullWhen(false)]      string?        text,     bool         spaceMatters)                  => !HasText(text, spaceMatters);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully      ([NotNullWhen(false)]      string?        text,     SpaceMatters spaceMatters) => !HasText(text, spaceMatters);
+    public static bool IsNully      ([NotNullWhen(false)]      string?        text,     SpaceMatters spaceMatters)                  => !HasText(text, spaceMatters);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully      ([NotNullWhen(false)]      StringBuilder? sb                                 ) => !HasSB(sb);
+    public static bool IsNully      ([NotNullWhen(false)]      StringBuilder? sb                                 )                  => !HasSB(sb);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully      ([NotNullWhen(false)]      StringBuilder? sb,       bool         spaceMatters) => !HasSB(sb, spaceMatters);
+    public static bool IsNully      ([NotNullWhen(false)]      StringBuilder? sb,       bool         spaceMatters)                  => !HasSB(sb, spaceMatters);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully      ([NotNullWhen(false)]      StringBuilder? sb,       SpaceMatters spaceMatters) => !HasSB(sb, spaceMatters);
+    public static bool IsNully      ([NotNullWhen(false)]      StringBuilder? sb,       SpaceMatters spaceMatters)                  => !HasSB(sb, spaceMatters);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully<T>   ([NotNullWhen(false)]      T              valOrObj                           ) => !HasValOrObj(valOrObj);
+    public static bool IsNully<T>   ([NotNullWhen(false)]      T              valOrObj                           )                  => !HasValOrObj(valOrObj);
     /// <inheritdoc cref="_isnully" />
     public static bool IsNully<T>   ([NotNullWhen(false)]      T              valOrObj, bool         zeroMatters ) where T : struct => !HasVal(valOrObj, zeroMatters);
     /// <inheritdoc cref="_isnully" />
