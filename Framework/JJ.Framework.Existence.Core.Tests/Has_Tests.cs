d0fc3d9bfc893d7a0893665f636da5776184807d
jjvanzon <jjvanzon79@gmail.com>
2026-02-02 03:16:20 +0100

    🏷️ Finalize rename

---
+++ Framework/JJ.Framework.Existence.Core.Tests/Has_Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace JJ.Framework.Existence.Core.Tests;
+
+[TestClass]
+public class Has_Tests
+{
+    private static readonly Dummy? NullyFilled = NullyFilledObj;
+
+    // Objects
+
+    [TestMethod]
+    public void Has_Object_True()
+    {
+        IsTrue(Has(NoNullObj));
+        IsTrue(Has(NullyFilled));
+    }
+    
+    [TestMethod]
+    public void Has_Object_False()
+    {
+        IsFalse(Has(NullObj));
+    }
+}
