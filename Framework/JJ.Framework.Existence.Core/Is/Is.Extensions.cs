dc2bbc892b44b1846f9e5dd8d890f7fb9fc7f7c0
jjvanzon <jjvanzon79@gmail.com>
2026-02-03 17:10:38 +0100

    🚩 Existence.Core: Add extension method overloads for `Is` that take flags in the front.

--- Framework/JJ.Framework.Existence.Core/Is/Is.Extensions.cs
+++ Framework/JJ.Framework.Existence.Core/Is/Is.Extensions.cs
@@ -28,4 +28,26 @@ public static class IsExtensions
     public static bool Is(this string? a, string? b, bool         spaceMatters, CaseMatters  caseMatters                       ) => IsUtil.Is(a, b, caseMatters,  spaceMatters);
     /// <inheritdoc cref="_is" />
     public static bool Is(this string? a, string? b, bool         caseMatters,  bool         spaceMatters                      ) => IsUtil.Is(a, b, caseMatters,  spaceMatters);
+    /// <inheritdoc cref="_is" />
+    public static bool Is(this string? a, CaseMatters  caseMatters,                             string? b                      ) => IsUtil.Is(a, b, caseMatters               );
+    /// <inheritdoc cref="_is" />
+    public static bool Is(this string? a, bool         caseMatters,                             string? b                      ) => IsUtil.Is(a, b, caseMatters               );
+    /// <inheritdoc cref="_is" />
+    public static bool Is(this string? a, SpaceMatters spaceMatters,                            string? b                      ) => IsUtil.Is(a, b, spaceMatters              );
+    /// <inheritdoc cref="_is" />
+    public static bool Is(this string? a, bool         spaceMatters, string? b, [Implic(Reason = OverloadByName)] int dummy = 0) => IsUtil.Is(a, b, spaceMatters: spaceMatters);
+    /// <inheritdoc cref="_is" />
+    public static bool Is(this string? a, CaseMatters  caseMatters,  SpaceMatters spaceMatters, string? b                      ) => IsUtil.Is(a, b, caseMatters,  spaceMatters);
+    /// <inheritdoc cref="_is" />
+    public static bool Is(this string? a, CaseMatters  caseMatters,  bool         spaceMatters, string? b                      ) => IsUtil.Is(a, b, caseMatters,  spaceMatters);
+    /// <inheritdoc cref="_is" />
+    public static bool Is(this string? a, bool         caseMatters,  SpaceMatters spaceMatters, string? b                      ) => IsUtil.Is(a, b, caseMatters,  spaceMatters);
+    /// <inheritdoc cref="_is" />
+    public static bool Is(this string? a, SpaceMatters spaceMatters, CaseMatters  caseMatters,  string? b                      ) => IsUtil.Is(a, b, caseMatters,  spaceMatters);
+    /// <inheritdoc cref="_is" />
+    public static bool Is(this string? a, SpaceMatters spaceMatters, bool         caseMatters,  string? b                      ) => IsUtil.Is(a, b, caseMatters,  spaceMatters);
+    /// <inheritdoc cref="_is" />
+    public static bool Is(this string? a, bool         spaceMatters, CaseMatters  caseMatters,  string? b                      ) => IsUtil.Is(a, b, caseMatters,  spaceMatters);
+    /// <inheritdoc cref="_is" />
+    public static bool Is(this string? a, bool         caseMatters,  bool         spaceMatters, string? b                      ) => IsUtil.Is(a, b, caseMatters,  spaceMatters);
 }
