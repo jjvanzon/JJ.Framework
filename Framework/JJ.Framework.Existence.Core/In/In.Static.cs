fd8537081851104892a73e64d8b611b7a87d7a45
jjvanzon <jjvanzon79@gmail.com>
2026-02-04 23:04:18 +0100

    🧹 Existence.Core: Slight ordering of In.Static overloads

--- Framework/JJ.Framework.Existence.Core/In/In.Static.cs
+++ Framework/JJ.Framework.Existence.Core/In/In.Static.cs
@@ -1,4 +1,6 @@
-﻿namespace JJ.Framework.Existence.Core;
+﻿// ReSharper disable MethodOverloadWithOptionalParameter
+
+namespace JJ.Framework.Existence.Core;
 
 public static partial class FilledInHelper
 {
@@ -12,11 +14,6 @@ public static partial class FilledInHelper
     /// <inheritdoc cref="_in" />
     public static bool In(string? value, bool         caseMatters,                                params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters);
     /// <inheritdoc cref="_in" />
-    public static bool In(string? value,                             SpaceMatters spaceMatters,   params IEnumerable<string?>? coll) => InUtil.In(value, coll, spaceMatters);
-    /// <inheritdoc cref="_in" />
-    // ReSharper disable once MethodOverloadWithOptionalParameter
-    public static bool In(string? value,                             bool         spaceMatters,          IEnumerable<string?>? coll, [Implic(Reason = NameOvl)] int dummy = 1) => InUtil.In(value, coll, spaceMatters, dummy);
-    /// <inheritdoc cref="_in" />
     public static bool In(string? value, CaseMatters  caseMatters,   SpaceMatters spaceMatters,   params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
     /// <inheritdoc cref="_in" />
     public static bool In(string? value, CaseMatters  caseMatters,   bool         spaceMatters,   params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
@@ -27,6 +24,10 @@ public static partial class FilledInHelper
 
     // Flags in Front / Swap Parameters
 
+    /// <inheritdoc cref="_in" />
+    public static bool In(string? value, SpaceMatters spaceMatters,                               params IEnumerable<string?>? coll) => InUtil.In(value, coll, spaceMatters);
+    /// <inheritdoc cref="_in" />
+    public static bool In(string? value, bool         spaceMatters,                                      IEnumerable<string?>? coll, [Implic(Reason = NameOvl)] int dummy = 1) => InUtil.In(value, coll, spaceMatters, dummy);
     /// <inheritdoc cref="_in" />
     public static bool In(string? value, SpaceMatters spaceMatters,  CaseMatters  caseMatters,    params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
     /// <inheritdoc cref="_in" />
@@ -43,7 +44,6 @@ public static partial class FilledInHelper
     /// <inheritdoc cref="_in" />
     public static bool In(string? value, IEnumerable<string?>? coll,                            SpaceMatters spaceMatters) => InUtil.In(value, coll, spaceMatters);
     /// <inheritdoc cref="_in" />
-    // ReSharper disable once MethodOverloadWithOptionalParameter
     public static bool In(string? value, IEnumerable<string?>? coll,                            bool         spaceMatters, [Implic(Reason = NameOvl)] int dummy = 1) => InUtil.In(value, coll, spaceMatters, dummy);
     /// <inheritdoc cref="_in" />
     public static bool In(string? value, IEnumerable<string?>? coll, CaseMatters  caseMatters,  SpaceMatters spaceMatters) => InUtil.In(value, coll, caseMatters, spaceMatters);
