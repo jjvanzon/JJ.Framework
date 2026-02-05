0f504dc92dec54816ceb5a218b1bc904f2180486
jjvanzon <jjvanzon79@gmail.com>
2026-02-05 01:50:12 +0100

    📥 Existence.Core - `In` command - Add overloads by parameter names, to use named arguments even when parameter lists are the same, by introducing an unused optional argument, that's never used, to appease the C# compiler.

--- Framework/JJ.Framework.Existence.Core/In/In.Extensions.cs
+++ Framework/JJ.Framework.Existence.Core/In/In.Extensions.cs
@@ -7,63 +7,70 @@ public static class InExtensions
     // Text
 
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, params IEnumerable<string?>? coll) => InUtil.In(value, coll);
-    
+    public static bool In(this string? value,                                                        params IEnumerable<string?>? coll) => InUtil.In(value, coll);
+
     // Flags in Front
 
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, CaseMatters caseMatters,                             params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters);
-    /// <inheritdoc cref="_in" />                                       
-    public static bool In(this string? value, bool        caseMatters,                             params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters);
-        /// <inheritdoc cref="_in" />                                   
-    public static bool In(this string? value, CaseMatters caseMatters,  SpaceMatters spaceMatters, params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
-    /// <inheritdoc cref="_in" />                                       
-    public static bool In(this string? value, CaseMatters caseMatters,  bool         spaceMatters, params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
-    /// <inheritdoc cref="_in" />                                       
-    public static bool In(this string? value, bool        caseMatters,  SpaceMatters spaceMatters, params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    public static bool In(this string? value, CaseMatters  caseMatters,                              params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters);
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, bool        caseMatters = default, bool spaceMatters = default, params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
-
+    public static bool In(this string? value, bool         caseMatters,                              params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters);
+    /// <inheritdoc cref="_in" />
+    public static bool In(this string? value, CaseMatters  caseMatters,   SpaceMatters spaceMatters, params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    /// <inheritdoc cref="_in" />
+    public static bool In(this string? value, CaseMatters  caseMatters,   bool         spaceMatters, params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    /// <inheritdoc cref="_in" />
+    public static bool In(this string? value, bool         caseMatters,   SpaceMatters spaceMatters, params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    /// <inheritdoc cref="_in" />
+    public static bool In(this string? value, bool         caseMatters,   bool         spaceMatters, params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    
     // Flags in Front / Swap Parameters
 
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, SpaceMatters spaceMatters,                          params IEnumerable<string?>? coll) => InUtil.In(value, coll, spaceMatters);
+    public static bool In(this string? value, SpaceMatters spaceMatters,                             params IEnumerable<string?>? coll) => InUtil.In(value, coll, spaceMatters);
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, bool         spaceMatters,                                 IEnumerable<string?>? coll, [Implic(Reason=NameOvl)]int dum=0) => InUtil.In(value, coll, spaceMatters, dum);
+    public static bool In(this string? value, bool         spaceMatters,                                    IEnumerable<string?>? coll, [Implic(Reason=NameOvl)]int dum=0) => InUtil.In(value, coll, spaceMatters, dum);
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, SpaceMatters spaceMatters, CaseMatters caseMatters, params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    public static bool In(this string? value, SpaceMatters spaceMatters,  CaseMatters  caseMatters,  params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, SpaceMatters spaceMatters, bool        caseMatters, params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    public static bool In(this string? value, SpaceMatters spaceMatters,  bool         caseMatters,  params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, bool         spaceMatters, CaseMatters caseMatters, params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    public static bool In(this string? value, bool         spaceMatters,  CaseMatters  caseMatters,  params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
     
     // Flags in Back
 
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, IEnumerable<string?>? coll, CaseMatters caseMatters                           ) => InUtil.In(value, coll, caseMatters);
+    public static bool In(this string? value, bool         spaceMatters,  bool         caseMatters, IEnumerable<string?>? coll, [Implic(Reason=NameOvl)]int dum=0) => InUtil.In(value, coll, caseMatters, spaceMatters);
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, IEnumerable<string?>? coll, bool        caseMatters                           ) => InUtil.In(value, coll, caseMatters);
+    public static bool In(this string? value, bool         spaceMatters,  bool         caseMatters, [Implic(Reason=NameOvl)]int dum=0, params IEnumerable<string?>? coll) => InUtil.In(value, coll, caseMatters, spaceMatters);
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, IEnumerable<string?>? coll, CaseMatters caseMatters, SpaceMatters spaceMatters) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    public static bool In(this string? value, IEnumerable<string?>? coll, CaseMatters  caseMatters                            ) => InUtil.In(value, coll, caseMatters);
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, IEnumerable<string?>? coll, bool        caseMatters, SpaceMatters spaceMatters) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    public static bool In(this string? value, IEnumerable<string?>? coll, bool         caseMatters                            ) => InUtil.In(value, coll, caseMatters);
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, IEnumerable<string?>? coll, CaseMatters caseMatters, bool         spaceMatters) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    public static bool In(this string? value, IEnumerable<string?>? coll, CaseMatters  caseMatters,  SpaceMatters spaceMatters) => InUtil.In(value, coll, caseMatters, spaceMatters);
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, IEnumerable<string?>? coll, bool        caseMatters, bool         spaceMatters) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    public static bool In(this string? value, IEnumerable<string?>? coll, bool         caseMatters,  SpaceMatters spaceMatters) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    /// <inheritdoc cref="_in" />
+    public static bool In(this string? value, IEnumerable<string?>? coll, CaseMatters  caseMatters,  bool         spaceMatters) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    /// <inheritdoc cref="_in" />
+    public static bool In(this string? value, IEnumerable<string?>? coll, bool         caseMatters,  bool         spaceMatters) => InUtil.In(value, coll, caseMatters, spaceMatters);
         
     // Flags in Back / Parameters Swapped
 
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, IEnumerable<string?>? coll, SpaceMatters spaceMatters                         ) => InUtil.In(value, coll, spaceMatters);
+    public static bool In(this string? value, IEnumerable<string?>? coll, SpaceMatters spaceMatters                           ) => InUtil.In(value, coll, spaceMatters);
     /// <inheritdoc cref="_in" />
     public static bool In(this string? value, IEnumerable<string?>? coll, bool         spaceMatters, [Implic(Reason=NameOvl)]int dum=0) => InUtil.In(value, coll, spaceMatters, dum);
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, IEnumerable<string?>? coll, SpaceMatters spaceMatters, CaseMatters caseMatters) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    public static bool In(this string? value, IEnumerable<string?>? coll, SpaceMatters spaceMatters, CaseMatters caseMatters  ) => InUtil.In(value, coll, caseMatters, spaceMatters);
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, IEnumerable<string?>? coll, bool         spaceMatters, CaseMatters caseMatters) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    public static bool In(this string? value, IEnumerable<string?>? coll, bool         spaceMatters, CaseMatters caseMatters  ) => InUtil.In(value, coll, caseMatters, spaceMatters);
     /// <inheritdoc cref="_in" />
-    public static bool In(this string? value, IEnumerable<string?>? coll, SpaceMatters spaceMatters, bool        caseMatters) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    public static bool In(this string? value, IEnumerable<string?>? coll, SpaceMatters spaceMatters, bool        caseMatters  ) => InUtil.In(value, coll, caseMatters, spaceMatters);
+    /// <inheritdoc cref="_in" />
+    public static bool In(this string? value, IEnumerable<string?>? coll, bool         spaceMatters, bool        caseMatters, [Implic(Reason=NameOvl)]int dum=0) => InUtil.In(value, coll, caseMatters, spaceMatters);
+
 
     // Values and Objects
 
