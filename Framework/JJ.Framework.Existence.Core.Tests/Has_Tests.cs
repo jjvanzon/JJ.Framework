f755c68093c442b2229023189ef3ddb758b3afc2
jjvanzon <jjvanzon79@gmail.com>
2026-02-07 16:21:59 +0100

    🚚 Existence.Core.Tests - Move `Has` tests for Booleans to main Has_Tests.cs file. (They'll become first-class citizens).

--- Framework/JJ.Framework.Existence.Core.Tests/Has_Tests.cs
+++ Framework/JJ.Framework.Existence.Core.Tests/Has_Tests.cs
@@ -238,6 +238,100 @@ public class Has_Tests : TestBase
         IsTrue (Has(              false, NullyFilledSB      ));
     }
 
+    // Booleans
+
+    [TestMethod]
+    public void Test_Bool_Has()
+    {
+        IsTrue (Has(true));
+        IsFalse(Has(false));
+        IsTrue (Has(True));
+        IsFalse(Has(False));
+        IsTrue (Has(NullyTrue));
+        IsFalse(Has(NullyFalse));
+        IsFalse(Has(NullBool));
+    }
+
+    [TestMethod]
+    public void Test_Bool_Has_ZeroMattersFlagsInBack()
+    {
+        IsTrue (Has     (true,       zeroMatters: false));
+        IsTrue (Has     (true,                    false));
+        IsTrue (Has     (true,       zeroMatters       ));
+        IsTrue (Has     (true,       zeroMatters: true ));
+        IsTrue (Has     (true,                    true ));
+        IsFalse(Has     (false,      zeroMatters: false));
+        IsFalse(Has     (false,                   false));
+        IsTrue (Has     (false,      zeroMatters       ));
+        IsTrue (Has     (false,      zeroMatters: true ));
+        IsTrue (Has     (false,                   true ));
+        IsTrue (Has     (True,       zeroMatters: false));
+        IsTrue (Has     (True,                    false));
+        IsTrue (Has     (True,       zeroMatters       ));
+        IsTrue (Has     (True,       zeroMatters: true ));
+        IsTrue (Has     (True,                    true ));
+        IsFalse(Has     (False,      zeroMatters: false));
+        IsFalse(Has     (False,                   false));
+        IsTrue (Has     (False,      zeroMatters       ));
+        IsTrue (Has     (False,      zeroMatters: true ));
+        IsTrue (Has     (False,                   true ));
+        IsTrue (Has     (NullyTrue,  zeroMatters: false));
+        IsTrue (Has     (NullyTrue,               false));
+        IsTrue (Has     (NullyTrue,  zeroMatters       ));
+        IsTrue (Has     (NullyTrue,  zeroMatters: true ));
+        IsTrue (Has     (NullyTrue,               true ));
+        IsFalse(Has     (NullyFalse, zeroMatters: false));
+        IsFalse(Has     (NullyFalse,              false));
+        IsTrue (Has     (NullyFalse, zeroMatters       ));
+        IsTrue (Has     (NullyFalse, zeroMatters: true ));
+        IsTrue (Has     (NullyFalse,              true ));
+        IsFalse(Has     (NullBool,   zeroMatters: false));
+        IsFalse(Has     (NullBool,                false));
+        IsFalse(Has     (NullBool,   zeroMatters       ));
+        IsFalse(Has     (NullBool,   zeroMatters: true ));
+        IsFalse(Has     (NullBool,                true ));
+    }
+
+    [TestMethod]
+    public void Test_Bool_Has_ZeroMattersFlagsInFront()
+    {
+        IsTrue (Has     (zeroMatters: false, true        ));
+        IsTrue (Has     (             false, true        ));
+        IsTrue (Has     (zeroMatters,        true        ));
+        IsTrue (Has     (zeroMatters: true,  true        ));
+        IsTrue (Has     (             true,  true        ));
+        IsFalse(Has     (zeroMatters: false, false       ));
+        IsFalse(Has     (             false, false       ));
+        IsTrue (Has     (zeroMatters,        false       ));
+        IsTrue (Has     (zeroMatters: true,  false       ));
+        IsTrue (Has     (             true,  false       ));
+        IsTrue (Has     (zeroMatters: false, True        ));
+        IsTrue (Has     (             false, True        ));
+        IsTrue (Has     (zeroMatters,        True        ));
+        IsTrue (Has     (zeroMatters: true,  True        ));
+        IsTrue (Has     (             true,  True        ));
+        IsFalse(Has     (zeroMatters: false, False       ));
+        IsFalse(Has     (             false, False       ));
+        IsTrue (Has     (zeroMatters,        False       ));
+        IsTrue (Has     (zeroMatters: true,  False       ));
+        IsTrue (Has     (             true,  False       ));
+        IsTrue (Has     (zeroMatters: false, NullyTrue   ));
+        IsTrue (Has     (             false, NullyTrue   ));
+        IsTrue (Has     (zeroMatters,        NullyTrue   ));
+        IsTrue (Has     (zeroMatters: true,  NullyTrue   ));
+        IsTrue (Has     (             true,  NullyTrue   ));
+        IsFalse(Has     (zeroMatters: false, NullyFalse  ));
+        IsFalse(Has     (             false, NullyFalse  ));
+        IsTrue (Has     (zeroMatters,        NullyFalse  ));
+        IsTrue (Has     (zeroMatters: true,  NullyFalse  ));
+        IsTrue (Has     (             true,  NullyFalse  ));
+        IsFalse(Has     (zeroMatters: false, NullBool    ));
+        IsFalse(Has     (             false, NullBool    ));
+        IsFalse(Has     (zeroMatters,        NullBool    ));
+        IsFalse(Has     (zeroMatters: true,  NullBool    ));
+        IsFalse(Has     (             true,  NullBool    ));
+    }
+
     // Values
 
     [TestMethod]
