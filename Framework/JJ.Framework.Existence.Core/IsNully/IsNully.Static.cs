074ae6298bd1a3790283741a89e3d6a808b94f66
jjvanzon <jjvanzon79@gmail.com>
2026-02-08 22:07:50 +0100

    ‼️ Corrected Bool-based IsNully methods' NotNullWhen behavior so input values are maked as not-nullable under the right circumstances + additional Bool-based overloads.

--- Framework/JJ.Framework.Existence.Core/IsNully/IsNully.Static.cs
+++ Framework/JJ.Framework.Existence.Core/IsNully/IsNully.Static.cs
@@ -3,63 +3,67 @@
 public static partial class FilledInHelper
 {
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully      ([NotNullWhen(false)]      string?        text                               )                  => !HasText(text);
+    public static bool IsNully      ([NotNullWhen(false)]       string?        text                                )                  => !HasText(text);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully      ([NotNullWhen(false)]      string?        text,     bool         spaceMatters)                  => !HasText(text, spaceMatters);
+    public static bool IsNully      ([NotNullWhen(false)]       string?        text,      bool         spaceMatters)                  => !HasText(text, spaceMatters);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully      ([NotNullWhen(false)]      string?        text,     SpaceMatters spaceMatters)                  => !HasText(text, spaceMatters);
+    public static bool IsNully      ([NotNullWhen(false)]       string?        text,      SpaceMatters spaceMatters)                  => !HasText(text, spaceMatters);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully      ([NotNullWhen(false)]      StringBuilder? sb                                 )                  => !HasSB(sb);
+    public static bool IsNully      ([NotNullWhen(false)]       StringBuilder? sb                                  )                  => !HasSB(sb);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully      ([NotNullWhen(false)]      StringBuilder? sb,       bool         spaceMatters)                  => !HasSB(sb, spaceMatters);
+    public static bool IsNully      ([NotNullWhen(false)]       StringBuilder? sb,        bool         spaceMatters)                  => !HasSB(sb, spaceMatters);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully      ([NotNullWhen(false)]      StringBuilder? sb,       SpaceMatters spaceMatters)                  => !HasSB(sb, spaceMatters);
+    public static bool IsNully      ([NotNullWhen(false)]       StringBuilder? sb,        SpaceMatters spaceMatters)                  => !HasSB(sb, spaceMatters);
+    /// <inheritdoc cref="_filledin" />
+    public static bool IsNully      ([NotNullWhen(false)]       bool           boolean                             )                  => !HasBool     (boolean                );
+    /// <inheritdoc cref="_filledin" />
+    public static bool IsNully      ([NotNullWhen(false)]       bool           boolean,   bool         zeroMatters )                  => !HasBool     (boolean,   zeroMatters );
+    /// <inheritdoc cref="_filledin" />
+    public static bool IsNully      ([NotNullWhen(false)]       bool           boolean,   ZeroMatters  zeroMatters )                  => !HasBool     (boolean,   zeroMatters );
+    /// <inheritdoc cref="_filledin" />
+    public static bool IsNully      ([NotNullWhen(false)]       bool?          nullyBool                           )                  => !HasBoolNully(nullyBool              );
+    /// <inheritdoc cref="_filledin" />
+    public static bool IsNully      ([NotNullWhen(false)]       bool?          nullyBool, bool         zeroMatters )                  => !HasBoolNully(nullyBool, zeroMatters );
+    /// <inheritdoc cref="_filledin" />
+    public static bool IsNully      ([NotNullWhen(false)]       bool?          nullyBool, ZeroMatters  zeroMatters )                  => !HasBoolNully(nullyBool, zeroMatters );
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully<T>   ([NotNullWhen(false)]      T              valOrObj                           )                  => !HasValOrObj(valOrObj);
+    public static bool IsNully<T>   ([NotNullWhen(false)]       T              valOrObj                            )                  => !HasValOrObj(valOrObj);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully<T>   ([NotNullWhen(false)]      T              val,      bool         zeroMatters ) where T : struct => !HasVal(val, zeroMatters);
+    public static bool IsNully<T>   ([NotNullWhen(false)]       T              val,       bool         zeroMatters ) where T : struct => !HasVal(val, zeroMatters);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully<T>   ([NotNullWhen(false)]      T              val,      ZeroMatters  zeroMatters ) where T : struct => !HasVal(val, zeroMatters);
+    public static bool IsNully<T>   ([NotNullWhen(false)]       T              val,       ZeroMatters  zeroMatters ) where T : struct => !HasVal(val, zeroMatters);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully<T>   ([NotNullWhen(false)]      T?             nullyVal                           ) where T : struct => !HasValNully(nullyVal);
+    public static bool IsNully<T>   ([NotNullWhen(false)]       T?             nullyVal                            ) where T : struct => !HasValNully(nullyVal);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully<T>   ([NotNullWhen(false)]      T?             nullyVal, bool         zeroMatters ) where T : struct => !HasValNully(nullyVal, zeroMatters);
+    public static bool IsNully<T>   ([NotNullWhen(false)]       T?             nullyVal,  bool         zeroMatters ) where T : struct => !HasValNully(nullyVal, zeroMatters);
     /// <inheritdoc cref="_isnully" />
-    public static bool IsNully<T>   ([NotNullWhen(false)]      T?             nullyVal, ZeroMatters  zeroMatters ) where T : struct => !HasValNully(nullyVal, zeroMatters);
+    public static bool IsNully<T>   ([NotNullWhen(false)]       T?             nullyVal,  ZeroMatters  zeroMatters ) where T : struct => !HasValNully(nullyVal, zeroMatters);
 
     // Flags in Front
 
     /// <inheritdoc cref="_filledin" />
-    public static bool IsNully      (bool         spaceMatters, [NotNullWhen(true )]      string?        text    )                  => !HasText    (text,     spaceMatters);
+    public static bool IsNully      (bool         spaceMatters, [NotNullWhen(false)]      string?        text      )                  => !HasText    (text,     spaceMatters);
     /// <inheritdoc cref="_filledin" />
-    public static bool IsNully      (SpaceMatters spaceMatters, [NotNullWhen(true )]      string?        text    )                  => !HasText    (text,     spaceMatters);
+    public static bool IsNully      (SpaceMatters spaceMatters, [NotNullWhen(false)]      string?        text      )                  => !HasText    (text,     spaceMatters);
     /// <inheritdoc cref="_filledin" />
-    public static bool IsNully      (bool         spaceMatters, [NotNullWhen(true )]      StringBuilder? sb      )                  => !HasSB      (sb,       spaceMatters);
+    public static bool IsNully      (bool         spaceMatters, [NotNullWhen(false)]      StringBuilder? sb        )                  => !HasSB      (sb,       spaceMatters);
     /// <inheritdoc cref="_filledin" />
-    public static bool IsNully      (SpaceMatters spaceMatters, [NotNullWhen(true )]      StringBuilder? sb      )                  => !HasSB      (sb,       spaceMatters);
+    public static bool IsNully      (SpaceMatters spaceMatters, [NotNullWhen(false)]      StringBuilder? sb        )                  => !HasSB      (sb,       spaceMatters);
+    // Ambiguous:
+    ///// <inheritdoc cref="_filledin" />
+    //public static bool FilledIn   (bool         zeroMatters,  [NotNullWhen(false)]     bool           boolean    )                  => !HasBool     (boolean,   zeroMatters );
     /// <inheritdoc cref="_filledin" />
-    public static bool IsNully<T>   (bool         zeroMatters,  [NotNullWhen(true )]      T              val     ) where T : struct => !HasVal     (val,      zeroMatters );
+    public static bool IsNully      (ZeroMatters  zeroMatters,  [NotNullWhen(false)]      bool           boolean   )                  => !HasBool     (boolean,   zeroMatters );
     /// <inheritdoc cref="_filledin" />
-    public static bool IsNully<T>   (ZeroMatters  zeroMatters,  [NotNullWhen(true )]      T              val     ) where T : struct => !HasVal     (val,      zeroMatters );
+    public static bool IsNully      (bool         zeroMatters,  [NotNullWhen(false)]      bool?          nullyBool )                  => !HasBoolNully(nullyBool, zeroMatters );
     /// <inheritdoc cref="_filledin" />
-    public static bool IsNully<T>   (bool         zeroMatters,  [NotNullWhen(true )]      T?             nullyVal) where T : struct => !HasValNully(nullyVal, zeroMatters );
+    public static bool IsNully      (ZeroMatters  zeroMatters,  [NotNullWhen(false)]      bool?          nullyBool )                  => !HasBoolNully(nullyBool, zeroMatters );
     /// <inheritdoc cref="_filledin" />
-    public static bool IsNully<T>   (ZeroMatters  zeroMatters,  [NotNullWhen(true )]      T?             nullyVal) where T : struct => !HasValNully(nullyVal, zeroMatters );
-
-    // Boolean Disambiguation
-
+    public static bool IsNully<T>   (bool         zeroMatters,  [NotNullWhen(false)]      T              val       ) where T : struct => !HasVal     (val,      zeroMatters );
     /// <inheritdoc cref="_filledin" />
-    public static bool IsNully      ([NotNullWhen(true )]      bool           val,      bool         zeroMatters )                  => !HasVal     (val,      zeroMatters );
+    public static bool IsNully<T>   (ZeroMatters  zeroMatters,  [NotNullWhen(false)]      T              val       ) where T : struct => !HasVal     (val,      zeroMatters );
     /// <inheritdoc cref="_filledin" />
-    public static bool IsNully      ([NotNullWhen(true )]      bool           val,      ZeroMatters  zeroMatters )                  => !HasVal     (val,      zeroMatters );
+    public static bool IsNully<T>   (bool         zeroMatters,  [NotNullWhen(false)]      T?             nullyVal  ) where T : struct => !HasValNully(nullyVal, zeroMatters );
     /// <inheritdoc cref="_filledin" />
-    public static bool IsNully      ([NotNullWhen(true )]      bool?          nullyVal, bool         zeroMatters )                  => !HasValNully(nullyVal, zeroMatters );
-    /// <inheritdoc cref="_filledin" />
-    public static bool IsNully      ([NotNullWhen(true )]      bool?          nullyVal, ZeroMatters  zeroMatters )                  => !HasValNully(nullyVal, zeroMatters );
-    /// <inheritdoc cref="_filledin" />
-    public static bool IsNully      (ZeroMatters  zeroMatters ,[NotNullWhen(true )]      bool           val      )                  => !HasVal     (val,      zeroMatters );
-    /// <inheritdoc cref="_filledin" />
-    public static bool IsNully      (bool         zeroMatters ,[NotNullWhen(true )]      bool?          nullyVal )                  => !HasValNully(nullyVal, zeroMatters );
-    /// <inheritdoc cref="_filledin" />
-    public static bool IsNully      (ZeroMatters  zeroMatters ,[NotNullWhen(true )]      bool?          nullyVal )                  => !HasValNully(nullyVal, zeroMatters );
+    public static bool IsNully<T>   (ZeroMatters  zeroMatters,  [NotNullWhen(false)]      T?             nullyVal  ) where T : struct => !HasValNully(nullyVal, zeroMatters );
 }
