a04911210e2b497ef1603e31819a0a464e69e0f4
jjvanzon <jjvanzon79@gmail.com>
2026-02-21 03:20:10 +0100

    🩹 Business.Core.Tests - EntityStatusManager - Fix for nullability errors (review pending)

--- Framework/JJ.Framework.Business.Core.Tests/EntityStatusManagerCoreTests.cs
+++ Framework/JJ.Framework.Business.Core.Tests/EntityStatusManagerCoreTests.cs
@@ -1,4 +1,6 @@
-﻿namespace JJ.Framework.Business.Core.Tests;
+﻿// ReSharper disable RedundantArgumentDefaultValue
+
+namespace JJ.Framework.Business.Core.Tests;
 
 [TestClass]
 public class EntityStatusManagerCoreTests
@@ -7,10 +9,10 @@ public class EntityStatusManagerCoreTests
     {
         public int ID { get; set; }
         public string? Name { get; set; }
-        public Entity RelatedEntity { get; set; }
+        public Entity? RelatedEntity { get; set; }
 
-        public Entity QuestionType { get; set; }
-        public Entity Source { get; set; }
+        public Entity? QuestionType { get; set; }
+        public Entity? Source { get; set; }
         public IList<Entity> QuestionCategories { get; set; }
         public IList<Entity> QuestionLinks { get; set; }
         public IList<Entity> QuestionFlags { get; set; }
