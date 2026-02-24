36b27c12385069aaccc145b6cb2677ef0956b4d4
jjvanzon <jjvanzon79@gmail.com>
2026-02-24 04:31:27 +0100

    ✂️ Introduced JJ.Framework.Common.Core.Package.Trimming.TestApp.csproj targeting net5.0–net10.0 with relevant package and project references. Included test sources and project imports. Updated Program.cs to run multiple test classes and exit on failure. Added global and static usings in Using.cs for improved test runner setup.

---
+++ Framework/JJ.Framework.Common.Core.Package.Trimming.TestApp/Using.cs
@@ -0,0 +1,6 @@
+global using JJ.Framework.Common.Core;
+global using JJ.Framework.Common.Core.Tests;
+global using JJ.Framework.Testing.Core.MSTestless;
+global using static System.Console;
+global using static System.Environment;
+global using static JJ.Framework.Testing.Core.TestRunner;
