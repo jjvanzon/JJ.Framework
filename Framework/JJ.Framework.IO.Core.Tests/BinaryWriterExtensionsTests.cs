c01493b1b3e9cc5afef70e3a196f57f65a2f2179
jjvanzon <jjvanzon79@gmail.com>
2026-02-25 01:48:46 +0100

    🧑‍💻 IO Tests - BinaryWriterExtensionsTests refactor

--- Framework/JJ.Framework.IO.Core.Tests/BinaryWriterExtensionsTests.cs
+++ Framework/JJ.Framework.IO.Core.Tests/BinaryWriterExtensionsTests.cs
@@ -1,84 +1,88 @@
-using System.IO;
 using System.Runtime.InteropServices;
-using System.Text;
 
 namespace JJ.Framework.IO.Core.Tests;
 
 [TestClass]
 public class BinaryWriterExtensionsTests
 {
+    private static readonly Encoding DefaultEncoding = Encoding.UTF8;
+
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     private struct TestStruct
     {
-        public int A;
-        public short B;
-        public byte C;
+        public int   MyInt;
+        public short MyShort;
+        public byte  MyByte;
     }
 
     [TestMethod]
     public void WriteStruct_ReadStruct_Roundtrip()
     {
-        var original = new TestStruct { A = 0x11223344, B = 0x5566, C = 0x77 };
+        var inputStruct = new TestStruct { MyInt = 287454020, MyShort = 21862, MyByte = 119 };
 
-        using var ms = new MemoryStream();
-        using (var writer = new BinaryWriter(ms, Encoding.UTF8, leaveOpen: true))
+        using var memoryStream = new MemoryStream();
+
+        using (var writer = new BinaryWriter(memoryStream, DefaultEncoding, leaveOpen: true))
         {
-            writer.WriteStruct(original);
+            writer.WriteStruct(inputStruct);
         }
 
-        ms.Position = 0;
+        memoryStream.Position = 0;
 
-        using (var reader = new BinaryReader(ms, Encoding.UTF8, leaveOpen: true))
+        using (var reader = new BinaryReader(memoryStream))
         {
-            var result = reader.ReadStruct<TestStruct>();
-            AreEqual(original.A, result.A);
-            AreEqual(original.B, result.B);
-            AreEqual(original.C, result.C);
+            var outputStruct = reader.ReadStruct<TestStruct>();
+            AreEqual(inputStruct.MyInt,   outputStruct.MyInt);
+            AreEqual(inputStruct.MyShort, outputStruct.MyShort);
+            AreEqual(inputStruct.MyByte,  outputStruct.MyByte);
         }
     }
 
     [TestMethod]
     public void WriteStruct_WritesExpectedNumberOfBytes()
     {
-        var s = new TestStruct { A = 1, B = 2, C = 3 };
+        var myStruct = new TestStruct { MyInt = 1, MyShort = 2, MyByte = 3 };
 
-        using var ms = new MemoryStream();
-        using (var writer = new BinaryWriter(ms, Encoding.UTF8, leaveOpen: true))
+        using var memoryStream = new MemoryStream();
+
+        using (var writer = new BinaryWriter(memoryStream))
         {
-            writer.WriteStruct(s);
+            writer.WriteStruct(myStruct);
         }
 
         int expectedSize = Marshal.SizeOf(typeof(TestStruct));
-        AreEqual(expectedSize, (int)ms.Length);
+
+        AreEqual(expectedSize, (int)memoryStream.Length);
     }
 
     [TestMethod]
     public void WriteMultiple_ReadMultiple_OrderPreserved()
     {
-        var s1 = new TestStruct { A = 1, B = 10, C = 100 };
-        var s2 = new TestStruct { A = 2, B = 20, C = 200 };
+        var inputStruct1 = new TestStruct { MyInt = 1, MyShort = 10, MyByte = 100 };
+        var inputStruct2 = new TestStruct { MyInt = 2, MyShort = 20, MyByte = 200 };
 
-        using var ms = new MemoryStream();
-        using (var writer = new BinaryWriter(ms, Encoding.UTF8, leaveOpen: true))
+        using var memoryStream = new MemoryStream();
+
+        using (var writer = new BinaryWriter(memoryStream, DefaultEncoding, leaveOpen: true))
         {
-            writer.WriteStruct(s1);
-            writer.WriteStruct(s2);
+            writer.WriteStruct(inputStruct1);
+            writer.WriteStruct(inputStruct2);
         }
 
-        ms.Position = 0;
+        memoryStream.Position = 0;
 
-        using (var reader = new BinaryReader(ms, Encoding.UTF8, leaveOpen: true))
+        using (var reader = new BinaryReader(memoryStream))
         {
-            var r1 = reader.ReadStruct<TestStruct>();
-            var r2 = reader.ReadStruct<TestStruct>();
+            var outputStruct1 = reader.ReadStruct<TestStruct>();
+            var outputStruct2 = reader.ReadStruct<TestStruct>();
 
-            AreEqual(s1.A, r1.A);
-            AreEqual(s1.B, r1.B);
-            AreEqual(s1.C, r1.C);
+            AreEqual(inputStruct1.MyInt,   outputStruct1.MyInt);
+            AreEqual(inputStruct1.MyShort, outputStruct1.MyShort);
+            AreEqual(inputStruct1.MyByte,  outputStruct1.MyByte);
 
-            AreEqual(s2.A, r2.A);
-            AreEqual(s2.B, r2.B);
-            AreEqual(s2.C, r2.C);
+            AreEqual(inputStruct2.MyInt,   outputStruct2.MyInt);
+            AreEqual(inputStruct2.MyShort, outputStruct2.MyShort);
+            AreEqual(inputStruct2.MyByte,  outputStruct2.MyByte);
         }
     }
 }
