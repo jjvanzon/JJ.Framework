f7cd489991c6c997fed65d6854fbf6ca36f1af71
jjvanzon <jjvanzon79@gmail.com>
2026-03-01 20:07:28 +0100

    🚧 DevOps - Fix package test solution some more.

--- Framework/JJ.Framework.Business.Core.Package.Trimming.TestApp/UsingMore.cs
+++
@@ -1,5 +0,0 @@
-global using JJ.Framework.Business.Core;
-global using JJ.Framework.Testing.Core.MSTestless;
-global using static System.Console;
-global using static System.Environment;
-global using static JJ.Framework.Testing.Core.TestRunner;
