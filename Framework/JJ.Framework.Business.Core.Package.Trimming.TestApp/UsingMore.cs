174bed5cd1499b5e283ebaa5d54393d9b4dc9744
jjvanzon <jjvanzon79@gmail.com>
2026-03-01 19:52:43 +0100

    🧑‍🔧 DevOps - Fix up (trimming) test solutions some more.

--- Framework/JJ.Framework.Business.Core.Package.Trimming.TestApp/UsingMore.cs
+++ Framework/JJ.Framework.Business.Core.Package.Trimming.TestApp/UsingMore.cs
@@ -1,5 +1,4 @@
 global using JJ.Framework.Business.Core;
-global using JJ.Framework.Business.Core.Tests;
 global using JJ.Framework.Testing.Core.MSTestless;
 global using static System.Console;
 global using static System.Environment;
