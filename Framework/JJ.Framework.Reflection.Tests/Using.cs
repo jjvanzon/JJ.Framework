f568d32ff38001d3fb8485a5de826484ee208dac
jjvanzon <jjvanzon79@gmail.com>
2026-03-02 19:17:20 +0100

    🪞 Reflection Tests - Organized Using and UsingMore for Trimming.TestApp

--- Framework/JJ.Framework.Reflection.Tests/Using.cs
+++ Framework/JJ.Framework.Reflection.Tests/Using.cs
@@ -1,9 +1,10 @@
 ﻿global using System.Reflection;
 global using System.Diagnostics.CodeAnalysis;
+// ReSharper disable once RedundantUsingDirective.Global
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using JJ.Framework.Testing.Legacy;
 global using JJ.Framework.Reflection.Legacy;
 global using JJ.Framework.Reflection.Core.Tests.Helpers;
-global using JJ.Framework.Testing.Legacy;
 global using static System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes;
 global using static JJ.Framework.Testing.Legacy.AssertHelper;
 global using static JJ.Framework.Testing.Core.AssertCore;
--- Framework/JJ.Framework.Reflection.Trimming.TestApp/UsingMore.cs
+++ Framework/JJ.Framework.Reflection.Trimming.TestApp/UsingMore.cs
@@ -1,1 +1,7 @@
-﻿global using JJ.Framework.Reflection.Legacy.Tests;
+﻿global using JJ.Framework.Testing.Core.MSTestless;
+global using JJ.Framework.Reflection.Legacy.Tests;
+global using JJ.Framework.Reflection.Tests.AccessorTests;
+global using JJ.Framework.Reflection.Tests.ExpressionHelperTests;
+global using static System.Console;
+global using static System.Environment;
+global using static JJ.Framework.Testing.Core.TestRunner;
