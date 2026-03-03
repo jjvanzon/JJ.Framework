b1f56dfe6400f0f138cd590d0e5ae5a44e6b3c5a
jjvanzon <jjvanzon79@gmail.com>
2026-03-03 02:37:36 +0100

    ⚒️ DevOps - Simplify ClearNCrunch.bat

--- ClearNCrunch.bat
+++ ClearNCrunch.bat
@@ -1,5 +1,5 @@
 @echo off
-REM setlocal
+
 echo Clearing NCrunch caches (will only remove cache folders, not .ncrunchproject/.ncrunchsolution files)...
 
 rem ------------------------------------------------------------------
@@ -11,25 +11,24 @@ rem ------------------------------------------------------------------
 for /d %%D in ("*_NCrunch*" "_ncrunch*" "*.ncrunch*" "ncrunch_*" "_nCrunch*") do (
   if exist "%%~fD" (
     echo Deleting "%%~fD"
-    echo COLD RUN. NOTHING DELETED
+    echo "COLD RUN. NOTHING DELETED FOR NOW. VERIFY VALIDITY FIRST."
     REM rd /s /q "%%~fD"
   )
 )
-
-rem Some caches may be created in a hidden ".ncrunch" folder
-REM if exist ".ncrunch" (
-REM   echo Deleting ".ncrunch"
-REM   rd /s /q ".ncrunch"
-REM )
 
 rem ------------------------------------------------------------------
 rem Remove per-user/global NCrunch cache under Local AppData.
 rem This is the heavy cache and safe to remove; NCrunch will re-create it.
 rem ------------------------------------------------------------------
-REM if exist "%LOCALAPPDATA%\NCrunch" (
-  echo Deleting "%LOCALAPPDATA%\NCrunch"
-  echo COLD RUN. NOTHING DELETED
-  REM rd /s /q "%LOCALAPPDATA%\NCrunch"
+echo Deleting "%LOCALAPPDATA%\NCrunch"
+rd /s /q "%LOCALAPPDATA%\NCrunch"
+
+REM Surplus:
+
+rem Some caches may be created in a hidden ".ncrunch" folder
+REM if exist ".ncrunch" (
+REM   echo Deleting ".ncrunch"
+REM   rd /s /q ".ncrunch"
 REM )
 
 rem Some installations use a dot-folder in the user profile
@@ -39,5 +38,3 @@ REM   rd /s /q "%USERPROFILE%\\.ncrunch"
 REM )
 
 echo Done.
-REM endlocal
-REM pause
