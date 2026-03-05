8cbef93d16c1373b5669ab45b9f6aab5cd9ee70e
jjvanzon <jjvanzon79@gmail.com>
2026-03-05 18:20:57 +0100

    💇 DevOps - Program.cs as Trimmer Root - Experiment where top-level-statements were sacrificed.

--- Framework/JJ.Framework.Existence.Core.Trimming.TestApp.ValuesToText/Program.cs
+++ Framework/JJ.Framework.Existence.Core.Trimming.TestApp.ValuesToText/Program.cs
@@ -1,10 +1,22 @@
-﻿bool success = 
-RunTests<Coalesce_3Args_ValuesToText_Examples>() &&
-RunTests<Coalesce_3Args_ValuesToText_Extensions>() &&
-RunTests<Coalesce_3Args_ValuesToText_ExtensionsZeroMattersFlagsInBack>() &&
-RunTests<Coalesce_3Args_ValuesToText_ExtensionsZeroMattersFlagsInFront>() &&
-RunTests<Coalesce_3Args_ValuesToText_Static>() &&
-RunTests<Coalesce_3Args_ValuesToText_StaticZeroMattersFlagsInBack>() &&
-RunTests<Coalesce_3Args_ValuesToText_StaticZeroMattersFlagsInFront>();
-WriteLine("Done.");
-if (!success) Exit(1);
+﻿namespace JJ.Framework.Existence.Core.Trimming.TestApp.ValuesToText
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            bool success =
+                RunTests<Coalesce_3Args_ValuesToText_Examples>() &&
+                RunTests<Coalesce_3Args_ValuesToText_Extensions>() &&
+                RunTests<Coalesce_3Args_ValuesToText_ExtensionsZeroMattersFlagsInBack>() &&
+                RunTests<Coalesce_3Args_ValuesToText_ExtensionsZeroMattersFlagsInFront>() &&
+                RunTests<Coalesce_3Args_ValuesToText_Static>() &&
+                RunTests<Coalesce_3Args_ValuesToText_StaticZeroMattersFlagsInBack>() &&
+                RunTests<Coalesce_3Args_ValuesToText_StaticZeroMattersFlagsInFront>();
+
+            System.Console.WriteLine("Done.");
+
+            if (!success)
+                System.Environment.Exit(1);
+        }
+    }
+}
