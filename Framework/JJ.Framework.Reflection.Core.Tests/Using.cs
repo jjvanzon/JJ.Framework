55f91004f11423114360a4d8b76477bb63823332
jjvanzon <jjvanzon79@gmail.com>
2026-03-07 21:04:39 +0100

    ⚠️ Reflection.Core.Tests - Re-enable redundant namespace warning

--- Framework/JJ.Framework.Reflection.Core.Tests/Using.cs
+++ Framework/JJ.Framework.Reflection.Core.Tests/Using.cs
@@ -1,7 +1,4 @@
-﻿// ReSharper disable RedundantUsingDirective.Global
-#pragma warning disable IDE0005
-
-global using System;
+﻿global using System;
 global using System.Collections;
 global using System.Collections.Generic;
 global using System.Linq;
