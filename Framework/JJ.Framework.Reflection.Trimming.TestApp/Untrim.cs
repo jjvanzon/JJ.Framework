f58cd7ca2116144c8a04793e49a2cd447268bc2d
jjvanzon <jjvanzon79@gmail.com>
2026-03-07 22:05:25 +0100

    🪩 Reflection.Legacy.Tests - Untrim types used with reflection.

---
+++ Framework/JJ.Framework.Reflection.Trimming.TestApp/Untrim.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable UnusedType.Global
+
+namespace JJ.Framework.Reflection.Legacy.Trimming.TestApp;
+
+internal static class Untrimmer
+{
+    public static void Untrim()
+    {
+        Tests.CoreTestHelpers.Namespace1.DuplicateClass_13017ef1 _dup1 = new();
+        Tests.CoreTestHelpers.Namespace2.DuplicateClass_13017ef1 _dup2 = new();
+    }
+}
