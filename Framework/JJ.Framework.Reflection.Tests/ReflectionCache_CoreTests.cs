8864f3461736015157b28c4cdf2df5c44d2a98cb
jjvanzon <jjvanzon79@gmail.com>
2026-03-08 03:29:01 +0100

    🥼 Reflection.Legacy.Tests - Expand Method-related tests for ReflectionCache variants.

--- Framework/JJ.Framework.Reflection.Tests/ReflectionCache_CoreTests.cs
+++ Framework/JJ.Framework.Reflection.Tests/ReflectionCache_CoreTests.cs
@@ -341,64 +341,184 @@ public class ReflectionCache_CoreTests
         }
     }
 
-    // Methods
+    // Method
     
     [TestMethod]
-    public void StaticReflectionCache_GetMethod_Test()
+    public void ReflectionCache_GetMethod_Test()
     {
-        for (int i = 0; i < REPEATS; i++)
+        var reflectionCacheLegacy  = new ReflectionCacheLegacy();
+        var reflectionCacheLegacy2 = new ReflectionCacheLegacy(BINDING_FLAGS_ALL);
+
+        Func<MethodInfo>[] synonyms =
+        [
+            () => reflectionCacheLegacy .GetMethod   (typeof(TestClass), "TestMethod"),
+            () => reflectionCacheLegacy2.GetMethod   (typeof(TestClass), "TestMethod"),
+            () => StaticReflectionCache .GetMethod   (typeof(TestClass), "TestMethod"),
+            () => reflectionCacheLegacy .TryGetMethod(typeof(TestClass), "TestMethod"),
+            () => reflectionCacheLegacy2.TryGetMethod(typeof(TestClass), "TestMethod"),
+            () => StaticReflectionCache .TryGetMethod(typeof(TestClass), "TestMethod")
+        ];
+
+        foreach (var func in synonyms)
         {
-            MethodInfo method = StaticReflectionCache.GetMethod(typeof(TestClass), "TestMethod");
-            AssertMethod(method);
-            
-            // NOTE: It's pretty strict you need to supply parameter types.
-            MethodInfo method2 = StaticReflectionCache.GetMethod(typeof(TestClass), "TestMethod2", typeof(int), typeof(string));
-            AssertMethod2(method2);
+            for (int i = 0; i < REPEATS; i++)
+            {
+                MethodInfo method = func();
+                AssertMethod(method);
+            }
         }
     }
     
     [TestMethod]
-    public void StaticReflectionCache_TryGetMethod_Test()
+    public void ReflectionCache_GetMethod_Test_WithParameterTypes()
     {
-        for (int i = 0; i < REPEATS; i++)
-        {
-            MethodInfo method = StaticReflectionCache.TryGetMethod(typeof(TestClass), "TestMethod");
-            AssertMethod(method);
+        var reflectionCacheLegacy  = new ReflectionCacheLegacy();
+        var reflectionCacheLegacy2 = new ReflectionCacheLegacy(BINDING_FLAGS_ALL);
 
-            // NOTE: It's pretty strict you need to supply parameter types.
-            MethodInfo method2 = StaticReflectionCache.TryGetMethod(typeof(TestClass), "TestMethod2", typeof(int), typeof(string));
-            AssertMethod2(method2);
-        } 
-     }
-    
-    [TestMethod]
-    public void StaticReflectionCache_GetMethod_NotFound_Throws()
-    {
-        ThrowsExceptionContaining(
-            () => StaticReflectionCache.GetMethod(typeof(TestClass), _nonExistentName), 
-            "Method", "not found");
+        // NOTE:It's pretty strict that you must supply parameter types.
+        Func<MethodInfo>[] synonyms =
+        [
+            () => reflectionCacheLegacy .GetMethod   (typeof(TestClass), "TestMethod2", typeof(int), typeof(string)),
+            () => reflectionCacheLegacy2.GetMethod   (typeof(TestClass), "TestMethod2", typeof(int), typeof(string)),
+            () => StaticReflectionCache .GetMethod   (typeof(TestClass), "TestMethod2", typeof(int), typeof(string)),
+            () => reflectionCacheLegacy .TryGetMethod(typeof(TestClass), "TestMethod2", typeof(int), typeof(string)),
+            () => reflectionCacheLegacy2.TryGetMethod(typeof(TestClass), "TestMethod2", typeof(int), typeof(string)),
+            () => StaticReflectionCache .TryGetMethod(typeof(TestClass), "TestMethod2", typeof(int), typeof(string)),
+        ];
+
+        foreach (var func in synonyms)
+        {
+            for (int i = 0; i < REPEATS; i++)
+            {
+                MethodInfo method2 = func();
+                AssertMethod2(method2);
+            }
+        }
     }
     
     [TestMethod]
-    public void StaticReflectionCache_TryGetMethod_NotFound_ReturnsNull()
+    public void ReflectionCache_TryGetMethod_Test()
     {
-        MethodInfo method = StaticReflectionCache.TryGetMethod(typeof(TestClass), _nonExistentName);
-        IsNull(() => method);
+        var reflectionCacheLegacy  = new ReflectionCacheLegacy();
+        var reflectionCacheLegacy2 = new ReflectionCacheLegacy(BINDING_FLAGS_ALL);
+
+        Func<MethodInfo>[] synonyms =
+        [
+            () => reflectionCacheLegacy .TryGetMethod(typeof(TestClass), "TestMethod"),
+            () => reflectionCacheLegacy2.TryGetMethod(typeof(TestClass), "TestMethod"),
+            () => StaticReflectionCache .TryGetMethod(typeof(TestClass), "TestMethod"),
+            () => reflectionCacheLegacy .GetMethod   (typeof(TestClass), "TestMethod"),
+            () => reflectionCacheLegacy2.GetMethod   (typeof(TestClass), "TestMethod"),
+            () => StaticReflectionCache .GetMethod   (typeof(TestClass), "TestMethod"),
+        ];
+
+        foreach (var func in synonyms)
+        {
+            for (int i = 0; i < REPEATS; i++)
+            {
+                MethodInfo method = func();
+                AssertMethod(method);
+            }
+        }
     }
     
     [TestMethod]
-    public void StaticReflectionCache_GetMethods()
+    public void ReflectionCache_TryGetMethod_Test_WithParameterTypes()
     {
-        for (int i = 0; i < REPEATS; i++)
-        {
-            IList<MethodInfo> methods = StaticReflectionCache.GetMethods(typeof(TestClass), BINDING_FLAGS_ALL);
-            IsNotNull(() => methods);
+        var reflectionCacheLegacy  = new ReflectionCacheLegacy();
+        var reflectionCacheLegacy2 = new ReflectionCacheLegacy(BINDING_FLAGS_ALL);
 
-            MethodInfo? method = methods.FirstOrDefault(x => x.Name == "TestMethod");
-            AssertMethod(method);
-            
-            MethodInfo? method2 = methods.FirstOrDefault(x => x.Name == "TestMethod2");
-            AssertMethod2(method2);
+        // NOTE:It's pretty strict that you must supply parameter types.
+        Func<MethodInfo>[] synonyms2 =
+        [
+            () => reflectionCacheLegacy .TryGetMethod(typeof(TestClass), "TestMethod2", typeof(int), typeof(string)),
+            () => reflectionCacheLegacy2.TryGetMethod(typeof(TestClass), "TestMethod2", typeof(int), typeof(string)),
+            () => StaticReflectionCache .TryGetMethod(typeof(TestClass), "TestMethod2", typeof(int), typeof(string)),
+            () => reflectionCacheLegacy .GetMethod   (typeof(TestClass), "TestMethod2", typeof(int), typeof(string)),
+            () => reflectionCacheLegacy2.GetMethod   (typeof(TestClass), "TestMethod2", typeof(int), typeof(string)),
+            () => StaticReflectionCache .GetMethod   (typeof(TestClass), "TestMethod2", typeof(int), typeof(string)),
+        ];
+
+        foreach (var func in synonyms2)
+        {
+            for (int i = 0; i < REPEATS; i++)
+            {
+                MethodInfo method2 = func();
+                AssertMethod2(method2);
+            }
+        }
+    }
+    
+    [TestMethod]
+    public void ReflectionCache_GetMethod_NotFound_Throws()
+    {
+        var reflectionCacheLegacy  = new ReflectionCacheLegacy();
+        var reflectionCacheLegacy2 = new ReflectionCacheLegacy(BINDING_FLAGS_ALL);
+
+        Action[] synonyms =
+        [
+            () => StaticReflectionCache .GetMethod(typeof(TestClass), _nonExistentName),
+            () => reflectionCacheLegacy .GetMethod(typeof(TestClass), _nonExistentName),
+            () => reflectionCacheLegacy2.GetMethod(typeof(TestClass), _nonExistentName),
+        ];
+
+        foreach (var action in synonyms)
+        {
+            ThrowsExceptionContaining(action, "Method", "not found");
+        }
+    }
+    
+    [TestMethod]
+    public void ReflectionCache_TryGetMethod_NotFound_ReturnsNull()
+    {
+        var reflectionCacheLegacy  = new ReflectionCacheLegacy();
+        var reflectionCacheLegacy2 = new ReflectionCacheLegacy(BINDING_FLAGS_ALL);
+
+        Func<MethodInfo>[] synonyms =
+        [
+            () => reflectionCacheLegacy .TryGetMethod(typeof(TestClass), _nonExistentName),
+            () => reflectionCacheLegacy2.TryGetMethod(typeof(TestClass), _nonExistentName),
+            () => StaticReflectionCache .TryGetMethod(typeof(TestClass), _nonExistentName),
+        ];
+
+        foreach (var func in synonyms)
+        {
+            for (int i = 0; i < REPEATS; i++)
+            {
+                MethodInfo method = func();
+                IsNull(() => method);
+            }
+        }
+    }
+    
+    // Methods
+
+    [TestMethod]
+    public void ReflectionCache_GetMethods()
+    {
+        var reflectionCacheLegacy = new ReflectionCacheLegacy();
+        var reflectionCacheLegacy2 = new ReflectionCacheLegacy(BINDING_FLAGS_ALL);
+
+        Func<IList<MethodInfo>>[] synonyms =
+        [
+            () => reflectionCacheLegacy .GetMethods(typeof(TestClass)),
+            () => reflectionCacheLegacy2.GetMethods(typeof(TestClass)),
+            () => StaticReflectionCache .GetMethods(typeof(TestClass), BINDING_FLAGS_ALL)
+        ];
+
+        foreach (var func in synonyms)
+        {
+            for (int i = 0; i < REPEATS; i++)
+            {
+                IList<MethodInfo> methods = func();
+                IsNotNull(() => methods);
+
+                MethodInfo? method = methods.FirstOrDefault(x => x.Name == "TestMethod");
+                AssertMethod(method);
+                
+                MethodInfo? method2 = methods.FirstOrDefault(x => x.Name == "TestMethod2");
+                AssertMethod2(method2);
+            }
         }
     }
     
