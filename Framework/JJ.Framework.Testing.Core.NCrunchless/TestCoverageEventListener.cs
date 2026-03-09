254f40c2f0297cf27404c99be7d1dbe9811e788c
jjvanzon <jjvanzon79@gmail.com>
2026-03-09 21:19:45 +0100

    👻 DevOps - Add minimal nCrunch instrumentation shim to make Trimming.TestApp.exe runs succeed (first for Business.Legacy.Trimming.TestApp).

---
+++ Framework/JJ.Framework.Testing.Core.NCrunchless/TestCoverageEventListener.cs
@@ -0,0 +1,11 @@
+﻿namespace nCrunch.TestRuntime.DotNetCore;
+
+/// <summary>
+/// Shim 
+/// </summary>
+public class TestCoverageEventListener
+{
+    public static void NCrunchMarkClassInstanceConstructed(int a, int b) { }
+    public static void NCrunchEnsureStackSpaceExists() { }
+    public static void NCrunchMarkCodeCoverage(int a, int b) { }
+}
