fe88827e3a564216fa7e10cfe16e730e65699281
jjvanzon <jjvanzon79@gmail.com>
2026-03-11 15:25:21 +0100

    🧪 Reflection.Legacy.Tests - Preliminary GetItemType_WithPropertyInfo test.

--- Framework/JJ.Framework.Reflection.Tests/ItemType_CoreTests.cs
+++ Framework/JJ.Framework.Reflection.Tests/ItemType_CoreTests.cs
@@ -1,6 +1,6 @@
 ﻿#pragma warning disable IDE0200
 
-﻿namespace JJ.Framework.Reflection.Legacy.Tests;
+namespace JJ.Framework.Reflection.Legacy.Tests;
 
 using static ReflectionHelper;
 
@@ -8,34 +8,36 @@ using static ReflectionHelper;
 [TestClass]
 public class ItemType_CoreTests
 {
+    public int[] TestColl { get; set; }
+
     [TestMethod]
     [Suppress("Trimmer", "IL2026", Justification = ObjectGetType)]
     public void ItemTypes_WithCollectionInstance()
     {
-        IList<Func<object, Type>> synonyms = 
+        IList<Func<object, Type>> synonyms =
         [
             x => GetItemType(x),
             x => x.GetItemType()
         ];
-        
+
         foreach (var getItemType in synonyms)
         {
             Type itemType = getItemType(new[] { 1, 2, 3 });
             AreEqual(typeof(int), itemType);
         }
     }
-    
+
     [TestMethod]
     public void ItemTypes_WithCollectionType()
     {
-        IList<Func<Type, Type>> synonyms = 
+        IList<Func<Type, Type>> synonyms =
         [
             x =>      GetItemType(x),
             x => x.   GetItemType( ),
             x =>   TryGetItemType(x),
             x => x.TryGetItemType( ) 
         ];
-         
+
         foreach (var getItemType in synonyms)
         {
             AreEqual(typeof(int), getItemType(typeof(int[])));
@@ -44,15 +46,34 @@ public class ItemType_CoreTests
         }
     }
 
+    [Suppress("Trimmer", "IL2026", Justification = PropertyType)]
     [TestMethod]
-    public void GetItemType_NoItemTypeException()
+    public void GetItemType_WithPropertyInfo()
     {
-        IList<Func<Type, Type>> funcs = 
+        IList<Func<PropertyInfo, Type>> synonyms =
         [
             x => GetItemType(x),
             x => x.GetItemType()
         ];
-        
+
+        PropertyInfo? property = typeof(ItemType_CoreTests).GetProperty(nameof(TestColl));
+        ThrowIfNull(property);
+
+        foreach (var getItemType in synonyms)
+        {
+            AreEqual(typeof(int), getItemType(property));
+        }
+    }
+
+    [TestMethod]
+    public void GetItemType_NoItemTypeException()
+    {
+        IList<Func<Type, Type>> funcs =
+        [
+            x => GetItemType(x),
+            x => x.GetItemType()
+        ];
+
         IList<Type> types =
         [
             GetType(),
@@ -63,7 +84,7 @@ public class ItemType_CoreTests
 
         foreach (Type type in types)
         foreach (var getItemType in funcs)
-        { 
+        {
             ThrowsExceptionContaining(() => getItemType(type), "has no item type.");
         }
     }
@@ -72,12 +93,12 @@ public class ItemType_CoreTests
     [TestMethod]
     public void TryGetItemType_NoItemType_ReturnsNull()
     {
-        IList<Func<Type, Type>> synonyms = 
+        IList<Func<Type, Type>> synonyms =
         [
             x => TryGetItemType(x),
             x => x.TryGetItemType()
         ];
-        
+
         IList<Type> types =
         [
             GetType(),
