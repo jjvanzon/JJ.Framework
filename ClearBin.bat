7b7b2f37234e4f23398a0615e06dc5eaf1dfadc6
jjvanzon <jjvanzon79@gmail.com>
2026-03-11 05:50:15 +0100

    🔃 DevOps - ClearBin.bat - Fix misplaced @echo off

--- ClearBin.bat
+++ ClearBin.bat
@@ -1,3 +1,5 @@
+@echo off
+
 REM /d    = directories
 REM /r    = recurse
 REM %~dp0 = Folder of bat file
@@ -5,7 +7,6 @@ REM %%D   = Folder path
 REM /s    = recursive
 REM /q    = quiet
 
-@echo off
 for /d /r "%~dp0" %%D in (bin) do (
     if exist "%%D" (
         echo Deleting "%%D"
