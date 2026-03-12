65f743cb772d3ee3cc1d8cd99564d4f625cf2cbe
jjvanzon <jjvanzon79@gmail.com>
2026-03-12 20:24:14 +0100

    🥼 PlatformCompatibility.Legacy - Trim Tests - Reference intentionally unrun tests to appease ReSharper error.

--- Framework/JJ.Framework.PlatformCompatibility.Trimming.TestApp/Program.cs
+++ Framework/JJ.Framework.PlatformCompatibility.Trimming.TestApp/Program.cs
@@ -1,4 +1,14 @@
-﻿bool success = 
+﻿// Not worth untrimming, refecerenced here
+// to avpod ReSharper unused type error.
+using JJ.Framework.PlatformCompatibility.Tests.Obsolete;
+var excludedTests = new[]
+{
+    typeof(Encoding_PlatformSafe_Obsolete_Tests),
+    typeof(PlatformExtensions_Obsolete_Tests),
+    typeof(PlatformHelper_Obsolete_Tests)
+};
+
+bool success = 
 RunTests<CultureInfo_PlatformSafe_Tests>() &&
 RunTests<Encoding_PlatformSafe_Tests>() &&
 RunTests<MemberTypes_PlatformSafe_Tests>() &&
@@ -6,6 +16,6 @@ RunTests<PropertyInfo_GetValue_PlatformSafe_Tests>() &&
 RunTests<Type_GetInterface_PlatformSafe_Tests>() &&
 RunTests<XDocument_XElement_PlatformSafe_Tests>() &&
 RunTests<PropertyInfo_PlatformSafe_CoreTests>();
-;
+
 WriteLine("Done.");
 if (!success) Exit(1);
