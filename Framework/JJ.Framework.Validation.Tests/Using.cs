939148d585b0c4b691443ba8da1a4506ae005fcf
jjvanzon <jjvanzon79@gmail.com>
2026-03-14 01:48:04 +0100

    📝 Validation.Legacy.Tests - ReSharper warning squelch "unused using" (inconsistently greyed out among .Tests and .Trimming.TestApp projects).

--- Framework/JJ.Framework.Validation.Tests/Using.cs
+++ Framework/JJ.Framework.Validation.Tests/Using.cs
@@ -1,8 +1,10 @@
 global using System;
 global using System.Linq;
 global using System.Collections;
+// ReSharper disable once RedundantUsingDirective.Global
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using JJ.Framework.Reflection.Legacy;
+// ReSharper disable once RedundantUsingDirective.Global
 global using JJ.Framework.Validation.Legacy;
 global using static System.Environment;
 global using static JJ.Framework.Testing.Core.AssertCore;
