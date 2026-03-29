54e623f02bb330d3235e04baa8c075619f27b2f6
jjvanzon <jjvanzon79@gmail.com>
2026-03-29 21:55:41 +0200

    📜 PlatformCompatibility.Core - Remove stail scripting from ClearNCrunch.bat with seemingly bogus code for cache clearing.

--- ClearNCrunch.bat
+++ ClearNCrunch.bat
@@ -2,33 +2,17 @@
 
 echo Clearing NCrunch caches (will only remove cache folders, not .ncrunchproject/.ncrunchsolution files)...
 
-rem Also look for NCrunch-generated files (e.g. nCrunchTemp_*.csproj.user) and report them.
-for /r %%F in ("nCrunchTemp_*.*" ".NCrunch*") do (
-  echo Deleting "%%~fF"
-  del "%%~fF"
-)
-
-rem ------------------------------------------------------------------
 rem Remove per-user/global NCrunch cache under Local AppData.
 rem This is the heavy cache and safe to remove; NCrunch will re-create it.
-rem ------------------------------------------------------------------
 if exist "%LOCALAPPDATA%\NCrunch" (
   echo Deleting "%LOCALAPPDATA%\NCrunch"
   rd /s /q "%LOCALAPPDATA%\NCrunch"
 )
 
-REM Surplus:
-
-rem Some caches may be created in a hidden ".ncrunch" folder
-REM if exist ".ncrunch" (
-REM   echo Deleting ".ncrunch"
-REM   rd /s /q ".ncrunch"
-REM )
-
-rem Some installations use a dot-folder in the user profile
-REM if exist "%USERPROFILE%\\.ncrunch" (
-REM   echo Deleting "%USERPROFILE%\\.ncrunch"
-REM   rd /s /q "%USERPROFILE%\\.ncrunch"
-REM )
+rem Also look for NCrunch-generated files (e.g. nCrunchTemp_*.csproj.user) and delete them.
+for /r %%F in ("nCrunchTemp_*.*" ".NCrunch*") do (
+  echo Deleting "%%~fF"
+  del "%%~fF"
+)
 
 echo Done.
