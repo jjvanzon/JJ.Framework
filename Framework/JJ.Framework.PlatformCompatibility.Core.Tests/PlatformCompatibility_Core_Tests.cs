c11ea31ae74086dc0ea52d8071e4ae9b94bb47d4
jjvanzon <jjvanzon79@gmail.com>
2026-03-30 19:40:40 +0200

    🎯 PlatformCompatibility.Core - Move tests to separate TestBase project. Test projects use TestBase. Add NetStandard variants of test projs. This makes tests hit both abstract (.NET Standard) target as well as all the explicitly supported concrete .NETs (461, 5, 6, 7, etc.).

--- Framework/JJ.Framework.PlatformCompatibility.Core.Tests/PlatformCompatibility_Core_Tests.cs
+++ Framework/JJ.Framework.PlatformCompatibility.Core.Tests/PlatformCompatibility_Core_Tests.cs
@@ -1,139 +1,29 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-using static System.Console;
-using static System.TimeSpan;
-// ReSharper disable VariableCanBeNotNullable
-// ReSharper disable UseSymbolAlias
-
+﻿
 namespace JJ.Framework.PlatformCompatibility.Core.Tests;
 
 [TestClass]
 public class PlatformCompatibility_Core_Tests
 {
-    // CallerArgumentExpression
-    
-    [TestMethod]
-    public void Test_CallerArgumentExpression_PlatformStub()
-    {
-        var attribute = new CallerArgumentExpressionAttribute("name");
-        AreEqual("name", attribute.ParameterName);
+    private readonly PlatformCompatibility_Core_Tests_Base _testBase = new();
 
-        int i = 5;
-        CallerArgumentExpressionDummy(i);
-    }
-    
-    private void CallerArgumentExpressionDummy(int arg, [CallerArgumentExpression(nameof(arg))] string expr = "")
-    {
-        AreEqual("i", expr);
-    }
+    [TestMethod]
+    public void Test_CallerArgumentExpression_PlatformStub() => _testBase.Test_CallerArgumentExpression_PlatformStub();
 
-    // ExceptionSupport
+    [TestMethod]
+    public void Test_ExceptionSupport_PlatformStub() => _testBase.Test_ExceptionSupport_PlatformStub();
     
     [TestMethod]
-    public void Test_ExceptionSupport_PlatformStub()
-    {
-        string? nullText = null;
-        string? emptyText = "";
-        string? spaceText = " \t \n ";
-        ThrowsExceptionContaining<ArgumentNullException>(() => ThrowIfNull            (nullText ), "nullText",  "null");
-        ThrowsExceptionContaining<ArgumentNullException>(() => ThrowIfNullOrWhiteSpace(nullText ), "nullText",  "null");
-        ThrowsExceptionContaining<ArgumentException    >(() => ThrowIfNullOrWhiteSpace(emptyText), "emptyText", "empty");
-        ThrowsExceptionContaining<ArgumentException    >(() => ThrowIfNullOrWhiteSpace(spaceText), "spaceText", "space");
-    }
-    
-    // HashCode
+    public void Test_HashCode_PlatformStub() => _testBase.Test_HashCode_PlatformStub();
     
     [TestMethod]
-    public void Test_HashCode_PlatformStub()
-    {
-        int hashCode1 =  HashCode.Combine(1);
-        int hashCode2 =  HashCode.Combine(1, 2l);
-        int hashCode3 =  HashCode.Combine(1, 2l, 3f);
-        int hashCode4 =  HashCode.Combine(1, 2l, 3f, 4d);
-        int hashCode5 =  HashCode.Combine(1, 2l, 3f, 4d, 5m);
-        int hashCode6 =  HashCode.Combine(1, 2l, 3f, 4d, 5m, "6");
-        int hashCode7 =  HashCode.Combine(1, 2l, 3f, 4d, 5m, "6", (char)7);
-        int hashCode8 =  HashCode.Combine(1, 2l, 3f, 4d, 5m, "6", (char)7, FromMinutes(8));
-        WriteLine(TextOf(HashCode.Combine(1)) + " = " + hashCode1);
-        WriteLine(TextOf(HashCode.Combine(1, 2l)) + " = " + hashCode2);
-        WriteLine(TextOf(HashCode.Combine(1, 2l, 3f)) + " = " + hashCode3);
-        WriteLine(TextOf(HashCode.Combine(1, 2l, 3f, 4d)) + " = " + hashCode4);
-        WriteLine(TextOf(HashCode.Combine(1, 2l, 3f, 4d, 5m)) + " = " + hashCode5);
-        WriteLine(TextOf(HashCode.Combine(1, 2l, 3f, 4d, 5m, "6")) + " = " + hashCode6);
-        WriteLine(TextOf(HashCode.Combine(1, 2l, 3f, 4d, 5m, "6", (char)7)) + " = " + hashCode7);
-        WriteLine(TextOf(HashCode.Combine(1, 2l, 3f, 4d, 5m, "6", (char)7, FromMinutes(8))) + " = " + hashCode8);
-        IsTrue(hashCode1 != 0);
-        IsTrue(hashCode2 != 0);
-        IsTrue(hashCode3 != 0);
-        IsTrue(hashCode4 != 0);
-        IsTrue(hashCode5 != 0);
-        IsTrue(hashCode6 != 0);
-        IsTrue(hashCode7 != 0);
-        IsTrue(hashCode8 != 0);
-    }
-    
-    // NotNullWhen
+    public void Test_NotNullWhen_PlatformStub() => _testBase.Test_NotNullWhen_PlatformStub();
     
     [TestMethod]
-    public void Test_NotNullWhen_PlatformStub()
-    {
-        var attr = new NotNullWhenAttribute(true);
-        IsTrue(attr.ReturnValue);
-        
-        // ReSharper disable once VariableCanBeNotNullable
-        string? nullyFilled = "";
-        string? @null = null;
-        
-        if (!CheckNotNull(nullyFilled))
-        // ncrunch: no coverage start
-        {
-            string num = nullyFilled; // Compiles: Successfully promoted to non-null.
-            NoNullRet(nullyFilled);
-        }
-        // ncrunch: no coverage end
-        
-        if (CheckIsNull(@null))
-        {
-            // Does not compile = correct = not promoted to non-null
-            //string noNull = nullText;
-        }
-    }
-    
-    private bool CheckNotNull([NotNullWhen(true)] string? text) => text != null;
-    private bool CheckIsNull([NotNullWhen(false)] string? text) => text == null;
-    
-    // OverloadResolutionPriority
-    
-    [TestMethod]
-    public void Test_PlatformStub_OverloadResolutionPriority_Create()
-    {
-        var prio1Attribute = new OverloadResolutionPriorityAttribute(1);
-        AreEqual(1, prio1Attribute.Priority);
+    public void Test_PlatformStub_OverloadResolutionPriority_Create() => _testBase.Test_PlatformStub_OverloadResolutionPriority_Create();
 
-        var prio0Attribute = new OverloadResolutionPriorityAttribute(0);
-        AreEqual(0, prio0Attribute.Priority);
-    }
+    [TestMethod]
+    public void Test_PlatformStub_OverloadResolutionPriority_Implicit() => _testBase.Test_PlatformStub_OverloadResolutionPriority_Implicit();
     
     [TestMethod]
-    public void Test_PlatformStub_OverloadResolutionPriority_Implicit()
-    {
-        AreEqual("Overload(A,B)", Overload("A", "B"));
-        AreEqual("Overload(A)", Overload("A"));
-    }
-    
-    private string Overload(string text) => $"Overload({text})";
-    // ReSharper disable once MethodOverloadWithOptionalParameter
-    private string Overload(string text1, string text2 = "C") => $"Overload({text1},{text2})";
-    
-    [TestMethod]
-    public void Test_PlatformStub_OverloadResolutionPriority_Explicit()
-    {
-        AreEqual("WithPrios(A,B)", WithPrios("A", "B"));
-        // Here, OverloadResolutionPriority(1) leads the single-arg call to the two-arg method instead.
-        AreEqual("WithPrios(A,C)", WithPrios("A"));
-    }
-    
-    private string WithPrios(string text) => $"WithPrios({text})"; // ncrunch: no coverage
-    [OverloadResolutionPriority(1)]
-    private string WithPrios(string text1, string text2 = "C") => $"WithPrios({text1},{text2})";
+    public void Test_PlatformStub_OverloadResolutionPriority_Explicit() => _testBase.Test_PlatformStub_OverloadResolutionPriority_Explicit();
 }
--- Framework/JJ.Framework.PlatformCompatibility.Core.Tests/Using.cs
+++ Framework/JJ.Framework.PlatformCompatibility.Core.Tests/Using.cs
@@ -1,4 +1,5 @@
 global using System;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using JJ.Framework.PlatformCompatibility.Core.TestBase;
 global using static JJ.Framework.Testing.Core.AssertCore;
 global using static JJ.Framework.Common.Core.NameHelper;
