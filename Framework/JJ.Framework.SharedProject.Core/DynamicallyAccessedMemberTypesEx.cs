02248ca88db0986ad111ad8f3f531a5890801925
jjvanzon <jjvanzon79@gmail.com>
2026-03-31 19:12:21 +0200

    🧼 SharedProject.Core - DynamicallyAccessedMemberTypesEx - Clean up magic numbers.

--- Framework/JJ.Framework.SharedProject.Core/DynamicallyAccessedMemberTypesEx.cs
+++ Framework/JJ.Framework.SharedProject.Core/DynamicallyAccessedMemberTypesEx.cs
@@ -12,14 +12,39 @@ namespace JJ.Framework.SharedProject.Core;
 internal static class DynamicallyAccessedMemberTypesEx
 {
     // Aliases
-    
+
     /// <inheritdoc cref="_properties" />
-    public const DynamicallyAccessedMemberTypes AllProps = PublicProperties | NonPublicProperties | (DynamicallyAccessedMemberTypes)0x40000;
+    public const DynamicallyAccessedMemberTypes AllProps = AllProperties;
     public const DynamicallyAccessedMemberTypes NonPublicProps = NonPublicProperties;
-    public const DynamicallyAccessedMemberTypes Intf = (DynamicallyAccessedMemberTypes)0x2000;
+    public const DynamicallyAccessedMemberTypes Intf = Interfaces;
     public const DynamicallyAccessedMemberTypes New = PublicParameterlessConstructor;
     public const DynamicallyAccessedMemberTypes PublicCtors = PublicConstructors;
     public const DynamicallyAccessedMemberTypes Ctors = AllCtors;
     public const DynamicallyAccessedMemberTypes AllCtors = PublicConstructors | NonPublicConstructors;
-    public const DynamicallyAccessedMemberTypes PropsFieldsMethods = AllProps | PublicFields | NonPublicFields | (DynamicallyAccessedMemberTypes)0x10000 | PublicMethods | NonPublicMethods | (DynamicallyAccessedMemberTypes)0x8000;
+    public const DynamicallyAccessedMemberTypes PropsFieldsMethods = AllProps | AllFields | AllMethods;
+
+    // Private helpers
+
+    // (Not defined in every framework, and this code can't reach the shim values from PlatformCompatibility.Core.)
+
+    private const DynamicallyAccessedMemberTypes Interfaces 
+        = (DynamicallyAccessedMemberTypes)0x2000;
+
+    private const DynamicallyAccessedMemberTypes InheritedNonPublicFields
+        = (DynamicallyAccessedMemberTypes)0x10000;
+
+    private const DynamicallyAccessedMemberTypes InheritedNonPublicProperties
+        = (DynamicallyAccessedMemberTypes)0x40000;
+
+    private const DynamicallyAccessedMemberTypes InheritedNonPublicMethods 
+        = (DynamicallyAccessedMemberTypes)0x8000;
+
+    private const DynamicallyAccessedMemberTypes AllFields
+        = PublicFields | NonPublicFields | InheritedNonPublicFields;
+
+    private const DynamicallyAccessedMemberTypes AllProperties
+        = PublicProperties | (NonPublicProperties | InheritedNonPublicProperties);
+
+    private const DynamicallyAccessedMemberTypes AllMethods 
+        = PublicMethods | NonPublicMethods | InheritedNonPublicMethods;
 }
