6655f30664b4ecbf373c90febbb109b635541176
jjvanzon <jjvanzon79@gmail.com>
2026-04-01 00:09:51 +0200

    👻 PlatformCompatibility.Core Tests - Working on distributing shim values across .NET versions for DynamicallyAccessedMemberTypes

--- Framework/JJ.Framework.PlatformCompatibility.Core/DynamicallyAccessedMemberTypesSupport.cs
+++ Framework/JJ.Framework.PlatformCompatibility.Core/DynamicallyAccessedMemberTypesSupport.cs
@@ -7,7 +7,7 @@ namespace JJ.Framework.PlatformCompatibility.Core;
 
 internal static class DynamicallyAccessedMemberTypesSupport
 {
-    #if NET5_0
+    #if !NET6_0_OR_GREATER
     public const DynamicallyAccessedMemberTypes Interfaces = (DynamicallyAccessedMemberTypes)8192;
     #endif
     #if NET5_0_OR_GREATER && !NET10_0
@@ -15,5 +15,15 @@ internal static class DynamicallyAccessedMemberTypesSupport
     public const DynamicallyAccessedMemberTypes AllFields = PublicFields | NonPublicFields;
     public const DynamicallyAccessedMemberTypes AllConstructors = PublicConstructors | NonPublicConstructors;
     public const DynamicallyAccessedMemberTypes AllMethods = PublicMethods | NonPublicMethods;
+    #endif
+    #if !NET10_0_OR_GREATER
+    public const DynamicallyAccessedMemberTypes NonPublicConstructorsWithInherited = (DynamicallyAccessedMemberTypes)0b000000000100000000000100;
+    public const DynamicallyAccessedMemberTypes NonPublicMethodsWithInherited      = (DynamicallyAccessedMemberTypes)0b000000001000000000010000;
+    public const DynamicallyAccessedMemberTypes NonPublicFieldsWithInherited       = (DynamicallyAccessedMemberTypes)0b000000010000000001000000;
+    public const DynamicallyAccessedMemberTypes NonPublicNestedTypesWithInherited  = (DynamicallyAccessedMemberTypes)0b000000100000000100000000;
+    public const DynamicallyAccessedMemberTypes NonPublicPropertiesWithInherited   = (DynamicallyAccessedMemberTypes)0b000001000000010000000000;
+    public const DynamicallyAccessedMemberTypes NonPublicEventsWithInherited       = (DynamicallyAccessedMemberTypes)0b000010000001000000000000;
+    public const DynamicallyAccessedMemberTypes PublicConstructorsWithInherited    = (DynamicallyAccessedMemberTypes)0b000100000000000000000011;
+    public const DynamicallyAccessedMemberTypes PublicNestedTypesWithInherited     = (DynamicallyAccessedMemberTypes)0b001000000000000010000000;
     #endif
 }
