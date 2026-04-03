e5dd2ec590c086bca043f45956741be70f9ea22e
jjvanzon <jjvanzon79@gmail.com>
2026-04-03 18:35:04 +0200

    🥼 MSTestless - Remove dependency on Testing.Core. Added MSTestless to some of the Pacakge.Trimming.TestApps.

--- Framework/Testing.MSTestless/MSTestless/Assert.cs
+++ Framework/Testing.MSTestless/MSTestless/Assert.cs
@@ -1,7 +1,5 @@
 ﻿// ReSharper disable UnusedMember.Global
 
-using JJ.Framework.Testing.Core;
-
 namespace Microsoft.VisualStudio.TestTools.UnitTesting;
 
 /// <inheritdoc cref="_assert" />
@@ -9,13 +7,28 @@ public static class Assert
 {
     /// <inheritdoc cref="_areequal" />
     public static void AreEqual<T>(T? expected, T? actual)
-        => AssertCore.AreEqual(expected, actual);
+    {
+        if (!EqualityComparer<T>.Default.Equals(expected, actual))
+        {
+            throw new Exception($"Assert.AreEqual failed. Expected: {expected}, Actual: {actual}");
+        }
+    }
     
     /// <inheritdoc cref="_aresame" />
     public static void AreSame<T>(T expected, T actual)
-        => AssertCore.AreSame(expected, actual);
+    {
+        if (!ReferenceEquals(expected, actual))
+        {
+            throw new Exception("Assert.AreSame failed. Expected same instance, but got different.");
+        }
+    }
     
     /// <inheritdoc cref="_istrue" />
     public static void IsTrue(bool value)
-        => AssertCore.IsTrue(value);
+    {
+        if (!value)
+        {
+            throw new Exception("Assert.IsTrue failed.");
+        }
+    }
 }
