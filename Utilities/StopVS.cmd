6a08d8c703a46a11b99f53f0510cc27a1a6ce21f
jjvanzon <jjvanzon79@gmail.com>
2026-04-03 18:44:10 +0200

    🔫 Utilities - Expand StopVS range.

--- Utilities/StopVS.cmd
+++ Utilities/StopVS.cmd
@@ -2,4 +2,6 @@ taskkill /im devenv.exe /f
 taskkill /im dotnet.exe /f
 taskkill /im msbuild.exe /f
 taskkill /im VBCSCompiler.exe /f
-taskkill /im copilot-language-server.exe
+taskkill /im copilot-language-server.exe /f
+taskkill /im Microsoft.CodeAnalysis.LanguageServer.exe /f
+taskkill /im vctip.exe /f
