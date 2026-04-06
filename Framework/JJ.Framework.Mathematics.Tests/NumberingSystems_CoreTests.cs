fc22b5dded3ffeb705d14922a85d50cab27cd317
jjvanzon <jjvanzon79@gmail.com>
2026-04-06 20:44:50 +0200

    🔤 Mathematics.Legacy.Tests - Archive FromBase tests with less imaginitive custom digit char array where the char codes were mostly sequential.

--- Framework/JJ.Framework.Mathematics.Tests/NumberingSystems_CoreTests.cs
+++ Framework/JJ.Framework.Mathematics.Tests/NumberingSystems_CoreTests.cs
@@ -8,36 +8,6 @@ using static NumberingSystems;
 public class NumberingSystems_CoreTests
 {
     // FromBase (with digit chars)
-    
-    [TestMethod]
-    public void FromBase_WithDigitChars_Base3()
-    {
-        int result = FromBase("XYZ", b: 3, ['X', 'Y', 'Z']);
-        
-        // X=0, Y=1, Z=2 in base 3
-        // XYZ = 0*3² + 1*3¹ + 2*3⁰ = 0 + 3 + 2 = 5
-        AreEqual(5, result);
-    }
-
-    [TestMethod]
-    public void FromBase_WithDigitChars_Base2()
-    {
-        int result = FromBase("bab", b: 2, ['a', 'b']);
-        
-        // b=1, a=0 in base 2
-        // bab = 1*2² + 0*2¹ + 1*2⁰ = 4 + 0 + 1 = 5
-        AreEqual(5, result);
-    }
-
-    [TestMethod]
-    public void FromBase_WithDigitChars_RoundTripToBase()
-    {
-        char[] digitChars = ['p', 'q', 'r', 's', 't'];
-        string converted = ToBase(123, digitChars);
-        int result = FromBase(converted, 5, digitChars);
-
-        AreEqual(123, result);
-    }
 
     [TestMethod]
     public void FromBase_WithDigitChars_SpecialCharacters()
