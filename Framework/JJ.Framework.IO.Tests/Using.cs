cdb3b52b52427e1ef5fe46c0ccbe059449d723a0
jjvanzon <jjvanzon79@gmail.com>
2026-04-07 06:21:23 +0200

    🔃 IO.Legacy.Tests - Reorder usings so ones used for just trim tests come after the redundant using warning squelch.

--- Framework/JJ.Framework.IO.Tests/Using.cs
+++ Framework/JJ.Framework.IO.Tests/Using.cs
@@ -1,7 +1,8 @@
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using JJ.Framework.IO.Legacy.Tests;
 global using System.Text;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using static JJ.Framework.Testing.Core.AssertCore;
+// ReSharper disable RedundantUsingDirective.Global
+global using JJ.Framework.IO.Legacy.Tests;
 global using static System.Console;
 global using static System.Environment;
-global using static JJ.Framework.Testing.Core.AssertCore;
 global using static JJ.Framework.Testing.Core.TestRunner;
