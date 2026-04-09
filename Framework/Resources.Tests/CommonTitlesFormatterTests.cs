e8aa9e6c59a6cb89e4ab0cefd2006b4803ed1602
jjvanzon <jjvanzon79@gmail.com>
2026-04-09 20:49:29 +0200

    ❕ Resources.Legacy - Use AssertCore via static using for more assertions.

--- Framework/Resources.Tests/CommonTitlesFormatterTests.cs
+++ Framework/Resources.Tests/CommonTitlesFormatterTests.cs
@@ -7,34 +7,34 @@ public class CommonTitlesFormatterTests
     public void EntityCount_WithEntityName_ReturnsFormattedString()
     {
         string result = CommonTitlesFormatter.EntityCount("Items");
-        Assert.AreEqual("Number of Items", result);
+        AreEqual("Number of Items", result);
     }
 
     [TestMethod]
     public void EntityCount_WithSingleCharacter_ReturnsFormattedString()
     {
         string result = CommonTitlesFormatter.EntityCount("X");
-        Assert.AreEqual("Number of X", result);
+        AreEqual("Number of X", result);
     }
 
     [TestMethod]
     public void EntityCount_WithLongEntityName_ReturnsFormattedString()
     {
         string result = CommonTitlesFormatter.EntityCount("VeryLongEntityNameForTesting");
-        Assert.AreEqual("Number of VeryLongEntityNameForTesting", result);
+        AreEqual("Number of VeryLongEntityNameForTesting", result);
     }
 
     [TestMethod]
     public void EntityCount_WithEmptyString_ReturnsFormattedString()
     {
         string result = CommonTitlesFormatter.EntityCount("");
-        Assert.AreEqual("Number of ", result);
+        AreEqual("Number of ", result);
     }
 
     [TestMethod]
     public void EntityCount_WithNull_ReturnsFormattedString()
     {
         string result = CommonTitlesFormatter.EntityCount(null);
-        Assert.AreEqual("Number of ", result);
+        AreEqual("Number of ", result);
     }
 }
