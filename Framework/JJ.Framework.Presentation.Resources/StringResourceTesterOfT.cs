9f8d1587c8f1bbb874ec3bb5e9beb0e11c35f882
jjvanzon <jjvanzon79@gmail.com>
2026-04-12 20:12:25 +0200

    🔃 StringResourceTester<T> - Change order to match test.

--- Framework/JJ.Framework.Presentation.Resources/StringResourceTesterOfT.cs
+++ Framework/JJ.Framework.Presentation.Resources/StringResourceTesterOfT.cs
@@ -1,38 +1,38 @@
-namespace JJ.Framework.StringResources.Legacy
+namespace JJ.Framework.StringResources.Legacy;
+
+/// <summary>
+/// Generic syntax sugar over <see cref="StringResourceTester"/>
+/// so you can write <c>new StringResourceTester&lt;MyResources&gt;(...)</c>
+/// instead of <c>new StringResourceTester(typeof(MyResources), ...)</c>.
+/// </summary>
+public class StringResourceTester<[Dyn(PubProps|PubMethods)] T> : StringResourceTester
 {
-    /// <summary>
-    /// Generic syntax sugar over <see cref="StringResourceTester"/>
-    /// so you can write <c>new StringResourceTester&lt;MyResources&gt;(...)</c>
-    /// instead of <c>new StringResourceTester(typeof(MyResources), ...)</c>.
-    /// </summary>
-    public class StringResourceTester<[Dyn(PubProps|PubMethods)] T> : StringResourceTester
-    {
-        // ReSharper disable once UnusedParameter.Local
-        /// <inheritdoc cref="StringResourceTester{T}" />
-        public StringResourceTester(
-            T resourceObject,
-            string[] known, string unknown, string @default, NoLog nolog)
-            : base(typeof(T), resourceObject, known, unknown, @default, nolog)
-        { }
 
-        // ReSharper disable once UnusedParameter.Local
-        /// <inheritdoc cref="StringResourceTester{T}" />
-        public StringResourceTester(
-            string[] known, string unknown, string @default, NoLog nolog)
-            : base(typeof(T), known, unknown, @default, nolog)
-        { }
+    // ReSharper disable once UnusedParameter.Local
+    /// <inheritdoc cref="StringResourceTester{T}" />
+    public StringResourceTester(
+        string[] known, string unknown, string @default, NoLog nolog)
+        : base(typeof(T), known, unknown, @default, nolog)
+    { }
 
-        /// <inheritdoc cref="StringResourceTester{T}" />
-        public StringResourceTester(
-            string[] known, string unknown, string @default, bool nolog = default)
-            : base(typeof(T), known, unknown, @default, nolog)
-        { }
+    /// <inheritdoc cref="StringResourceTester{T}" />
+    public StringResourceTester(
+        string[] known, string unknown, string @default, bool nolog = default)
+        : base(typeof(T), known, unknown, @default, nolog)
+    { }
 
-        /// <inheritdoc cref="StringResourceTester{T}" />
-        public StringResourceTester(
-            T resourceObject,
-            string[] known, string unknown, string @default, bool nolog = default)
-            : base(typeof(T), resourceObject, known, unknown, @default, nolog)
-        { }
-    }
+    // ReSharper disable once UnusedParameter.Local
+    /// <inheritdoc cref="StringResourceTester{T}" />
+    public StringResourceTester(
+        T resourceObject,
+        string[] known, string unknown, string @default, NoLog nolog)
+        : base(typeof(T), resourceObject, known, unknown, @default, nolog)
+    { }
+
+    /// <inheritdoc cref="StringResourceTester{T}" />
+    public StringResourceTester(
+        T resourceObject,
+        string[] known, string unknown, string @default, bool nolog = default)
+        : base(typeof(T), resourceObject, known, unknown, @default, nolog)
+    { }
 }
