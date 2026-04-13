05838888eb946debc53f37912757a752aae5bbf2
jjvanzon <jjvanzon79@gmail.com>
2026-04-13 02:27:15 +0200

    ⛹️ Framework.Testing - Port Throws assertions with partial exception message matching from main branch. They are so freaking handy. Will make porting test code from main easier.

--- Framework/Testing/AssertHelper.cs
+++ Framework/Testing/AssertHelper.cs
@@ -5,6 +5,8 @@ using JetBrains.Annotations;
 using JJ.Framework.Exceptions.Basic;
 using JJ.Framework.Logging;
 using JJ.Framework.Reflection;
+using static System.StringComparison;
+
 // ReSharper disable InvertIf
 // ReSharper disable RedundantIfElseBlock
 
@@ -333,6 +335,80 @@ namespace JJ.Framework.Testing
             string separator = !string.IsNullOrEmpty(message) ? " " : "";
 
             return $"Assert.{methodName} failed.{separator}{message}";
+        }
+
+        // Throws (Containing)
+
+        public static void Throws(Func<object?> statement, params string[] expectedTexts)
+            => Throws(() => { statement(); }, expectedTexts);
+
+        public static void Throws(Action statement, params string[] expectedTexts)
+        {
+            if (statement == null) throw new NullException(() => statement);
+            if (expectedTexts == null) throw new NullException(() => expectedTexts);
+            
+            try
+            {
+                statement();
+            }
+            catch (Exception ex)
+            {
+                foreach (string expectedText in expectedTexts)
+                {
+                    Contains(expectedText, ex.Message);
+                }
+                
+                return;
+            }
+
+            throw new Exception("An exception should have occurred.");
+        }
+        
+        public static void Throws(Func<object?> statement, Type exceptionType, params string[] expectedTexts)
+            => Throws(() => { statement(); }, exceptionType, expectedTexts);
+
+        public static void Throws(Action statement, Type exceptionType, params string[] expectedTexts)
+        {
+            if (statement == null) throw new NullException(() => statement);
+            if (exceptionType == null) throw new NullException(() => exceptionType);
+            if (expectedTexts == null) throw new NullException(() => expectedTexts);
+            
+            try
+            {
+                statement();
+            }
+            catch (Exception ex)
+            {
+                AreEqual(exceptionType, () => ex.GetType());
+                
+                foreach (string expectedText in expectedTexts)
+                {
+                    Contains(expectedText, ex.Message);
+                }
+                
+                return;
+            }
+
+            throw new Exception("An exception should have occurred.");
+        }
+
+        public static void Throws<TException>(Func<object?> statement, params string[] expectedTexts)
+            => Throws(statement, typeof(TException), expectedTexts);
+
+        public static void Throws<TException>(Action statement, params string[] expectedTexts)
+            => Throws(statement, typeof(TException), expectedTexts);
+                
+        // Partial String Comparison
+
+        private static void Contains(string? expectedText, string? actualText)
+        {
+            actualText ??= "";
+            expectedText ??= "";
+
+            if (!(actualText.IndexOf(expectedText, OrdinalIgnoreCase) >= 0))
+            {
+                throw new Exception($"Message does not contain: '{expectedText}'. Full message: '{actualText}'");
+            }
         }
     }
 }
