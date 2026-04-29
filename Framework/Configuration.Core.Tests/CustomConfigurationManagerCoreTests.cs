49bb26e0aba230e08d44d303f213c1a2d9c58fe9
jjvanzon <jjvanzon79@gmail.com>
2026-04-29 22:03:12 +0200

    🪚 Configuration.Core TrimTests - Suppress Trim warning, which reveals link to .targets file (for config copy actions) beinf frail.

--- Framework/Configuration.Core.Tests/CustomConfigurationManagerCoreTests.cs
+++ Framework/Configuration.Core.Tests/CustomConfigurationManagerCoreTests.cs
@@ -1,15 +1,15 @@
 namespace JJ.Framework.Configuration.Core.Tests;
 
+[Suppress("Trimmer", "IL2026", Justification = PropertyTypeAnd + ObjectGetType)]
 [TestClass]
 public class CustomConfigurationManagerCoreTests
 {
-    const string SectionName = "jj.framework.configuration.core.tests";
+    private const string SECTION_NAME = "jj.framework.configuration.core.tests";
 
     [TestMethod]
     public void CustomConfigurationManagerCore_TryGetSection_BySectionName_ReturnsSection()
     {
-        var section = CustomConfigurationManagerCore.TryGetSection<TestConfigurationSection>(SectionName);
-
+        var section = CustomConfigurationManagerCore.TryGetSection<TestConfigurationSection>(SECTION_NAME);
         IsNotNull(section);
         AreEqual(42, section.IntValue);
         AreEqual("hello", section.StringValue);
@@ -19,7 +19,6 @@ public class CustomConfigurationManagerCoreTests
     public void CustomConfigurationManagerCore_TryGetSection_BySectionName_WhenAbsent_ReturnsNull()
     {
         var section = CustomConfigurationManagerCore.TryGetSection<TestConfigurationSection>("nonexistent.section");
-
         IsNull(section);
     }
 }
