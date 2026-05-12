a4ed73d1db596b23fcfc8c85318bbda34bd57b6d
jjvanzon <jjvanzon79@gmail.com>
2026-05-12 23:46:55 +0200

    💇 Compilation.Core - TrimTests - Add untrimming of the internally not used DotNetInfo.Command setter. (More untrimming should follow.)

---
+++ Framework/Compilation.Core.TrimTests/Untrimmer.cs
@@ -0,0 +1,10 @@
+﻿// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+
+namespace JJ.Framework.Compilation.Core.TrimTests;
+
+internal static class Untrimmer
+{
+    [NoTrim("Command", "JJ.Framework.Compilation.Core.DotNetInfo", "JJ.Framework.Compilation.Core")]
+    public static void Untrim() { }
+}
