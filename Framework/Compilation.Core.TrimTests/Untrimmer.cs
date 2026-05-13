77494ea8ea077caa452a4756641a13f58f4bf64e
jjvanzon <jjvanzon79@gmail.com>
2026-05-13 20:44:09 +0200

    💈 Compilation.Core - TrimTests - A little more rigorous and same in the Untrimmer about internal things accessed.

--- Framework/Compilation.Core.TrimTests/Untrimmer.cs
+++ Framework/Compilation.Core.TrimTests/Untrimmer.cs
@@ -5,16 +5,17 @@ namespace JJ.Framework.Compilation.Core.TrimTests;
 
 internal static class Untrimmer
 {
-    private const string MainAsm = "JJ.Framework.Compilation.Core";
+    public const string MainAsm = "JJ.Framework.Compilation.Core";
 
-    [NoTrim(AllCtors,          $"{MainAsm}.DotNetInfo",       MainAsm)]
-    [NoTrim("set_Command",     $"{MainAsm}.DotNetInfo",       MainAsm)]
-    [NoTrim("set_CommandEnum", $"{MainAsm}.DotNetInfo",       MainAsm)]
-    [NoTrim("set_Args",        $"{MainAsm}.DotNetInfo",       MainAsm)]
-    [NoTrim("set_IsRebuild",   $"{MainAsm}.DotNetInfo",       MainAsm)]
-    [NoTrim("set_ID",          $"{MainAsm}.DotNetInfo",       MainAsm)]
-    [NoTrim("set_Ver",         $"{MainAsm}.DotNetInfo",       MainAsm)]
-    [NoTrim("FormatArgs",      $"{MainAsm}.DotNetArgBuilder", MainAsm)]
-    [NoTrim("Enrich",          $"{MainAsm}.DotNetEnricher",   MainAsm)]
+    [NoTrim(AllCtors,      $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("Command",     $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("CommandEnum", $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("Args",        $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("IsRebuild",   $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("ID",          $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("Ver",         $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("FormatArgs",  $"{MainAsm}.DotNetArgBuilder", MainAsm)]
+    [NoTrim("Enrich",      $"{MainAsm}.DotNetEnricher",   MainAsm)]
+    [NoTrim("Log",         $"{MainAsm}.DotNetLogger",     MainAsm)]
     public static void Untrim() { }
 }
