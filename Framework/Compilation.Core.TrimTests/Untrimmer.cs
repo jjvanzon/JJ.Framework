675d8dbad353f2ad82ab10a93c3a234adfd253b5
jjvanzon <jjvanzon79@gmail.com>
2026-05-13 20:21:31 +0200

    🧑‍💻 Compilation.Core - TrimTests - Add things to the untrimmer for private things accessed through accessors.

--- Framework/Compilation.Core.TrimTests/Untrimmer.cs
+++ Framework/Compilation.Core.TrimTests/Untrimmer.cs
@@ -5,6 +5,16 @@ namespace JJ.Framework.Compilation.Core.TrimTests;
 
 internal static class Untrimmer
 {
-    [NoTrim("Command", "JJ.Framework.Compilation.Core.DotNetInfo", "JJ.Framework.Compilation.Core")]
+    private const string MainAsm = "JJ.Framework.Compilation.Core";
+
+    [NoTrim(AllCtors,          $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("set_Command",     $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("set_CommandEnum", $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("set_Args",        $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("set_IsRebuild",   $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("set_ID",          $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("set_Ver",         $"{MainAsm}.DotNetInfo",       MainAsm)]
+    [NoTrim("FormatArgs",      $"{MainAsm}.DotNetArgBuilder", MainAsm)]
+    [NoTrim("Enrich",          $"{MainAsm}.DotNetEnricher",   MainAsm)]
     public static void Untrim() { }
 }
