27570b622aaf45918567edda40a0d80413c088d1
jjvanzon <jjvanzon79@gmail.com>
2026-05-16 23:10:09 +0200

    🤾‍♀️ Exceptions.Core - Add ThrowExtension text.NotNull() for syntax sugar (returns text again for chaining and inlining).

--- Framework/Exceptions.Core/ThrowExtensions.cs
+++ Framework/Exceptions.Core/ThrowExtensions.cs
@@ -5,4 +5,8 @@ public static class ThrowExtensions
     // ReSharper disable once UnusedParameter.Global
     public static T NotNull<T>(this object chainedFrom, [NotNull] T? x, [ArgExpress(nameof(x))] string expression = "")
         => Throw.NotNull(x, expression);
+
+    // ReSharper disable once UnusedParameter.Global
+    public static T NotNull<T>([NotNull] this T? x, [ArgExpress(nameof(x))] string expression = "")
+        => Throw.NotNull(x, expression);
 }
