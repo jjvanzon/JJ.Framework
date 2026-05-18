36408081d25816b81681eeca3b19b22b2d889ea1
jjvanzon <jjvanzon79@gmail.com>
2026-05-18 05:40:58 +0200

    📛 Compilation.Core - TrimTests - Use nameof to Untrim setters of DotNetArgs properties. The getters are public and nameof is less frail. Setters are untrimmed so test accessors can reach them.

--- Framework/Compilation.Core.TrimTests/Untrimmer.cs
+++ Framework/Compilation.Core.TrimTests/Untrimmer.cs
@@ -6,19 +6,22 @@ namespace JJ.Framework.Compilation.Core.TrimTests;
 internal static class Untrimmer
 {
     public const string MainAsm = "JJ.Framework.Compilation.Core";
-    [NoTrim(AllCtors,            typeof(DotNetArgs))]
-    [NoTrim("set_CommandEnum",   typeof(DotNetArgs))]
-    [NoTrim("set_Command",       typeof(DotNetArgs))]
-    [NoTrim("set_ID",            typeof(DotNetArgs))]
-    [NoTrim("set_Ver",           typeof(DotNetArgs))]
-    [NoTrim("set_Args",          typeof(DotNetArgs))]
-    [NoTrim("set_IsRebuild",     typeof(DotNetArgs))]
-    [NoTrim("set_FullArgs",      typeof(DotNetArgs))]
+    
+    [NoTrim(AllCtors,                                typeof(DotNetArgs))]
+    [NoTrim("set_" + nameof(DotNetArgs.CommandEnum), typeof(DotNetArgs))]
+    [NoTrim("set_" + nameof(DotNetArgs.Command),     typeof(DotNetArgs))]
+    [NoTrim("set_" + nameof(DotNetArgs.ID),          typeof(DotNetArgs))]
+    [NoTrim("set_" + nameof(DotNetArgs.Ver),         typeof(DotNetArgs))]
+    [NoTrim("set_" + nameof(DotNetArgs.Args),        typeof(DotNetArgs))]
+    [NoTrim("set_" + nameof(DotNetArgs.IsRebuild),   typeof(DotNetArgs))]
+    [NoTrim("set_" + nameof(DotNetArgs.FullArgs),    typeof(DotNetArgs))]
+
     [NoTrim("FormatArgs",        $"{MainAsm}.DotNetArgFormatter",   MainAsm)]
     [NoTrim("Enrich",            $"{MainAsm}.DotNetEnricher",       MainAsm)]
     [NoTrim("Log",               $"{MainAsm}.DotNetLogger",         MainAsm)]
     [NoTrim("CommandDescriptor", $"{MainAsm}.DiagnosticsFormatter", MainAsm)]
     [NoTrim("IDVerDescriptor",   $"{MainAsm}.DiagnosticsFormatter", MainAsm)]
     [NoTrim("ArgsDescriptor",    $"{MainAsm}.DiagnosticsFormatter", MainAsm)]
+
     public static void Untrim() { }
 }
