760a4971c58f3311cbb74f3e8a5fb01af3b3349f
jjvanzon <jjvanzon79@gmail.com>
2026-05-19 02:50:58 +0200

    🆙 Compilation.Core - Package Tests - Update to latest on Dev-Feed with DiagnosticsFormatter changes. Correct DiagnosticsFormatterAccessor that Package.TrimTests have trouble mapping null arguments with; specify those parameters types explicitly.

--- Framework/Compilation.Core.Tests/Accessors/DiagnosticsFormatterAccessor.cs
+++ Framework/Compilation.Core.Tests/Accessors/DiagnosticsFormatterAccessor.cs
@@ -4,17 +4,19 @@ namespace JJ.Framework.Compilation.Core.Tests.Accessors;
 [Suppress("Trimmer", "IL3050", Justification = GenericMethod)]
 internal static class DiagnosticsFormatterAccessor
 {
+    private static readonly Type? _ = null;
+
     private static readonly AccessorCore _accessor = new("DiagnosticsFormatter");
 
     public static string? Descriptor(DotNetArgs? args) 
-        => (string?)_accessor.Call(args);
+        => (string?)_accessor.Call([ args ], [ typeof(DotNetArgs) ]);
 
     public static string? CommandDescriptor(DotNetCommandEnum @enum, string? command, bool isRebuild) 
-        => (string?)_accessor.Call(@enum, command, isRebuild);
+        => (string?)_accessor.Call([ @enum, command, isRebuild ], [ _, typeof(string), _ ]);
 
     public static string? IDVerDescriptor(string? id, string? ver)
-        => (string?)_accessor.Call(nameof(IDVerDescriptor), [ id, ver ], [ typeof(string), typeof(string) ]);
+        => (string?)_accessor.Call([ id, ver ], [ typeof(string), typeof(string) ]);
 
     public static string? ArgsDescriptor(string? args, string? fullArgs)
-        => (string?)_accessor.Call(nameof(ArgsDescriptor), [ args, fullArgs ], [ typeof(string), typeof(string) ]);
+        => (string?)_accessor.Call([ args, fullArgs ], [ typeof(string), typeof(string) ]);
 }
