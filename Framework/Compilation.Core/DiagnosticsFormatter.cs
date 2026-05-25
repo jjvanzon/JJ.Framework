b92634af3404381598f06aa4808e3b6a9548df0b
jjvanzon <jjvanzon79@gmail.com>
2026-05-26 00:35:27 +0200

    🩺 Compilation.Core - Preliminary diagnostics descriptors for DotNetResult, to replace the result text and exception message.

---
+++ Framework/Compilation.Core/DiagnosticsFormatter.cs
@@ -0,0 +1,11 @@
+﻿namespace JJ.Framework.Compilation.Core;
+
+internal static partial class DiagnosticsFormatter
+{
+    public static string Descriptor(DotNetArgs? args, DotNetOptions opt, string sep = " | ")
+    {
+        // TODO: Remove redundancies from opt when already in args.
+        string descriptor = $"{Descriptor(args)}{sep}{Descriptor(opt)}";
+        return descriptor;
+    }
+}
--- Framework/Compilation.Core/DotNetResult.cs
+++ Framework/Compilation.Core/DotNetResult.cs
@@ -1,8 +1,10 @@
 ﻿namespace JJ.Framework.Compilation.Core;
 
+[DebuggerDisplay("{DebuggerDisplay}")]
 public record DotNetResult
 {
     public override string ToString() => Text;
+    private string DebuggerDisplay => Text;
     public static implicit operator string(DotNetResult? result) => result?.Text ?? "";
 
     public string Text { get; }
@@ -40,19 +42,101 @@ public record DotNetResult
         bool hasError    = HasExitCode || HasErrorInOutput | HasTimeOut; // Don't consider error text, which has welcome messages and such in it these days.
         Successful       = !hasError;
 
+        Text = FormatText();
+        
+        /*
         Text = Join(NewLine,
                     HasExitCode   ? $"Exit Code = {exitCode}" : "",
                     HasErrorText  ? $"Error = {errorText}" : "",
                     HasOutputText ? $"Output = {outputText}" : "");
+        */
     }
 
     public void Assert()
     {
         if (Successful) return;
+        throw new Exception(FormatExceptionText());
+        /*
         throw new Exception(
             $"dotnet {Args} failed " +
             $"{new { HasExitCode, HasErrorText, HasErrorInOutput, HasTimeOut, Args.FullArgs }}: " +
             $"{TimeOutMessage} " +
             $"Exit code {ExitCode} {ErrorText} {OutputText}");
+        */
+    }
+
+    private string FormatText() => Descriptor(NewLine);
+    private string FormatExceptionText() => Descriptor(" ");
+
+    private string Descriptor(string sep)
+    {
+        bool exitCodeWasAdded = false;
+        bool dotNetWasAdded = false;
+
+        string failurePart = "";
+        if (HasTimeOut)
+        {
+            failurePart = "dotnet TIME OUT !";
+            dotNetWasAdded = true;
+        }
+        else if (HasErrorInOutput)
+        {
+            failurePart = "dotnet ERROR !";
+            dotNetWasAdded = true;
+        }
+        else if (HasExitCode)
+        {
+            failurePart = "dotnet EXIT CODE = " + ExitCode + " !";
+            exitCodeWasAdded = true;
+            dotNetWasAdded = true;
+        }
+
+        string dotNetPart = dotNetWasAdded ? "" : "dotnet ";
+        
+        string argsPart = DiagnosticsFormatter.Descriptor(Args, Opt, sep);
+
+        string timeOutPart = "";
+        if (HasTimeOut)
+        {
+            timeOutPart = TimeOutMessage;
+        }
+        
+        string exitCodePart = "";
+        if (HasExitCode && !exitCodeWasAdded)
+        {
+            exitCodePart = "ExitCode = " + ExitCode;
+        }
+        
+        string errorTextPart = "";
+        if (HasErrorText)
+        {
+            if (Successful)
+            {
+                errorTextPart = $"stdErr ={sep}{ErrorText}";
+            }
+            else
+            {
+                errorTextPart = $"Error ={sep}{ErrorText}";
+            }
+        }
+        
+        string outputTextPart = "";
+        if (HasOutputText)
+        {
+            outputTextPart = $"Output ={sep}{OutputText}";
+        }
+
+        string[] elements = 
+        [
+            failurePart,
+            argsPart,
+            timeOutPart,
+            exitCodePart,
+            errorTextPart,
+            outputTextPart
+        ];
+
+        var descriptor = dotNetPart + Join(sep, elements.Where(FilledIn));
+        return descriptor;
     }
 }
