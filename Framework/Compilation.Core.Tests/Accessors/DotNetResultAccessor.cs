1c633e1eb60a460e601377b594ead77e688e2e7a
jjvanzon <jjvanzon79@gmail.com>
2026-05-27 22:42:53 +0200

    🔬 Compilation.Core.Tests - Make Arg and Opt constructor parameters optional in DotNetResultAccessor to make variablization more terse.

--- Framework/Compilation.Core.Tests/Accessors/DotNetResultAccessor.cs
+++ Framework/Compilation.Core.Tests/Accessors/DotNetResultAccessor.cs
@@ -5,14 +5,18 @@ internal class DotNetResultAccessor
 {
     private readonly AccessorCore _accessor;
 
+    // Every constructor argument made optional, for better overview in the tests.
+
+    private static readonly DotNetArgs DefaultArgs = new DotNetArgsAccessor().Obj;
+
     public DotNetResultAccessor(
-        DotNetOptions opt,
-        DotNetArgs args,
+        DotNetOptions opt = default,
+        DotNetArgs? args = null,
         int exitCode = 0,
         string errorText = "",
         string outputText = "",
         bool hasTimeOut = false)
-        => _accessor = new(typeof(DotNetResult), opt, args, exitCode, errorText, outputText, hasTimeOut);
+        => _accessor = new(typeof(DotNetResult), opt, args ?? DefaultArgs, exitCode, errorText, outputText, hasTimeOut);
 
     public DotNetResultAccessor(DotNetResult obj)
         => _accessor = new(typeof(DotNetResult), obj);
