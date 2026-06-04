10ddd86bc2f1b2d65a079bf56d9234f7881c9a19
jjvanzon <jjvanzon79@gmail.com>
2026-06-05 01:31:54 +0200

    🥼 Compilation.Core.Tests - Add new BuildTests.cs with TODO's of what to test.

---
+++ Framework/Compilation.Core.Tests/BuildTests.cs
@@ -0,0 +1,15 @@
+﻿namespace JJ.Framework.Compilation.Core.Tests;
+
+[TestClass]
+public class BuildTests
+{
+    // TODO: Test:
+    // - Build
+    // - BuildConf
+    // - Args
+    // - As CommandEnum
+    // - As Command text
+    // - With File / Without File
+    // - With Dir / Without Dir
+    // - Error cases
+}
