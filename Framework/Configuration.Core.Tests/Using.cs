53dcef4cb9ecf73bd92929df4b9d29693dfe62b6
jjvanzon <jjvanzon79@gmail.com>
2026-06-05 01:27:55 +0200

    ⚠️ Compilation.Core.Tests - Missing namespace import

--- Framework/Configuration.Core.Tests/Using.cs
+++ Framework/Configuration.Core.Tests/Using.cs
@@ -2,8 +2,10 @@ global using System.Diagnostics;
 global using System.Text.RegularExpressions;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using JJ.Framework.Compilation.Core;
+global using JJ.Framework.Compilation.Core.Primitives;
 global using static System.Reflection.Assembly;
 global using static System.IO.File;
 global using static JJ.Framework.Testing.Core.AssertCore;
 global using static JJ.Framework.Common.Legacy.EmbeddedResourceHelper;
-global using static JJ.Framework.Compilation.Core.DotNetVerbosity;
+global using static JJ.Framework.Compilation.Core.Primitives.DotNetVerbosity;
+
