393e8b0d908cc2e47bf565cc6aa66e5af1a06793
jjvanzon <jjvanzon79@gmail.com>
2026-06-07 22:39:23 +0200

    🧫 Compilation.Core.Tests - InstallPackageTests - Enable working cases after more flexible command match. Keep one as error case (can't be resolved).

--- Framework/Compilation.Core.Tests/InstallPackageTests.cs
+++ Framework/Compilation.Core.Tests/InstallPackageTests.cs
@@ -7,20 +7,20 @@ public class InstallPackageTests : DotNetTestHelper
 {
     // TODO: Test:
     // x Main case
-    // - As command enum
-    // - As command text
-    // - Rework: Recognize command in text and arg in Enricher, which adjusts behavior in CmdFormatter.
+    // x As command enum
+    // x As command text
+    // x As args only
+    // - No opt
+    // x Rework: Recognize command in text and arg in Enricher, which adjusts behavior in CmdFormatter.
     // - Invalid package name syntax
     // - Invalid version syntax
     // - Package does not exist (might pass, just to break compilation)
     // - Package version does not exist
     // - All options on
-    // - No opt
     // - File situations
     // - Overload space
     // - With Args
     // - With AutoRestore (should be irrelevant but still work).
-    // - Without opt
 
     public InstallPackageTests() => AssertInitialState();
 
@@ -28,35 +28,51 @@ public class InstallPackageTests : DotNetTestHelper
     private const string Ver = "4.6.6251";
 
     [TestMethod]
-    public void Test_InstallPackage_MainCase() => Assert(InstallPackage(ID, Ver, Opt()));
+    public void Test_InstallPackage_MainCase() 
+        => Assert(InstallPackage(ID, Ver, Opt()));
 
     [TestMethod]
-    public void Test_InstallPackage_NoOpt() => InTempDir(() => Assert(InstallPackage(ID, Ver)));
+    public void Test_InstallPackage_NoOpt() => InTempDir(() 
+        => Assert(InstallPackage(ID, Ver)));
 
     [TestMethod]
-    public void Test_InstallPackage_AsCommandEnum() => Assert(DotNet.Exe(installpackage, $"{ID} -v {Ver}", Opt()));
+    public void Test_InstallPackage_AsCommandEnum() 
+        => Assert(DotNet.Exe(installpackage, $"{ID} -v {Ver}", Opt()));
 
     [TestMethod]
-    public void Test_InstallPackage_AsCommandEnum_NoOpt() => InTempDir(() => Assert(DotNet.Exe(installpackage, $"{ID} -v {Ver}")));
+    public void Test_InstallPackage_AsCommandEnum_NoOpt() => InTempDir(()
+        => Assert(DotNet.Exe(installpackage, $"{ID} -v {Ver}")));
 
     [TestMethod]
-    public void Test_InstallPackage_AsCommandTextAndArgs() => Assert(DotNet.Exe("add package ", $"{ID} -v {Ver}", Opt()));
-
-    // TODO: --project parameter expected.
-
-    /*
-    [TestMethod]
-    public void Test_InstallPackage_AsCommandTextOnly() => Assert(DotNet.Exe($"package add {ID} -v {Ver}", Opt()));
+    public void Test_InstallPackage_AsCommandTextAndArgs() 
+        => Assert(DotNet.Exe("add package", $"{ID} -v {Ver}", Opt()));
 
     [TestMethod]
-    public void Test_InstallPackage_NoCommandAsArgOnly() => Assert(DotNet.Exe("", $"package add {ID} -v {Ver}", Opt()));
-    */
+    public void Test_InstallPackage_AsCommandTextAndArgs_NoOpt() => InTempDir(()
+        => Assert(DotNet.Exe("add package", $"{ID} -v {Ver}")));
 
-    //[TestMethod]
-    //public void Test_InstallPackage_AsCommandText_NoOpt() => InTempDir(() => Assert(DotNet.Exe($"package add {ID} -v {Ver}")));
+    [TestMethod]
+    public void Test_InstallPackage_AsCommandTextOnly() 
+        => Assert(DotNet.Exe($"add package {ID} -v {Ver}", Opt()));
 
-    //[TestMethod]
-    //public void Test_InstallPackage_NoCommandAsArgOnly_NoOpt() => InTempDir(() => Assert(DotNet.Exe("", $"package add {ID} -v {Ver}", Opt())));
+    [TestMethod]
+    public void Test_InstallPackage_AsCommandTextOnly_NoOpt() => InTempDir(()
+        => Assert(DotNet.Exe($"add package {ID} -v {Ver}")));
+
+    [TestMethod]
+    public void Test_InstallPackage_NoCommandAsArgOnly_NoOpt() => InTempDir(()
+        => Assert(DotNet.Exe("", $"add package {ID} -v {Ver}")));
+
+    // Error Cases
+    
+    [TestMethod]
+    public void Test_InstallPackage_ErrorCase_NoCommand_ArgOnly_WithOpt_Limitation_DoesNotWork()
+    {
+        // Args placed at the end, after file; no resolution.
+        Throws(
+            () => DotNet.Exe("", $"add package {ID} -v {Ver}", Opt()), 
+            $"--project \"Temp.csproj\" add package {ID} -v {Ver}");
+    }
 
     // Assertion
 
@@ -70,7 +86,6 @@ public class InstallPackageTests : DotNetTestHelper
     private void Assert(DotNetResult result)
     {
         AssertResultOk(result);
-        AssertContains(result, "dotnet installpackage | add package");
         AssertContains(result, $"package '{ID}' version '{Ver}' added");
 
         AssertExists(AssetsFilePath);
