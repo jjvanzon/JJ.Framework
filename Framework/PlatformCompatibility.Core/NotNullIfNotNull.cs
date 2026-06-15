09354e38ab913d6386c81dd3160de4ec357f4bac
jjvanzon <jjvanzon79@gmail.com>
2026-06-15 23:00:47 +0200

    👻 PlatformCompatibility.Core - NotNullIfNotNull shim

---
+++ Framework/PlatformCompatibility.Core/NotNullIfNotNull.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+
+#if NETFRAMEWORK || NETSTANDARD2_0
+
+namespace System.Diagnostics.CodeAnalysis;
+
+using static AttributeTargets;
+
+[AttributeUsage(Parameter | Property | ReturnValue, AllowMultiple = true, Inherited = false)]
+internal sealed class NotNullIfNotNullAttribute : Attribute
+{
+    public NotNullIfNotNullAttribute(string parameterName) => ParameterName = parameterName;
+    public string ParameterName { get; }
+}
+
+#endif
