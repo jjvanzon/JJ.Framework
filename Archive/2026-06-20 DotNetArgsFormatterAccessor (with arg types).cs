5b7e03c299f6f5a0d7287df899c76a468062f2cf
jjvanzon <jjvanzon79@gmail.com>
2026-06-22 19:45:41 +0200

    🔼 DevOps - Bump minor version to 4.8 / 0.255

---
+++ Archive/2026-06-20 DotNetArgsFormatterAccessor (with arg types).cs
@@ -0,0 +1,30 @@
+namespace JJ.Framework.Compilation.Core.Tests.Accessors;
+
+[Suppress("Trimmer", "IL2026", Justification = BasesAnd + GenericMethod)]
+[Suppress("Trimmer", "IL3050", Justification = GenericMethod)]
+internal static class DotNetArgsFormatterAccessor
+{
+    private static readonly Type? _ = null;
+
+    private static readonly AccessorCore _accessor = new("DotNetArgsFormatter");
+
+    public static string? Descriptor(DotNetArgs? args) 
+        => (string?)_accessor.Call([args], [typeof(DotNetArgs)]);
+
+    public static string? Stringify(DotNetArgs? args)
+        => (string?)_accessor.Call(args);
+
+    public static string? DebuggerDisplay(DotNetArgs? args)
+        => (string?)_accessor.Call(args);
+
+    // Parts
+
+    public static string? CommandDescriptor(DotNetCommandEnum @enum, string? command, bool isRebuild) 
+        => (string?)_accessor.Call([@enum, command, isRebuild], [_, typeof(string), _]);
+
+    public static string? IDVerDescriptor(string? id, string? ver)
+        => (string?)_accessor.Call([id, ver], [typeof(string), typeof(string)]);
+
+    public static string? ArgPropsDescriptor(string? args, string? fullArgs)
+        => (string?)_accessor.Call([args, fullArgs], [typeof(string), typeof(string)]);
+}
