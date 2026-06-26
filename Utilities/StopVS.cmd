d78f3952153ff99d47957133740aa8a6efed8fa7
jjvanzon <jjvanzon79@gmail.com>
2026-06-26 23:28:40 +0200

    🛑 Utilities: Add dotnet.exe to StopCS script.

--- Utilities/StopVS.cmd
+++ Utilities/StopVS.cmd
@@ -5,3 +5,4 @@ taskkill /im VBCSCompiler.exe /f
 taskkill /im copilot-language-server.exe /f
 taskkill /im Microsoft.CodeAnalysis.LanguageServer.exe /f
 taskkill /im vctip.exe /f
+taskkill /im dotnet.exe /f
