7fdb8caf6944e5bc24b5ceec3f3a69dce1a4d8e2
jjvanzon <jjvanzon79@gmail.com>
2026-07-02 00:48:42 +0200

    🧽 IO.Legacy - StreamHelperLegacy - File scoped namespace and clean up redundant usings.

--- Framework/JJ.Framework.IO/StreamHelperLegacy.cs
+++ Framework/JJ.Framework.IO/StreamHelperLegacy.cs
@@ -1,39 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using JJ.Framework.Reflection;
-using static JJ.Framework.IO.Legacy.StreamHelper;
+﻿using static JJ.Framework.IO.Legacy.StreamHelper;
 
-namespace JJ.Framework.IO.Legacy
+namespace JJ.Framework.IO.Legacy;
+
+/// <inheritdoc cref="_streamhelperlegacy" />
+public static class StreamHelperLegacy
 {
-    /// <inheritdoc cref="_streamhelperlegacy" />
-    public static class StreamHelperLegacy
+    /// <inheritdoc cref="_stringtostream" />
+    public static Stream StringToStream(string text, Encoding encoding, bool includeByteOrderMark = false)
     {
-        /// <inheritdoc cref="_stringtostream" />
-        public static Stream StringToStream(string text, Encoding encoding, bool includeByteOrderMark = false)
-        {
-            byte[] bytes = StringToBytes(text, encoding, includeByteOrderMark);
-            Stream stream = BytesToStream(bytes);
-            return stream;
-        }
-
-        /// <inheritdoc cref="_stringtobytes" />
-        public static byte[] StringToBytes(string text, Encoding encoding, bool includeByteOrderMark = false)
-        {
-            if (encoding == null) throw new NullException(() => encoding);
-
-            byte[] bytes = encoding.GetBytes(text);
-
-            if (Equals(encoding, Encoding.UTF8) && includeByteOrderMark)
-            {
-                bytes = encoding.GetPreamble().Concat(bytes).ToArray();
-            }
-
-            return bytes;
-        }
-
+        byte[] bytes = StringToBytes(text, encoding, includeByteOrderMark);
+        Stream stream = BytesToStream(bytes);
+        return stream;
     }
+
+    /// <inheritdoc cref="_stringtobytes" />
+    public static byte[] StringToBytes(string text, Encoding encoding, bool includeByteOrderMark = false)
+    {
+        if (encoding == null) throw new NullException(() => encoding);
+
+        byte[] bytes = encoding.GetBytes(text);
+
+        if (Equals(encoding, Encoding.UTF8) && includeByteOrderMark)
+        {
+            bytes = encoding.GetPreamble().Concat(bytes).ToArray();
+        }
+
+        return bytes;
+    }
+
 }
