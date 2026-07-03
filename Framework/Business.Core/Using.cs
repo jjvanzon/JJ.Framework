73c87e3904a78d94921fc3de9a9a917600a4dad7
jjvanzon <jjvanzon79@gmail.com>
2026-07-04 00:16:54 +0200

    🧽 Business.Core - Remove dependency on Text.Core. Reset target framework spread to defaults (from TargetFrameworks.xml referenced by Directory.Build.props) to exclude net5.0 (was only added to check stub coverage, but not currently part of the default spread for reasons).

--- Framework/Business.Core/Using.cs
+++ Framework/Business.Core/Using.cs
@@ -2,9 +2,6 @@
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Text;
-global using System.Diagnostics;
-global using JJ.Framework.Text.Core;
 global using static System.String;
-global using static System.Array;
 global using static JJ.Framework.Business.Core.DiagnosticsFormatter;
 
