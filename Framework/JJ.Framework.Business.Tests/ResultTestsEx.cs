9c93aa8c77098435a356fd19eb31b641b5862f83
jjvanzon <jjvanzon79@gmail.com>
2026-07-04 02:41:02 +0200

    🔬 Business.Legacy.Tests - Result constructor tests

--- Framework/JJ.Framework.Business.Tests/ResultTestsEx.cs
+++ Framework/JJ.Framework.Business.Tests/ResultTestsEx.cs
@@ -39,6 +39,67 @@ public class ResultTestsEx
         AreEqual("min > max", result.Messages[0]);
     }
 
+    [TestMethod]
+    public void Test_Result_Constructors()
+    {
+        {
+            var result = new Result();
+            IsTrue(result.Success);
+            NotNull(result.Messages);
+            AreEqual(0, result.Messages.Count);
+        }
+        {
+            var result = new Result(success: true);
+            IsTrue(result.Success);
+            NotNull(result.Messages);
+            AreEqual(0, result.Messages.Count);
+        }
+        {
+            var result = new Result(success: false);
+            IsFalse(result.Success);
+            NotNull(result.Messages);
+            AreEqual(0, result.Messages.Count);
+        }
+        {
+            var result = new Result("Just a warning.");
+            IsTrue(result.Success);
+            NotNull(result.Messages);
+            AreEqual(1, result.Messages.Count);
+            AreEqual("Just a warning.", result.Messages[0]);
+        }
+        {
+            var result = new Result("Just some warnings.", "You may want to take a look.");
+            IsTrue(result.Success);
+            NotNull(result.Messages);
+            AreEqual(2, result.Messages.Count);
+            AreEqual("Just some warnings.", result.Messages[0]);
+            AreEqual("You may want to take a look.", result.Messages[1]);
+        }
+        {
+            var result = new Result(success: false, "Oops");
+            IsFalse(result.Success);
+            NotNull(result.Messages);
+            AreEqual(1, result.Messages.Count);
+            AreEqual("Oops", result.Messages[0]);
+        }
+        {
+            var result = new Result(success: false, "Oops", "a daisy");
+            IsFalse(result.Success);
+            NotNull(result.Messages);
+            AreEqual(2, result.Messages.Count);
+            AreEqual("Oops", result.Messages[0]);
+            AreEqual("a daisy", result.Messages[1]);
+        }
+        {
+            var result = new Result([ "Oops", "a daisy" ], success: false);
+            IsFalse(result.Success);
+            NotNull(result.Messages);
+            AreEqual(2, result.Messages.Count);
+            AreEqual("Oops", result.Messages[0]);
+            AreEqual("a daisy", result.Messages[1]);
+        }
+    }
+
     private Result<int> GenerateNum(int min, int max)
     {
         if (min > max)
