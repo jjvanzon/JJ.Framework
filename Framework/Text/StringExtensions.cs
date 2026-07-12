a71d9a350492acd999c8ba12aa99fd8500e8abad
jjvanzon <jjvanzon79@gmail.com>
2026-07-12 17:36:08 +0200

    🚣 JJ.Framework.Text - Port back StringExtension changes from main to this legacy branch; includes white space changes and replacing some null-strictness by 'null-sanity'.

--- Framework/Text/StringExtensions.cs
+++ Framework/Text/StringExtensions.cs
@@ -36,18 +36,18 @@ namespace JJ.Framework.Text
         public static string Repeat(this string stringToRepeat, int repeatCount)
         {
             if (stringToRepeat == null) throw new ArgumentNullException(nameof(stringToRepeat));
-
-            char[] sourceChars = stringToRepeat.ToCharArray();
-            int sourceLength = sourceChars.Length;
-
+            
+            char[] sourceChars  = stringToRepeat.ToCharArray();
+            int    sourceLength = sourceChars.Length;
+            
             int destLength = sourceLength * repeatCount;
-            var destChars = new char[destLength];
-
+            var destChars  = new char[destLength];
+            
             for (var i = 0; i < destLength; i += sourceLength)
             {
                 Array.Copy(sourceChars, 0, destChars, i, sourceLength);
             }
-
+            
             var destString = new string(destChars);
             return destString;
         }
@@ -84,16 +84,17 @@ namespace JJ.Framework.Text
 
             return input.Right(length);
         }
-
+        
         /// <summary>
         /// Takes the part of a string until the specified delimiter. Excludes the delimiter itself.
         /// </summary>
         public static string TakeEndUntil(this string input, string until)
         {
+            if (input == null) return "";
             if (until == null) throw new ArgumentNullException(nameof(until));
             int index = input.LastIndexOf(until, StringComparison.Ordinal);
             if (index == -1) return "";
-            int length = input.Length - index - 1;
+            int    length = input.Length - index - 1;
             string output = input.Right(length);
             return output;
         }
@@ -108,15 +109,17 @@ namespace JJ.Framework.Text
 
         [Obsolete("Use TakeEnd instead.", true)]
         public static string TakeRight(this string input, int length) => throw new NotSupportedException("Use TakeEnd instead.");
-
+        
         /// <summary>
         /// Returns the left part of a string.
         /// Can return less characters than the length provided if string is shorter.
         /// </summary>
         public static string TakeStart(this string input, int length)
         {
+            input ??= "";
+            
             if (length > input.Length) length = input.Length;
-
+            
             return input.Left(length);
         }
 
@@ -143,23 +146,29 @@ namespace JJ.Framework.Text
         /// </summary>
         public static string TrimEnd(this string input, string end)
         {
-            if (string.IsNullOrEmpty(end)) throw new Exception($"{nameof(end)} is null or empty.");
+            input ??= "";
+
+            if (string.IsNullOrEmpty(end)) return input;
 
             string temp = input;
-
+            
             while (temp.EndsWith(end))
             {
                 temp = temp.TrimEnd(end.Length);
             }
-
+            
             return temp;
         }
-
+        
         /// <summary>
         /// Will trim off repetitions of the same value from the given string.
         /// These are variations of the standard .NET methods that instead of just taking char[] can take a string or a length.
         /// </summary>
-        public static string TrimEnd(this string input, int length) => input.Left(input.Length - length);
+        public static string TrimEnd(this string input, int length)
+        {
+            input ??= "";
+            return input.Left(input.Length - length);
+        }
 
         /// <summary>
         /// Cuts off the part of a string until the specified delimiter and returns what remains including the delimiter itself.
@@ -210,23 +219,28 @@ namespace JJ.Framework.Text
         /// </summary>
         public static string TrimStart(this string input, string start)
         {
-            if (string.IsNullOrEmpty(start)) throw new Exception($"{nameof(start)} is null or empty.");
-
+            if (string.IsNullOrEmpty(start)) return input;
+            if (input == null) return "";
+            
             string temp = input;
-
+            
             while (temp.StartsWith(start))
             {
                 temp = temp.TrimStart(start.Length);
             }
-
+            
             return temp;
         }
-
+        
         /// <summary>
         /// Will trim off repetitions of the same value from the given string.
         /// These are variations of the standard .NET methods that instead of just taking char[] can take a string or a length.
         /// </summary>
-        public static string TrimStart(this string input, int length) => input.Right(input.Length - length);
+        public static string TrimStart(this string input, int length)
+        {
+            input ??= "";
+            return input.Right(input.Length - length);
+        }
 
         /// <summary>
         /// Cuts off the part of a string until the specified delimiter and returns what remains including the delimiter itself.
