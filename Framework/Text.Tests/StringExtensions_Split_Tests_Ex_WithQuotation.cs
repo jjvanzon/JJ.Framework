b4345baf2b136b211d9967b7771767a26c14d047
jjvanzon <jjvanzon79@gmail.com>
2026-07-12 22:39:37 +0200

    ⚓ Port SplitWithQuotation from main to this legacy branch.

---
+++ Framework/Text.Tests/StringExtensions_Split_Tests_Ex_WithQuotation.cs
@@ -0,0 +1,78 @@
+﻿// ReSharper disable ConvertToConstant.Local
+
+using System.Linq;
+
+namespace JJ.Framework.Text.Tests;
+
+[TestClass]
+public class SplitWithQuotationCoreTests
+{
+    private static readonly string? _null = null;
+    
+    [TestMethod]
+    public void SplitWithQuotation_SimpleExample()
+    {
+        string[] actual   = "apple|~banana|split~|cherry".SplitWithQuotation("|", '~').ToArray();
+        string[] expected = [ "apple", "banana|split", "cherry" ];
+        CollectionAssert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void SplitWithQuotation_ComplexExample()
+    {
+        string input = 
+        """
+        1234,"1234","12,34","12""34",1"23"4,"12"34","12"34"
+        """;
+        
+        string[] split = input.SplitWithQuotation(",", '"').ToArray();
+        
+        IsNotNull(         () => split);
+        AreEqual(6,        () => split.Length);
+        AreEqual("1234",   () => split[0]);
+        AreEqual("1234",   () => split[1]);
+        AreEqual("12,34",  () => split[2]);
+        AreEqual("12\"34", () => split[3]);
+
+        // Quirk: Quotes in the middle are untouched:
+        
+        //AreEqual("1234", split[4]); // Right
+        AreEqual("1\"23\"4", () => split[4]); // Wrong
+        
+        //AreEqual("""1234,1234""", split[5]); // Right?
+        AreEqual("""12"34","12"34""", () => split[5]); // Wrong
+    }
+
+    [TestMethod]
+    public void SplitWithQuotation_WithoutCharQuote_BehavesLikeNormalSplit()
+    {
+        string[] expected = [ "a", "b", "c", "e" ];
+        string[] split    = "a, b, c, , e".SplitWithQuotation(", ", RemoveEmptyEntries, null).ToArray();
+        CollectionAssert.AreEqual(expected, split);
+    }
+    
+    [TestMethod]
+    public void SplitWithQuotation_NoSeparator_ThrowsException()
+    {
+        Throws<ArgumentNullException>(() => "1234".SplitWithQuotation(null, '"'), "separator", "cannot be null");
+        
+        // "" is not technically null, but it calls it that in the error anyway.
+        Throws<ArgumentNullException>(() => "1234".SplitWithQuotation("", '"'), "separator", "cannot be null");
+        
+        // Space is fine though.
+        "1234".SplitWithQuotation(" ", '"');
+    }
+    
+    [TestMethod]
+    public void SplitWithQuotation_NullOrEmptyInput_ReturnsEmptyCollection()
+    {
+        string[] split1 = "".SplitWithQuotation(",", '"').ToArray();
+        IsNotNull(() => split1);
+        AreEqual(0, () => split1.Length);
+     
+        // null input still returns non-null collection.
+        string[] split2 = _null.SplitWithQuotation(",", '"').ToArray();
+        IsNotNull(() => split2);
+        AreEqual(0, () => split2.Length);
+    }
+}
--- Framework/Text.Tests/Using.cs
+++ Framework/Text.Tests/Using.cs
@@ -3,3 +3,4 @@ global using System.Collections.Generic;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using static System.StringSplitOptions;
 global using static JJ.Framework.Testing.AssertHelper;
+global using static JJ.Framework.Testing.AssertHelperEx;
