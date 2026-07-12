015be55642521da78363755913872001f063ecfb
jjvanzon <jjvanzon79@gmail.com>
2026-07-12 17:16:21 +0200

    ⛵ JJ.Framework.Text - Port back StringBuildWithIndentation(Legacy) changes from main to this legacy branch.

--- Framework/Text/StringBuilderWithIndentation.cs
+++ Framework/Text/StringBuilderWithIndentation.cs
@@ -1,33 +1,44 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-using System.Text;
-
-namespace JJ.Framework.Text
+﻿namespace JJ.Framework.Text
 {
     public class StringBuilderWithIndentation
     {
-        public StringBuilderWithIndentation(string tabString = "\t") => _tabString = tabString;
-
+        public StringBuilderWithIndentation()
+            : this("\t")
+        { }
+        
+        public StringBuilderWithIndentation(string tabString)
+            : this(tabString, Environment.NewLine)
+        { }
+        
+        public StringBuilderWithIndentation(string tabString , string enter)
+        {
+            _tabString = tabString;
+            _enter     = enter;
+        }
+        
         private readonly string _tabString;
-        private readonly StringBuilder _sb = new StringBuilder();
-
-        public override string ToString() => _sb.ToString();
-        public void Indent() => IndentLevel++;
-        public void Unindent() => IndentLevel--;
-        public void AppendLine() => _sb.AppendLine();
-        public void Append(object x) => _sb.Append(x);
-
+        private readonly string _enter;
+        
+        private readonly StringBuilder _sb = new();
+        
+        public override string ToString()       => _sb.ToString();
+        public          void   Indent()         => IndentLevel++;
+        public          void   Unindent()       => IndentLevel--;
+        public          void   AppendEnter()    => Append(_enter);
+        public          void   AppendLine()     => AppendLine("");
+        public          void   Append(object x) => _sb.Append(x);
+        
         public void AppendLine(string value)
         {
             AppendTabs();
-
+            
             _sb.Append(value);
-
-            _sb.Append(Environment.NewLine);
+            
+            _sb.Append(_enter);
         }
-
+        
         public void AppendFormat(string format, params object[] args) => _sb.AppendFormat(format, args);
-
+        
         private int _indentLevel;
         public int IndentLevel
         {
@@ -38,17 +49,17 @@ namespace JJ.Framework.Text
                 _indentLevel = value;
             }
         }
-
+        
         public void AppendTabs()
         {
             string tabs = GetTabs();
             _sb.Append(tabs);
         }
-
+        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public string GetTabs()
         {
-            string tabs = _tabString?.Repeat(_indentLevel);
+            string tabs = _tabString.Repeat(_indentLevel);
             return tabs;
         }
     }
