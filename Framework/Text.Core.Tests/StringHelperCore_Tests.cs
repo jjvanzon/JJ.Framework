004313f6da0ad0f984f323ebeafc12c336a22dde
jjvanzon <jjvanzon79@gmail.com>
2026-07-13 23:05:54 +0200

    ⚗️ Text.Core.Tests - Replace PrettyByteCount test with one ported from JJ.Synthesizer. Archive the original.

--- Framework/Text.Core.Tests/StringHelperCore_Tests.cs
+++ Framework/Text.Core.Tests/StringHelperCore_Tests.cs
@@ -97,16 +97,29 @@ public class StringHelperCore_Tests
         #endif
     }
 
-    // TODO: Explore more cases
-
     [TestMethod]
     public void Test_StringHelperCore_PrettyByteCount()
     {
-        AreEqual("1000 GB", PrettyByteCount(1000L * 1024 * 1024 * 1024));
-        AreEqual("1000 MB", PrettyByteCount(1000L * 1024 * 1024));
-        AreEqual("1000 kB", PrettyByteCount(1000L * 1024));
-        AreEqual("1000 bytes", PrettyByteCount(1000L));
+        AreEqual("100 bytes",  PrettyByteCount(100));
+        AreEqual("1000 bytes", PrettyByteCount(1000));
+        AreEqual("5119 bytes", PrettyByteCount(5 * 1024 - 1));
+        AreEqual("5 kB",       PrettyByteCount(5 * 1024 + 1));
+        AreEqual("10 kB",      PrettyByteCount(10 * 1024));
+        AreEqual("100 kB",     PrettyByteCount(100 * 1024));
+        AreEqual("1000 kB",    PrettyByteCount(1000 * 1024));
+        AreEqual("5120 kB",    PrettyByteCount(5 * 1024 * 1024 - 1));
+        AreEqual("5 MB",       PrettyByteCount(5 * 1024 * 1024 + 1));
+        AreEqual("100 MB",     PrettyByteCount(100 * 1024 * 1024));
+        AreEqual("1000 MB",    PrettyByteCount(1000 * 1024 * 1024));
+        AreEqual("5120 MB",    PrettyByteCount(5L * 1024 * 1024 * 1024 - 1));
+        AreEqual("5 GB",       PrettyByteCount(5L * 1024 * 1024 * 1024 + 1));
+        AreEqual("10 GB",      PrettyByteCount(10L * 1024 * 1024 * 1024));
+        AreEqual("100 GB",     PrettyByteCount(100L * 1024 * 1024 * 1024));
+        AreEqual("1000 GB",    PrettyByteCount(1000L * 1024 * 1024 * 1024));
+        AreEqual("10000 GB",   PrettyByteCount(10000L * 1024 * 1024 * 1024));
     }
+    
+    // TODO: Explore more cases
 
     [TestMethod]
     public void Test_StringHelperCore_WithShortGuids() 
