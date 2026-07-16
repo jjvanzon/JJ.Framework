da0cdda535e6332d4a004586cc4550c1e6e59c6c
jjvanzon <jjvanzon79@gmail.com>
2026-07-16 16:53:22 +0200

    💇 Text.Core - Add new TrimTests project.

--- Framework/Text.Core.Tests/Using.cs
+++ Framework/Text.Core.Tests/Using.cs
@@ -5,3 +5,8 @@ global using static System.TimeSpan;
 global using static System.Environment;
 global using static JJ.Framework.Testing.Core.AssertCore;
 global using static JJ.Framework.Text.Core.StringHelperCore;
+// ReSharper disable RedundantUsingDirective.Global
+global using JJ.Framework.Text.Core.Tests;
+global using static System.Console;
+global using static JJ.Framework.Testing.Core.TestRunner;
+
---
+++ Framework/Text.Core.TrimTests/Program.cs
@@ -0,0 +1,9 @@
+﻿bool success = 
+RunTests<StringExtensionsCore_Tests>() &&
+RunTests<StringHelperCore_Basics_Tests>() &&
+RunTests<StringHelperCore_PrettyByteCount_Tests>() &&
+RunTests<StringHelperCore_PrettyTime_Tests>() &&
+RunTests<StringHelperCore_ShortGuids_Tests>() &&
+RunTests<StringHelperCore_SpacingAndPunctuation_Tests>();
+WriteLine("Done.");
+if (!success) Exit(1);
