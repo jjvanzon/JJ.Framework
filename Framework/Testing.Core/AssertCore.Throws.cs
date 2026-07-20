ae708142def7f7eb6b66dff40790dfd43da5084f
jjvanzon <jjvanzon79@gmail.com>
2026-07-20 20:41:44 +0200

    🧑‍💻 Testing.Core - AssertCore.Throws - After-the-fact refactoring.

--- Framework/Testing.Core/AssertCore.Throws.cs
+++ Framework/Testing.Core/AssertCore.Throws.cs
@@ -7,14 +7,47 @@ namespace JJ.Framework.Testing.Core;
 public static partial class AssertCore
 {
     // Throws (Containing)
+    
+    public static Exception Throws<TException>(Func<object?> statement, params string[] expectedTexts)
+        => Throws(statement, typeof(TException), expectedTexts);
 
+    public static Exception Throws<TException>(Action statement, params string[] expectedTexts)
+        => Throws(statement, typeof(TException), expectedTexts);
+    
+    public static Exception Throws(Func<object?> statement, Type exceptionType, params string[] expectedTexts)
+        => Throws(() => { statement(); }, exceptionType, expectedTexts);
+
+    public static Exception Throws(Action statement, Type exceptionType, params string[] expectedTexts)
+    {
+        ThrowIfNull(statement);
+        ThrowIfNull(exceptionType);
+        ThrowIfNull(expectedTexts);
+        
+        Exception? actualException = null;
+        try
+        {
+            statement();
+        }
+        catch (Exception ex)
+        {
+            if (ExceptionIsMatch(ex, exceptionType, expectedTexts))
+            {
+                return ex;
+            }
+
+            actualException = ex;
+        }
+
+        throw new Exception(FormatShouldHaveThrownMessage(exceptionType, expectedTexts, actualException));
+    }
+    
     public static Exception Throws(Func<object?> statement, params string[] expectedTexts)
         => Throws(() => { statement(); }, expectedTexts);
 
     public static Exception Throws(Action statement, params string[] expectedTexts)
     {
-        if (statement == null) throw new NullException(() => statement);
-        if (expectedTexts == null) throw new NullException(() => expectedTexts);
+        ThrowIfNull(statement);
+        ThrowIfNull(expectedTexts);
         
         Exception? actualException = null;
         try
@@ -34,53 +67,23 @@ public static partial class AssertCore
         throw new Exception(FormatShouldHaveThrownMessage(expectedTexts, actualException));
         
     }
-    
-    public static Exception Throws(Func<object?> statement, Type exceptionType, params string[] expectedTexts)
-        => Throws(() => { statement(); }, exceptionType, expectedTexts);
 
-    public static Exception Throws(Action statement, Type exceptionType, params string[] expectedTexts)
-    {
-        if (statement == null) throw new NullException(() => statement);
-        if (exceptionType == null) throw new NullException(() => exceptionType);
-        if (expectedTexts == null) throw new NullException(() => expectedTexts);
-        
-        Exception? actualException = null;
-        try
-        {
-            statement();
-        }
-        catch (Exception ex)
-        {
-            if (ExceptionIsMatch(ex, exceptionType, expectedTexts))
-            {
-                return ex;
-            }
-
-            actualException = ex;
-        }
-
-        throw new Exception(FormatShouldHaveThrownMessage(exceptionType, expectedTexts, actualException));
-    }
-
-    private static bool ExceptionIsMatch(Exception ex, string?[] expectedTexts) => ExceptionIsMatch(ex, default, expectedTexts);
-    private static bool ExceptionIsMatch(Exception ex, Type? expectedType, string?[]? expectedTexts)
+    private static bool ExceptionIsMatch(Exception ex, string[] expectedTexts) => ExceptionIsMatch(ex, default, expectedTexts);
+    private static bool ExceptionIsMatch(Exception ex, Type? expectedType, string?[] expectedTexts)
     {
         bool anyExpectedTexts = AnyExpectedTexts(expectedTexts);
 
         if (expectedType == null && !anyExpectedTexts) 
         {
-            // No expectations filled in? 
-            // Exception always match.
+            // No expectations filled in? Exception always match. Prevent some allocations and looping.
             return true;
         }
 
         Exception[] exceptions = ex.SelfAndAncestors(x => x.InnerException).ToArray();
-
         foreach (Exception exception in exceptions)
         {
             if (expectedType != null)
             {
-                // TODO: Base types?
                 bool typeIsMatch = exception.GetType() == expectedType;
                 if (!typeIsMatch)
                 {
@@ -91,7 +94,7 @@ public static partial class AssertCore
 
             if (anyExpectedTexts)
             {
-                bool messageIsMatch = expectedTexts!.All(x => exception.Message.Contains(x, caseMatters: false));
+                bool messageIsMatch = expectedTexts.All(x => exception.Message.Contains(x, caseMatters: false));
                 if (!messageIsMatch)
                 {
                     // No match: continue searching.
@@ -117,32 +120,28 @@ public static partial class AssertCore
 
     private static string FormatShouldHaveThrownMessage(Type? exceptionType, string[]? expectedTexts, Exception? actualException)
     {
-        string exceptionTypePart = "Exception";
+        string formattedExceptionType = "Exception";
         if (exceptionType != null)
         {
-            exceptionTypePart = exceptionType.Name.CutRight("Exception");
+            formattedExceptionType = exceptionType.Name.CutRight("Exception");
         }
         
-        string expectedTextsPart = "";
+        string formattedExpectedTexts = "";
         if (AnyExpectedTexts(expectedTexts))
         {
-            expectedTextsPart = " with message containing: " + Join(", ", expectedTexts.Select(x => '"' + x + '"'));
+            formattedExpectedTexts = " with message containing: " + Join(", ", expectedTexts.Select(x => '"' + x + '"'));
         }
 
-        string actualExceptionPart = "";
+        string formattedActualException = "";
         if (actualException != null)
         {
-           actualExceptionPart = " Actual: " + actualException;
+           formattedActualException = " Actual: " + actualException;
         }
 
-        return exceptionTypePart + " should have been thrown" + expectedTextsPart + "." + actualExceptionPart;
+        return formattedExceptionType + " should have been thrown" + formattedExpectedTexts + "." + formattedActualException;
     }
 
-    public static Exception Throws<TException>(Func<object?> statement, params string[] expectedTexts)
-        => Throws(statement, typeof(TException), expectedTexts);
-
-    public static Exception Throws<TException>(Action statement, params string[] expectedTexts)
-        => Throws(statement, typeof(TException), expectedTexts);
+    // OnOtherThread
 
     public static void ThrowsExceptionOnOtherThread(Func<object?> statement)
         => AssertHelper.ThrowsExceptionOnOtherThread(() => statement());
@@ -153,25 +152,25 @@ public static partial class AssertCore
     public static void ThrowsOnOtherThread(Action statement)
         => AssertHelper.ThrowsExceptionOnOtherThread(statement);
 
-    // Overloads with Func
+    // ThrowsException as Synonym
     
-    // (extends Action variants, avoids shadow by MSTest)
+    // (extends Action variants of legacy AssertHelper, avoids shadow by MSTest)
 
     public static void ThrowsException(Func<object?> statement)
-        => Throws(() => statement());
+        => Throws(statement);
         
     public static void ThrowsException(Func<object?> statement, string expectedMessage) 
-        => Throws(() => statement(), expectedMessage);
+        => Throws(statement, expectedMessage);
     
     public static void ThrowsException(Func<object?> statement, Type exceptionType)
-        => Throws(() => statement(), exceptionType);
+        => Throws(statement, exceptionType);
 
     public static void ThrowsException(Func<object?> statement, Type exceptionType, string expectedMessage)
-        => Throws(() => statement(), exceptionType, expectedMessage);
+        => Throws(statement, exceptionType, expectedMessage);
     
     public static void ThrowsException<ExceptionType>(Func<object?> statement)
-        => Throws<ExceptionType>(() => statement());
+        => Throws<ExceptionType>(statement);
 
     public static void ThrowsException<ExceptionType>(Func<object?> statement, string expectedMessage)
-        => Throws<ExceptionType>(() => statement(), expectedMessage);
+        => Throws<ExceptionType>(statement, expectedMessage);
 }
