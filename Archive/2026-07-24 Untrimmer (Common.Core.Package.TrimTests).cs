f2567791aedd59aa27b2e1dd05b9eae851086eac
jjvanzon <jjvanzon79@gmail.com>
2026-07-24 18:06:43 +0200

    🪮 Common.Core - Package.TrimTests - Employ the Untrimmer.

---
+++ Archive/2026-07-24 Untrimmer (Common.Core.Package.TrimTests).cs
@@ -0,0 +1,9 @@
+﻿//// ReSharper disable UnusedType.Global
+
+//namespace JJ.Framework.Common.Core.Package.TrimTests;
+
+//internal static class Untrimmer
+//{
+//    [NoTrim(All, "JJ.Framework.Common.Core.CommonStringExtensionsCore", "JJ.Framework.Common.Core")]
+//    public static void Untrim() { }
+//}
--- Framework/Common.Core.TrimTests/Untrimmer.cs
+++ Framework/Common.Core.TrimTests/Untrimmer.cs
@@ -5,8 +5,5 @@ namespace JJ.Framework.Common.Core.TrimTests;
 internal static class Untrimmer
 {
     [NoTrim(All, "JJ.Framework.Common.Core.CommonStringExtensionsCore", "JJ.Framework.Common.Core")]
-    //[NoTrim("ObsoleteMessage", "JJ.Framework.Common.Core.CommonStringExtensionsCore", "JJ.Framework.Common.Core")]
-    //[NoTrim("RemoveAccents", "JJ.Framework.Common.Core.CommonStringExtensionsCore", "JJ.Framework.Common.Core")]
-    //[NoTrim("RemoveAccentsObsolete", "JJ.Framework.Common.Core.CommonStringExtensionsCore", "JJ.Framework.Common.Core")]
     public static void Untrim() { }
 }
