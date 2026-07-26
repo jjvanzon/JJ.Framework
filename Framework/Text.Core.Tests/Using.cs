de06d2013157844cec24bd0fa4ee7feda0aa7b0a
jjvanzon <jjvanzon79@gmail.com>
2026-07-27 01:01:00 +0200

    🪥 DevOps - Remove unused using

--- Framework/Text.Core.Tests/Using.cs
+++ Framework/Text.Core.Tests/Using.cs
@@ -1,6 +1,5 @@
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using JJ.Framework.Existence.Core;
-global using static System.StringComparison;
 global using static System.TimeSpan;
 global using static System.Environment;
 global using static JJ.Framework.Testing.Core.AssertCore;
